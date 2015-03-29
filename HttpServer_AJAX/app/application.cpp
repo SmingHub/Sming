2c17ac261c54cb88683b4fecf1476c8fca73c077
anakod <info@anakod.ru>
2015-03-29 08:01:13 +0600

    + AJAX Server Example. Dynamic processing user commands in realtime!

--- HttpServer_AJAX/app/application.cpp
+++ HttpServer_AJAX/app/application.cpp
@@ -2,11 +2,8 @@
 #include <SmingCore/SmingCore.h>
 
 // Put you SSID and Password here
-//#define WIFI_SSID "PleaseEnterSSID"
-//#define WIFI_PWD "PleaseEnterPass"
-
-#define WIFI_SSID "DontConnect"
-#define WIFI_PWD "ignoreme"
+#define WIFI_SSID "PleaseEnterSSID"
+#define WIFI_PWD "PleaseEnterPass"
 
 HttpServer server;
 FTPServer ftp;
