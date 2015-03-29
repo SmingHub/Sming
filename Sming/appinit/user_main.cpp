98a5c68fa52f44ec8c21fe6b224cb5cff1d8d91b
anakod <info@anakod.ru>
2015-03-29 07:59:08 +0600

    + AJAX Server Example. Dynamic processing user commands in realtime!

--- Sming/appinit/user_main.cpp
+++ Sming/appinit/user_main.cpp
@@ -6,9 +6,10 @@ extern void init();
 extern "C" void user_init(void)
 {
 	system_timer_reinit();
+	uart_div_modify(UART_ID_0, UART_CLK_FREQ / 115200);
 	cpp_core_initialize();
 	spiffs_mount();
 	System.initialize();
 
-	init();
+	init(); // User init
 }
