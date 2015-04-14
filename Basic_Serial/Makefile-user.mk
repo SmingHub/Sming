dd1dfa0c66ef8fa3ce6e3df19a717fa4e6e36831
Dmitry Kireev <dmitry@kireev.co>
2015-04-14 00:15:48 -0700

    Modify Makefile, Makefile-user, .project, .cproject to be crossplatform

---
+++ Basic_Serial/Makefile-user.mk
@@ -0,0 +1,14 @@
+## Local build configuration
+
+## COM port parameter is reqruied to flash firmware correctly.
+## If these parameters are not set - defaults will be used
+## Uncomment and change one of the examples.
+
+## Windows: 
+# COM_PORT	 ?= COM3
+
+# MacOS / Linux:
+# COM_PORT	 ?= /dev/tty.usbserial
+
+# Com port speed
+# COM_SPEED	?= 115200
