b655536029ec05a4303e64b0e54b655c61ddb2be
JustACatRu <JustACatRu@users.noreply.github.com>
2015-04-24 07:48:35 +0300

    + Timer changes
    + added setIntervalUs
    + added setIntervalMs
    + added setInterrupt
    * little code refactor

--- Sming/SmingCore/Timer.h
+++ Sming/SmingCore/Timer.h
@@ -17,22 +17,24 @@ public:
 	Timer();
 	~Timer();
 
-	// Init in Milliseconds. Method return "this" reference
-	Timer& IRAM_ATTR initializeMs(uint32_t milliseconds=1000000, InterruptCallback interrupt = NULL);
 	// Init in Microseconds. Method return "this" reference
-	Timer& IRAM_ATTR initializeUs(uint32_t microseconds=1000000, InterruptCallback interrupt = NULL);
-	void IRAM_ATTR start(bool repeating=true);
+	Timer& IRAM_ATTR initializeUs(uint32_t microseconds = 1000000, InterruptCallback interrupt = NULL);
+	// Init in Milliseconds. Method return "this" reference
+	Timer& IRAM_ATTR initializeMs(uint32_t milliseconds = 1000000, InterruptCallback interrupt = NULL);
+	void IRAM_ATTR start(bool repeating = true);
 	void __inline IRAM_ATTR startOnce() { start(false); }
 	void IRAM_ATTR stop();
 	void restart();
 	bool isStarted();
 	uint32_t getIntervalUs();
 	uint32_t getIntervalMs();
-	//void resume();
+    void setIntervalUs(uint32_t microseconds = 1000000);
+    void setIntervalMs(uint32_t milliseconds = 1000000);
+    void setInterrupt(InterruptCallback interrupt = NULL);
 
+    //void resume();
     //void attachInterrupt(InterruptCallback interrupt);
     //void detachInterrupt();
-    //void setPeriod(long microseconds);
 
 protected:
     static void IRAM_ATTR processing(void *arg);
