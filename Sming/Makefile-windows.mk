b84ed48162cd71095dfd2c72dfc2af248083db4b
Dmitry Kireev <dmitry@kireev.co>
2015-04-23 22:51:48 -0700

    fix windows app, flashbin; make sure esp_home is proper-posix

--- Sming/Makefile-windows.mk
+++ Sming/Makefile-windows.mk
@@ -8,8 +8,7 @@ SDK_TOOLS	 ?= $(ESP_HOME)/utils
 # Other tools mappings
 ESPTOOL		 ?= $(SDK_TOOLS)/esptool.exe
 GEN_APPBIN   := PATH="$(ESP_HOME)/xtensa-lx106-elf/bin:$(PATH)" && $(SDK_TOOLS)/gen_appbin.exe
-GEN_APPBIN   := PATH="$(ESP_HOME)/xtensa-lx106-elf/bin:$(PATH)" && $(SDK_TOOLS)/gen_flashbin.exe
-
+GEN_FLASHBIN := PATH="$(ESP_HOME)/xtensa-lx106-elf/bin:$(PATH)" && $(SDK_TOOLS)/gen_flashbin.exe
 KILL_TERM    ?= $(shell taskkill /f /im termite.exe)
 GET_FILESIZE ?= stat --printf="%s"
 
