4d81b47c36d7c79f421d04400339896011f8f9a9
Dmitry Kireev <dmitry@kireev.co>
2015-04-23 19:04:28 -0700

    Add crossplatform compatibility, add proper absolute root handing, add spiffy, srec_cat drafts, 'files' directory for spiffs files Fixes #24, Fixes #30, Draft Fixes #26

---
+++ Sming/Makefile-macos.mk
@@ -0,0 +1,15 @@
+# ESP8266 sdk package home directory
+ESP_HOME ?= /opt/esp-open-sdk
+
+# base directory of the ESP8266 SDK package, absolute
+SDK_BASE	?= $(ESP_HOME)/sdk
+SDK_TOOLS	 ?= $(SDK_BASE)/tools
+
+# Other tools mappings
+ESPTOOL		 ?= $(ESP_HOME)/esptool/esptool.py
+GEN_APPBIN   ?= $(SDK_TOOLS)/gen_appbin.py
+GEN_FLASHBIN ?= $(SDK_TOOLS)/gen_flashbin.py
+KILL_TERM    ?= $(shell pkill screen)
+GET_FILESIZE ?= stat -L -f%z
+
+COM_PORT     ?= /dev/tty.usbserial
---
+++ Sming/Makefile-windows.mk
@@ -0,0 +1,15 @@
+# ESP8266 sdk package home directory
+ESP_HOME ?= c:/Espressif
+
+# base directory of the ESP8266 SDK package, absolute
+SDK_BASE	?= $(ESP_HOME)/ESP8266_SDK
+SDK_TOOLS	 ?= $(ESP_HOME)/utils
+
+# Other tools mappings
+ESPTOOL		 ?= $(SDK_TOOLS)/esptool.exe
+GEN_APPBIN   ?= $(SDK_TOOLS)/gen_appbin.exe
+GEN_FLASHBIN ?= $(SDK_TOOLS)/gen_flashbin.exe
+KILL_TERM    ?= $(shell taskkill /f /im termite.exe)
+GET_FILESIZE ?= stat --printf="%s"
+
+COM_PORT	 ?= COM3
