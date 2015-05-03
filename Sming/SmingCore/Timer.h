bae5d49f581108cc676a5258c13bb443b2083c70
hreintke <hreintke@tauri.nl>
2015-05-03 15:10:50 +0200

    Update Timer to use Delegate

--- Sming/SmingCore/Timer.h
+++ Sming/SmingCore/Timer.h
@@ -9,6 +9,7 @@
 #define _SMING_CORE_Timer_H_
 
 #include "../SmingCore/Interrupts.h"
+#include "../SmingCore/Delegate.h"
 #include "../Wiring/WiringFrameworkDependencies.h"
 
 class Timer
@@ -17,31 +18,33 @@ public:
 	Timer();
 	~Timer();
 
-	// Init in Milliseconds. Method return "this" reference
-	Timer& IRAM_ATTR initializeMs(uint32_t milliseconds=1000000, InterruptCallback interrupt = NULL);
 	// Init in Microseconds. Method return "this" reference
-	Timer& IRAM_ATTR initializeUs(uint32_t microseconds=1000000, InterruptCallback interrupt = NULL);
-	void IRAM_ATTR start(bool repeating=true);
+	Timer& IRAM_ATTR initializeUs(uint32_t microseconds = 1000000, InterruptCallback interrupt = NULL);
+	// Init in Milliseconds. Method return "this" reference
+	Timer& IRAM_ATTR initializeMs(uint32_t milliseconds = 1000000, InterruptCallback interrupt = NULL);
+	Timer& IRAM_ATTR initializeMs(uint32_t milliseconds, Delegate<void()> delegatefunction = NULL);
+	void IRAM_ATTR start(bool repeating = true);
 	void __inline IRAM_ATTR startOnce() { start(false); }
 	void IRAM_ATTR stop();
 	void restart();
 	bool isStarted();
 	uint32_t getIntervalUs();
 	uint32_t getIntervalMs();
-	//void resume();
-
-    //void attachInterrupt(InterruptCallback interrupt);
-    //void detachInterrupt();
-    //void setPeriod(long microseconds);
+    void setIntervalUs(uint32_t microseconds = 1000000);
+    void setIntervalMs(uint32_t milliseconds = 1000000);
+    void setCallback(InterruptCallback interrupt = NULL);
+    void setCallback(Delegate<void()> delegatefunction);
 
 protected:
     static void IRAM_ATTR processing(void *arg);
 
 private:
     os_timer_t timer;
-    uint32_t interval;
-    InterruptCallback callback;
-    bool started;
+    uint32_t interval = 0;
+    Delegate<void()> delegate_func = NULL;
+    bool started = false;
+    bool delegated = false;
+
 };
 
 #endif /* _SMING_CORE_Timer_H_ */
