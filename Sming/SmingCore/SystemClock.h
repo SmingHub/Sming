142b3aa0adb87aa4ed232d0c0715713836131dd7
hreintke <hreintke@tauri.nl>
2015-05-26 16:56:37 +0200

    Added working example of Delegate Callback
    Updates SystemClock to use enums

--- Sming/SmingCore/SystemClock.h
+++ Sming/SmingCore/SystemClock.h
@@ -6,25 +6,39 @@
 #include "../../Wiring/WString.h"
 #include "../SmingCore/Network/NtpClient.h"
 
+typedef enum
+{
+	eSCUtc 		= 0,
+	eSCLocal	= 1
+} eSysClockTime;
+
+typedef enum
+{
+	eSCInitial	= 0,
+	eSCSet		= 1
+} eSysClockStatus;
+
 class NtpClient;
 
 class SystemClockClass
 {
 public:
-	DateTime now(bool timeUtc = false);
-	void setTime(time_t time, bool timeUtc = false);
-	String getSystemTimeString(bool timeUtc = false);
+	DateTime now(eSysClockTime timeType = eSCLocal);
+	void setTime(time_t time, eSysClockTime timeType = eSCLocal);
+	String getSystemTimeString(eSysClockTime timeType = eSCLocal);
 	bool setTimezone(double reqTimezone);
 
 	void setNtpSync(String reqServer, int reqInterval);
 
+	NtpClient* ntpClient = nullptr;
 private:
 	uint32_t systemTime;
 	uint32_t prevMillis;
 	double timezoneDiff = 0.0;
 	
 	DateTime dateTime;
-	NtpClient* ntpClient = nullptr;
+
+	eSysClockStatus SCStatus = eSCInitial;
 	
 };
 
