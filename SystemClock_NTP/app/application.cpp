711f45817526a45170e033b3aa9884f451b6abd3
hreintke <hreintke@tauri.nl>
2015-05-27 19:14:12 +0200

    Use other declaration of ntpClientDemo

--- SystemClock_NTP/app/application.cpp
+++ SystemClock_NTP/app/application.cpp
@@ -71,7 +71,9 @@ NtpClient ntpClient("pool.ntp.org", 30);
 ntpClientDemo *demo;
 
 // CallBack example 1 
-//ntpClientDemo dm1 = ntpClientDemo();
+// ntpClientDemo dm1 = ntpClientDemo();
+// or use 
+// ntpClientDemo dm1;
 
 void onPrintSystemTime() {
 	Serial.print("Time    : ");
