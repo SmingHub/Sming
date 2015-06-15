88939f3a672d3f19c13a5e316479899400946b6c
anakod <anakod@gmail.com>
2015-06-16 04:20:35 +0500

    * user_rf_pre_init double implementation fix * Global WiFi settings for Basic_WiFi

--- Basic_WiFi/app/application.cpp
+++ Basic_WiFi/app/application.cpp
@@ -1,9 +1,11 @@
 #include <user_config.h>
 #include <SmingCore/SmingCore.h>
 
-// Put you SSID and Password here
-#define WIFI_SSID "EnterSSID"
-#define WIFI_PWD "EnterPassword"
+// If you want, you can define WiFi settings globally in Eclipse Environment Variables
+#ifndef WIFI_SSID
+	#define WIFI_SSID "PleaseEnterSSID" // Put you SSID and Password here
+	#define WIFI_PWD "PleaseEnterPass"
+#endif
 
 // Will be called when WiFi station network scan was completed
 void listNetworks(bool succeeded, BssList list)
--- Sming/appinit/user_rf_pre_init.c
+++
@@ -1,5 +0,0 @@
-// Provide default empty implementation of user_rf_pre_init
-// to maintain compatibility with older SDK versions.
-void user_rf_pre_init(void)
-{
-}
