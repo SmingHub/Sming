2f87549d37db17bf45e1a89aa5ce84145c8285c9
Dmitry Kireev <dmitry@kireev.co>
2015-06-28 22:15:01 -0700

    Move default COM port setting higher in Make Files

--- Sming/Makefile-linux.mk
+++ Sming/Makefile-linux.mk
@@ -1,6 +1,9 @@
 # ESP8266 sdk package home directory
 ESP_HOME ?= /opt/esp-open-sdk
 
+# Default COM port
+COM_PORT     ?= /dev/ttyUSB0
+
 # base directory of the ESP8266 SDK package, absolute
 SDK_BASE	?= $(ESP_HOME)/sdk
 SDK_TOOLS	 ?= $(SDK_BASE)/tools
@@ -9,6 +12,3 @@ SDK_TOOLS	 ?= $(SDK_BASE)/tools
 ESPTOOL		 ?= $(ESP_HOME)/esptool/esptool.py
 KILL_TERM    ?= pkill screen
 GET_FILESIZE ?= stat --printf="%s"
-
-COM_PORT     ?= /dev/ttyUSB0
-
