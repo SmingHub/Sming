e004266493e0acc679759a8419575ec5606ea425
Dmitry Kireev <dmitry@kireev.co>
2015-07-02 23:22:22 -0700

    Update terminal port speed to use COM_SPEED_SERIAL

--- Sming/Makefile-linux.mk
+++ Sming/Makefile-linux.mk
@@ -10,7 +10,7 @@ SDK_TOOLS	 ?= $(SDK_BASE)/tools
 
 # Other tools mappings
 ESPTOOL		 ?= $(ESP_HOME)/esptool/esptool.py
-KILL_TERM    ?= pkill -9 -f "$(COM_PORT) $(COM_SPEED_ESPTOOL)" || exit 0
+KILL_TERM    ?= pkill -9 -f "$(COM_PORT) $(COM_SPEED_SERIAL)" || exit 0
 GET_FILESIZE ?= stat --printf="%s"
-TERMINAL     ?= python -m serial.tools.miniterm $(COM_PORT) $(COM_SPEED_ESPTOOL)
+TERMINAL     ?= python -m serial.tools.miniterm $(COM_PORT) $(COM_SPEED_SERIAL)
 MEMANALYZER  ?= $(OBJDUMP) -h -j .data -j .rodata -j .bss -j .text -j .irom0.text
