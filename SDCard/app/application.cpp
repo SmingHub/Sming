8ee6f86460d5cee92310b20a8cc24e4daba3e129
Andrei Diea <diea_andrei@yahoo.com>
2015-07-16 16:37:32 +0300

    example benchmark

---
+++ SDCard/app/application.cpp
@@ -0,0 +1,54 @@
+
+
+#include <user_config.h>
+#include <SmingCore/SmingCore.h>
+#include <Libraries/SDCard/SDCard.h>
+
+
+
+void displayComfort();
+
+void init()
+{
+	Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
+	Serial.systemDebugOutput(true); // Allow debug output to serial
+
+	SDCard.begin();
+
+	FIL file;
+	FRESULT fRes;
+	uint32_t t1, t2, td, byteswritten, i;
+	uint8_t sensor_data[1024];
+
+	for(i=0; i<sizeof(sensor_data); i++) 												//init our data
+		sensor_data[i] = '0'+(i%10);									//0123..890123...
+
+	t1 = system_get_time();												//get time at test start
+
+	Serial.print("Write 1K in 1K increment\n");
+	fRes = f_open(&file, "file_1k.txt", FA_WRITE | FA_CREATE_ALWAYS);	//open file
+
+	if (fRes == FR_OK)
+	{
+		f_write(&file, sensor_data, sizeof(sensor_data), &byteswritten);				//write data to the file
+
+		f_close(&file);													//close the file
+
+		if (byteswritten != 1024)
+		{
+			Serial.print("Write to file FAIL\n");
+		}
+	}
+	else
+	{
+		Serial.print("fopen FAIL ");
+		Serial.print((unsigned int)fRes);								//print failure error code
+	}
+
+	t2 = system_get_time();												//get the time at test end
+
+	Serial.print("Test end: ");											//print the write speed
+	Serial.print(1000000.0f / (t2-t1));
+	Serial.print(" kBps\n");
+
+}
