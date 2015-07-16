31cbb135c1cdb00faff46202423f6f1f456b4f5f
Andrei Diea <diea_andrei@yahoo.com>
2015-07-16 15:43:21 +0300

    Replace Adafruit DHT with libDHT; update example

--- Humidity_DHT22/app/application.cpp
+++ Humidity_DHT22/app/application.cpp
@@ -4,7 +4,9 @@
 
 #define WORK_PIN 0 // GPIO0
 
-DHT dht(WORK_PIN, DHT22);
+DHT dht(WORK_PIN);
+
+void displayComfort();
 
 void init()
 {
@@ -12,6 +14,8 @@ void init()
 	Serial.systemDebugOutput(true); // Allow debug output to serial
 
 	dht.begin();
+
+	/*first reading method (Adafruit compatible) */
 
 	float h = dht.readHumidity();
 	float t = dht.readTemperature();
@@ -26,6 +30,92 @@ void init()
 		Serial.print(" %\t");
 		Serial.print("Temperature: ");
 		Serial.print(t);
-		Serial.println(" *C");
+		Serial.println(" *C\n");
 	}
+
+
+	/* improved reading method */
+	TempAndHumidity th;
+	if(dht.readTempAndHumidity(th))
+	{
+		Serial.print("Humidity: ");
+		Serial.print(h);
+		Serial.print(" %\t");
+		Serial.print("Temperature: ");
+		Serial.print(t);
+		Serial.println(" *C\n");
+	}
+	else
+	{
+		Serial.print("Failed to read from DHT: ");
+		Serial.print(dht.getLastError());
+	}
+
+	/* other goodies */
+
+	/*
+	 * Heatindex is the percieved temperature taking humidity into account
+	 * More: https://en.wikipedia.org/wiki/Heat_index
+	 * */
+	Serial.printf("Heatindex: %d *C\n", dht.getHeatIndex());
+
+	/*
+	 * Dewpoint is the temperature where condensation starts.
+	 * Water vapors will start condensing on an object having this temperature or below.
+	 * More: https://en.wikipedia.org/wiki/Dew_point
+	 * */
+	Serial.printf("Dewpoint: %d *C\n", dht.getDewPoint(DEW_ACCURATE_FAST));
+
+	/*
+	 * Determine thermal comfort according to http://epb.apogee.net/res/refcomf.asp
+	 * */
+	displayComfort();
+
 }
+
+void displayComfort()
+{
+	ComfortState cf;
+
+	Serial.print("Comfort is at ");
+	Serial.print(dht.getComfortRatio(cf));
+	Serial.print(" percent, which is ");
+
+	switch(cf)
+	{
+	case Comfort_OK:
+		Serial.print("OK");
+		break;
+	case Comfort_TooHot:
+		Serial.print("Too Hot");
+		break;
+	case Comfort_TooCold:
+		Serial.print("Too Cold");
+		break;
+	case Comfort_TooDry:
+		Serial.print("Too Dry");
+		break;
+	case Comfort_TooHumid:
+		Serial.print("Too Humid");
+		break;
+	case Comfort_HotAndHumid:
+		Serial.print("Hot And Humid");
+		break;
+	case Comfort_HotAndDry:
+		Serial.print("Hot And Dry");
+		break;
+	case Comfort_ColdAndHumid:
+		Serial.print("Cold And Humid");
+		break;
+	case Comfort_ColdAndDry:
+		Serial.print("Cold And Dry");
+		break;
+	default:
+		Serial.print("Unknown:");
+		Serial.print(cf);
+		break;
+	}
+
+	Serial.print("\n");
+}
+
