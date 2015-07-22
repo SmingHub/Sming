f4eaa42b72b295d1e7e425171bd98c9d0e1e16b7
Andrei Diea <diea_andrei@yahoo.com>
2015-07-22 18:39:25 +0300

    mprintf

--- Sming/system/m_printf.cpp
+++ Sming/system/m_printf.cpp
@@ -1,82 +1,24 @@
 /*
-Modified: (github.com/)ADiea
+Author: (github.com/)ADiea
 Project: Sming for ESP8266 - https://github.com/anakod/Sming
 License: MIT
 Date: 20.07.2015
-Descr: embedded version of printf with float support
-Contribution: simplify the original implementation even more e.g. remove some format sub-specifiers like
-				-uppercase
-				-left-justify
-				-width and precision specified separately with * and .*
-				-zeropadding
-				-plus and space for sign display
-*/
-
-/* File : barebones/ee_printf.c
-	This file contains an implementation of ee_printf that only requires a method to output a char to a UART without pulling in library code.
-
-This code is based on a file that contains the following:
- Copyright (C) 2002 Michael Ringgaard. All rights reserved.
-
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions
- are met:
- 
- 1. Redistributions of source code must retain the above copyright 
-    notice, this list of conditions and the following disclaimer.  
- 2. Redistributions in binary form must reproduce the above copyright
-    notice, this list of conditions and the following disclaimer in the
-    documentation and/or other materials provided with the distribution.  
- 3. Neither the name of the project nor the names of its contributors
-    may be used to endorse or promote products derived from this software
-    without specific prior written permission. 
- 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
- SUCH DAMAGE.
-
+Descr: embedded very simple version of printf with float support
 */
 
 #include <stdarg.h>
 #include "../../SmingCore/SmingCore.h"
 
-
 void (*cbc_printchar)(char ch) = uart_tx_one_char;
 
 #define SIGN    	(1<<1)	/* Unsigned/signed long */
-#define HEX_PREP 	(1<<5)	/* 0x */
-
-typedef union {
-long L;
-float F;
-} LF_t;
-
-
 
 static const char *lower_digits = "0123456789abcdefghijklmnopqrstuvwxyz";
-
-static size_t strnlen(const char *s, size_t count);
-
-static size_t strnlen(const char *s, size_t count)
-{
-  const char *sc;
-  for (sc = s; *sc != '\0' && count--; ++sc);
-  return sc - s;
-}
-
 
 static char *ee_number(char *str, long num, int base, int type)
 {
   char sign = ' ', tmp[66];
-  int i = -1;
+  int i = 0;
 
   if (base < 2 || base > 36) return 0;
   
@@ -120,94 +62,73 @@ static char *ee_number(char *str, long num, int base, int type)
 
 char* ftoa(char *buf, float f)
 {
-	long mantissa, int_part, frac_part;
-	short exp2;
-	LF_t x;
-	uint8_t m, max = 6;
+
+	size_t n = 0;
+	uint8_t digits = 6;
 
 	do
 	{
-		if (f == 0.0)
-		{
-			*buf++ = '0';
-			break;
-		}
-		else if(isnan(f))
-		{
-			*buf++ = 'n';
-			*buf++ = 'a';
-			*buf++ = 'n';
-			break;
-		}
 
-		x.F = f;
+	  if (isnan(f))
+	  {
+		  *buf++ = 'n'; *buf++ = 'a'; *buf++ = 'n';
+		  break;
+	  }
+	  if (isinf(f))
+	  {
+		  *buf++ = 'i'; *buf++ = 'n'; *buf++ = 'f';
+		  break;
+	  }
+	  if (f > 4294967040.0)
+	  {
+		  *buf++ = 'o'; *buf++ = 'v'; *buf++ = 'f';
+		  break;
+	  }
+	  if (f <-4294967040.0)
+	  {
+		  *buf++ = '-'; *buf++ = 'o'; *buf++ = 'v'; *buf++ = 'f';
+		  break;
+	  }
 
-		exp2 = (unsigned char) (x.L >> 23) - 127;
-		mantissa = (x.L & 0xFFFFFF) | 0x800000;
-		frac_part = 0;
-		int_part = 0;
+	  // Handle negative numbers
+	  if (f < 0.0)
+	  {
+		  *buf++ = '-';
+	    f = -f;
+	  }
 
-		if (exp2 >= 31)
-		{
-			*buf++ = 'I';
-			*buf++ = 'n';
-			*buf++ = 'f';
-			break;
-		}
-		else if (exp2 < -23)
-		{
-			*buf++ = '-';
-			*buf++ = 'I';
-			*buf++ = 'n';
-			*buf++ = 'f';
-			break;
-		}
-		else if (exp2 >= 23)
-			int_part = mantissa << (exp2 - 23);
-		else if (exp2 >= 0)
-		{
-			int_part = mantissa >> (23 - exp2);
-			frac_part = (mantissa << (exp2 + 1)) & 0xFFFFFF;
-		}
-		else
-			/* if (exp2 < 0) */
-			frac_part = (mantissa & 0xFFFFFF) >> -(exp2 + 1);
+	  // Round correctly so that print(1.999, 2) prints as "2.00"
+	  double rounding = 0.0000005;
 
+	  f += rounding;
 
+	  // Extract the integer part of the number and print it
+	  unsigned long int_part = (unsigned long)f;
+	  double remainder = f - (double)int_part;
 
-		if (x.L < 0)
-			*buf++ = '-';
+	  buf = ee_number(buf, int_part, 10, 0);
 
-		if (int_part == 0)
-			*buf++ = '0';
-		else
-		{
-			buf = ee_number(buf, int_part, 10, 0);
-		}
-		*buf++ = '.';
+	  // Print the decimal point, but only if there are digits beyond
+	  *buf++ = '.';
 
-		if (frac_part == 0)
-			*buf++ = '0';
-		else
-		{
-			/* print BCD */
-			for (m = 0; m < max; m++)
-			{
-				/* frac_part *= 10; */
-				frac_part = (frac_part << 3) + (frac_part << 1);
+	  // Extract digits from the remainder one at a time
+	  while (digits-- > 0)
+	  {
+	    remainder *= 10.0;
+	    uint8_t toPrint = uint8_t(remainder);
+	    *buf++ = toPrint + '0';
+	    remainder -= toPrint;
+	  }
 
-				*buf++ = (frac_part >> 24) + '0';
-				frac_part &= 0xFFFFFF;
-			}
-			/* delete ending zeroes */
-			for (--buf; buf[0] == '0' && buf[-1] != '.'; --buf)
-				;
-			++buf;
-		}
-	} while(0);
+		/* delete ending zeroes */
+		for (--buf; buf[0] == '0' && buf[-1] != '.'; --buf)
+			;
+		++buf;
+
+	}
+	while(0);
 
 	return buf;
-
 }
 
 
@@ -222,9 +143,6 @@ static int ee_vsprintf(char *buf, const char *fmt, va_list args)
   const char *s;
 
   int flags;            // Flags to number()
-
-  int field_width;      // Width of output field
-  int precision;        // Min. # of digits for integers; max number of chars for from string
   int qualifier;        // 'h', 'l', or 'L' for integer fields
 
   for (str = buf; *fmt; fmt++)
@@ -234,18 +152,21 @@ static int ee_vsprintf(char *buf, const char *fmt, va_list args)
       *str++ = *fmt;
       continue;
     }
-                  
+
     // Process flags
     flags = 0;
 
-    fmt++; // This also skips first '%'
+    fmt++; // This skips first '%'
 
-    // Get field width
-    field_width = -1;
+    //actual percent sign
+    if(*fmt == '%')
+    {
+    	*str++ = *fmt;
+    	continue;
+    }
 
-    // Get the precision
-    precision = 6;
-
+    while((*fmt >= '0' && *fmt <= '9') || '.' == *fmt || '+' == *fmt || '-' == *fmt || '#' == *fmt || '*' == *fmt)
+    	fmt++;
 
     // Get the conversion qualifier
     qualifier = -1;
@@ -261,17 +182,16 @@ static int ee_vsprintf(char *buf, const char *fmt, va_list args)
     switch (*fmt)
     {
       case 'c':
-        while (--field_width > 0) *str++ = ' ';
         *str++ = (unsigned char) va_arg(args, int);
-
         continue;
 
       case 's':
         s = va_arg(args, char *);
-        if (!s) s = "<NULL>";
-        len = strnlen(s, precision);
-        while (len < field_width--) *str++ = ' ';
-        for (i = 0; i < len; ++i) *str++ = *s++;
+
+        if (!s)
+        	s = "<NULL>";
+
+        for (i = 0; *s; ++i) *str++ = *s++;
 
         continue;
 
