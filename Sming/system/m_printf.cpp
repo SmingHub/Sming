371cf6cf7386f972720deea34371e093bbd7afde
Andrei Diea <diea_andrei@yahoo.com>
2015-07-23 18:01:57 +0300

    printf optimize

--- Sming/system/m_printf.cpp
+++ Sming/system/m_printf.cpp
@@ -12,269 +12,144 @@ Descr: embedded very simple version of printf with float support
 void (*cbc_printchar)(char ch) = uart_tx_one_char;
 
 #define SIGN    	(1<<1)	/* Unsigned/signed long */
-/*
-static const char *lower_digits = "0123456789abcdefghijklmnopqrstuvwxyz";
 
-static char *ee_number(char *str, long num, int base, int type)
+static int m_vsprintf(char *buf, const char *fmt, va_list args)
 {
-  char sign = ' ', tmp[66];
-  int i = 0;
+	int i, base, flags, qualifier;
+	char *str;
+	const char *s;
 
-  if (base < 2 || base > 36) return 0;
-  
-  if (type & SIGN)
-  {
-    if (num < 0)
-    {
-      sign = '-';
-      num = -num;
-    }
-  }
+	char tempNum[24];
 
-  if (num == 0)
-    tmp[i++] = '0';
-  else
-  {
-    while (num != 0)
-    {
-      tmp[i++] = lower_digits[((unsigned long) num) % (unsigned) base];
-      num = ((unsigned long) num) / (unsigned) base;
-    }
-  }
-
-  if (sign) *str++ = sign;
-  
-	if (base == 8)
-	  *str++ = '0';
-	else if (base == 16)
+	for (str = buf; *fmt; fmt++)
 	{
-	  *str++ = '0';
-	  *str++ = 'x';
+		if (*fmt != '%')
+		{
+			*str++ = *fmt;
+			continue;
+		}
+
+		// Process flags
+		flags = 0;
+
+		fmt++; // This skips first '%'
+
+		//actual percent sign
+		if (*fmt == '%')
+		{
+			*str++ = *fmt;
+			continue;
+		}
+
+		//skip precision, width and flags data - not supported
+		while ((*fmt >= '0' && *fmt <= '9') || '.' == *fmt || '+' == *fmt
+				|| '-' == *fmt || '#' == *fmt || '*' == *fmt)
+			fmt++;
+
+		// Get the conversion qualifier
+		qualifier = -1;
+		if (*fmt == 'l' || *fmt == 'L')
+		{
+			qualifier = 'l';
+			fmt++;
+		}
+
+		// Default base
+		base = 10;
+
+		switch (*fmt)
+		{
+		case 'c':
+			*str++ = (unsigned char) va_arg(args, int);
+			continue;
+
+		case 's':
+			s = va_arg(args, char *);
+
+			if (!s)
+			{
+				s = "<NULL>";
+			}
+			else
+			{
+				while (*s)
+					*str++ = *s++;
+			}
+
+			continue;
+
+		case 'p':
+			s = ultoa((unsigned long) va_arg(args, void *), tempNum, 16);
+			while (*s)
+				*str++ = *s++;
+			continue;
+
+		case 'o':
+			base = 8;
+			break;
+
+		case 'x':
+			*str++ = '0';
+			*str++ = 'x';
+			base = 16;
+			break;
+
+		case 'd':
+		case 'i':
+			flags |= SIGN;
+		case 'u':
+			break;
+
+		case 'f':
+
+			s = dtostrf(va_arg(args, double), 0, 6, tempNum);
+			while (*s)
+				*str++ = *s++;
+			continue;
+
+		default:
+			if (*fmt != '%')
+				*str++ = '%';
+			if (*fmt)
+				*str++ = *fmt;
+			else
+				--fmt;
+			continue;
+		}
+
+		if (qualifier == 'l')
+			s = ultoa(va_arg(args, unsigned long), tempNum, base);
+		else if (flags & SIGN)
+			s = ltoa(va_arg(args, int), tempNum, base);
+		else
+			s = ultoa(va_arg(args, unsigned int), tempNum, base);
+
+		while (*s)
+			*str++ = *s++;
 	}
 
-  while (i-- > 0) *str++ = tmp[i];
-
-  return str;
-}
-
-
-
-
-char* ftoa(char *buf, float f)
-{
-
-	size_t n = 0;
-	uint8_t digits = 6;
-
-	do
-	{
-
-	  if (isnan(f))
-	  {
-		  *buf++ = 'n'; *buf++ = 'a'; *buf++ = 'n';
-		  break;
-	  }
-	  if (isinf(f))
-	  {
-		  *buf++ = 'i'; *buf++ = 'n'; *buf++ = 'f';
-		  break;
-	  }
-	  if (f > 4294967040.0)
-	  {
-		  *buf++ = 'o'; *buf++ = 'v'; *buf++ = 'f';
-		  break;
-	  }
-	  if (f <-4294967040.0)
-	  {
-		  *buf++ = '-'; *buf++ = 'o'; *buf++ = 'v'; *buf++ = 'f';
-		  break;
-	  }
-
-	  // Handle negative numbers
-	  if (f < 0.0)
-	  {
-		  *buf++ = '-';
-	    f = -f;
-	  }
-
-	  // Round correctly so that print(1.999, 2) prints as "2.00"
-	  double rounding = 0.0000005;
-
-	  f += rounding;
-
-	  // Extract the integer part of the number and print it
-	  unsigned long int_part = (unsigned long)f;
-	  double remainder = f - (double)int_part;
-
-	  buf = ee_number(buf, int_part, 10, 0);
-
-	  // Print the decimal point, but only if there are digits beyond
-	  *buf++ = '.';
-
-	  // Extract digits from the remainder one at a time
-	  while (digits-- > 0)
-	  {
-	    remainder *= 10.0;
-	    uint8_t toPrint = uint8_t(remainder);
-	    *buf++ = toPrint + '0';
-	    remainder -= toPrint;
-	  }
-
-
-		for (--buf; buf[0] == '0' && buf[-1] != '.'; --buf)
-			;
-		++buf;
-
-	}
-	while(0);
-
-	return buf;
-}
-*/
-
-
-
-static int ee_vsprintf(char *buf, const char *fmt, va_list args)
-{
-  int len;
-  unsigned long num;
-  int i, base;
-  char *str;
-  const char *s;
-
-  char tempNum[24];
-
-  int flags;            // Flags to number()
-  int qualifier;        // 'h', 'l', or 'L' for integer fields
-
-  for (str = buf; *fmt; fmt++)
-  {
-    if (*fmt != '%')
-    {
-      *str++ = *fmt;
-      continue;
-    }
-
-    // Process flags
-    flags = 0;
-
-    fmt++; // This skips first '%'
-
-    //actual percent sign
-    if(*fmt == '%')
-    {
-    	*str++ = *fmt;
-    	continue;
-    }
-
-    while((*fmt >= '0' && *fmt <= '9') || '.' == *fmt || '+' == *fmt || '-' == *fmt || '#' == *fmt || '*' == *fmt)
-    	fmt++;
-
-    // Get the conversion qualifier
-    qualifier = -1;
-    if (*fmt == 'l' || *fmt == 'L')
-    {
-      qualifier = *fmt;
-      fmt++;
-    }
-
-    // Default base
-    base = 10;
-
-    switch (*fmt)
-    {
-      case 'c':
-        *str++ = (unsigned char) va_arg(args, int);
-        continue;
-
-      case 's':
-        s = va_arg(args, char *);
-
-        if (!s)
-        	s = "<NULL>";
-
-        for (i = 0; *s; ++i) *str++ = *s++;
-
-        continue;
-
-      case 'p':
-      	s = ultoa((unsigned long) va_arg(args, void *), tempNum, 16);
-      	for (i = 0; *s; ++i) *str++ = *s++;
-       // str = ee_number(str, , 16, flags);
-        continue;
-
-      // Integer number formats - set up the flags and "break"
-      case 'o':
-        base = 8;
-        break;
-
-
-      case 'x':
-        base = 16;
-        break;
-
-      case 'd':
-      case 'i':
-        flags |= SIGN;
-      case 'u':
-        break;
-
-
-      case 'f':
-    	s = dtostrf(va_arg(args, double), 0, 6, tempNum);
-    	for (i = 0; *s; ++i) *str++ = *s++;
-        //str = ftoa(str, va_arg(args, double));
-        continue;
-
-
-      default:
-        if (*fmt != '%') *str++ = '%';
-        if (*fmt)
-          *str++ = *fmt;
-        else
-          --fmt;
-        continue;
-    }
-
-    if (qualifier == 'l')
-      num = va_arg(args, unsigned long);
-    else if (flags & SIGN)
-      num = va_arg(args, int);
-    else
-      num = va_arg(args, unsigned int);
-
-    if(flags & SIGN)
-    {
-    	s = ltoa(num, tempNum, base);
-    }
-    else
-    {
-      	s = ultoa(num, tempNum, base);
-    }
-    for (i = 0; *s; ++i) *str++ = *s++;
-    //str = ee_number(str, num, base, flags);
-  }
-
-  *str = '\0';
-  return str - buf;
+	*str = '\0';
+	return str - buf;
 }
 
 
 int m_printf(const char *fmt, ...)
 {
-  char buf[15*80],*p;
-  va_list args;
-  int n=0;
+	char buf[15 * 80], *p;
+	va_list args;
+	int n = 0;
 
-  va_start(args, fmt);
-  ee_vsprintf(buf, fmt, args);
-  va_end(args);
-  p=buf;
-  while (*p) {
-	  cbc_printchar(*p);
-	n++;
-	p++;
-  }
+	va_start(args, fmt);
+	m_vsprintf(buf, fmt, args);
+	va_end(args);
 
-  return n;
+	p = buf;
+	while (*p)
+	{
+		cbc_printchar(*p);
+		n++;
+		p++;
+	}
+
+	return n;
 }
--- Sming/system/stringconversion.cpp
+++ Sming/system/stringconversion.cpp
@@ -34,163 +34,99 @@ char* ultoa(unsigned long val, char* buffer, unsigned int base)
 	return buffer;
 }
 
-char* ftoa(char *buf, float f)
-{
-
-	size_t n = 0;
-	uint8_t digits = 6;
-
-	do
-	{
-
-	  if (isnan(f))
-	  {
-		  *buf++ = 'n'; *buf++ = 'a'; *buf++ = 'n';
-		  break;
-	  }
-	  if (isinf(f))
-	  {
-		  *buf++ = 'i'; *buf++ = 'n'; *buf++ = 'f';
-		  break;
-	  }
-	  if (f > 4294967040.0)
-	  {
-		  *buf++ = 'o'; *buf++ = 'v'; *buf++ = 'f';
-		  break;
-	  }
-	  if (f <-4294967040.0)
-	  {
-		  *buf++ = '-'; *buf++ = 'o'; *buf++ = 'v'; *buf++ = 'f';
-		  break;
-	  }
-
-	  // Handle negative numbers
-	  if (f < 0.0)
-	  {
-		  *buf++ = '-';
-	    f = -f;
-	  }
-
-	  // Round correctly so that print(1.999, 2) prints as "2.00"
-	  double rounding = 0.0000005;
-
-	  f += rounding;
-
-	  // Extract the integer part of the number and print it
-	  unsigned long int_part = (unsigned long)f;
-	  double remainder = f - (double)int_part;
-
-	  buf = ee_number(buf, int_part, 10, 0);
-
-	  // Print the decimal point, but only if there are digits beyond
-	  *buf++ = '.';
-
-	  // Extract digits from the remainder one at a time
-	  while (digits-- > 0)
-	  {
-	    remainder *= 10.0;
-	    uint8_t toPrint = uint8_t(remainder);
-	    *buf++ = toPrint + '0';
-	    remainder -= toPrint;
-	  }
-
-
-		for (--buf; buf[0] == '0' && buf[-1] != '.'; --buf)
-			;
-		++buf;
-
-	}
-	while(0);
-
-	return buf;
-}
-
-
-//extern char * ets_strcat ( char * destination, const char * source );
-//#define strcat ets_strcat
 // Author zitron: http://forum.arduino.cc/index.php?topic=37391#msg276209
-//some modif by ADiea
+// modif by ADiea: remove dependencies from strcat floor round
 char *dtostrf(double floatVar, int minStringWidthIncDecimalPoint, int numDigitsAfterDecimal, char *outputBuffer)
 {
-	 if (outputBuffer == NULL) return NULL;
+	char temp[24], num[24];
+	unsigned long mult = 1, int_part;
+	int16_t i, processedFracLen = numDigitsAfterDecimal;
+	double remainder;
 
-	 char *buf = outputBuffer;
+	if (outputBuffer == NULL)
+		return NULL ;
 
-	 *outputBuffer = 0;
+	char *buf = outputBuffer, *s;
 
-	 if (isnan(floatVar))
-	 {*buf++ = 'N'; *buf++ = 'a'; *buf++ = 'N';}
-	 else if (isinf(floatVar))
-	 {*buf++ = 'I'; *buf++ = 'n'; *buf++ = 'f';}
-	 else if (floatVar > 4294967040.0)
-	 {*buf++ = 'O'; *buf++ = 'V'; *buf++ = 'F';}  // constant determined empirically
-	 else if (floatVar <-4294967040.0)
-	 {*buf++ = 'o'; *buf++ = 'v'; *buf++ = 'f';}  // constant determined empirically
+	if (isnan(floatVar))
+		strcpy(outputBuffer, "NaN");
+	else if (isinf(floatVar))
+		strcpy(outputBuffer, "Inf");
+	else if (floatVar > 4294967040.0)  // constant determined empirically
+		strcpy(outputBuffer, "OVF");
+	else if (floatVar < -4294967040.0)   // constant determined empirically
+		strcpy(outputBuffer, "ovf");
+	else
+	{
+		//start building the number
+		buf = num;
 
-	 if (*outputBuffer != 0)
-	 {
-		 *buf++ = 0;
-		 return outputBuffer;
-	 }
+		if (floatVar < 0.0)
+		{
+			*buf++ = '-';  //print "-" sign
+			floatVar = -floatVar;
+		}
 
-	 char temp[24];
-	 int16_t i;
+		// Extract the integer part of the number and print it
+
+		if (processedFracLen > 9)
+			processedFracLen = 9; // Prevent overflow!
+
+		i = processedFracLen;
+
+		while (i-- > 0)
+			mult *= 10;
+
+		floatVar += 0.5 / (float) mult;      // compute rounding factor
+
+		int_part = (unsigned long) floatVar;
+
+		s = ltoa(int_part, buf, 10);
+
+		buf += strlen(s); //go to end of string
 
 
-	 if(floatVar < 0.0){
-		*buf++ = '-';  //print "-" sign
-	   floatVar *= -1;
-	 }
+		if (numDigitsAfterDecimal != 0)
+		{
+			*buf++ = '.'; // print the decimal point
 
-	 if( numDigitsAfterDecimal == 0) {
-	   strcat(outputBuffer, ltoa(round(floatVar), temp, 10));  //prints the int part
-	 }
-	 else {
-	   unsigned long frac, mult = 1;
-	   int16_t  processedFracLen = numDigitsAfterDecimal;
-	   if (processedFracLen > 9) processedFracLen = 9; // Prevent overflow!
+			long x = ((floatVar - int_part) * mult);
 
-	   int16_t padding = processedFracLen - 1;
+			s = ltoa( x, temp, 10);
 
-	   int16_t k = processedFracLen;
-	   while (k-- > 0)
-	     mult = mult<<3 + mult<<2; //mult *= 10
+			i = processedFracLen - strlen(s) + 1;
 
-	   floatVar += 0.5/(float)mult;      // compute rounding factor
+			while (--i > 0)
+				*buf++ = '0';    // print padding zeros
 
-	   strcat(outputBuffer, ltoa(floor(floatVar), temp, 10));  //prints the integer part without rounding
-	   strcat(outputBuffer, ".\0"); // print the decimal point
+			while (*s)
+				*buf++ = *s++;
 
-	   frac = (floatVar - floor(floatVar)) * mult;
+			i = numDigitsAfterDecimal - processedFracLen;
 
-	   unsigned long frac1 = frac;
+			while (i-- > 0)
+				*buf++ = '0';    // ending fraction zeroes
+		}
 
-	   while(frac1 /= 10)
-	     padding--;
+		//terminate num
+		*buf = 0;
+		buf = outputBuffer;
 
-	   while(padding--)
-	     strcat(outputBuffer,"0\0");    // print padding zeros
+		// generate width space padding
+		i = minStringWidthIncDecimalPoint - strlen(num);
+		while (--i > 0)
+		{
+			*buf++ = ' ';
+		}
 
-	   strcat(outputBuffer,ltoa(frac,temp,10));  // print fraction part
+		//Write output buffer
+		s = num;
+		while (*s)
+			*buf++ = *s++;
 
-	   k = numDigitsAfterDecimal - processedFracLen;
-	   while (k-- > 0)
-	     strcat(outputBuffer,"0\0"); // ending fraction zeroes
-	 }
+		//termninate outputBuffer
+		*buf = 0;
+	}
 
-	 // generate width space padding
-	 if ((minStringWidthIncDecimalPoint != 0) && (minStringWidthIncDecimalPoint >= strlen(outputBuffer))){
-	   int16_t J=0;
-	   J = minStringWidthIncDecimalPoint - strlen(outputBuffer);
-
-	   for (i=0; i< J; i++) {
-	     temp[i] = ' ';
-	   }
-
-	   temp[i++] = '\0';
-	   strcat(temp,outputBuffer);
-	   strcpy(outputBuffer,temp);
-	 }
-
-	 return outputBuffer;
+	return outputBuffer;
 }
