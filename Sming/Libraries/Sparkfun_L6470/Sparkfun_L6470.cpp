8759007d46f6fd36f56da6cdac688172b6091ddc
zhivko <klemen.zivkovic@gmail.com>
2015-08-21 22:53:44 +0200

    Latest not yet working code...

--- Sming/Libraries/Sparkfun_L6470/Sparkfun_L6470.cpp
+++ Sming/Libraries/Sparkfun_L6470/Sparkfun_L6470.cpp
@@ -6,116 +6,124 @@
 #include "../../SmingCore/pins_arduino.h"
 #include "c:/Espressif/ESP8266_SDK/include/eagle_soc.h"
 
-
 // reduces how much is refreshed, which speeds it up!
 // originally derived from Steve Evans/JCW's mod but cleaned up and
 // optimized
 //#define enablePartialUpdate
 
-
-Sparkfun_L6470::Sparkfun_L6470(int8_t SCLK, int8_t DIN, int8_t DC,
-    int8_t CS, int8_t RST)  {
-  _din = DIN;
-  _sclk = SCLK;
-  _dc = DC;
-  _rst = RST;
-  _cs = CS;
+Sparkfun_L6470::Sparkfun_L6470(int8_t SCLK, int8_t DIN, int8_t DC, int8_t CS,
+		int8_t RST)
+{
+	_din = DIN;
+	_sclk = SCLK;
+	_dc = DC;
+	_rst = RST;
+	_cs = CS;
 }
 
-Sparkfun_L6470::Sparkfun_L6470(int8_t SCLK, int8_t DIN, int8_t DC,
-    int8_t RST)  {
-  _din = DIN;
-  _sclk = SCLK;
-  _dc = DC;
-  _rst = RST;
-  _cs = -1;
+Sparkfun_L6470::Sparkfun_L6470(int8_t SCLK, int8_t DIN, int8_t DC, int8_t RST)
+{
+	_din = DIN;
+	_sclk = SCLK;
+	_dc = DC;
+	_rst = RST;
+	_cs = -1;
 }
 
-Sparkfun_L6470::Sparkfun_L6470(int8_t DC, int8_t CS, int8_t RST) {
-  // -1 for din and sclk specify using hardware SPI
-  _din = -1;
-  _sclk = -1;
-  _dc = DC;
-  _rst = RST;
-  _cs = CS;
+Sparkfun_L6470::Sparkfun_L6470(int8_t DC, int8_t CS, int8_t RST)
+{
+	// -1 for din and sclk specify using hardware SPI
+	_din = -1;
+	_sclk = -1;
+	_dc = DC;
+	_rst = RST;
+	_cs = CS;
 }
 
+void Sparkfun_L6470::begin()
+{
+	/*
+	 if (isHardwareSPI()) {
+	 // Setup hardware SPI.
+	 SPI.begin();
+	 SPI.setClockDivider(PCD8544_SPI_CLOCK_DIV);
+	 SPI.setDataMode(SPI_MODE0);
+	 SPI.setBitOrder(MSBFIRST);
+	 }
+	 else {
+	 */
+	// Setup software SPI.
+	// Set software SPI specific pin outputs.
+	pinMode(_din, OUTPUT);
+	pinMode(_sclk, OUTPUT);
 
-void Sparkfun_L6470::begin(uint8_t contrast, uint8_t bias) {
- /*
-	if (isHardwareSPI()) {
-    // Setup hardware SPI.
-    SPI.begin();
-    SPI.setClockDivider(PCD8544_SPI_CLOCK_DIV);
-    SPI.setDataMode(SPI_MODE0);
-    SPI.setBitOrder(MSBFIRST);
-  }
-  else {
-*/
-    // Setup software SPI.
-
-    // Set software SPI specific pin outputs.
-    pinMode(_din, OUTPUT);
-    pinMode(_sclk, OUTPUT);
-
-    // Set software SPI ports and masks.
-    clkport     = portOutputRegister(digitalPinToPort(_sclk));
-    clkpinmask  = digitalPinToBitMask(_sclk);
-    mosiport    = portOutputRegister(digitalPinToPort(_din));
-    mosipinmask = digitalPinToBitMask(_din);
+	// Set software SPI ports and masks.
+	clkport = portOutputRegister(digitalPinToPort(_sclk));
+	clkpinmask = digitalPinToBitMask(_sclk);
+	mosiport = portOutputRegister(digitalPinToPort(_din));
+	mosipinmask = digitalPinToBitMask(_din);
 //  }
 
-  // Set common pin outputs.
-  pinMode(_dc, OUTPUT);
-  if (_rst > 0)
-      pinMode(_rst, OUTPUT);
-  if (_cs > 0)
-      pinMode(_cs, OUTPUT);
+// Set common pin outputs.
+	pinMode(_dc, OUTPUT);
+	if (_rst > 0)
+		pinMode(_rst, OUTPUT);
+	if (_cs > 0)
+		pinMode(_cs, OUTPUT);
 
-  // toggle RST low to reset
-  if (_rst > 0) {
-    digitalWrite(_rst, LOW);
-    delay(500);
-    digitalWrite(_rst, HIGH);
-  }
+	// toggle RST low to reset
+	if (_rst > 0)
+	{
+		digitalWrite(_rst, LOW);
+		delay(500);
+		digitalWrite(_rst, HIGH);
+	}
 
 }
 
-
-inline void Sparkfun_L6470::spiWrite(uint8_t d) {
-  if (isHardwareSPI()) {
-    // Hardware SPI write.
-    SPI.transfer(d);
-  }
-  else {
-    // Software SPI write with bit banging.
-    for(uint8_t bit = 0x80; bit; bit >>= 1) {
-      *clkport &= ~clkpinmask;
-      if(d & bit) *mosiport |=  mosipinmask;
-      else        *mosiport &= ~mosipinmask;
-      *clkport |=  clkpinmask;
-    }
-  }
+inline void Sparkfun_L6470::spiWrite(uint8_t d)
+{
+	if (isHardwareSPI())
+	{
+		// Hardware SPI write.
+		SPI.transfer(d);
+	}
+	else
+	{
+		// Software SPI write with bit banging.
+		for (uint8_t bit = 0x80; bit; bit >>= 1)
+		{
+			*clkport &= ~clkpinmask;
+			if (d & bit)
+				*mosiport |= mosipinmask;
+			else
+				*mosiport &= ~mosipinmask;
+			*clkport |= clkpinmask;
+		}
+	}
 }
 
-bool Sparkfun_L6470::isHardwareSPI() {
-  return (_din == -1 && _sclk == -1);
+bool Sparkfun_L6470::isHardwareSPI()
+{
+	return (_din == -1 && _sclk == -1);
 }
 
-void Sparkfun_L6470::command(uint8_t c) {
-  digitalWrite(_dc, LOW);
-  if (_cs > 0)
-    digitalWrite(_cs, LOW);
-  shiftOut(_din, _sclk, MSBFIRST, c);
-  if (_cs > 0)
-    digitalWrite(_cs, HIGH);
+void Sparkfun_L6470::command(uint8_t c)
+{
+	digitalWrite(_dc, LOW);
+	if (_cs > 0)
+		digitalWrite(_cs, LOW);
+	shiftOut(_din, _sclk, MSBFIRST, c);
+	if (_cs > 0)
+		digitalWrite(_cs, HIGH);
 }
 
-void Sparkfun_L6470::data(uint8_t c) {
-  digitalWrite(_dc, HIGH);
-  if (_cs > 0)
-    digitalWrite(_cs, LOW);
-  shiftOut(_din, _sclk, MSBFIRST, c);
-  if (_cs > 0)
-    digitalWrite(_cs, HIGH);
+void Sparkfun_L6470::data(uint8_t c)
+{
+	digitalWrite(_dc, HIGH);
+	if (_cs > 0)
+		digitalWrite(_cs, LOW);
+	shiftOut(_din, _sclk, MSBFIRST, c);
+	if (_cs > 0)
+		digitalWrite(_cs, HIGH);
 }
--- Stepper_Sparkfun_L6470/app/application.cpp
+++ Stepper_Sparkfun_L6470/app/application.cpp
@@ -3,97 +3,94 @@
 #include <Libraries/Sparkfun_L6470/Sparkfun_L6470.h>
 //#include <Libraries/Sparkfun_L6470/SparkFundSPINConstants.h>
 
-
 // Put you SSID and Password here
-#define WIFI_SSID "AndroidAP"
+#define WIFI_SSID "AsusKZ"
 #define WIFI_PWD "Doitman1"
 #define NOTE_DIVISOR 2  // My cheesy way of reducing the note frequencies to a ranges
 
-
 /* Pinout:
-int8_t  MISO = 12;
-int8_t  MOSI= 13;
-int8_t  CLK= 14;
-int8_t  CS =15;
-int8_t  DC =5;
-int8_t  RST= 4;
-*/
+ int8_t  MISO = 12;
+ int8_t  MOSI= 13;
+ int8_t  CLK= 14;
+ int8_t  CS =15;
+ int8_t  DC =5;
+ int8_t  RST= 4;
+ */
 
-//Adafruit_ILI9341 tft;
-//Sparkfun_L6470(int8_t SCLK, int8_t DIN, int8_t DC, int8_t RST);
-
-Sparkfun_L6470 stepper = Sparkfun_L6470(14, 12, 5, 4);
+//Sparkfun_L6470::Sparkfun_L6470(int8_t SCLK, int8_t DIN, int8_t DC, int8_t CS,	int8_t RST)
+Sparkfun_L6470 stepper = Sparkfun_L6470(14, 13, 5, 15, 4);
 
 Timer guiTimer;
 int stepDir = 1;
 
 void dSPINConfig(void)
 {
-    stepper.configSyncPin(BUSY_PIN, 0); // BUSY pin low during operations;
-                                        //  second paramter ignored.
-    stepper.configStepMode(STEP_FS);   // 0 microsteps per step
-    stepper.setMaxSpeed(10000);        // 10000 steps/s max
-    stepper.setFullSpeed(10000);       // microstep below 10000 steps/s
-    stepper.setAcc(10000);             // accelerate at 10000 steps/s/s
-    stepper.setDec(10000);
-    stepper.setSlewRate(SR_530V_us);  // Upping the edge speed increases torque.
-    stepper.setOCThreshold(OC_750mA);  // OC threshold 750mA
-    stepper.setPWMFreq(PWM_DIV_2, PWM_MUL_2); // 31.25kHz PWM freq
-    stepper.setOCShutdown(OC_SD_DISABLE); // don't shutdown on OC
-    stepper.setVoltageComp(VS_COMP_DISABLE); // don't compensate for motor V
-    stepper.setSwitchMode(SW_USER);    // Switch is not hard stop
-    stepper.setOscMode(INT_16MHZ_OSCOUT_16MHZ); // for boardA, we want 16MHz
+
+	stepper.begin();
+	stepper.configSyncPin(BUSY_PIN, 0); // BUSY pin low during operations;
+										//  second paramter ignored.
+	stepper.configStepMode(STEP_FS);   // 0 microsteps per step
+	stepper.setMaxSpeed(10000);        // 10000 steps/s max
+	stepper.setFullSpeed(10000);       // microstep below 10000 steps/s
+	stepper.setAcc(10000);             // accelerate at 10000 steps/s/s
+	stepper.setDec(10000);
+	stepper.setSlewRate(SR_530V_us);  // Upping the edge speed increases torque.
+	stepper.setOCThreshold(OC_750mA);  // OC threshold 750mA
+	stepper.setPWMFreq(PWM_DIV_2, PWM_MUL_2); // 31.25kHz PWM freq
+	stepper.setOCShutdown(OC_SD_DISABLE); // don't shutdown on OC
+	stepper.setVoltageComp(VS_COMP_DISABLE); // don't compensate for motor V
+	stepper.setSwitchMode(SW_USER);    // Switch is not hard stop
+	stepper.setOscMode(INT_16MHZ_OSCOUT_16MHZ); // for boardA, we want 16MHz
 //  internal osc, 16MHz out. boardB and
 //  boardC will be the same in all respects
 //  but this, as they will bring in and
 //  output the clock to keep them
 //  all in phase.
-    stepper.setAccKVAL(255);        // We'll tinker with these later, if needed.
-    stepper.setDecKVAL(255);
-    stepper.setRunKVAL(255);
-    stepper.setHoldKVAL(32);  // This controls the holding current; keep it low.
+	stepper.setAccKVAL(255);        // We'll tinker with these later, if needed.
+	stepper.setDecKVAL(255);
+	stepper.setRunKVAL(255);
+	stepper.setHoldKVAL(32);  // This controls the holding current; keep it low.
 }
 
 void playNote()
 {
-    if (stepDir == 1)
-    {
-        Serial.println("Direction=1");
-        stepper.run(FWD, 40);
-        stepDir = 0;
-    }
-    else
-    {
-        Serial.println("Direction=0");
-        stepper.run(REV, 200);
-        stepDir = 1;
-    }
+	if (stepDir == 1)
+	{
+		Serial.println("Direction=1");
+		stepper.run(FWD, 40);
+		stepDir = 0;
+	}
+	else
+	{
+		Serial.println("Direction=0");
+		stepper.run(REV, 200);
+		stepDir = 1;
+	}
 
 }
-
 
 void basicGui()
 {
-    //playNote();
-    Serial.println("runned");
-    stepper.run(FWD, 40);
+	//playNote();
+	Serial.println("Runned\n");
+	os_printf_plus("Status: %d\n", stepper.getStatus());
+	stepper.run(FWD, 40);
 }
-
 
 void init()
 {
 	WDT.enable(false);
-    Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
-    Serial.systemDebugOutput(true); // Allow debug output to serial
-    WifiStation.config(WIFI_SSID, WIFI_PWD);
-    WifiStation.enable(true);
-    //WifiAccessPoint.enable(false);
-    //delay(3000);
-    Serial.println("L6470 test start");
-    Serial.println("Config start");
-    dSPINConfig();
-    Serial.println("Config end.");
+	Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
+	Serial.systemDebugOutput(true); // Allow debug output to serial
+	WifiStation.config(WIFI_SSID, WIFI_PWD);
+	WifiStation.enable(true);
+	//WifiAccessPoint.enable(false);
+	//delay(3000);
+	Serial.println("L6470 test start");
+	Serial.println("Config start");
+	dSPINConfig();
+	Serial.println("Config end.");
 
-    //guiTimer.initializeMs(2000, basicGui).start();
-    WifiStation.waitConnection(basicGui, 30, playNote);
+	//guiTimer.initializeMs(2000, basicGui).start();
+	WifiStation.waitConnection(basicGui, 30, playNote);
 }
