2b0e17883067e8cff01605c50bd88e3ba0b5e745
zhivko <klemen.zivkovic@gmail.com>
2015-08-27 13:02:05 +0200

    Committed capacity sensor I2C reading

--- Basic_ScannerI2C/app/application.cpp
+++ Basic_ScannerI2C/app/application.cpp
@@ -76,62 +76,12 @@ void scanBus()
 		Serial.println("No I2C devices found\n");
 	else
 	{
-		Serial.println("done scanning\n");
-
-		unsigned char Setup[4];
-		unsigned char Data[3];
-		/********************************************************************
-		 DATA AQUISITION LOOP
-		 ********************************************************************/
-		// The loop performes a single convertion and reads out the convertion results
-		while (1)
-		{            // Endless loop
-
-			// START CONVERSION ON AD774x
-			// ==============================
-			Setup[0] = 0xA0;  // (Cap Setup Reg) Enable Cap Channel , CAPDIFF on
-			Setup[1] = 0x81; // (VT Setup Reg) Enable Voltage/Temperature, Chop on
-			Setup[2] = 0x0E;  // (EXC Setup Reg) Enable Exc, Level = Vdd*3/8
-			Setup[3] = 0xA2; // (Configuration Reg) Start single conversion, Default rate
-			I2C_Snd(1, 0x48, 0x07, 4, Setup); // Send setup to AD7747
-			// Send to I2C:
-			// 1    .. Transaction with 1-byte address pointer
-			// 0x90 .. AD774x device address
-			// 0x07 .. starting adress pointer - AD774x Cap Setup register
-			// 4    .. 4 bytes to be sent
-			// Setup .. array of bytes to be sent
-
-			// WAIT FOR AD774x RDY
-			do
-			{
-				I2C_Rcv(1, 0x48, 0x00, 1, Data);  // Read AD774x Status
-			} while (Data[0] & 0x04);           // Wait for RDY bit = 0
-
-			// READ AD774x DATA CAP CHANNEL
-			I2C_Rcv(1, 0x90, 0x01, 3, Data);    // Read AD774x Data
-
-			// SEND DATA TO UART
-			// ==============================
-			Serial.printf("%02bX%02bX%02bX\t", Data[0], Data[1], Data[2]);
-
-			// READ AD774x DATA VT CHANNEL
-			I2C_Rcv(1, 0x48, 0x04, 3, Data);    // Read AD774x Data
-
-			// SEND DATA TO UART
-			// ==============================
-			Serial.printf("%02bX%02bX%02bX\t", Data[0], Data[1], Data[2]);
-
-			// to convert the code to degC you have to calculate the following formula
-			// degC = decimal(Data) / 2048 - 4096
-
-		}  // while
 
 	}
 }
 
 void init()
 {
-	Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
 	Serial.systemDebugOutput(false); // Disable debug output
 
 	WDT.enable(false); // First (but not the best) option: fully disable watch dog timer
@@ -143,183 +93,76 @@ void init()
 	procTimer.initializeMs(3000, scanBus).start();
 }
 
-unsigned char I2C_Rcv_Byte(boolean noack)
-// Receives one byte from I2C
-//
-// noack should be 0 if other bytes will be received
-// i.e., slave will be sent ACK
-//
-// noack should be 1 if this is the last byte
-// i.e., slave will be sent NACK
+void readAD7747()
 {
-	unsigned char i;
-	unsigned char d;
 
-	MDE = 0;        // SDA input
-	for (i = 8; i > 0; i--)
+	Wire.begin();                   //sets up i2c for operation
+	Serial.begin(9600);                 // we will monitor this via serial cable
+	Wire.beginTransmission(0x48);
+	Wire.write(0xBF);
+	Wire.write(0x00);
+	Wire.endTransmission();
+	delay(4);
+	Wire.beginTransmission(0x48);                   // begins write cycle
+	Wire.write(0x07);                   //address pointer for cap setup register
+	Wire.write(0xA0);                 //b'10100000' found from datasheet page 16
+	Wire.endTransmission();                   //ends write cycle
+	delay(4); // Wait for data to clock out? I'm not 100% sure why this delay is here (or why it's 4ms)
+	Wire.beginTransmission(0x48);                   //begins transmission again
+	Wire.write(0x09); //address pointer for capacitive channel excitation register
+	Wire.write(0x0E);                   //recommended value from datasheet
+	Wire.endTransmission();
+	delay(4);
+	Wire.beginTransmission(0x48);
+	Wire.write(0x0A);           //address pointer for the configuration register
+	Wire.write(0x21); //b'00100001' for continuous conversion, arbitrary VTF setting, and mid-range capacitive conversion time
+	Wire.endTransmission();
+	Wire.beginTransmission(0x48);
+	Wire.write(0x0B);           //CAP DAC A Register address (Positive pin data)
+	Wire.write(0x80);                   //b'10111111' for enable Cap DAC A
+	Wire.endTransmission();
+	Serial.println("Hello!");   //test to make sure serial connection is working
+
+	while (true)
 	{
-		MCO = 1;      // SCL hi
-		// consider using _crol_(d,1) ?
-		d <<= 1;      // rotate data
-		d |= MDI;     // read SDA -> data
-		MCO = 0;      // SCL lo
+		Wire.beginTransmission(0x48);   //talking to chip
+		Wire.write(byte(0x00));   //status register address
+		Wire.endTransmission();
+		Wire.requestFrom(0x48, 1);   //request status register data
+		int readycap;
+		readycap = Wire.read();
+		if ((readycap & 0x1) == 0)
+		{                // ready?
+			Serial.println("Data Ready");
+			delay(500);
+			Wire.beginTransmission(0x48);    //arduino asks for data from ad7747
+			Wire.write(0x01);   //set address point to capacitive DAC register 1
+
+			Wire.endTransmission();      //pointer is set so now we can read the
+
+			Serial.println("Data Incoming");
+			delay(500);
+			Wire.requestFrom(0x48, 3);   //reads data from cap DAC registers 1-3
+
+			while (Wire.available())
+			{
+				unsigned char hi, mid, lo;      //1 byte numbers
+				long capacitance;      //will be a 3byte number
+				float pf;      //scaled value of capacitance
+				hi = Wire.read();
+				mid = Wire.read();
+				lo = Wire.read();
+				capacitance = (hi << 16) + (mid << 8) + lo - 0x800000;
+				pf = (float) capacitance * -1 / (float) 0x800000 * 8.192f;
+				Serial.print(pf, DEC); //prints the capacitance data in decimal through serial port
+
+				Serial.print(",");
+			}
+			Serial.println();
+		}
+		Serial.println("Loop Done");
+		Serial.println("  ");
+		delay(1000);
 	}
-	MDE = 1;        // SDA output
-	MDO = noack;    // SDA = ACK .. NACK for the last byte
-	MCO = 1;        // SCL hi
-	MCO = 0;        // SCL lo
-	MDE = 0;        // SDA input - release
-	return (d);
-
-} // end of I2C_Rcv_Byte
-
-
-bool I2C_Snd_Byte(unsigned char d)
-// Sends one byte to I2C
-//
-// Returns 0 if byte is acknowledged by slave
-// Returns 1 if transaction fails
-{
-  unsigned char i;
-  unsigned char noack;
-
-  MDE = 1;        // SDA output
-  for (i=8; i>0; i--)
-  {
-    // consider _crol_(d,1) ?
-    MDO = d >> 7; // send data -> SDA
-    MCO = 1;      // SCL hi
-    MCO = 0;      // SCL lo
-    d <<= 1;      // rotate data
-  }
-  MDE = 0;        // SDA input
-  MCO = 1;        // SCL hi
-  noack = MDI;      // check SDA for ACK
-  MCO = 0;        // SCL lo
-//  return(noack);
-
-  return(0); // *************************** FOR DEBUG - NO ACK CHECK !
-
-} // end of I2C_Snd_Byte
-
-
-unsigned char I2C_Snd(unsigned char Type, unsigned char Adr, unsigned int Ptr,
-                      unsigned char Length, unsigned char* Buffer)
-{
-  unsigned char i;
-
-  //I2C_Start();                  // Start I2C transaction
-  Wire.beginTransmission(0x48);
-
-  if (I2C_Snd_Byte(Adr & 0xFE)) // Send slave address, write
-  {
-    // if NACK, terminate transaction
-    I2C_Stop();
-    return(1);
-  }
-
-  if (Type > 1)                 // Check if 16-bit pointer is used
-  {
-    if (I2C_Snd_Byte(Ptr >> 8)) // Send high byte of pointer
-    {
-      // if NACK, terminate transaction
-      I2C_Stop();
-      return(1);
-    }
-  }
-
-  if (Type > 0)                 // Check if pointer is used
-  {
-    if (I2C_Snd_Byte(Ptr & 0x00FF)) // Send pointer (low byte)
-    {
-      // if NACK, terminate transaction
-      I2C_Stop();
-      return(1);
-    }
-  }
-
-  for (i=0; i<Length; i++)
-  {
-    if (I2C_Snd_Byte(*(Buffer+i)))    // Send data
-    {
-      // if NACK, terminate transaction
-      I2C_Stop();
-      return(1);
-    }
-  }
-
-  I2C_Stop();                   // Stop I2C transaction
-  return(0);                    // Confirm no error
-
-} // end of I2C_Snd
-
-unsigned char I2C_Rcv(unsigned char Type, unsigned char Adr, unsigned int Ptr,
-                      unsigned char Length, unsigned char* Buffer)
-{
-  unsigned char i;
-
-  if (!Length) return(1);       // Length MUST be > 0 !!!
-
-  // ----------------------------------------------------------------
-  // This code is executed only if the I2C pointer is used
-  // ----------------------------------------------------------------
-
-  if (Type > 0)                 // Check if pointer is used
-  {
-    I2C_Start();                  // Start I2C transaction
-
-    if (I2C_Snd_Byte(Adr & 0xFE)) // Send slave address, write
-    {
-      // if NACK, terminate transaction
-      I2C_Stop();
-      return(1);
-    }
-
-    if (Type > 1)                 // Check if 16-bit pointer is used
-    {
-      if (I2C_Snd_Byte(Ptr >> 8)) // Send high byte of pointer
-      {
-        // if NACK, terminate transaction
-        I2C_Stop();
-        return(1);
-      }
-    }
-
-    if (I2C_Snd_Byte(Ptr & 0xFF)) // Send pointer (low byte)
-    {
-      // if NACK, terminate transaction
-      I2C_Stop();
-      return(1);
-    }
-  }
-
-  // ----------------------------------------------------------------
-  // From here the code is executed regardless on the I2C pointer
-  // ----------------------------------------------------------------
-
-  I2C_Start();                  // Start I2C transaction
-
-  if (I2C_Snd_Byte(Adr | 0x01)) // Send slave address, read
-  {
-    // if NACK, terminate transaction
-    I2C_Stop();
-    return(1);
-  }
-
-  // Cycle through data except the last byte
-  Length--;
-  for (i=0; i<Length; i++)
-  {
-    *(Buffer+i) = I2C_Rcv_Byte(0);  // Receive data and do ACK
-  }
-
-  // The last byte
-  *(Buffer+i) = I2C_Rcv_Byte(1);  // Receive data and do NOT ACK
-
-  I2C_Stop();                   // Stop I2C transaction
-  return(0);                    // Confirm no error
-
-} // end of I2C_Rcv
-
-//*******************************************************************
+}
 
