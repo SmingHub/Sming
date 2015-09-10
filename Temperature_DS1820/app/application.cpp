ec46fe27e9bbe5df4166e51a8e4da7b8af078237
flexiti <mariuszb@flexiti.pl>
2015-09-10 23:45:30 +0200

    Update application.cpp

--- Temperature_DS1820/app/application.cpp
+++ Temperature_DS1820/app/application.cpp
@@ -6,24 +6,59 @@
 
 DS18S20 ReadTemp;
 Timer procTimer;
-
+//**********************************************************
+// DS18S20 example, reading
+// You can connect multiple sensors to a single port
+// (At the moment 4 pcs - it depends on the definition in the library)
+// Measuring time: 1.2 seconds * number of sensors
+// The main difference with the previous version of the demo:
+//  - Do not use the Delay function () which discourages by manufacturer of ESP8266
+//  - We can read several sensors
+// Usage:
+//  Call Init to setup pin eg. ReadTemp.Init(2);   //pin 2 selected
+//  Call ReadTemp.StartMeasure();
+//   if ReadTemp.MeasureStatus() false read sensors
+//   You can recognize sensors by the ID or index.
+//***********************************************************
 void readData()
 {
-	if (ReadTemp.IsValidTemperature())
-	{
+	uint8_t a;
+	uint64_t info;
 
-		Serial.println("**********************************");
-	    Serial.print("Demo reding temperature = ");
-		Serial.print(ReadTemp.GetCelsius());
-		Serial.print(" Celsius, (");
-		Serial.print(ReadTemp.GetFahrenheit());
-		Serial.println(" Fahrenheit)");
-		Serial.println("**********************************");
+	if (!ReadTemp.MeasureStatus())  // the last measurement completed
+	{
+      if (ReadTemp.GetSensorsCount())   // is minimum 1 sensor detected ?
+		Serial.println("******************************************");
+	    Serial.println(" Reding temperature DEMO");
+	    for(a=0;a<ReadTemp.GetSensorsCount();a++)   // prints for all sensors
+	    {
+	      Serial.print(" T");
+	      Serial.print(a+1);
+	      Serial.print(" = ");
+	      if (ReadTemp.IsValidTemperature(a))   // temperature read correctly ?
+	        {
+	    	  Serial.print(ReadTemp.GetCelsius(a));
+	    	  Serial.print(" Celsius, (");
+	    	  Serial.print(ReadTemp.GetFahrenheit(a));
+	    	  Serial.println(" Fahrenheit)");
+	        }
+	      else
+	    	  Serial.println("Temperature not valid");
+
+	      Serial.print(" <Sensor id.");
+
+	      info=ReadTemp.GetSensorID(a)>>32;
+	      Serial.print((uint32_t)info,16);
+	      Serial.print((uint32_t)ReadTemp.GetSensorID(a),16);
+	      Serial.println(">");
+	    }
+		Serial.println("******************************************");
+		ReadTemp.StartMeasure();  // next measure, result after 1.2 seconds * number of sensors
 	}
 	else
-		Serial.println("No valid Measure so far!");
+		Serial.println("No valid Measure so far! wait please");
 
-	ReadTemp.StartMeasure();  // next measure
+
 }
 
 void init()
@@ -31,9 +66,10 @@ void init()
 	Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
 	Serial.systemDebugOutput(true); // Allow debug output to serial
 
-        ReadTemp.Init(2);  // select PIN It's required for one-wire initialization!
-	ReadTemp.StartMeasure();
+    ReadTemp.Init(4);  			// select PIN It's required for one-wire initialization!
+	ReadTemp.StartMeasure(); // first measure start,result after 1.2 seconds * number of sensors
 
 	procTimer.initializeMs(10000, readData).start();   // every 10 seconds
 }
+
 
