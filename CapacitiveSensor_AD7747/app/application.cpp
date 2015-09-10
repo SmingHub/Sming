7468ec5ec918980c17e4c6a5b4b11ff145480f51
zhivko <klemen.zivkovic@gmail.com>
2015-09-10 11:42:01 +0200

    Added streaming to connected clients.

--- CapacitiveSensor_AD7747/app/application.cpp
+++ CapacitiveSensor_AD7747/app/application.cpp
@@ -1,30 +1,5 @@
 // --------------------------------------
-// i2c_scanner (Sming version)
-//
-// Version 1
-//    This program (or code that looks like it)
-//    can be found in many places.
-//    For example on the Arduino.cc forum.
-//    The original author is not know.
-// Version 2, Juni 2012, Using Arduino 1.0.1
-//     Adapted to be as simple as possible by Arduino.cc user Krodal
-// Version 3, Feb 26  2013
-//    V3 by louarnold
-// Version 4, March 3, 2013, Using Arduino 1.0.3
-//    by Arduino.cc user Krodal.
-//    Changes by louarnold removed.
-//    Scanning addresses changed from 0...127 to 1...119,
-//    according to the i2c scanner by Nick Gammon
-//    http://www.gammon.com.au/forum/?id=10896
-// Version 5, March 28, 2013
-//    As version 4, but address scans now to 127.
-//    A sensor seems to use address 120.
-// Version 6, March 15  2015
-//    Sming version by Skurydin Alexey
-//
-//
-// This sketch tests the standard 7-bit addresses
-// Devices with higher bit address might not be seen properly.
+// Capacity AD7747 sensor example based on  Scanneri2c_scanner (Sming version)
 //
 
 /*
@@ -154,9 +129,18 @@ void readPeriodically()
 		}
 		//Serial.println();
 	}
-	//Serial.print(system_get_time());
-	//Serial.println(" Loop Done");
+
 	system_soft_wdt_feed();
+	char buf[22];
+	dtostrf(pf, 10, 8, buf);
+	socket.sendString(buf);	
+	
+	WebSocketsList &clients = server.getActiveWebSockets();
+	for (int i = 0; i < clients.count(); i++)
+	{
+		clients[i].sendString(buf);
+	}	
+	
 	procTimer.initializeMs(150, readPeriodically).startOnce();
 
 }
@@ -274,7 +258,8 @@ void couldntConnect()
 
 void init()
 {
-
+	debugf("Memory info: flashID %d size: %d\n",spi_flash_get_id(),fs_size());
+	
 	System.setCpuFrequency(eCF_160MHz);
 
 	Serial.systemDebugOutput(false); // Disable debug output
