c6ecd035c853a1e9a90273103e9244ea6d018fb5
hreintke <hreintke@tauri.nl>
2015-10-19 15:59:32 +0200

    ReleaseNumber 140

--- Sming/SmingCore/SmingCore.h
+++ Sming/SmingCore/SmingCore.h
@@ -8,7 +8,7 @@
 #ifndef _NET_WIRING_
 #define _NET_WIRING_
 
-#define SMING_VERSION 120 // Major Minor Sub
+#define SMING_VERSION 140 // Major Minor Sub
 
 #include "../Wiring/WiringFrameworkIncludes.h"
 
