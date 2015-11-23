355d400a376229aab6fbe6602e5da8a830c9b359
John Doe <johndoe8967@gmx.at>
2015-11-23 21:01:55 +0100

    - configure WIFI - config only 1 Servo - linear ramp with 10s delay at center position

--- Basic_Servo/app/application.cpp
+++ Basic_Servo/app/application.cpp
@@ -7,26 +7,45 @@
 #include <SmingCore/SmingCore.h>
 #include "HardwareServo.h"
 
-#define noPins 4
+#ifndef WIFI_SSID
+	#define WIFI_SSID "PleaseEnterSSID" // Put you SSID and Password here
+	#define WIFI_PWD "PleaseEnterPass"
+#endif
+
+
+#define noPins 1
 #define SERVO_PIN 2 // GPIO2
 
 
 Timer procTimer;
-uint8 pins[noPins] = {SERVO_PIN,SERVO_PIN,SERVO_PIN,SERVO_PIN};
+uint8 pins[noPins] = {SERVO_PIN};
 
-uint32 value[noPins] = {0,0,0,0};
+uint32 value[noPins] = {0};
+
+uint16 centerdelay = 0;
+
 void calcValue() {
-	value[0] +=10;
+	// wait some time at middle of the servo range
+	if (value[0] == 800) {
+		centerdelay--;
+		if (centerdelay == 0) value[0] +=10;	// restart after waiting
+	}
+	else {										// linear ramp by increasing the value in steps
+		centerdelay=50;							// delay 50 times 200ms = 10s
+		value[0] +=10;
+	}
 
-	if (value[0] >= 1000) value[0] = 0;
 
-	for (uint8 i=0; i<noPins; i++) {
+	if (value[0] >= 1600) value[0] = 0;			// overflow and restart linear ramp
+
+	for (uint8 i=0; i<noPins; i++) {			// store all values and print to serial
 		value[i] = value[0]+i;
 		Serial.print(" Value: ");
 		Serial.print(value[i]);
 	}
 	Serial.println();
-	hardwareServo.SetValues(value);
+
+	hardwareServo.SetValues(value);				// set all values
 }
 
 TimerDelegate calcDelegate = calcValue;
@@ -37,7 +56,7 @@ void connectOk()
 {
 	Serial.println("I'm CONNECTED");
 	hardwareServo.Init(pins,noPins);
-	procTimer.initializeMs(2000,calcDelegate).start();
+	procTimer.initializeMs(200,calcDelegate).start();
 }
 
 void connectFail()
@@ -49,9 +68,11 @@ void connectFail()
 void init()
 {
 	WifiStation.enable(true);
+	WifiStation.config(WIFI_SSID, WIFI_PWD); // Put you SSID and Password here
+
 	System.setCpuFrequency(eCF_80MHz);
 
-	Serial.print("Init");
+	Serial.print("Init Basic Servo Sample");
 	Serial.println();
 	WifiStation.waitConnection(connectOk, 30, connectFail);
 }
