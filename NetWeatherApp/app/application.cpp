5ee5c925a65beed1c9232563682297bcfb08d342
osboxes.org <quanttrom@gmail.com>
2015-12-17 02:40:59 -0500

    More Configuration values added to JSON

--- NetWeatherApp/app/application.cpp
+++ NetWeatherApp/app/application.cpp
@@ -7,7 +7,6 @@
 
 // CANDIDATE FOR A FSM???
 
-#define MODE_SWITCH_DELAY 4000 // Switch every 4 seconds
 
 #include "special_chars.h"
 
@@ -30,7 +29,7 @@ void onGetWeather(HttpClient& client, bool successful);
 void heartBeatBlink();
 void WiFiConnected();
 void WiFiFail();
-void getTime();
+void waitForTimeWeather();
 void showTime();
 void secondsUpdater();
 void getWeather();
@@ -38,7 +37,8 @@ void showWeather();
 void onGotTime(NtpClient& client, time_t time);
 
 
-NtpClient ntpClient("pool.ntp.org", 60*60, onGotTime);
+// Update Time every hour
+NtpClient ntpClient("pool.ntp.org", SECS_PER_HOUR, onGotTime);
 
 
 
@@ -51,9 +51,10 @@ DateTime weather_SunSet;
 
 
 DynamicJsonBuffer jsonConfigBuffer;
+// Declare pointer in order to avoid initializing just yet
+JsonObject* cfg= NULL;
 
-
-
+void saveConfig(JsonObject& cfg_local);
 
 JsonObject& getConfig(){
 
@@ -75,28 +76,52 @@ JsonObject& getConfig(){
 	}
 
 	JsonObject& config=jsonConfigBuffer.createObject();
-		JsonObject& network=jsonConfigBuffer.createObject();
-	config["network"]=network;
-		network["ssid"] = WIFI_SSID;
-		network["password"] = WIFI_PWD;
 
-//		saveCfg();
+	JsonObject& network=jsonConfigBuffer.createObject();
+	config["network"]=network;
+	network["ssid"] = WIFI_SSID;
+	network["password"] = WIFI_PWD;
+
+	JsonObject& weather=jsonConfigBuffer.createObject();
+	config["weather"]=weather;
+	weather["city_id"]="6094817";
+	weather["url"]="http://api.openweathermap.org/data/2.5/weather";
+	weather["api_key"]="a5e4c3d55c24560c4327ca0a862897c5";
+	weather["units"]="metric";
+	weather["refresh_rate"]= 5*SECS_PER_MIN*1000;
+
+	// Eastern Standard Time - EST
+	config["time_zone"] = -5.0;
+
+	config["display_time"] = 4*1000;
+
+
+	saveConfig(config);
 
 	return config;
 }
 
 
+void saveConfig(JsonObject& cfg_local){
+	Serial.print("JSON CONFIG: ");
+	cfg_local.printTo(Serial);
+	Serial.println();
+
+
+}
+
 void init()
 {
 	spiffs_mount(); // Mount file system, in order to work with files
-	JsonObject& cfg=getConfig();
+	// Initialize local reference with our config
+	JsonObject& cfg_local=getConfig();
+	// point global pointer to our reference
+	cfg=&cfg_local;
+
 
 	Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
 	Serial.systemDebugOutput(true); // Allow debug output to serial
 
-	Serial.print("JSON CONFIG: ");
-	cfg.printTo(Serial);
-	Serial.println();
 
 
 	//Change CPU freq. to 160MHZ
@@ -109,13 +134,13 @@ void init()
 	pinMode(HEARTBEAT_LED_PIN, OUTPUT);
 	heartBeatTimer.initializeMs(1000, heartBeatBlink).start();
 
-	WifiStation.config( cfg["network"]["ssid"].toString() , cfg["network"]["password"].toString());
-	WifiStation.enable(true);
 	WifiAccessPoint.enable(false);
+	WifiStation.config( cfg_local["network"]["ssid"].toString() , cfg_local["network"]["password"].toString());
+	WifiStation.enable(true);
 
 
 	// set timezone hourly difference to UTC
-	SystemClock.setTimeZone(-5);
+	SystemClock.setTimeZone(cfg_local["time_zone"].as<double>());
 
 
 	lcd.begin(16, 2);   // initialize the lcd for 16 chars 2 lines, turn on backlight
@@ -137,9 +162,6 @@ void init()
 
   WifiStation.waitConnection(WiFiConnected, 30, WiFiFail);
 
-  // Get weather every 5 minutes
-  weatherTimer.initializeMs(5*60*1000, getWeather).start(true);
-
 
 
 }
@@ -149,10 +171,12 @@ void onGotTime(NtpClient &client, time_t time){
 	readyTime=true;
 	client.setAutoQuery(true);
 	client.setAutoUpdateSystemClock(true);
-	SystemClock.setTime(time, eTZ_UTC); // update systemclock utc value
+	SystemClock.setTime(time,eTZ_UTC);
 }
 
 void WiFiConnected(){
+	// Localize the config
+	JsonObject& cfg_local = *cfg;
 
 	lcd.clear();
 	lcd.print("SSID:");
@@ -166,8 +190,9 @@ void WiFiConnected(){
 
 
 	getWeather();
+	weatherTimer.initializeMs(cfg_local["weather"]["refresh_rate"], getWeather).start(true);
 
-	stagingTimer.initializeMs(5000, getTime).startOnce();
+	stagingTimer.initializeMs(cfg_local["display_time"].as<int>(), waitForTimeWeather).startOnce();
 
 }
 
@@ -181,7 +206,9 @@ void WiFiFail(){
 	WifiStation.waitConnection(WiFiConnected, 10, WiFiFail); // Repeat and check again
 }
 
-void getTime(){
+void waitForTimeWeather(){
+	// Localize the config
+	JsonObject& cfg_local = *cfg;
 	lcd.clear();
 	lcd.print("Updating Net");
 	lcd.setCursor(0,1);
@@ -190,19 +217,22 @@ void getTime(){
 
 	// Wait until we have ready weather and time
 	if (readyWeather && readyTime ){
-		showTime();
+		Serial.println("WeatherTime NOT READY !!!");
+		stagingTimer.initializeMs(cfg_local["display_time"].as<int>(), showTime).startOnce();
 	} else {
-		stagingTimer.initializeMs(100, getTime).startOnce();
+		stagingTimer.initializeMs(100, waitForTimeWeather).startOnce();
 	}
 }
 
 void showTime(){
+	// Localize the config
+	JsonObject& cfg_local = *cfg;
 	lcd.clear();
 	lcd.noBlink();
 	lcd.setCursor(2,0);
 	lcd.print("\4 ");
-	// check if time is good in the future
-	DateTime time_now = SystemClock.now(eTZ_Local);
+
+	DateTime time_now = SystemClock.now();
 
 	lcd.print(time_now.toShortTimeString(true));
 	// Wait 3 seconds then show the weather
@@ -215,18 +245,20 @@ void showTime(){
 	// Update the time every 1s so it looks like it's moving and repeat
 	secondsUpdaterTimer.initializeMs(1000, secondsUpdater).start(true);
 
-	stagingTimer.initializeMs(MODE_SWITCH_DELAY, showWeather).startOnce();
+	stagingTimer.initializeMs(cfg_local["display_time"].as<int>(), showWeather).startOnce();
 }
 
 void secondsUpdater(){
 
 	lcd.setCursor(4,0);
-	DateTime time_now = SystemClock.now(eTZ_Local);
+	DateTime time_now = SystemClock.now();
 	lcd.print(time_now.toShortTimeString(true));
 
 }
 
 void showWeather(){
+	// Localize the config
+	JsonObject& cfg_local = *cfg;
 	secondsUpdaterTimer.stop();
 	lcd.clear();
 	lcd.noBlink();
@@ -240,23 +272,25 @@ void showWeather(){
 	lcd.setCursor(0,1);
 	lcd.print(weather_Description);
 
-	// Wait 3 seconds then show the time
-	stagingTimer.initializeMs(MODE_SWITCH_DELAY, showTime).startOnce();
+
+	stagingTimer.initializeMs(cfg_local["display_time"].as<int>(), showTime).startOnce();
 }
 
-void onGetWeather(HttpClient& client, bool successful)
-{
-	if (successful){
-		Serial.println("Successful onGetWeather");
-	  // Get weather every 5 minutes
-	  weatherTimer.initializeMs(5*60*1000, getWeather).start();
+void onGetWeather(HttpClient& client, bool successful) {
+	// Localize the config
+	JsonObject& cfg_local = *cfg;
 
-	}
-	else{
-		Serial.println("Failed onGetWeather");
-		// Get weather every 30s
-		weatherTimer.initializeMs(30*1000, getWeather).start();
-	}
+//	if (successful){
+//		Serial.println("Successful onGetWeather");
+//	  // Get weather every 5 minutes
+//	  weatherTimer.initializeMs(5*60*1000, getWeather).start();
+//
+//	}
+//	else{
+//		Serial.println("Failed onGetWeather");
+//		// Get weather every 30s
+//		weatherTimer.initializeMs(30*1000, getWeather).start();
+//	}
 
 	String response = client.getResponseString();
 	Serial.println("Server response: '" + response + "'");
@@ -268,29 +302,29 @@ void onGetWeather(HttpClient& client, bool successful)
 
 		DynamicJsonBuffer jsonBuffer;
 		// apparently "ArduinoJson v5.0-beta, you can avoid the String copy because JsonBuffer::parseObject() can take a String as a parameter"
-		JsonObject& root = jsonBuffer.parseObject(response_char);
-		if (!root.success()) {                            // everything OK
+		JsonObject& jsonWeather = jsonBuffer.parseObject(response_char);
+		if (!jsonWeather.success()) {                            // everything OK
 		      Serial.println("JSON Parser failed !!!");
 		      return;
 		  }
 
-		if (root["main"]["temp"].is<double>()) {
+		if (jsonWeather["main"]["temp"].is<double>()) {
 			// we have a double
-			double temp_temp = root["main"]["temp"];
+			double temp_temp = jsonWeather["main"]["temp"];
 			weather_Temperature = round(temp_temp);
 		} else {
 			// most likely an integer
-			weather_Temperature = root["main"]["temp"];
+			weather_Temperature = jsonWeather["main"]["temp"];
 		}
 
 
 
-		weather_Humidity = (int) root["main"]["humidity"];
-		weather_Description = root["weather"][0]["description"];
+		weather_Humidity = (int) jsonWeather["main"]["humidity"];
+		weather_Description = jsonWeather["weather"][0]["description"];
 
-		// crude dirty TimeZone Subtract
-		weather_SunRise = (long) root["sys"]["sunrise"] - (60*60*5);
-		weather_SunSet = (long) root["sys"]["sunset"]- (60*60*5);
+		// Adjust for timezone
+		weather_SunRise = (long) jsonWeather["sys"]["sunrise"] + (SECS_PER_HOUR * cfg_local["time_zone"].as<double>());
+		weather_SunSet = (long) jsonWeather["sys"]["sunset"] + (SECS_PER_HOUR * cfg_local["time_zone"].as<double>());
 
 		Serial.println("===========");
 		Serial.print("Temperature: ");
@@ -320,8 +354,16 @@ void onGetWeather(HttpClient& client, bool successful)
 
 void getWeather(){
 
-	httpWeather.downloadString("http://api.openweathermap.org/data/2.5/weather?id=6094817&units=metric&appid=a5e4c3d55c24560c4327ca0a862897c5", onGetWeather);
-//	httpWeather.downloadString("http://www.quanttrom.com/weather.json", onGetWeather);
+
+	// Localize the config
+	JsonObject& cfg_local = *cfg;
+	// Extract only weather related config
+	JsonObject& cfg_weather = cfg_local["weather"];
+
+	String url = cfg_weather["url"].toString() + "?id=" + cfg_weather["city_id"].toString() + "&units=" + cfg_weather["units"].toString() + "&appid=" + cfg_weather["api_key"].toString();
+//	String url = "http://www.quanttrom.com/weather.json";
+
+	httpWeather.downloadString( url ,onGetWeather);
 
 	Serial.println("===========");
 	Serial.println("WIFI Status:");
