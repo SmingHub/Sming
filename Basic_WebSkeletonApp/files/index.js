6df2b54e062920d6fbd0df60926c047d4db1dbce
hreintke <hreintke@tauri.nl>
2016-01-31 17:37:47 +0100

    Remove example from PR

--- Basic_WebSkeletonApp/files/index.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-	
-	(function worker() {
-		$.getJSON('/state', function(data) {
-			document.getElementById('counter').textContent = data.counter;
-			setTimeout(worker, 5000);
-		});
-	})();
-});
