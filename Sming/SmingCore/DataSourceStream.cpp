1966de6b916751e3876e9e4f75f25252a02c5098
Reinis Veips <reinis.veips@wot.lv>
2016-03-24 23:09:02 +0200

    Fixes potential memory leak in DataSourceStream if realloc fails.

--- Sming/SmingCore/DataSourceStream.cpp
+++ Sming/SmingCore/DataSourceStream.cpp
@@ -50,10 +50,15 @@ size_t MemoryDataStream::write(const uint8_t* data, size_t len)
 		{
 			capacity = required < 256 ? required + 128 : required + 64;
 			debugf("realloc %d -> %d", size, capacity);
-			buf = (char*)realloc(buf, capacity);
+			char * new_buf;
+			//realloc can fail, store the result in temporary pointer
+			new_buf = (char*)realloc(buf, capacity);
 
-			if (buf == NULL)
+			if (new_buf == NULL)
+			{
 				return 0;
+			}
+			buf = new_buf;
 		}
 		buf[cur + len] = '\0';
 		memcpy(buf + cur, data, len);
