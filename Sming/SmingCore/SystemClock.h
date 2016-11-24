7305816dccf9bd0199737db554b8f70463999b82
riban <brian@riban.co.uk>
2016-11-24 23:57:55 +0000

    Adds / updates API documentation for: DateTime, Clock, Debug, NtpCient, RTC, SystemClock, some non-Sming (ESP) functions.

--- Sming/SmingCore/SystemClock.h
+++ Sming/SmingCore/SystemClock.h
@@ -1,4 +1,5 @@
-/** @defgroup   system_clock System clock functions
+/** @defgroup   systemclock System clock functions
+ *  @ingroup    datetime
  *  @brief      Provides system clock functions
 */
 
@@ -17,7 +18,7 @@
 /// Time zones
 enum TimeZone
 {
-	eTZ_UTC		= 0, ///< Use universal time coordinate (UTC / GMT)
+	eTZ_UTC		= 0, ///< Use universal time coordinate (UTC)
 	eTZ_Local	= 1 ///< Use local time
 };
 
@@ -31,40 +32,42 @@ enum SystemClockStatus
 
 class NtpClient;
 
+/** @brief  System clock class
+ *  @addtogroup systemclock
+ *  @{
+ */
 class SystemClockClass
 {
 public:
-    /** @addtogroup system_clock
-     *  @{
-     */
 
     /** @brief  Get the current date and time
-     *  @param  timeType Time zone to use
+     *  @param  timeType Time zone to use (UTC / local)
      *  @retval DateTime Current date and time
      */
 	DateTime now(TimeZone timeType = eTZ_Local);
 
     /** @brief  Set the system clock's time
-     *  @param  time Time to set clock to
-     *  @param  timeType Time zone to use
+     *  @param  time Unix time to set clock to (quantity of seconds since 00:00:00 1970-01-01)
+     *  @param  timeType Time zone of Unix time, i.e. is time provided as local or UTC?
+     *  @note   System time is always stored as local timezone time
      */
 	void setTime(time_t time, TimeZone timeType = eTZ_Local);
 
     /** @brief  Get current time as a string
-     *  @param  timeType Time zone to use
+     *  @param  timeType Time zone to present time as, i.e. return local or UTC time
      *  @retval String Current time in format: dd.mm.yy hh:mm:ss
-     *  @todo   Allow time format to be defined
+     *  @note   Date separator may be changed by adding #define DT_DATE_SEPARATOR "/" to source code
      */
 	String getSystemTimeString(TimeZone timeType = eTZ_Local);
 
     /** @brief  Sets the local time zone offset
-     *  @param  localTimezoneOffset Offset from UTC (GMT) of local time zone in hours
+     *  @param  localTimezoneOffset Offset from UTC of local time zone in hours (-12..+12)
      *  @retval bool True on success
      *  @todo   Why does this need to be set to 2 for UK during winter?
+     *  @note   Supports whole hour and fraction of hour offsets from -12 hours to +12 hours
      */
 	bool setTimeZone(double localTimezoneOffset);
 
-    /** @} */
 
 private:
 	double timezoneDiff = 0.0;
@@ -75,10 +78,10 @@ private:
 /**	@brief	Global instance of system clock object
  *	@note	Use SystemClock.<i>function</i> to access system clock functions
  *	@note	Example:
- *  @code   SystemClock.setTimeZone(-2);
+ *  @code   SystemClock.setTimeZone(+9.5); //Darwin, Australia
  *	@endcode
- *	@ingroup system_clock
 */
 extern SystemClockClass SystemClock;
 
+/** @} */
 #endif /* APP_SYSTEMCLOCK_H_ */
