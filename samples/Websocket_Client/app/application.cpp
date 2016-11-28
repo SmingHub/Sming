25a2e05d5ea886e3f85599b7157c0b0527905f88
Alexander V. Ribchansky <avr39ripe@gmail.com>
2016-11-28 12:11:36 +0200

    Websocket client implementation for Sming (#809)
    * Websocket client for sming based on great initial work of
    @hrsavla. Some parts of code inspired by: https://github.com/Links2004/arduinoWebSockets
    and https://github.com/m8rge/cwebsocket.
    
    There is three parts:
    WebsocketFrameClass - generic class for encode/decode websocket frames.
    WebsocketFrameClass can be used in Sming Websocket server as almost drop-ins replacement for
    cwebsocket.
    WebsocketClient - websocket client class itself, uses WebsocketFrameClass
    samples/Websocket_Client - sample app for testing text/binary websocket client
    
    Current Websocket client features:
    - text messages
    - binary messages
    - ping/pong
    Limitations/unimplementet:
    - both text and binary messages are 64K limited
    - no continuation frame/fragmentation supported

---
+++ samples/Websocket_Client/app/application.cpp
@@ -0,0 +1,159 @@
+/* Websocket Client Demo
+ * By hrsavla https://github.com/hrsavla
+ * 08/08/2015
+ * This is a simple demo of Websocket client
+ * Client tries to connect to echo.websocket.org
+ * It sents 25 messages then client connection is closed.
+ * It reconnects and sends 25 messages and continues doing same.
+ *
+ * This demo shows connecton, closing , reconnection methods of
+ * websocket client.
+ */
+#include <user_config.h>
+#include <SmingCore/SmingCore.h>
+#include <SmingCore/Network/WebsocketClient.h>
+
+#ifndef WIFI_SSID
+	#define WIFI_SSID "PutSsidHere" // Put you SSID and Password here
+	#define WIFI_PWD "PutPasswordHere"
+#endif
+
+//Uncomment next line to enable websocket binary transfer test
+//#define WS_BINARY
+
+WebsocketClient wsClient;
+Timer msgTimer;
+Timer restartTimer;
+
+int msg_cnt = 0;
+
+String ws_Url =  "ws://echo.websocket.org";
+
+void wsDisconnected(WebsocketClient& wsClient, bool success);
+void wsMessageSent();
+void wsConnected(WebsocketClient& wsClient,wsMode Mode)
+{
+	if (Mode == wsMode::Connected)
+	{
+		msgTimer.initializeMs(1 * 1000, wsMessageSent);
+		msgTimer.start();
+	}
+	else
+	{
+		Serial.println("Connection with server not successful. Reconnecting..");
+		wsClient.connect(ws_Url);
+	}
+}
+void wsMessageReceived(WebsocketClient& wsClient, String message)
+{
+    Serial.printf("WebSocket message received: %s\n", message.c_str());
+    Serial.printf("Free Heap: %d\n", system_get_free_heap_size());
+    
+}
+
+void wsBinReceived(WebsocketClient& wsClient, uint8_t* data, size_t size)
+{
+	Serial.printf("WebSocket BINARY received\n");
+	for (uint8_t i = 0; i< size; i++)
+	{
+		Serial.printf("wsBin[%u] = 0x%02X\n", i, data[i]);
+	}
+
+	Serial.printf("Free Heap: %d\n", system_get_free_heap_size());
+}
+
+void restart()
+{
+	msg_cnt = 0;
+	wsClient.connect(ws_Url);
+
+	msgTimer.setCallback(wsMessageSent);
+	msgTimer.setIntervalMs(1*1000);
+	msgTimer.start();
+}
+
+void wsDisconnected(WebsocketClient& wsClient, bool success)
+{
+	if (success == true)
+	{
+		Serial.println("Websocket Client Disconnected Normally. End of program ..");
+	}
+	else
+	{
+		Serial.println("Websocket Client Disconnected. Reconnecting ..");
+
+	}
+	 msgTimer.setCallback(restart);
+	 msgTimer.setIntervalMs(60*1000);
+	 msgTimer.startOnce();
+}
+
+
+void wsMessageSent()
+{
+   if(WifiStation.isConnected() == true)
+   { // Check if Esp8266 is connected to router
+	   	if (msg_cnt > 10)
+		{
+			Serial.println("End Websocket client session");
+			wsClient.disconnect(); // clean disconnect.
+			msgTimer.stop();
+		}
+    	else
+    	{
+#ifndef WS_BINARY
+			String message = "Hello " + String(msg_cnt++);
+			Serial.printf("Sending websocket message: %s\n", message.c_str());
+			wsClient.sendMessage(message);
+#else
+    		uint8_t buf[] = {0xF0, 0x00, 0xF0};
+			buf[1] = msg_cnt++;
+			Serial.printf("Sending websocket binary buffer\n");
+			for (uint8_t i = 0; i< 3; i++)
+			{
+				Serial.printf("wsBin[%u] = 0x%02X\n", i, buf[i]);
+			}
+
+    		wsClient.sendBinary(&buf[0], 3);
+#endif
+    	}
+
+   }
+
+}
+
+void STAGotIP(IPAddress ip, IPAddress mask, IPAddress gateway)
+{
+	Serial.printf("GOTIP - IP: %s, MASK: %s, GW: %s\n", ip.toString().c_str(),
+																mask.toString().c_str(),
+																gateway.toString().c_str());
+
+    Serial.printf("Connecting to Websocket Server %s\n", ws_Url.c_str());
+
+    wsClient.setWebSocketMessageHandler(wsMessageReceived);
+    wsClient.setWebSocketBinaryHandler(wsBinReceived);
+    wsClient.setWebSocketDisconnectedHandler(wsDisconnected);
+    wsClient.setWebSocketConnectedHandler(wsConnected);
+    wsClient.connect(ws_Url);
+}
+
+void STADisconnect(String ssid, uint8_t ssid_len, uint8_t bssid[6], uint8_t reason)
+{
+	Serial.printf("DISCONNECT - SSID: %s, REASON: %d\n", ssid.c_str(), reason);
+
+	WifiStation.disconnect();
+	WifiStation.connect();
+}
+
+void init()
+{
+    Serial.begin(115200);
+    Serial.systemDebugOutput(false);
+    WifiAccessPoint.enable(false);
+
+    WifiStation.config(WIFI_SSID, WIFI_PWD);
+    WifiStation.enable(true);
+
+    WifiEvents.onStationGotIP(&STAGotIP);
+    WifiEvents.onStationDisconnect(&STADisconnect);
+}
