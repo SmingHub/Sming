e350d9ab58a83beb9969ea0605a4fbff846479cd
Slavey Karadzhov <slav@attachix.com>
2016-12-04 22:09:45 +0100

    Fixed Echo_Ssl sample.

--- samples/Echo_Ssl/Makefile-user.mk
+++ samples/Echo_Ssl/Makefile-user.mk
@@ -36,4 +36,16 @@ MODULES = app
 DISABLE_SPIFFS = 1
 # SPIFF_FILES = files
 
+# We need rBoot in order to be able to run bigger Flash roms.
+
+#### overridable rBoot options ####
+## use rboot build mode
+RBOOT_ENABLED ?= 1
+## enable big flash support (for multiple roms, each in separate 1mb block of flash)
+RBOOT_BIG_FLASH ?= 1
+## two rom mode (where two roms sit in the same 1mb block of flash)
+#RBOOT_TWO_ROMS  ?= 1
+## size of the flash chip
+SPI_SIZE  ?= 4M
+
 ENABLE_SSL=1
