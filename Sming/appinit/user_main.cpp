f899ab421ffb37a0b2eb6f05d3a060d1a2dc9d37
Dmitry <dmitry@example.org>
2017-01-11 19:33:42 +0300

    support to sdk 2.0

--- Sming/appinit/user_main.cpp
+++ Sming/appinit/user_main.cpp
@@ -9,6 +9,40 @@ extern "C" {
 
 extern void init();
 
+extern "C" uint32 ICACHE_FLASH_ATTR
+user_rf_cal_sector_set(void)
+{
+    enum flash_size_map size_map = system_get_flash_size_map();
+    uint32 rf_cal_sec = 0;
+
+    switch (size_map) {
+        case FLASH_SIZE_4M_MAP_256_256:
+            rf_cal_sec = 128 - 5;
+            break;
+
+        case FLASH_SIZE_8M_MAP_512_512:
+            rf_cal_sec = 256 - 5;
+            break;
+
+        case FLASH_SIZE_16M_MAP_512_512:
+        case FLASH_SIZE_16M_MAP_1024_1024:
+            rf_cal_sec = 512 - 5;
+            break;
+
+        case FLASH_SIZE_32M_MAP_512_512:
+        case FLASH_SIZE_32M_MAP_1024_1024:
+            rf_cal_sec = 1024 - 5;
+            break;
+
+        default:
+            rf_cal_sec = 0;
+            break;
+    }
+
+    return rf_cal_sec;
+}
+
+
 extern "C" void  __attribute__((weak)) user_init(void)
 {
 	system_timer_reinit();
