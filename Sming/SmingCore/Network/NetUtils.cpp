959e763dc5da2965e324c74f0f8f427d1ccb39dc
Andrei Diea <diea_andrei@yahoo.com>
2017-01-26 17:46:32 +0200

    addindexOfIgnoreCase;remake PR

--- Sming/SmingCore/Network/NetUtils.cpp
+++ Sming/SmingCore/Network/NetUtils.cpp
@@ -1,188 +1,182 @@
-#include "NtpClient.h"
+/****
+ * Sming Framework Project - Open Source framework for high efficiency native ESP8266 development.
+ * Created 2015 by Skurydin Alexey
+ * http://github.com/anakod/Sming
+ * All files of the Sming Core are provided under the LGPL v3 license.
+ ****/
 
-NtpClient::NtpClient()
- : NtpClient(NTP_DEFAULT_SERVER, NTP_DEFAULT_QUERY_INTERVAL_SECONDS, nullptr)
+#include "NetUtils.h"
+
+#include "../Wiring/WString.h"
+#include "lwip/tcp_impl.h"
+
+bool NetUtils::ipClientRoutingFixed = false;
+
+/// HELPERS ///
+
+int NetUtils::pbufFindChar(pbuf *buf, char wtf, int startPos /* = 0*/)
 {
-}
+	int ofs = 0;
 
-NtpClient::NtpClient(NtpTimeResultDelegate onTimeReceivedCb)
- : NtpClient(NTP_DEFAULT_SERVER, NTP_DEFAULT_QUERY_INTERVAL_SECONDS, onTimeReceivedCb)
-{
-}
-
-NtpClient::NtpClient(String reqServer, int reqIntervalSeconds, NtpTimeResultDelegate delegateFunction /* = NULL */)
-{
-	// init timer but do not start, correct interval set later below.
-	autoUpdateTimer.initializeMs(NTP_DEFAULT_QUERY_INTERVAL_SECONDS * 1000,
-			TimerDelegate(&NtpClient::requestTime, this));
-
-	timeoutTimer.initializeMs(NTP_RESPONSE_TIMEOUT_MS,
-			TimerDelegate(&NtpClient::requestTime, this));
-
-
-	this->server = reqServer;
-	this->delegateCompleted = delegateFunction;
-	if (!delegateFunction)
+	while (buf->len <= startPos)
 	{
-		autoUpdateSystemClock = true;
+		ofs += buf->len;
+		startPos -= buf->len;
+		buf = buf->next;
+		if (buf == NULL) return -1;
 	}
 
-	if (reqIntervalSeconds == 0)
+	do
 	{
-		setAutoQuery(false);
-	}
-	else
-	{
-		setAutoQueryInterval(reqIntervalSeconds);
-		setAutoQuery(true);
-		requestTime();
-	}
-}
-
-NtpClient::~NtpClient()
-{
-}
-
-
-void NtpClient::requestTime()
-{
-	if (!WifiStation.isConnected())
-	{
-		connectionTimer.initializeMs(1000, TimerDelegate(&NtpClient::requestTime, this)).startOnce();
-		return;
-	}
-
-	struct ip_addr resolvedIp;
-	int result = dns_gethostbyname(this->server.c_str(), &resolvedIp,
-			staticDnsResponse, (void*) this);
-
-	switch (result)
-	{
-	case ERR_OK:
-		// Documentation says this will be the result if the string is already
-		// an ip address in dotted decimal form or if the host is found in dns cache.
-		// however I'm not sure if the dns cache is working since this never seems to
-		// be called for a host lookup other than an ip address.
-		// Doesn't really matter since the loockup will be fast anyways, the host
-		// is most likely found in the dns cache of the next node the query is sent to.
-		internalRequestTime(resolvedIp);
-		break;
-	case ERR_INPROGRESS:
-		// currently finding ip, internalRequestTime() will be called when its found.
-		//debugf("DNS IP lookup in progress.");
-		break;
-	default:
-		debugf("DNS lookup error occurred.");
-		break;
-	}
-}
-
-
-void NtpClient::internalRequestTime(IPAddress serverIp)
-{
-
-	// connect to current active serverIp, on NTP_PORT
-	this->connect(serverIp,NTP_PORT);
-
-	uint8_t packet[NTP_PACKET_SIZE];
-
-	// Setup the NTP request packet
-	memset(packet, 0, NTP_PACKET_SIZE);
-
-	// These are the only required values for a SNTP request. See page 14:
-	// https://tools.ietf.org/html/rfc4330
-	// However size of packet should still be 48 bytes.
-	packet[0] = (NTP_VERSION << 3 | 0x03); // LI (0 = no warning), Protocol version (4), Client mode (3)
-	packet[1] = 0;     	// Stratum, or type of clock, unspecified.
-
-
-	// Start timeout timer, if no response is recieved within NTP_RESPONSE_TIMEOUT
-	// a new request will be sent.
-	timeoutTimer.startOnce();
-
-	// Send to server, serverAddress & port is set in connect
-	NtpClient::send((char*) packet, NTP_PACKET_SIZE);
-}
-
-
-void NtpClient::setNtpServer(String server)
-{
-	this->server = server;
-}
-
-void NtpClient::setAutoQuery(bool autoQuery)
-{
-	if (autoQuery)
-		autoUpdateTimer.start();
-	else
-		autoUpdateTimer.stop();
-}
-
-void NtpClient::setAutoQueryInterval(int seconds)
-{
-	// minimum 10 seconds interval.
-	if (seconds < 10)
-		autoUpdateTimer.setIntervalMs(10000);
-	else
-		autoUpdateTimer.setIntervalMs(seconds * 1000);
-}
-
-void NtpClient::setAutoUpdateSystemClock(bool autoUpdateClock)
-{
-	autoUpdateSystemClock = autoUpdateClock;
-}
-
-void NtpClient::onReceive(pbuf *buf, IPAddress remoteIP, uint16_t remotePort)
-{
-	// stop timeout timer since we received a response.
-	if(timeoutTimer.isStarted()) {
-		timeoutTimer.stop();
-	}
-
-
-	// We do some basic check to see if it really is a ntp packet we receive.
-	// NTP version should be set to same as we used to send, NTP_VERSION
-	// NTP_VERSION 3 has time in same location so accept that too
-	// Mode should be set to NTP_MODE_SERVER
-
-	uint8_t versionMode = pbuf_get_at(buf, 0);
-	uint8_t ver = (versionMode & 0b00111000) >> 3;
-	uint8_t mode = (versionMode & 0x07);
-
-	if (mode == NTP_MODE_SERVER && (ver == NTP_VERSION || ver == (NTP_VERSION -1)))
-	{
-		//Most likely a correct NTP packet received.
-
-		uint8_t data[4];
-		pbuf_copy_partial(buf, data, 4, 40); // Copy only timestamp.
-
-		uint32_t timestamp = (data[0] << 24 | data[1] << 16 | data[2] << 8
-				| data[3]);
-
-		// Unix time starts on Jan 1 1970, subtract 70 years:
-		uint32_t epoch = timestamp - 0x83AA7E80;
-
-		if (autoUpdateSystemClock)
+		for (int i = startPos; i < buf->len; i++)
 		{
-			SystemClock.setTime(epoch, eTZ_UTC); // update systemclock utc value
+			char* sbuf = (char*)buf->payload;
+			if (sbuf[i] == wtf)
+			{
+				//debugf("%d %d", ofs, i);
+				return ofs + i;
+			}
 		}
+		ofs += buf->len;
+		buf = buf->next;
+		startPos = 0;
+	} while (buf != NULL);
 
-		if (delegateCompleted)
+	return -1;
+}
+
+bool NetUtils::pbufIsStrEqual(pbuf *buf, const char* compared, int startPos)
+{
+	int cur = startPos;
+
+	while (buf->len <= cur)
+	{
+		cur -= buf->len;
+		buf = buf->next;
+		if (buf == NULL) return false;
+	}
+
+	for (const char* cmp = compared; *cmp; cmp++)
+	{
+		if (buf == NULL) return false;
+
+		char &target = ((char*)buf->payload)[cur];
+		if (target != *cmp)
+			return false;
+
+		cur++;
+		if (cur >= buf->len)
 		{
-			this->delegateCompleted(*this, epoch);
+			cur = 0;
+			buf = buf->next;
 		}
 	}
+
+	return true;
 }
 
-void NtpClient::staticDnsResponse(const char *name, struct ip_addr *ip, void *arg)
+int NetUtils::pbufFindStr(pbuf *buf, const char* wtf, int startPos /* = 0*/)
 {
-	// DNS has been resolved
+	int cur = startPos;
+	if (startPos < 0) startPos = 0;
+	if (wtf == NULL || strlen(wtf) == -1) return -1;
 
-	NtpClient *self = (NtpClient*) arg;
-
-	if (ip != NULL)
+	while (true)
 	{
-		// We do a new request since the last one was never done.
-		self->internalRequestTime(*ip);
+		cur = pbufFindChar(buf, wtf[0], cur);
+		if (cur == -1) return -1;
+
+		if (pbufIsStrEqual(buf, wtf, cur))
+			return cur;
+		cur++;
 	}
+
+	return -1;
+}
+
+char* NetUtils::pbufAllocateStrCopy(pbuf *buf, int startPos, int length)
+{
+	char* stringPtr = new char[length + 1];
+	stringPtr[length] = '\0';
+	pbuf_copy_partial(buf, stringPtr, length, startPos);
+	return stringPtr;
+}
+
+String NetUtils::pbufStrCopy(pbuf *buf, int startPos, int length)
+{
+	char* stringPtr = new char[length + 1];
+	stringPtr[length] = '\0';
+	pbuf_copy_partial(buf, stringPtr, length, startPos);
+	String res = String(stringPtr);
+	delete[] stringPtr;
+	return res;
+}
+
+bool NetUtils::FixNetworkRouting()
+{
+//	if (ipClientRoutingFixed) return true;
+//	if (wifi_get_opmode() != STATIONAP_MODE) return true;
+//
+//	ip_info info;
+//	wifi_get_ip_info(STATION_IF, &info);
+//
+//	if (info.ip.addr == 0) return false;
+//
+//	for (netif *netif = netif_list; netif != NULL; netif = netif->next)
+//	{
+//		if (netif->ip_addr.addr == info.ip.addr)
+//		{
+//			debugf("Fixed default network interface: %d.%d.%d.%d", IP2STR(&info.ip));
+//			netif_default = netif;
+//			ipClientRoutingFixed = true;
+//			return true;
+//		}
+//	}
+//
+//	return false;
+	return true; // Should work on standard lwip
+}
+
+/////////////////////////////////
+
+// enum tcp_state, see file /include/lwip/tcp.h
+const char * const deb_tcp_state_str[] = {
+  "CLOSED",      // 0  CLOSED
+  "LISTEN",      // 1  LISTEN
+  "SYN_SENT",    // 2  SYN_SENT
+  "SYN_RCVD",    // 3  SYN_RCVD
+  "ESTABLISHED", // 4  ESTABLISHED
+  "FIN_WAIT_1",  // 5  FIN_WAIT_1
+  "FIN_WAIT_2",  // 6  FIN_WAIT_2
+  "CLOSE_WAIT",  // 7  CLOSE_WAIT
+  "CLOSING",     // 8  CLOSING
+  "LAST_ACK",    // 9  LAST_ACK
+  "TIME_WAIT"    // 10 TIME_WAIT
+};
+
+void NetUtils::debugPrintTcpList()
+{
+	struct tcp_pcb *pcb;
+	debugf("********** Lwip Active PCB states:\r\n");
+	bool prt_none = true;
+	for(pcb = tcp_active_pcbs; pcb != NULL; pcb = pcb->next) {
+		debugf("LWIP_DEBUG: Port %u | %u flg:%02x tmr:%04x %s\r\n", pcb->local_port, pcb->remote_port, pcb->flags, pcb->tmr, deb_tcp_state_str[pcb->state]);
+		prt_none = false;
+	}
+	if(prt_none) debugf("LWIP_DEBUG: none\r\n");
+	debugf("********** Lwip Listen PCB states:\r\n");
+	prt_none = true;
+	for(pcb = (struct tcp_pcb *)tcp_listen_pcbs.pcbs; pcb != NULL; pcb = pcb->next) {
+		debugf("LWIP_DEBUG: Port %u | %u flg:%02x tmr:%04x %s\r\n", pcb->local_port, pcb->remote_port, pcb->flags, pcb->tmr, deb_tcp_state_str[pcb->state]);
+		prt_none = false;
+	}
+	if(prt_none) debugf("LWIP_DEBUG: none\r\n");
+	debugf("********** Lwip TIME-WAIT PCB states:\r\n");
+	prt_none = true;
+	for(pcb = tcp_tw_pcbs; pcb != NULL; pcb = pcb->next) {
+		debugf("LWIP_DEBUG: Port %u | %u flg:%02x tmr:%04x %s\r\n", pcb->local_port, pcb->remote_port, pcb->flags, pcb->tmr, deb_tcp_state_str[pcb->state]);
+		prt_none = false;
+	}
+	if(prt_none) debugf("LWIP_DEBUG: none\r\n");
+	return;
 }
