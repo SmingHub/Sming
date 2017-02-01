c2481b9d311e9bf41f04e9307930005914319c73
ADiea <ADiea@users.noreply.github.com>
2017-02-01 18:08:10 +0200

    Add custom userdata to websocket obj (#890)
    * Add custom userdata to websocket obj
    * modify sample: use custom userData to get context from a websocket connection
    
    * fix typos;fix coding style
    
    * move CUserSettings to different files

---
+++ samples/HttpServer_WebSockets/app/CUserData.cpp
@@ -0,0 +1,61 @@
+#include "CUserData.h"
+
+//Simplified container modelling a user session
+CUserData::CUserData(const char* uName, const char* uData):userName(uName), userData(uData)
+{
+
+}
+CUserData::~CUserData()
+{
+	logOut();
+}
+
+void CUserData::addSession(WebSocket& connection)
+{
+	activeWebSockets.addElement(&connection);
+	connection.setUserData((void*)this);
+}
+
+void CUserData::removeSession(WebSocket& connection)
+{
+	for(int i=0; i < activeWebSockets.count(); i++)
+	{
+		if(connection == *(activeWebSockets[i]))
+		{
+			activeWebSockets[i]->setUserData(nullptr);
+			activeWebSockets.remove(i);
+			Serial.println("Removed user session");
+			return;
+		}
+	}
+}
+
+void CUserData::printMessage(WebSocket& connection,const String &msg)
+{
+	int i=0;
+	for(; i < activeWebSockets.count(); i++)
+	{
+		if(connection == *(activeWebSockets[i]))
+		{
+			break;
+		}
+	}
+
+	if(i < activeWebSockets.count())
+	{
+		Serial.print("Received msg on connection ");
+		Serial.print(i);
+		Serial.print(" :");
+		Serial.print(msg);
+	}
+}
+
+void CUserData::logOut()
+{
+	for(int i=0; i < activeWebSockets.count(); i++)
+	{
+		activeWebSockets[i]->setUserData(nullptr);
+	}
+
+	activeWebSockets.removeAllElements();
+}
