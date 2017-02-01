635ad09bd4ead2e7b1e8dd0394b590cc38cd945b
slaff <slaff2@gmail.com>
2017-02-01 18:01:36 +0100

    Simplified HardwareSerial callback logic. (#923)
    * Simplified the callback logic for better responsiveness.
    * Removed extra read buffer logic. Because there is already existing read buffer.
    And additional buffering, if desired, should take place in the callback(s) not here.

--- Sming/SmingCore/HardwareSerial.cpp
+++ Sming/SmingCore/HardwareSerial.cpp
@@ -179,39 +179,27 @@ void HardwareSerial::callbackHandler(uart_t *uart) {
 		return;
 	}
 
-	int receivedChar;
-	while ((receivedChar = uart_read_char(uart)) != -1) {
-		if (memberData[uart->uart_nr].useRxBuff) {
-			// TODO: add code to buffer the characters ...
-			// The buffering needs rethinking:
-			//  - one possibility is to buffer until character C. example: \n
-			//  - another possibility is to buffer until N characters are available. example: 10
-			//  - or add a bufferCallback that takes care to do whatever buffering logic is desired.
-			//  The buffering logic must be extremely "light" because it will be called on every UART interrupt.
+	uint8_t lastPos = uart->rx_buffer->wpos;
+	if(!lastPos) {
+		lastPos = uart->rx_buffer->size;
+	}
+	uint8_t receivedChar = uart->rx_buffer->buffer[lastPos-1];
+	if ((memberData[uart->uart_nr].HWSDelegate) || (memberData[uart->uart_nr].commandExecutor)) {
+		uint32 serialQueueParameter;
+		uint16 cc = uart_rx_available(uart);
+		serialQueueParameter = (cc * 256) + receivedChar; // can be done by bitlogic, avoid casting to ETSParam
+		serialQueueParameter += (uart->uart_nr << 25); // the left most byte contains the uart_nr. Up to 256 uarts are supported
+
+		if (memberData[uart->uart_nr].HWSDelegate) {
+			system_os_post(USER_TASK_PRIO_0, SERIAL_SIGNAL_DELEGATE, serialQueueParameter);
 		}
-
-		if ((memberData[uart->uart_nr].HWSDelegate) || (memberData[uart->uart_nr].commandExecutor)) {
-			uint32 serialQueueParameter;
-			uint16 cc;
-			//    	  cc = (pRxBuff->pWritePos < pRxBuff->pReadPos) ? ((pRxBuff->pWritePos + RX_BUFF_SIZE) - pRxBuff->pReadPos)
-			//      													: (pRxBuff->pWritePos - pRxBuff->pReadPos);
-			cc = (uart->rx_buffer->wpos < uart->rx_buffer->rpos) ?
-					((uart->rx_buffer->wpos + uart->rx_buffer->size) - uart->rx_buffer->rpos) :
-					(uart->rx_buffer->wpos - uart->rx_buffer->rpos);
-			serialQueueParameter = (cc * 256) + receivedChar; // can be done by bitlogic, avoid casting to ETSParam
-			serialQueueParameter += (uart->uart_nr << 25); // the left most byte contains the uart_nr. Up to 256 uarts are supported
-
-			if (memberData[uart->uart_nr].HWSDelegate) {
-				system_os_post(USER_TASK_PRIO_0, SERIAL_SIGNAL_DELEGATE, serialQueueParameter);
-			}
-			if (memberData[uart->uart_nr].commandExecutor) {
-				system_os_post(USER_TASK_PRIO_0, SERIAL_SIGNAL_COMMAND, serialQueueParameter);
-			}
+		if (memberData[uart->uart_nr].commandExecutor) {
+			system_os_post(USER_TASK_PRIO_0, SERIAL_SIGNAL_COMMAND, serialQueueParameter);
 		}
 	}
 }
 
-bool HardwareSerial::setCallback(StreamDataReceivedDelegate reqDelegate, bool useSerialRxBuffer /* = true */)
+bool HardwareSerial::setCallback(StreamDataReceivedDelegate reqDelegate)
 {
 	if (!uart || !uart_rx_enabled(uart)) {
 		return false;
@@ -220,7 +208,6 @@ bool HardwareSerial::setCallback(StreamDataReceivedDelegate reqDelegate, bool us
 	uart->callback = callbackHandler;
 
 	memberData[uartNr].HWSDelegate = reqDelegate;
-	memberData[uartNr].useRxBuff = useSerialRxBuffer;
 
 	// Start Serial task
 	if (!serialQueue) {
@@ -234,7 +221,6 @@ bool HardwareSerial::setCallback(StreamDataReceivedDelegate reqDelegate, bool us
 void HardwareSerial::resetCallback()
 {
 	memberData[uartNr].HWSDelegate = nullptr;
-	memberData[uartNr].useRxBuff = true;
 
 	uart->callback = 0;
 }
