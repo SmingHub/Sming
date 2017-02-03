36078c0be111aac487c79be56cb4517c1ada7cd9
Skurydin Alexey <anakod@gmail.com>
2017-02-03 21:15:07 +0500

    Feature/yeelight (#925)
    * + Yeelight WiFi bulbs support
    * + monitor changes on the fly
    
    * + Example
    
    * * Optimizations and improvements
    
    * * COM port baud rate by default
    
    * - TCP example fix
    
    * + YeelightBulbState enum
    + docs

---
+++ samples/LED_YeelightBulb/app/application.cpp
@@ -0,0 +1,48 @@
+#include <user_config.h>
+#include <SmingCore/SmingCore.h>
+
+// If you want, you can define WiFi settings globally in Eclipse Environment Variables
+#ifndef WIFI_SSID
+	#define WIFI_SSID "PleaseEnterSSID" // Put you SSID and Password here
+	#define WIFI_PWD "PleaseEnterPass"
+#endif
+
+
+// Enter your bulb IP here:
+YeelightBulb bulb(IPAddress("192.168.1.100"));
+
+Timer procTimer;
+bool state = false;
+
+void blink()
+{
+	state = !state;
+
+	if (state)
+	{
+		int h = random(0, 360);
+		bulb.setHSV(h, 40); // Set color: [HS]V
+	}
+	bulb.setState(state);
+}
+
+// Will be called when WiFi station was connected to AP
+void connectOk()
+{
+	debugf("I'm CONNECTED");
+
+	// Connection to Yeelight Bulb will be established on any first action:
+	bulb.updateState(); // Read actual bulb state
+	procTimer.initializeMs(5000, blink).start();
+}
+
+void init()
+{
+	Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
+
+	// Station - WiFi client
+	WifiStation.enable(true);
+	WifiStation.config(WIFI_SSID, WIFI_PWD); // Put you SSID and Password here
+
+	WifiStation.waitConnection(connectOk);
+}
