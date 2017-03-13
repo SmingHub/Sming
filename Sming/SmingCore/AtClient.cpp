6e9c5e86d841cacaad99d0abe72c448b0d2d73e9
Slavey Karadzhov <slavey.karadzhov@zend.com>
2017-03-13 12:09:14 +0100

    Initial public version of the asynch AtClient command handler. Useful for communicating with AT modems, AT webcams or other devices that use AT command set.

---
+++ Sming/SmingCore/AtClient.cpp
@@ -0,0 +1,168 @@
+/*
+ * AtClient.cpp
+ *
+ *  Created on: Feb 23, 2017
+ *      Author: slavey
+ */
+
+#include <AtClient.h>
+
+#ifndef debugf
+#define debugf(fmt, ...)
+#endif
+
+AtClient::AtClient(HardwareSerial* stream) : stream(stream) {
+	this->stream->setCallback(StreamDataReceivedDelegate(&AtClient::processor, this));
+}
+
+void AtClient::processor(Stream &source, char arrivedChar, uint16_t availableCharsCount) {
+	if(!currentCommand.text.length()) {
+		return;
+	}
+
+	if(state == eAtError) {
+		// discard input at error state
+		return;
+	}
+
+	if(currentCommand.onReceive) {
+		if(currentCommand.onReceive(*this, source)) {
+			next();
+		}
+		return;
+	}
+
+	if(arrivedChar != '\n') {
+		return;
+	}
+
+	commandTimer.stop();
+	debugf("Processing: %d ms, %s", millis(), currentCommand.text.substring(0, 20).c_str());
+
+	char response[availableCharsCount];
+	for (int i = 0; i < availableCharsCount; i++) {
+		response[i] = stream->read();
+		if (response[i] == '\r' || response[i] == '\n') {
+			response[i] = '\0';
+		}
+	}
+
+	debugf("Got response: %s", response);
+
+	String reply(response);
+	if(reply.indexOf(AT_REPLY_OK) + reply.indexOf(currentCommand.response2) == -2) {
+		// we did not get what we wanted. Check if we should repeat.
+		if(--currentCommand.retries > 0) {
+			sendDirect(currentCommand);
+			return;
+		}
+
+		if(currentCommand.breakOnError) {
+			state = eAtError;
+			return;
+		}
+	}
+
+	if(currentCommand.onComplete) {
+		if(!currentCommand.onComplete(*this, reply)) {
+			return;
+		}
+	}
+
+	next();
+}
+
+void AtClient::send(String text, String altResponse /* ="" */, uint32_t timeoutMs /* = AT_TIMEOUT */, int retries /* = 0 */) {
+	AtCommand atCommand;
+	atCommand.text = text;
+	atCommand.response2 = altResponse;
+	atCommand.timeout = timeoutMs;
+	atCommand.retries = retries;
+
+	send(atCommand);
+}
+
+void AtClient::send(String text, AtReceiveCallback onReceive, uint32_t timeoutMs /* = AT_TIMEOUT */, int retries /* = 0 */) {
+	AtCommand atCommand;
+	atCommand.text = text;
+	atCommand.onReceive = onReceive;
+	atCommand.timeout = timeoutMs;
+	atCommand.retries = retries;
+
+	send(atCommand);
+}
+
+void AtClient::send(String text, AtCompleteCallback onComplete, uint32_t timeoutMs /* = AT_TIMEOUT */, int retries /* = 0 */) {
+	AtCommand atCommand;
+	atCommand.text = text;
+	atCommand.onComplete = onComplete;
+	atCommand.timeout = timeoutMs;
+	atCommand.retries = retries;
+
+	send(atCommand);
+}
+
+// Low Level  Communication Functions
+
+void AtClient::send(AtCommand command) {
+	if(currentCommand.text.length()) {
+		queue.enqueue(command);
+		return;
+	}
+
+	sendDirect(command);
+}
+
+void AtClient::sendDirect(AtCommand command) {
+	state = eAtRunning;
+	commandTimer.stop();
+	currentCommand = command;
+	stream->print(command.text);
+	debugf("Sent: timeout: %d, current %d ms, name: %s", currentCommand.timeout, millis(), command.text.substring(0, 20).c_str());
+	commandTimer.initializeMs(currentCommand.timeout, TimerDelegate(&AtClient::ticker, this)).startOnce();
+}
+
+// Low Level Queue Functions
+void AtClient::resend() {
+	state = eAtOK;
+	if(currentCommand.text.length()) {
+		sendDirect(currentCommand);
+		return;
+	}
+
+	next();
+}
+
+void AtClient::next() {
+	if(state == eAtError) {
+		debugf("We are at error state! No next");
+		return;
+	}
+
+	state = eAtOK;
+	currentCommand.text = "";
+	if(queue.count() > 0) {
+		send(queue.dequeue());
+	}
+}
+
+void AtClient::ticker() {
+	debugf("Ticker =================> ");
+	if(!currentCommand.text.length()) {
+		commandTimer.stop();
+		debugf("Error: Timeout without command?!");
+		return;
+	}
+
+	currentCommand.retries--;
+	debugf("Retries: %d", currentCommand.retries);
+	if(currentCommand.retries > 0) {
+		commandTimer.restart();
+		sendDirect(currentCommand);
+		return;
+	}
+
+	state = eAtError;
+
+	debugf("Timeout: %d ms, %s", millis(), currentCommand.text.c_str());
+}
---
+++ Sming/SmingCore/AtClient.h
@@ -0,0 +1,155 @@
+/****
+ * Sming Framework Project - Open Source framework for high efficiency native ESP8266 development.
+ * Created 2017 by Slavey Karadzhov
+ * http://github.com/anakod/Sming
+ * All files of the Sming Core are provided under the LGPL v3 license.
+ ****/
+
+/**	@defgroup serial AtCommand serial
+ *	@brief	Asynchronous AT command client
+ *  @{
+ */
+
+#ifndef _SMING_CORE_ATCLIENT_H_
+#define _SMING_CORE_ATCLIENT_H_
+
+#include "../SmingCore/HardwareSerial.h"
+#include "../Wiring/FILO.h"
+#include "../SmingCore/Delegate.h"
+#include "../SmingCore/Timer.h"
+
+#define AT_REPLY_OK "OK"
+#ifndef AT_TIMEOUT
+#define AT_TIMEOUT 2000
+#endif
+
+class AtClient;
+
+typedef Delegate<bool(AtClient& atClient, Stream& source)> AtReceiveCallback; // << If the callback returns true then this means that we have
+																			  //     finished successfully processing the command
+typedef Delegate<bool(AtClient& atClient, String& reply)> AtCompleteCallback; // << If the callback returns true then this means that we have
+																			  //     finished successfully processing the command
+
+typedef struct {
+	String text; // << the actual AT command
+	String response2; // << alternative successful response
+	int timeout; // << timeout in milliseconds
+	int retries; // << number of retries before giving up
+	bool breakOnError = true; // << stop executing next command if that one has failed
+	AtReceiveCallback onReceive   = 0; // << if set you can process manually all incoming data in a callback
+	AtCompleteCallback onComplete = 0; // if set then you can process the complete response manually
+} AtCommand;
+
+typedef enum {
+	eAtOK = 0,
+	eAtRunning,
+	eAtError
+} AtState;
+
+template<typename T, int rawSize>
+class SimpleQueue: public FIFO<T, rawSize> {
+	virtual const T& operator[](unsigned int) const { }
+	virtual T& operator[](unsigned int) { }
+};
+
+/**
+ * @brief Class that facilitates the communication with an AT device.
+ */
+class AtClient {
+
+public:
+	AtClient(HardwareSerial* stream);
+	virtual ~AtClient() {}
+
+	/**
+	 * @brief Sends AT command
+	 * @param text String The actual AT command text. For example AT+CAMSTOP
+	 * @param altResponse String Expected response on success in addition to the default one which is OK
+	 * @param timeoutMs uint32_t Time in milliseconds to wait for response
+	 * @param retries int Retries on error
+	 */
+	void send(String text, String altResponse = "",  uint32_t timeoutMs = AT_TIMEOUT, int retries = 0);
+
+	/**
+	 * @brief Sends AT command
+	 * @param text String The actual AT command text. For example AT+CAMSTOP
+	 * @param AtReceiveCallback onReceive
+	 * @param timeoutMs uint32_t Time in milliseconds to wait for response
+	 * @param retries int Retries on error
+	 */
+	void send(String text, AtReceiveCallback onReceive, uint32_t timeoutMs = AT_TIMEOUT, int retries = 0);
+
+	/**
+	 * @brief Sends AT command
+	 * @param text String The actual AT command text. For example AT+CAMSTOP
+	 * @param AtCompleteCallback onComplete
+	 * @param timeoutMs uint32_t Time in milliseconds to wait for response
+	 * @param retries int Retries on error
+	 */
+	void send(String text, AtCompleteCallback onComplete, uint32_t timeoutMs = AT_TIMEOUT, int retries = 0);
+
+	// Low Level Functions
+
+	/**
+	 * @brief Adds a command to the queue.
+	 * 		  If you need all the flexibility then use that command
+	 * 		  and manually set your AtCommand arguments.
+	 * @param command AtCommand
+	 */
+	void send(AtCommand command);
+
+	/**
+	 * @brief Executes directly (does not queue it) a command
+	 * @param command AtCommand
+	 */
+	void sendDirect(AtCommand command);
+
+	/**
+	 * @brief Returns the current state
+	 * @return AtState
+	 */
+	AtState getState() {
+		return state;
+	}
+
+	/*
+	 * @brief Repeats the execution of the current command
+	 * 		  Useful if the current State is not eAtOK
+	 */
+	void resend();
+
+	/*
+	 * @brief Replaces the current command with the next on in the queue
+	 */
+	void next();
+
+	AtCommand currentCommand; // << The current command
+
+protected:
+	/**
+	 * @brief Processes response data.
+	*/
+	virtual void processor(Stream &source, char arrivedChar, uint16_t availableCharsCount);
+
+private:
+	SimpleQueue<AtCommand, 10> queue; // << Queue for the commands to be executed
+	HardwareSerial* stream; // << The main communication stream
+	Timer commandTimer; // timer used for commands with timeout
+	AtState state = eAtOK;
+
+	/**
+	 * @brief Timeout checker method
+	 */
+	void ticker();
+};
+
+/**
+ *  @code   AtClient camera(&Serial);
+ *  camera("ATE0\r");
+ *  camera("AT+CAMSTOP\r");
+ *
+ *  @endcode
+ */
+
+/** @} */
+#endif /* _SMING_CORE_ATCLIENT_H_ */
