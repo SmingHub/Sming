7209d64a034af12b3396f7651a2dec5916a0937a
slaff <slaff2@gmail.com>
2017-04-13 09:56:33 +0200

    Removing obsolete classes: (#1070)
    - HttpFirmwareUpdate is replaced by rBootHttpUpdate.
    
    Reasoning:
    - rBootHttpUpdate is superior as quality and functionality compared to HttpFirmwareUpdate.
    - Removing HttpFirmwareUpdate decreases the maintenance effort.
    - Having only one OTA component makes it easy for the developers.

--- Sming/SmingCore/SmingCore.h
+++ Sming/SmingCore/SmingCore.h
@@ -47,7 +47,6 @@
 #include "Network/TcpClient.h"
 #include "Network/TcpConnection.h"
 #include "Network/UdpConnection.h"
-#include "Network/HttpFirmwareUpdate.h"
 #include "Network/rBootHttpUpdate.h"
 #include "Network/URL.h"
 
