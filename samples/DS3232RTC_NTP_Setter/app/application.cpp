b2a98dbc791d348bad438c0766e42cd759b068e6
Jarek Zgoda <jarek.zgoda@gmail.com>
2017-04-18 17:21:22 +0200

    Cleaned up DS RTC example
    * cleaned up comments
    * removed unnecessary code
    * cleaned up serial printout

--- samples/DS3232RTC_NTP_Setter/app/application.cpp
+++ samples/DS3232RTC_NTP_Setter/app/application.cpp
@@ -5,9 +5,6 @@
 #include <Libraries/DS3232RTC/DS3232RTC.h>
 
 
-// Connect DS3232 / DS3231 to GND, VCC, GPIO0 - SCL, GPIO2 - SDA or any other free GPIO and change below Wire.pins(0,2)
-// to you SCL and SDA pins
-
 // If you want, you can define WiFi settings globally in Eclipse Environment Variables
 #ifndef WIFI_SSID
 	#define WIFI_SSID "ENTER_YOUR_SSID" // Put you SSID and Password here
@@ -22,9 +19,11 @@ NtpClient ntpClient ("pool.ntp.org", 30, onNtpReceive);
 
 void onPrintSystemTime() {
 	DateTime _date_time = DSRTC.get();
-	Serial.printf("Current time\nSystem(LOCAL TZ): %s\nUTC(UTC TZ): %s\nDSRTC(UTC TZ): %s\n\n", SystemClock.getSystemTimeString().c_str(),
-								SystemClock.getSystemTimeString(eTZ_UTC).c_str(),
-								_date_time.toFullDateTimeString().c_str());
+	Serial.printf("Current time\n\tSystem(LOCAL TZ): %s\n\tUTC(UTC TZ): %s\n\tDSRTC(UTC TZ): %s\n\n",
+		SystemClock.getSystemTimeString().c_str(),
+		SystemClock.getSystemTimeString(eTZ_UTC).c_str(),
+		_date_time.toFullDateTimeString().c_str()
+	);
 }
 
 void onNtpReceive(NtpClient& client, time_t timestamp) {
@@ -33,34 +32,26 @@ void onNtpReceive(NtpClient& client, time_t timestamp) {
 	Serial.printf("Time synchronized: %s\n", SystemClock.getSystemTimeString().c_str());
 }
 
-void gotIP(IPAddress ip, IPAddress netmask, IPAddress gateway)
+void systemReady()
 {
 	ntpClient.requestTime();
-}
-
-void connectFail(String ssid, uint8_t ssid_len, uint8_t bssid[6], uint8_t reason)
-{
-	debugf("I'm NOT CONNECTED!");
 }
 
 void init()
 {
 	Serial.begin(SERIAL_BAUD_RATE);
-	Serial.systemDebugOutput(true); // Allow debug print to serial
 	Serial.println("Sming DSRTC_NTP_SETTER started!");
-//Uncomment proper line, or set right pins by hand
 	Wire.pins(0, 2); //Change to your SCL - 0,SDA - 2 GPIO pin number
-//	Wire.pins(5, 4); //Change to your SCL - 5,SDA - 4 GPIO pin number
 	Wire.begin();
 
 	// Station - WiFi client
 	WifiStation.config(WIFI_SSID, WIFI_PWD); // Put you SSID and Password here
 	WifiStation.enable(true);
-	WifiEvents.onStationGotIP(gotIP);
-	WifiEvents.onStationDisconnect(connectFail);
 
 	// set timezone hourly difference to UTC
 	SystemClock.setTimeZone(2); // GMT+2
+
+	System.onReady(systemReady);
 
 	printTimer.initializeMs(2000, onPrintSystemTime).start();
 }
