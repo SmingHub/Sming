036a95b02ed922e2703aa20517473adbba7ed425
Slavey Karadzhov <slav@attachix.com>
2017-07-17 11:07:04 +0200

    Updated the Adafruit SSD1306 library. Added better Arduino compatibility.

---
+++ Sming/SmingCore/ArduinoCompat.h
@@ -0,0 +1,29 @@
+/****
+ * Sming Framework Project - Open Source framework for high efficiency native ESP8266 development.
+ * Created 2015 by Skurydin Alexey
+ * http://github.com/anakod/Sming
+ * All files of the Sming Core are provided under the LGPL v3 license.
+ *
+ * Arduino Compatibility Layer
+ *
+ * @author: 2017 - Slavey Karadzhov <slav@attachix.com>
+ *
+ ****/
+
+#ifndef SMINGCORE_ARDUINOCOMPAT_H_
+#define SMINGCORE_ARDUINOCOMPAT_H_
+
+#include "SmingCore.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void yield();
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* SMINGCORE_ARDUINOCOMPAT_H_ */
