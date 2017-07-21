b20b39c50d7c89c4ced3bf0056a385646a26abab
Slavey Karadzhov <slav@attachix.com>
2017-07-21 14:39:02 +0200

    Automated the API documentation update on new release.

---
+++ .travis/deployment.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+
+TAG=$1
+if [ -z $TAG ]; then
+	echo "Usage:\n\t$0 <tag>\n";
+	exit 1;
+fi
+
+
+# Get information about the release
+# TODO: ...
+
+# Update documentation
+cd $SMING_HOME
+make docs
+cd ..
+
+git checkout gh-pages
+
+DOCS_DIR=$SMING_HOME/../api
+
+rm -rf $DOCS_DIR
+cp -r $SMING_HOME/../docs/api/sming/ $DOCS_DIR
+git add -A $DOCS_DIR 
+git commit -m "Updated the API docs to version $TAG."
+
+git push https://${GH_TOKEN}@$github.com/${TRAVIS_REPO_SLUG}.git gh-pages
