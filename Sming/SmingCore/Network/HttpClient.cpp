4986b67eab8bbfca78e91c05058e203fce9f4f15
Slavey Karadzhov <slav@attachix.com>
2017-11-14 10:54:46 +0100

    Fixed an error breaking SSL session resumption, HttpConnection reusage and Http pipelining.

--- Sming/SmingCore/Network/HttpClient.cpp
+++ Sming/SmingCore/Network/HttpClient.cpp
@@ -162,7 +162,10 @@ void HttpClient::cleanup()
 
 HttpClient::~HttpClient()
 {
-	cleanup();
+	// DON'T call cleanup.
+	// If you want to free all resources from HttpClients the correct sequence will be to
+	// 1. Delete all instances of HttpClient
+	// 2. Call the static method HttpClient::cleanup();
 }
 
 String HttpClient::getCacheKey(URL url)
