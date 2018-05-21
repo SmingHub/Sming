82f14ffd106177a41a3386d5e514cd7ff9dd1395
Vijay Gill <vijay.gill@hotmail.com>
2018-05-21 01:57:35 +0100

    Initial attempt to add support for Nextion displays.

---
+++ samples/Nextion_Button/app/application.cpp
@@ -0,0 +1,52 @@
+using namespace std;
+#include <user_config.h>
+#include <SmingCore/SmingCore.h>
+#include <Nextion.h>
+
+#include <string>
+#include <string.h>
+
+#define GPIO_LED 2
+
+
+NexButton b0 = NexButton(0, 1, "b0");
+NexText t0 = NexText(0, 2, "g0");
+
+NexTouch *nex_listen_list[] = 
+{
+        &b0,
+        NULL
+};
+
+Timer timerNextion;
+
+void loopNextion()
+{
+    nexLoop(nex_listen_list);
+}
+
+bool ledState = true;
+
+void b0PopCallback(void *ptr)
+{
+    digitalWrite(GPIO_LED, ledState);
+    // state == false => on
+    // state == true  => off
+    if(!ledState)
+    {
+        t0.setText("ESP8266 says: LED is on");
+    }
+    else
+    {
+        t0.setText("ESP8266 says: LED is off");
+    }
+    ledState = !ledState;
+}
+
+void init()
+{
+    pinMode(GPIO_LED, OUTPUT);
+    nexInit();
+    b0.attachPop(b0PopCallback, &b0);
+    timerNextion.initializeMs(100, loopNextion).start();
+}
