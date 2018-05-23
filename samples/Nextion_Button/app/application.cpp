393178181d5a70dbb31380b73fe56993e2a25942
Vijay Gill <vijay.gill@hotmail.com>
2018-05-23 23:25:06 +0100

    Added comment to example cpp file.

--- samples/Nextion_Button/app/application.cpp
+++ samples/Nextion_Button/app/application.cpp
@@ -8,6 +8,21 @@ using namespace std;
 
 #define GPIO_LED 2
 
+// See this example in action: https://youtu.be/lHk6fqDBHyI
+// The HMI file included in this example needs to be compiled
+// with the Nextion Editor and uploaded to the Nextion display
+// using standard method.
+// On WEMOS mini D1 (where this example was tested), the
+// Nextion device is connected to RX/TX pins as required.
+// BUT it needs to be disconnected when uploading the firmware.
+// So the process is:
+// 1. Make changes to the cpp code
+// 2. Build it using "make"
+// 3. Disconnect the Nextion display if it is connected to Rx/Tx.
+// 4. Upload firmware (built in step 2) using "make flash".
+// 5. Connect Nextion display back again to Rx/Tx.
+// Note: I always unplugged the ESP8266 from USB (connecting with computer)
+//       while fiddling with the connections between ESP8266 and Nextion display.
 
 NexButton b0 = NexButton(0, 1, "b0");
 NexText t0 = NexText(0, 2, "g0");
