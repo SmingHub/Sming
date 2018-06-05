4078a536a4ad200de8108163ac88b8d49ad69df4
frankdownunder <frank.eden@gmail.com>
2018-06-05 20:38:49 +1000

    MqttClient: allows changing server and port setting in runtime  (#1390)

--- Sming/SmingCore/Network/MqttClient.cpp
+++ Sming/SmingCore/Network/MqttClient.cpp
@@ -9,11 +9,23 @@
 
 #include "../SmingCore.h"
 
-MqttClient::MqttClient(String serverHost, int serverPort, MqttStringSubscriptionCallback callback /* = NULL*/)
-	: TcpClient((bool)false)
+MqttClient::MqttClient(bool autoDestruct/* = false*/)
+	: TcpClient(autoDestruct)	
 {
-	server = serverHost;
-	port = serverPort;
+	memset(buffer, 0, MQTT_MAX_BUFFER_SIZE + 1);
+	waitingSize = 0;
+	posHeader = 0;
+	current = NULL;
+	mqtt_init(&broker);
+}
+
+
+// Deprecated . . .
+MqttClient::MqttClient(String serverHost, int serverPort, MqttStringSubscriptionCallback callback /* = NULL*/)
+	: TcpClient(false)
+{
+	url.Host = serverHost;
+	url.Port = serverPort;
 	this->callback = callback;
 	waitingSize = 0;
 	posHeader = 0;
@@ -21,11 +33,12 @@ MqttClient::MqttClient(String serverHost, int serverPort, MqttStringSubscription
 	mqtt_init(&broker);
 }
 
+// Deprecated . . .
 MqttClient::MqttClient(IPAddress serverIp, int serverPort, MqttStringSubscriptionCallback callback /* = NULL*/)
-	: TcpClient((bool)false)
+	: TcpClient(false)
 {
-	this->serverIp = serverIp;
-	port = serverPort;
+	url.Host = serverIp.toString();
+	url.Port = serverPort;
 	this->callback = callback;
 	waitingSize = 0;
 	posHeader = 0;
@@ -37,6 +50,10 @@ MqttClient::~MqttClient()
 {
 	mqtt_free(&broker);
 }
+void MqttClient::setCallback(MqttStringSubscriptionCallback callback)
+{
+	this->callback = callback;
+}
 
 void MqttClient::setKeepAlive(int seconds)
 {
@@ -45,16 +62,35 @@ void MqttClient::setKeepAlive(int seconds)
 
 void MqttClient::setPingRepeatTime(int seconds)
 {
-	if (PingRepeatTime > keepAlive)
-	   PingRepeatTime = keepAlive;
-	else
-	   PingRepeatTime = seconds;
+	if (PingRepeatTime > keepAlive)	{
+		PingRepeatTime = keepAlive;
+	}
+	else {
+		PingRepeatTime = seconds;
+	}
 }
 
 bool MqttClient::setWill(const String& topic, const String& message, int QoS, bool retained /* = false*/)
 {
 	return mqtt_set_will(&broker, topic.c_str(), message.c_str(), QoS, retained);
 }
+
+bool MqttClient::connect(const URL& url, const String& clientName, uint32_t sslOptions)
+{
+	this->url = url;
+	waitingSize = 0;
+	posHeader = 0;
+	current = NULL;
+	
+	bool useSsl = (url.Protocol == "mqtts");
+	return privateConnect(
+		clientName,
+		url.User, 
+		url.Password, 
+		useSsl, 
+		sslOptions);
+}
+
 
 bool MqttClient::connect(const String& clientName, boolean useSsl /* = false */, uint32_t sslOptions /* = 0 */)
 {
@@ -63,26 +99,26 @@ bool MqttClient::connect(const String& clientName, boolean useSsl /* = false */,
 
 bool MqttClient::connect(const String& clientName, const String& username, const String& password, boolean useSsl /* = false */, uint32_t sslOptions /* = 0 */)
 {
-	if (getConnectionState() != eTCS_Ready)
-	{
+	return privateConnect(clientName, username, password, useSsl, sslOptions);
+}
+bool MqttClient::privateConnect(const String& clientName, const String& username, const String& password, boolean useSsl /* = false */, uint32_t sslOptions /* = 0 */)
+{
+	if (getConnectionState() != eTCS_Ready)	{
 		close();
 		debug_d("MQTT closed previous connection");
 	}
 
 	debug_d("MQTT start connection");
-	if (clientName.length() > 0)
-		 mqtt_set_clientid(&broker, clientName.c_str());
+	if (clientName.length() > 0) {
+		mqtt_set_clientid(&broker, clientName.c_str());
+	}
 
-	if (username.length() > 0)
+	if (username.length() > 0) {
 		mqtt_init_auth(&broker, username.c_str(), password.c_str());
-
-	if(server.length() > 0 ) {
-		TcpClient::connect(server, port, useSsl, sslOptions);
-	}
-	else {
-		TcpClient::connect(serverIp, port, useSsl, sslOptions);
 	}
 
+	TcpClient::connect(url.Host, url.Port, useSsl, sslOptions);
+	
 	mqtt_set_alive(&broker, keepAlive);
 	broker.socket_info = (void*)this;
 	broker.send = staticSendPacket;
@@ -102,10 +138,10 @@ bool MqttClient::publishWithQoS(String topic, String message, int QoS, bool reta
 {
 	uint16_t msgId = 0;
 	int res = mqtt_publish_with_qos(&broker, topic.c_str(), message.c_str(), message.length(), retained, QoS, &msgId);
-	if(QoS == 0 && onDelivery) {
+	if (QoS == 0 && onDelivery) {
 		debug_d("The delivery callback is ignored for QoS 0.");
 	}
-	else if(QoS >0 && onDelivery && msgId) {
+	else if (QoS >0 && onDelivery && msgId) {
 		onDeliveryQueue[msgId] = onDelivery;
 	}
 	return res > 0;
@@ -172,15 +208,13 @@ void MqttClient::debugPrintResponseType(int type, int len)
 
 err_t MqttClient::onReceive(pbuf *buf)
 {
-	if (buf == NULL)
-	{
+	if (buf == NULL) {
 		// Disconnected, close it
 		TcpClient::onReceive(buf);
 	}
 	else
 	{
-		if (buf->len < 1)
-		{
+		if (buf->len < 1) {
 			// Bad packet?
 			debug_e("> MQTT WRONG PACKET? (len: %d)", buf->len);
 			close();
@@ -191,12 +225,10 @@ err_t MqttClient::onReceive(pbuf *buf)
 		while (received < buf->tot_len)
 		{
 			int type = 0;
-			if (waitingSize == 0)
-			{
+			if (waitingSize == 0) {
 				// It's begining of new packet
 				int pos = received;
-				if (posHeader == 0)
-				{
+				if (posHeader == 0) {
 					//debug_d("start posHeader");
 					pbuf_copy_partial(buf, &buffer[posHeader], 1, pos);
 					pos++;
@@ -206,15 +238,16 @@ err_t MqttClient::onReceive(pbuf *buf)
 				{
 					//debug_d("add posHeader");
 					pbuf_copy_partial(buf, &buffer[posHeader], 1, pos);
-					if ((buffer[posHeader] & 128) == 0)
+					if ((buffer[posHeader] & 128) == 0) {
 						posHeader = 0; // Remaining Length ended
-					else
+					}
+					else {
 						posHeader++;
+					}
 					pos++;
 				}
 
-				if (posHeader == 0)
-				{
+				if (posHeader == 0) {
 					//debug_d("start len calc");
 					// Remaining Length field processed
 					uint16_t rem_len = mqtt_parse_rem_len(buffer);
@@ -227,8 +260,7 @@ err_t MqttClient::onReceive(pbuf *buf)
 					debugPrintResponseType(type, waitingSize);
 
 					// Prevent overflow
-					if (waitingSize < MQTT_MAX_BUFFER_SIZE)
-					{
+					if (waitingSize < MQTT_MAX_BUFFER_SIZE) {
 						current = buffer;
 						buffer[waitingSize] = 0;
 					}
@@ -241,29 +273,26 @@ err_t MqttClient::onReceive(pbuf *buf)
 
 			int available = std::min(waitingSize, buf->tot_len - received);
 			waitingSize -= available;
-			if (current != NULL)
-			{
+			if (current != NULL) {
 				pbuf_copy_partial(buf, current, available, received);
 				current += available;
 
-				if (waitingSize == 0)
-				{
+				if (waitingSize == 0) {
 					// Full packet received
-					if(type == MQTT_MSG_PUBLISH)
-					{
+					if (type == MQTT_MSG_PUBLISH) {
 						const uint8_t *ptrTopic, *ptrMsg;
 						uint16_t lenTopic, lenMsg;
 						lenTopic = mqtt_parse_pub_topic_ptr(buffer, &ptrTopic);
 						lenMsg = mqtt_parse_pub_msg_ptr(buffer, &ptrMsg);
 						// Additional check for wrong packet/parsing error
-						if (lenTopic + lenMsg < MQTT_MAX_BUFFER_SIZE)
-						{
+						if (lenTopic + lenMsg < MQTT_MAX_BUFFER_SIZE) {
 							debug_d("%d: %d\n", lenTopic, lenMsg);
 							String topic, msg;
 							topic.setString((char*)ptrTopic, lenTopic);
 							msg.setString((char*)ptrMsg, lenMsg);
-							if (callback)
+							if (callback) {
 								callback(topic, msg);
+							}
 						}
 						else
 						{
@@ -274,7 +303,7 @@ err_t MqttClient::onReceive(pbuf *buf)
 						// message with QoS 1 or 2 was received and this is the confirmation
 						const uint16_t msgId = mqtt_parse_msg_id(buffer);
 						debug_d("message with id: %d was delivered", msgId);
-						if(onDeliveryQueue.contains(msgId)) {
+						if (onDeliveryQueue.contains(msgId)) {
 							// there is a callback for this message
 							onDeliveryQueue[msgId](msgId, type);
 							onDeliveryQueue.remove(msgId);
@@ -283,7 +312,9 @@ err_t MqttClient::onReceive(pbuf *buf)
 				}
 			}
 			else
+			{
 				debug_d("SKIP: %d (%d)", available, waitingSize + available); // Too large!
+			}
 			received += available;
 		}
 
@@ -298,8 +329,7 @@ void MqttClient::onReadyToSendData(TcpConnectionEvent sourceEvent)
 {
 	// Send PINGREQ every PingRepeatTime time, if there is no outgoing traffic
 	// PingRepeatTime should be <= keepAlive
-	if (lastMessage && (millis() - lastMessage >= PingRepeatTime*1000))
-	{
+	if (lastMessage && (millis() - lastMessage >= PingRepeatTime*1000)) {
 		mqtt_ping(&broker);
 	}
 	TcpClient::onReadyToSendData(sourceEvent);
