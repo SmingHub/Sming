9778dbcfe7e5571535f298e024eaa57d9ee9b4dc
slaff <slaff2@gmail.com>
2018-06-20 09:58:54 +0200

    Update choco packages on new release. (#1406)

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -23,7 +23,21 @@ DOCS_DIR=$SMING_HOME/../api
 
 rm -rf $DOCS_DIR
 cp -r $SMING_HOME/../docs/api/sming/ $DOCS_DIR
-git add -A $DOCS_DIR 
+git add -A $DOCS_DIR
 git commit -m "Updated the API docs to version $TAG." || 1
 
 git push https://${SMING_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+
+# Update the Choco packages
+cd /tmp
+git clone https://github.com/slaff/chocolatey-packages.git
+cd chocolatey-packages
+FILES_TO_CHANGE="packages/sming/sming.nuspec packages/sming.upgrade/sming.upgrade.nuspec packages/sming.examples/sming.examples.nuspec packages/sming.core/sming.core.nuspec"
+
+for FILE in $FILES_TO_CHANGE;
+do
+     xmlstarlet ed --inplace -N "ns=http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd" -u "/ns:package/ns:metadata/ns:version"  -v "$TAG" $FILE;
+done
+
+git commit -m "Updated chocolatey packages to latest stable $TAG version." || 1
+git push https://${SMING_TOKEN}@github.com/slaff/chocolatey-packages.git master
