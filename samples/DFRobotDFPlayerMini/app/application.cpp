1814b6d16ec11ad4c3ca8f32b333839152091b5d
Vijay Gill <vijay.gill@hotmail.com>
2018-06-20 19:14:56 +0100

    Fixed build of example.

--- samples/DFRobotDFPlayerMini/app/application.cpp
+++ samples/DFRobotDFPlayerMini/app/application.cpp
@@ -1,15 +1,17 @@
 using namespace std;
 #include <user_config.h>
 #include <SmingCore/SmingCore.h>
-#include <Sming/Libraries/DFRobotDFPlayerMini/DFRobotDFPlayerMini.h>
+#include <Libraries/DFRobotDFPlayerMini/DFRobotDFPlayerMini.h>
 
 #include <string>
 #include <string.h>
 
+DFRobotDFPlayerMini player;
+
 void init()
 {
     Serial.begin(9600);
-    myDFPlayer.begin(Serial);
-    myDFPlayer.volume(10);
-    myDFPlayer.play(1);
+    player.begin(Serial);
+    player.volume(10);
+    player.play(1);
 }
