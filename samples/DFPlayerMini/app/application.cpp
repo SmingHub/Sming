8b5b53bb1d9666cc55b26a2c9c53e449045738ef
Vijay Gill <vijay.gill@hotmail.com>
2018-06-28 08:49:06 +0100

    Added initial support for DFPlayerMini (#1408)

---
+++ samples/DFPlayerMini/app/application.cpp
@@ -0,0 +1,42 @@
+#include <user_config.h>
+#include <SmingCore/SmingCore.h>
+#include <Libraries/DFRobotDFPlayerMini/DFRobotDFPlayerMini.h>
+
+#define GPIO_LED 2
+
+Timer timerDFPlayer;
+DFRobotDFPlayerMini player;
+void nextSong()
+{
+    player.next();
+}
+
+
+Timer timerLedBlink;
+bool ledState = true;
+void blink()
+{
+    digitalWrite(GPIO_LED, ledState);
+    ledState = !ledState;
+}
+
+void init()
+{
+    Serial.begin(9600);
+    
+    pinMode(GPIO_LED, OUTPUT);
+    timerLedBlink.initializeMs(100, blink).start();
+
+    while(!player.begin(Serial))
+    {
+        delay(500);
+    }
+
+    timerLedBlink.stop();
+    digitalWrite(GPIO_LED, 0);
+
+    player.volume(15);
+
+    timerDFPlayer.initializeMs(10000, nextSong).start();
+
+}
