227a533d12ee1d791fc513a419abe098a43bd691
mikee47 <mike@sillyhouse.net>
2018-10-24 16:31:13 +0100

    Add parentheses as suggested by compiler warnings

--- Sming/Services/WebHelpers/aw-sha1.cpp
+++ Sming/Services/WebHelpers/aw-sha1.cpp
@@ -46,22 +46,22 @@ static inline void sha1mix(unsigned *_sha1_restrict r, unsigned *_sha1_restrict 
 		mix(d ^ (b & (c ^ d)), 0x5a827999);
 
 	for (; i < 20; ++i) {
-		w[i & 0xf] = rol(w[i + 13 & 0xf] ^ w[i + 8 & 0xf] ^ w[i + 2 & 0xf] ^ w[i & 0xf], 1);
+		w[i & 0xf] = rol(w[(i + 13) & 0xf] ^ w[(i + 8) & 0xf] ^ w[(i + 2) & 0xf] ^ w[i & 0xf], 1);
 		mix(d ^ (b & (c ^ d)), 0x5a827999);
 	}
 
 	for (; i < 40; ++i) {
-		w[i & 0xf] = rol(w[i + 13 & 0xf] ^ w[i + 8 & 0xf] ^ w[i + 2 & 0xf] ^ w[i & 0xf], 1);
+		w[i & 0xf] = rol(w[(i + 13) & 0xf] ^ w[(i + 8) & 0xf] ^ w[(i + 2) & 0xf] ^ w[i & 0xf], 1);
 		mix(b ^ c ^ d, 0x6ed9eba1);
 	}
 
 	for (; i < 60; ++i) {
-		w[i & 0xf] = rol(w[i + 13 & 0xf] ^ w[i + 8 & 0xf] ^ w[i + 2 & 0xf] ^ w[i & 0xf], 1);
+		w[i & 0xf] = rol(w[(i + 13) & 0xf] ^ w[(i + 8) & 0xf] ^ w[(i + 2) & 0xf] ^ w[i & 0xf], 1);
 		mix((b & c) | (d & (b | c)), 0x8f1bbcdc);
 	}
 
 	for (; i < 80; ++i) {
-		w[i & 0xf] = rol(w[i + 13 & 0xf] ^ w[i + 8 & 0xf] ^ w[i + 2 & 0xf] ^ w[i & 0xf], 1);
+		w[i & 0xf] = rol(w[(i + 13) & 0xf] ^ w[(i + 8) & 0xf] ^ w[(i + 2) & 0xf] ^ w[i & 0xf], 1);
 		mix(b ^ c ^ d, 0xca62c1d6);
 	}
 
@@ -75,7 +75,7 @@ static inline void sha1mix(unsigned *_sha1_restrict r, unsigned *_sha1_restrict 
 	r[4] += e;
 }
 
-static void sha1(unsigned char h[SHA1_SIZE], const void *_sha1_restrict p, size_t n) {
+void sha1(unsigned char h[SHA1_SIZE], const void *_sha1_restrict p, size_t n) {
 	size_t i = 0;
 	unsigned w[16], r[5] = {0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476, 0xc3d2e1f0};
 
@@ -92,9 +92,9 @@ static void sha1(unsigned char h[SHA1_SIZE], const void *_sha1_restrict p, size_
 	memset(w, 0, sizeof w);
 
 	for (; i < n; ++i)
-		w[i >> 2 & 0xf] |= ((const unsigned char *) p)[i] << ((3 ^ i & 3) << 3);
+		w[i >> 2 & 0xf] |= ((const unsigned char *) p)[i] << (((3 ^ i) & 3) << 3);
 
-	w[i >> 2 & 0xf] |= 0x80 << ((3 ^ i & 3) << 3);
+	w[i >> 2 & 0xf] |= 0x80 << (((3 ^ i) & 3) << 3);
 
 	if ((n & 0x3f) > 56) {
 		sha1mix(r, w);
