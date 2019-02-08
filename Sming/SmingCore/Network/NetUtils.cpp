SmingHub/Sming

commit 611a9fffd099fa73906ea91544f2d2e60f16b6bf

General tidy of Netutils module (#1599)
* General tidy of Netutils module

Use `const` qualifier where appropriate
Use `unsigned` instead of `int` where appropriate
Replace commented-out code with conditional compilation
Refactor `debugPrintTcpList`

* debugPrintTcpList() tweaks following testing

Remove double carriage returns, add PCB type as parameter to `debugPrintTcp()`

Sming/SmingCore/Network/NetUtils.cpp
@@ -6,32 +6,36 @@
  ****/
 
 #include "NetUtils.h"
+#include "CStringArray.h"
 
-#include "../Wiring/WString.h"
+#include "WString.h"
 #ifdef __linux__
 #include "lwip/priv/tcp_priv.h"
 #else
 #include "lwip/tcp_impl.h"
 #endif
 
+#ifdef FIX_NETWORK_ROUTING
 bool NetUtils::ipClientRoutingFixed = false;
+#endif
 
 /// HELPERS ///
 
-int NetUtils::pbufFindChar(pbuf* buf, char wtf, int startPos /* = 0*/)
+int NetUtils::pbufFindChar(const pbuf* buf, char wtf, unsigned startPos)
 {
-	int ofs = 0;
+	unsigned ofs = 0;
 
 	while(buf->len <= startPos) {
 		ofs += buf->len;
 		startPos -= buf->len;
 		buf = buf->next;
-		if(buf == NULL)
+		if(buf == nullptr) {
 			return -1;
+		}
 	}
 
 	do {
-		for(int i = startPos; i < buf->len; i++) {
+		for(unsigned i = startPos; i < buf->len; i++) {
 			char* sbuf = (char*)buf->payload;
 			if(sbuf[i] == wtf) {
 				//debug_d("%d %d", ofs, i);
@@ -41,24 +45,25 @@ int NetUtils::pbufFindChar(pbuf* buf, char wtf, int startPos /* = 0*/)
 		ofs += buf->len;
 		buf = buf->next;
 		startPos = 0;
-	} while(buf != NULL);
+	} while(buf != nullptr);
 
 	return -1;
 }
 
-bool NetUtils::pbufIsStrEqual(pbuf* buf, const char* compared, int startPos)
+bool NetUtils::pbufIsStrEqual(const pbuf* buf, const char* compared, unsigned startPos)
 {
-	int cur = startPos;
+	unsigned cur = startPos;
 
 	while(buf->len <= cur) {
 		cur -= buf->len;
 		buf = buf->next;
-		if(buf == NULL)
+		if(buf == nullptr) {
 			return false;
+		}
 	}
 
 	for(const char* cmp = compared; *cmp; cmp++) {
-		if(buf == NULL)
+		if(buf == nullptr)
 			return false;
 
 		char& target = ((char*)buf->payload)[cur];
@@ -75,116 +80,121 @@ bool NetUtils::pbufIsStrEqual(pbuf* buf, const char* compared, int startPos)
 	return true;
 }
 
-int NetUtils::pbufFindStr(pbuf* buf, const char* wtf, int startPos /* = 0*/)
+int NetUtils::pbufFindStr(const pbuf* buf, const char* wtf, unsigned startPos)
 {
-	int cur = startPos;
-	if(startPos < 0)
-		startPos = 0;
-	if(wtf == NULL || strlen(wtf) == -1)
+	if(wtf == nullptr) {
 		return -1;
+	}
 
+	unsigned cur = startPos;
+	if(startPos < 0) {
+		startPos = 0;
+	}
 	while(true) {
 		cur = pbufFindChar(buf, wtf[0], cur);
-		if(cur == -1)
+		if(cur < 0) {
 			return -1;
+		}
 
-		if(pbufIsStrEqual(buf, wtf, cur))
+		if(pbufIsStrEqual(buf, wtf, cur)) {
 			return cur;
+		}
 		cur++;
 	}
 
 	return -1;
 }
 
-char* NetUtils::pbufAllocateStrCopy(pbuf* buf, int startPos, int length)
+char* NetUtils::pbufAllocateStrCopy(const pbuf* buf, unsigned startPos, unsigned length)
 {
 	char* stringPtr = new char[length + 1];
+	if(stringPtr == nullptr) {
+		return nullptr;
+	}
 	stringPtr[length] = '\0';
-	pbuf_copy_partial(buf, stringPtr, length, startPos);
+	pbuf_copy_partial(const_cast<pbuf*>(buf), stringPtr, length, startPos);
 	return stringPtr;
 }
 
-String NetUtils::pbufStrCopy(pbuf* buf, int startPos, int length)
+String NetUtils::pbufStrCopy(const pbuf* buf, unsigned startPos, unsigned length)
 {
 	char* stringPtr = new char[length + 1];
+	if(stringPtr == nullptr) {
+		return nullptr;
+	}
 	stringPtr[length] = '\0';
-	pbuf_copy_partial(buf, stringPtr, length, startPos);
+	pbuf_copy_partial(const_cast<pbuf*>(buf), stringPtr, length, startPos);
 	String res = String(stringPtr);
 	delete[] stringPtr;
 	return res;
 }
 
+#ifdef FIX_NETWORK_ROUTING
 bool NetUtils::FixNetworkRouting()
 {
-	//	if (ipClientRoutingFixed) return true;
-	//	if (wifi_get_opmode() != STATIONAP_MODE) return true;
-	//
-	//	ip_info info;
-	//	wifi_get_ip_info(STATION_IF, &info);
-	//
-	//	if (info.ip.addr == 0) return false;
-	//
-	//	for (netif *netif = netif_list; netif != NULL; netif = netif->next)
-	//	{
-	//		if (netif->ip_addr.addr == info.ip.addr)
-	//		{
-	//			debug_d("Fixed default network interface: %d.%d.%d.%d", IP2STR(&info.ip));
-	//			netif_default = netif;
-	//			ipClientRoutingFixed = true;
-	//			return true;
-	//		}
-	//	}
-	//
-	//	return false;
-	return true; // Should work on standard lwip
+	if(ipClientRoutingFixed) {
+		return true;
+	}
+
+	if(wifi_get_opmode() != STATIONAP_MODE) {
+		return true;
+	}
+
+	ip_info info;
+	wifi_get_ip_info(STATION_IF, &info);
+
+	if(info.ip.addr == 0) {
+		return false;
+	}
+
+	for(netif* netif = netif_list; netif != nullptr; netif = netif->next) {
+		if(netif->ip_addr.addr == info.ip.addr) {
+			debug_d("Fixed default network interface: %d.%d.%d.%d", IP2STR(&info.ip));
+			netif_default = netif;
+			ipClientRoutingFixed = true;
+			return true;
+		}
+	}
+
+	return false;
 }
+#endif
 
 /////////////////////////////////
 
 // enum tcp_state, see file /include/lwip/tcp.h
-const char* const deb_tcp_state_str[] = {
-	"CLOSED",	  // 0  CLOSED
-	"LISTEN",	  // 1  LISTEN
-	"SYN_SENT",	// 2  SYN_SENT
-	"SYN_RCVD",	// 3  SYN_RCVD
-	"ESTABLISHED", // 4  ESTABLISHED
-	"FIN_WAIT_1",  // 5  FIN_WAIT_1
-	"FIN_WAIT_2",  // 6  FIN_WAIT_2
-	"CLOSE_WAIT",  // 7  CLOSE_WAIT
-	"CLOSING",	 // 8  CLOSING
-	"LAST_ACK",	// 9  LAST_ACK
-	"TIME_WAIT"	// 10 TIME_WAIT
-};
+static DEFINE_FSTR(deb_tcp_state_str, "CLOSED\0"	  // 0  CLOSED
+									  "LISTEN\0"	  // 1  LISTEN
+									  "SYN_SENT\0"	// 2  SYN_SENT
+									  "SYN_RCVD\0"	// 3  SYN_RCVD
+									  "ESTABLISHED\0" // 4  ESTABLISHED
+									  "FIN_WAIT_1\0"  // 5  FIN_WAIT_1
+									  "FIN_WAIT_2\0"  // 6  FIN_WAIT_2
+									  "CLOSE_WAIT\0"  // 7  CLOSE_WAIT
+									  "CLOSING\0"	 // 8  CLOSING
+									  "LAST_ACK\0"	// 9  LAST_ACK
+									  "TIME_WAIT");   // 10 TIME_WAIT
+
+static void debugPrintTcp(const char* type, const struct tcp_pcb* pcb)
+{
+	debugf("********** Lwip %s PCB states:", type);
+	CStringArray stateStr(deb_tcp_state_str);
+	unsigned active = 0;
+	while(pcb != nullptr) {
+		debugf("LWIP_DEBUG: Port %u | %u flg:%02x tmr:%04x %s", pcb->local_port, pcb->remote_port, pcb->flags, pcb->tmr,
+			   stateStr[pcb->state]);
+		active++;
+		pcb = pcb->next;
+	}
+
+	if(active == 0) {
+		debugf("LWIP_DEBUG: none");
+	}
+}
 
 void NetUtils::debugPrintTcpList()
 {
-	struct tcp_pcb* pcb;
-	debugf("********** Lwip Active PCB states:\r\n");
-	bool prt_none = true;
-	for(pcb = tcp_active_pcbs; pcb != NULL; pcb = pcb->next) {
-		debugf("LWIP_DEBUG: Port %u | %u flg:%02x tmr:%04x %s\r\n", pcb->local_port, pcb->remote_port, pcb->flags,
-			   pcb->tmr, deb_tcp_state_str[pcb->state]);
-		prt_none = false;
-	}
-	if(prt_none)
-		debugf("LWIP_DEBUG: none\r\n");
-	debugf("********** Lwip Listen PCB states:\r\n");
-	prt_none = true;
-	for(pcb = (struct tcp_pcb*)tcp_listen_pcbs.pcbs; pcb != NULL; pcb = pcb->next) {
-		debugf("LWIP_DEBUG: Port %u | %u flg:%02x tmr:%04x %s\r\n", pcb->local_port, pcb->remote_port, pcb->flags,
-			   pcb->tmr, deb_tcp_state_str[pcb->state]);
-		prt_none = false;
-	}
-	if(prt_none)
-		debugf("LWIP_DEBUG: none\r\n");
-	debugf("********** Lwip TIME-WAIT PCB states:\r\n");
-	prt_none = true;
-	for(pcb = tcp_tw_pcbs; pcb != NULL; pcb = pcb->next) {
-		debugf("LWIP_DEBUG: Port %u | %u flg:%02x tmr:%04x %s\r\n", pcb->local_port, pcb->remote_port, pcb->flags,
-			   pcb->tmr, deb_tcp_state_str[pcb->state]);
-		prt_none = false;
-	}
-	if(prt_none)
-		debugf("LWIP_DEBUG: none\r\n");
-	return;
+	debugPrintTcp(_F("Active"), tcp_active_pcbs);
+	debugPrintTcp(_F("Listen"), tcp_listen_pcbs.pcbs);
+	debugPrintTcp(_F("TIME-WAIT"), tcp_tw_pcbs);
 }

Sming/SmingCore/Network/DNSServer.cpp
@@ -13,7 +13,6 @@
  */
 #include "DNSServer.h"
 #include "UdpConnection.h"
-#include "NetUtils.h"
 #include "WString.h"
 
 bool DNSServer::start(uint16_t port, const String& domainName, const IPAddress& resolvedIP)

Sming/SmingCore/Network/NetUtils.h
@@ -19,19 +19,28 @@ class NetUtils
 {
 public:
 	// Helpers
-	static bool pbufIsStrEqual(pbuf* buf, const char* compared, int startPos);
-	static int pbufFindChar(pbuf* buf, char wtf, int startPos = 0);
-	static int pbufFindStr(pbuf* buf, const char* wtf, int startPos = 0);
-	static char* pbufAllocateStrCopy(pbuf* buf, int startPos, int length);
-	static String pbufStrCopy(pbuf* buf, int startPos, int length);
+	static bool pbufIsStrEqual(const pbuf* buf, const char* compared, unsigned startPos);
+	static int pbufFindChar(const pbuf* buf, char wtf, unsigned startPos = 0);
+	static int pbufFindStr(const pbuf* buf, const char* wtf, unsigned startPos = 0);
+	static char* pbufAllocateStrCopy(const pbuf* buf, unsigned startPos, unsigned length);
+	static String pbufStrCopy(const pbuf* buf, unsigned startPos, unsigned length);
 
+#ifdef FIX_NETWORK_ROUTING
 	static bool FixNetworkRouting();
+#else
+	static bool FixNetworkRouting()
+	{
+		return true; // Should work on standard lwip
+	}
+#endif
 
 	// Debug
 	static void debugPrintTcpList();
 
 private:
+#ifdef FIX_NETWORK_ROUTING
 	static bool ipClientRoutingFixed;
+#endif
 };
 
 /** @} */

commit b2ea043acfc47c5a2635219a6376338f886971f3

Fix signed/unsigned bug introduced in #1599 and highlighted by codacy

Sming/SmingCore/Network/NetUtils.cpp
@@ -86,10 +86,7 @@ int NetUtils::pbufFindStr(const pbuf* buf, const char* wtf, unsigned startPos)
 		return -1;
 	}
 
-	unsigned cur = startPos;
-	if(startPos < 0) {
-		startPos = 0;
-	}
+	int cur = startPos;
 	while(true) {
 		cur = pbufFindChar(buf, wtf[0], cur);
 		if(cur < 0) {