SmingHub/Sming

commit 907957254960b62c57b46d55f3b33ed63ba9bfe0

Introduce ObjectMap (#1638)
* Introduce ObjectMap

* Move `ResourceTree` class into separate file, `HttpServer::resourceTree` public

* Make `ObjectMap::operator[]` safe

* Move resource path management into `HttpResourceTree` class

Rename `ResourceTree` to `HttpResourceTree`
Deprecate `HttpServeri::addPath()` and `setDefault` methods
Make `HttpCompatResource` private, move into `HttpResourceTree` module

* Rename `resourceTree` to `paths`

* Add missing file

* Update `readme.md`

* Revise `ObjectMap` so non-existent values are not implicitly created by `operator[]`

`Value` revised to work with map directly rather than just taking value reference - safer and more useful
Move example code to class comment

* Fix codacy advisory, clarify use of HttpConnectionPool

* Revert changes to `HttpClient::send`, confusing and less efficient

* Add `ObjectMap::extract()` method

Sming/SmingCore/Data/ObjectMap.h
@@ -1,0 +1,333 @@
+/****
+ * Sming Framework Project - Open Source framework for high efficiency native ESP8266 development.
+ * Created 2015 by Skurydin Alexey
+ * http://github.com/SmingHub/Sming
+ * All files of the Sming Core are provided under the LGPL v3 license.
+ *
+ * ObjectMap.h
+ *
+ * @author: 31 Jul 2018 - Mikee47 <mike@sillyhouse.net>
+ *
+ */
+
+#ifndef _SMING_CORE_DATA_OBJECT_MAP_H_
+#define _SMING_CORE_DATA_OBJECT_MAP_H_
+
+#include "WVector.h"
+
+/**
+ * @brief Implementation of a HashMap for owned objects, i.e. anything created with new().
+ * @note Once added to the map the object is destroyed when no longer required.
+ *
+ *  Example:
+ *
+ *  ```
+ *	void test()
+ *	{
+ *		ObjectMap<String, MyType> map;
+ *		MyType* object1 = new MyType();
+ *		if (map["key1"] == nullptr) { 	// Does NOT create entry in map
+ *			map["key1"] = object1; 		// Entry now created, "key1" -> object1
+ *		}
+ *		MyType* object2 = new MyType();
+ *		map["key1"] = object2;			// object1 is destroyed, "key1" -> object2
+ *
+ *		// Demonstrate use of value reference
+ *		auto value = map["key1"];		// Returns ObjectMap<String, MyType>::Value object
+ *		value = new MyType();			// "key1" -> new object
+ *		value = nullptr; 				// Free object, "key1" -> nullptr (but still in map)
+ *		value.remove();					// Free object1 and remove from map
+ *
+ *		// As soon as `map` goes out of scope, all contained objects are detroyed
+ *		map["key1"] = new MyType();
+ *		map["key2"] = new MyType();
+ *	}
+ * 	```
+ *
+ */
+template <typename K, typename V> class ObjectMap
+{
+public:
+	ObjectMap()
+	{
+	}
+
+	~ObjectMap()
+	{
+		clear();
+	}
+
+	/**
+	 * @brief Class to provide safe access to mapped value
+	 * @note ObjectMap `operator[]` returns one of these, which provides behaviour consistent with V*
+	 */
+	class Value
+	{
+	public:
+		Value(ObjectMap<K, V>& map, const K& key) : map(map), key(key)
+		{
+		}
+
+		const K& getKey() const
+		{
+			return key;
+		}
+
+		V* getValue() const
+		{
+			return map.find(key);
+		}
+
+		Value& operator=(V* newValue)
+		{
+			map.set(key, newValue);
+			return *this;
+		}
+
+		operator V*() const
+		{
+			return getValue();
+		}
+
+		V* operator->() const
+		{
+			return getValue();
+		}
+
+		/**
+		 * @brief Remove this value from the map
+		 * @retval bool true if the value was found and removed
+		 */
+		bool remove()
+		{
+			return map.remove(key);
+		}
+
+		/**
+		 * @brief Get the value for a given key and remove it from the map, without destroying it
+		 * @retval V*
+		 * @note The returned object must be freed by the caller when no longer required
+		 */
+		V* extract()
+		{
+			return map.extract(key);
+		}
+
+	private:
+		ObjectMap<K, V>& map;
+		K key;
+	};
+
+	/**
+	 * @brief Get the number of entries in this map
+	 * @retval int Entry count
+	 */
+	unsigned count() const
+	{
+		return entries.count();
+	}
+
+	/*
+	 * @brief Get a key at a specified index, non-modifiable
+	 * @param idx the index to get the key at
+	 * @return The key at index idx
+	 */
+	const K& keyAt(unsigned idx) const
+	{
+		return entries[idx].key;
+	}
+
+	/*
+	 * @brief Get a key at a specified index
+	 * @param idx the index to get the key at
+	 * @return Reference to the key at index idx
+	 */
+	K& keyAt(unsigned idx)
+	{
+		return entries[idx].key;
+	}
+
+	/*
+	 * @brief Get a value at a specified index, non-modifiable
+	 * @param idx the index to get the value at
+	 * @retval The value at index idx
+	 * @note The caller must not use `delete` on the returned value
+	 */
+	const V* valueAt(unsigned idx) const
+	{
+		return entries[idx].value;
+	}
+
+	/*
+	 * @brief Get a value at a specified index
+	 * @param idx the index to get the value at
+	 * @retval Value Reference to value at index idx
+	 * @see `operator[]`
+	 */
+	Value valueAt(unsigned idx)
+	{
+		return Value(*this, entries[idx].key);
+	}
+
+	/**
+	 * @brief Get value for given key, if it exists
+	 * @param key
+	 * @retval const V* Will be null if not found in the map
+	 * @note The caller must not use `delete` on the returned value
+	 */
+	const V* operator[](const K& key) const
+	{
+		return find(key);
+	}
+
+	/** @brief Access map entry by reference
+	 *  @param key
+	 *  @retval Value Guarded access to mapped value corresponding to given key
+	 *  @note If the given key does not exist in the map it will NOT be created
+	 * 	@see `valueAt()`
+	 *
+	 */
+	Value operator[](const K& key)
+	{
+		return get(key);
+	}
+
+	/** @brief Get map entry value
+	 *  @param key
+	 *  @retval Value
+	 *  @see `operator[]`
+	 */
+	Value get(const K& key)
+	{
+		return Value(*this, key);
+	}
+
+	/** @brief Set a key value
+	 *  @param key
+	 *  @param value
+	 */
+	void set(const K& key, V* value)
+	{
+		int i = indexOf(key);
+		if(i >= 0) {
+			delete entries[i].value;
+			entries[i].value = value;
+		} else {
+			entries.addElement(new Entry(key, value));
+		}
+	}
+
+	/**
+	 * @brief Find the value for a given key, if it exists
+	 * @param key
+	 * @retval V* Points to the object if it exists, otherwise nullptr
+	 * @note If you need to modify the existing map entry, use `operator[]` or `valueAt()`
+	 */
+	V* find(const K& key) const
+	{
+		int index = indexOf(key);
+		return (index < 0) ? nullptr : entries[index].value;
+	}
+
+	/**
+	 * @brief Get the index of a key
+	 * @param key
+	 * @retval int The index of the key, or -1 if key does not exist
+	 */
+	int indexOf(const K& key) const
+	{
+		for(unsigned i = 0; i < entries.count(); i++) {
+			if(entries[i].key == key) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	/**
+	 * @brief Check if a key is contained within this map
+	 * @param key the key to check
+	 * @retval bool true if key exists
+	 */
+	bool contains(const K& key) const
+	{
+		return indexOf(key) >= 0;
+	}
+
+	/**
+	 * @brief Remove entry at given index
+	 * @param index location to remove from this map
+	 */
+	void removeAt(unsigned index)
+	{
+		entries.remove(index);
+	}
+
+	/**
+	 * @brief Remove a key from this map
+	 * @param key The key identifying the entry to remove
+	 * @retval bool true if the value was found and removed
+	 */
+	bool remove(const K& key)
+	{
+		int index = indexOf(key);
+		if(index < 0) {
+			return false;
+		} else {
+			removeAt(index);
+			return true;
+		}
+	}
+
+	/**
+	 * @brief Get the value for a given key and remove it from the map, without destroying it
+	 * @param key
+	 * @retval V*
+	 * @note The returned object must be freed by the caller when no longer required
+	 */
+	V* extract(const K& key)
+	{
+		V* value = nullptr;
+		int i = indexOf(key);
+		if(i >= 0) {
+			value = entries[i].value;
+			entries[i].value = nullptr;
+			entries.remove(i);
+		}
+		return value;
+	}
+
+	/**
+	 * @brief Clear the map of all entries
+	 */
+	void clear()
+	{
+		entries.clear();
+	}
+
+protected:
+	/**
+	 * @brief An entry in the ObjectMap
+	 */
+	struct Entry {
+		K key;
+		V* value = nullptr;
+
+		Entry(const K& key, V* value) : key(key), value(value)
+		{
+		}
+
+		~Entry()
+		{
+			delete value;
+		}
+	};
+
+	Vector<Entry> entries;
+
+private:
+	// Copy constructor unsafe, so prevent access
+	ObjectMap(ObjectMap<K, V>& that);
+};
+
+#endif // _SMING_CORE_DATA_OBJECT_MAP_H_

Sming/SmingCore/Network/Http/HttpRequest.cpp
@@ -148,11 +148,6 @@ void HttpRequest::reset()
 	responseStream = nullptr;
 
 	postParams.clear();
-	for(unsigned i = 0; i < files.count(); i++) {
-		String key = files.keyAt(i);
-		delete files[key];
-		files[key] = nullptr;
-	}
 	files.clear();
 }
 

Sming/SmingCore/Network/Http/HttpRequest.h
@@ -22,6 +22,7 @@
 #include "Data/Stream/MultipartStream.h"
 #include "HttpHeaders.h"
 #include "HttpParams.h"
+#include "Data/ObjectMap.h"
 
 class HttpClient;
 class HttpServerConnection;
@@ -111,7 +112,7 @@ public:
 	 */
 	HttpRequest* setFile(const String& formElementName, IDataSourceStream* stream)
 	{
-		if(stream) {
+		if(stream != nullptr) {
 			files[formElementName] = stream;
 		}
 		return this;
@@ -288,7 +289,7 @@ protected:
 #endif
 
 private:
-	HashMap<String, IDataSourceStream*> files;
+	ObjectMap<String, IDataSourceStream> files;
 
 	HttpParams* queryParams = nullptr; // << @todo deprecate
 };

Sming/SmingCore/Network/Http/HttpResource.h
@@ -14,7 +14,7 @@
 #define _SMING_CORE_NETWORK_HTTP_HTTP_RESOURCE_H_
 
 #include "WString.h"
-#include "WHashMap.h"
+#include "Data/ObjectMap.h"
 #include "Delegate.h"
 
 #include "HttpResponse.h"
@@ -26,8 +26,8 @@ typedef Delegate<int(HttpServerConnection& connection, HttpRequest&, const char*
 	HttpServerConnectionBodyDelegate;
 typedef Delegate<int(HttpServerConnection& connection, HttpRequest&, char* at, int length)>
 	HttpServerConnectionUpgradeDelegate;
-typedef Delegate<int(HttpServerConnection&, HttpRequest&, HttpResponse&)> HttpResourceDelegate;
-typedef Delegate<void(HttpRequest&, HttpResponse&)> HttpPathDelegate;
+typedef Delegate<int(HttpServerConnection& connection, HttpRequest& request, HttpResponse& response)>
+	HttpResourceDelegate;
 
 class HttpResource
 {
@@ -49,26 +49,5 @@ public:
 	HttpResourceDelegate onRequestComplete = nullptr;		 ///< request is complete OR upgraded
 	HttpServerConnectionUpgradeDelegate onUpgrade = nullptr; ///< request is upgraded and raw data is passed to it
 };
-
-class HttpCompatResource : public HttpResource
-{
-public:
-	HttpCompatResource(const HttpPathDelegate& callback) : callback(callback)
-	{
-		onRequestComplete = HttpResourceDelegate(&HttpCompatResource::requestComplete, this);
-	}
-
-private:
-	int requestComplete(HttpServerConnection& connection, HttpRequest& request, HttpResponse& response)
-	{
-		callback(request, response);
-		return 0;
-	}
-
-private:
-	HttpPathDelegate callback;
-};
-
-typedef HashMap<String, HttpResource*> ResourceTree;
 
 #endif /* _SMING_CORE_NETWORK_HTTP_HTTP_RESOURCE_H_ */

Sming/SmingCore/Network/Http/HttpResourceTree.cpp
@@ -1,0 +1,44 @@
+/****`
+ * Sming Framework Project - Open Source framework for high efficiency native ESP8266 development.
+ * Created 2015 by Skurydin Alexey
+ * http://github.com/anakod/Sming
+ * All files of the Sming Core are provided under the LGPL v3 license.
+ *
+ * HttpResourceTree.cpp
+ *
+ * @author: 2017 - Slavey Karadzhov <slav@attachix.com>
+ *
+ ****/
+
+#include "HttpResourceTree.h"
+
+class HttpCompatResource : public HttpResource
+{
+public:
+	explicit HttpCompatResource(const HttpPathDelegate& callback) : callback(callback)
+	{
+		onRequestComplete = HttpResourceDelegate(&HttpCompatResource::requestComplete, this);
+	}
+
+private:
+	int requestComplete(HttpServerConnection& connection, HttpRequest& request, HttpResponse& response)
+	{
+		callback(request, response);
+		return 0;
+	}
+
+private:
+	HttpPathDelegate callback;
+};
+
+/* HttpResourceTree */
+
+void HttpResourceTree::set(String path, const HttpPathDelegate& callback)
+{
+	if(path.length() > 1 && path.endsWith("/")) {
+		path.remove(path.length() - 1);
+	}
+	debug_i("'%s' registered", path.c_str());
+
+	set(path, new HttpCompatResource(callback));
+}

Sming/SmingCore/Network/Http/HttpResourceTree.h
@@ -1,0 +1,84 @@
+/****`
+ * Sming Framework Project - Open Source framework for high efficiency native ESP8266 development.
+ * Created 2015 by Skurydin Alexey
+ * http://github.com/anakod/Sming
+ * All files of the Sming Core are provided under the LGPL v3 license.
+ *
+ * HttpResourceTree.h
+ *
+ * @author: 2017 - Slavey Karadzhov <slav@attachix.com>
+ *
+ ****/
+
+#ifndef _SMING_CORE_NETWORK_HTTP_HTTP_RESOURCE_TREE_H_
+#define _SMING_CORE_NETWORK_HTTP_HTTP_RESOURCE_TREE_H_
+
+#include "HttpResource.h"
+
+typedef Delegate<void(HttpRequest& request, HttpResponse& response)> HttpPathDelegate;
+
+/** @brief Identifies the default resource path */
+#define RESOURCE_PATH_DEFAULT String('*')
+
+/** @brief Class to map URL paths to classes which handle them
+ */
+class HttpResourceTree : public ObjectMap<String, HttpResource>
+{
+public:
+	/** @brief Set the default resource handler
+	 *  @param resource The default resource handler
+	 */
+	void setDefault(HttpResource* resource)
+	{
+		set(RESOURCE_PATH_DEFAULT, resource);
+	}
+
+	/** @brief Set the default resource handler, identified by "*" wildcard
+	 *  @param resource The default resource handler
+	 */
+	void setDefault(const HttpResourceDelegate& onRequestComplete)
+	{
+		set(RESOURCE_PATH_DEFAULT, onRequestComplete);
+	}
+
+	/** @brief Set the default resource handler, identified by "*" wildcard */
+	void setDefault(const HttpPathDelegate& callback)
+	{
+		set(RESOURCE_PATH_DEFAULT, callback);
+	}
+
+	/** @brief Get the current default resource handler, if any
+	 *  @retval HttpResource*
+	 */
+	HttpResource* getDefault()
+	{
+		return find(RESOURCE_PATH_DEFAULT);
+	}
+
+	using ObjectMap::set;
+
+	/**
+	 * @brief Set a callback to handle the given path
+	 * @param path URL path
+	 * @param onRequestComplete Delegate to handle this path
+	 * @note Path should start with slash. Trailing slashes will be removed.
+	 * @note Any existing handler for this path is replaced
+	 */
+	void set(const String& path, const HttpResourceDelegate& onRequestComplete)
+	{
+		HttpResource* resource = new HttpResource;
+		resource->onRequestComplete = onRequestComplete;
+		set(path, resource);
+	}
+
+	/**
+	 * @brief Add a new path resource with a callback
+	 * @param path URL path
+	 * @param callback The callback that will handle this path
+	 * @note Path should start with slash. Trailing slashes will be removed
+	 * @note Any existing handler for this path is replaced
+	 */
+	void set(String path, const HttpPathDelegate& callback);
+};
+
+#endif /* _SMING_CORE_NETWORK_HTTP_HTTP_RESOURCE_TREE_H_ */

Sming/SmingCore/Network/Http/HttpServerConnection.cpp
@@ -11,7 +11,7 @@
  ****/
 
 #include "HttpServerConnection.h"
-
+#include "HttpResourceTree.h"
 #include "HttpServer.h"
 #include "TcpServer.h"
 #include "WebConstants.h"
@@ -42,10 +42,9 @@ int HttpServerConnection::onPath(const URL& uri)
 
 	request.setURL(uri);
 
-	if(resourceTree->contains(request.uri.Path)) {
-		resource = (*resourceTree)[request.uri.Path];
-	} else if(resourceTree->contains("*")) {
-		resource = (*resourceTree)["*"];
+	resource = resourceTree->find(request.uri.Path);
+	if(resource == nullptr) {
+		resource = resourceTree->getDefault();
 	}
 
 	return 0;
@@ -107,7 +106,7 @@ int HttpServerConnection::onHeadersComplete(const HttpHeaders& headers)
 		error = 1;
 	}
 
-	if(request.headers.contains(HTTP_HEADER_CONTENT_TYPE)) {
+	if(bodyParsers != nullptr && request.headers.contains(HTTP_HEADER_CONTENT_TYPE)) {
 		String contentType = request.headers[HTTP_HEADER_CONTENT_TYPE];
 		int endPos = contentType.indexOf(';');
 		if(endPos != -1) {
@@ -124,11 +123,12 @@ int HttpServerConnection::onHeadersComplete(const HttpHeaders& headers)
 		Vector<String> types;
 		types.add(contentType);
 		types.add(majorType);
-		types.add("*");
+		types.add(String('*'));
 
 		for(unsigned i = 0; i < types.count(); i++) {
-			if(bodyParsers->contains(types.at(i))) {
-				bodyParser = (*bodyParsers)[types.at(i)];
+			const String& type = types[i];
+			if(bodyParsers->contains(type)) {
+				bodyParser = (*bodyParsers)[type];
 				break;
 			}
 		}

Sming/SmingCore/Network/Http/HttpServerConnection.h
@@ -33,6 +33,7 @@
 #define HTTP_SERVER_EXPOSE_DATE 0
 #endif
 
+class HttpResourceTree;
 class HttpServerConnection;
 
 typedef Delegate<void(HttpServerConnection& connection)> HttpServerConnectionDelegate;
@@ -53,12 +54,12 @@ public:
 		}
 	}
 
-	void setResourceTree(ResourceTree* resourceTree)
+	void setResourceTree(HttpResourceTree* resourceTree)
 	{
 		this->resourceTree = resourceTree;
 	}
 
-	void setBodyParsers(BodyParsers* bodyParsers)
+	void setBodyParsers(const BodyParsers* bodyParsers)
 	{
 		this->bodyParsers = bodyParsers;
 	}
@@ -137,10 +138,10 @@ public:
 	void* userData = nullptr; ///< use to pass user data between requests
 
 private:
-	ResourceTree* resourceTree = nullptr;
-	HttpResource* resource = nullptr;
+	HttpResourceTree* resourceTree = nullptr; ///< A reference to the current resource tree - we don't own it
+	HttpResource* resource = nullptr;		  ///< Resource for currently executing path
 
-	HttpRequest request = HttpRequest(URL());
+	HttpRequest request;
 	HttpResponse response;
 
 	HttpResourceDelegate headersCompleteDelegate = nullptr;
@@ -148,8 +149,8 @@ private:
 	HttpServerConnectionBodyDelegate onBodyDelegate = nullptr;
 	HttpServerProtocolUpgradeCallback upgradeCallback = nullptr;
 
-	BodyParsers* bodyParsers = nullptr;
-	HttpBodyParserDelegate bodyParser = nullptr;
+	const BodyParsers* bodyParsers = nullptr;	///< const reference ensures we cannot modify map, only look stuff up
+	HttpBodyParserDelegate bodyParser = nullptr; ///< Active body parser for this message, if any
 };
 
 /** @} */

commit 3335622d89d56867e532346f82b05574de2be285

Fix bug introduced in previous PR #1638
In `HttpConnection::multipartProducer()` the `IDataSourceStream*` entry from `files` must be extracted, not removed
Add `extractAt()` method added to `ObjectMap` to support this operation more efficiently

Sming/SmingCore/Data/ObjectMap.h
@@ -287,12 +287,23 @@ public:
 	 */
 	V* extract(const K& key)
 	{
-		V* value = nullptr;
 		int i = indexOf(key);
-		if(i >= 0) {
-			value = entries[i].value;
-			entries[i].value = nullptr;
-			entries.remove(i);
+		return (i < 0) ? nullptr : extractAt(i);
+	}
+
+	/**
+	 * @brief Get the value at a given index and remove it from the map, without destroying it
+	 * @param index
+	 * @retval V*
+	 * @note The returned object must be freed by the caller when no longer required
+	 */
+	V* extractAt(unsigned index)
+	{
+		V* value = nullptr;
+		if(index < entries.count()) {
+			value = entries[index].value;
+			entries[index].value = nullptr;
+			entries.remove(index);
 		}
 		return value;
 	}

Sming/SmingCore/Network/Http/HttpConnection.cpp
@@ -115,7 +115,7 @@ HttpPartResult HttpConnection::multipartProducer()
 
 	if(outgoingRequest->files.count()) {
 		String name = outgoingRequest->files.keyAt(0);
-		auto file = outgoingRequest->files[name];
+		auto file = outgoingRequest->files.extractAt(0);
 		result.stream = file;
 
 		HttpHeaders* headers = new HttpHeaders();
@@ -124,7 +124,6 @@ HttpPartResult HttpConnection::multipartProducer()
 		(*headers)[HTTP_HEADER_CONTENT_TYPE] = ContentType::fromFullFileName(file->getName());
 		result.headers = headers;
 
-		outgoingRequest->files.remove(name);
 		return result;
 	}
 