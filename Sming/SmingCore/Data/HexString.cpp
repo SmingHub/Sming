5300c0bca4fbc87ca5d754e7d90faafb319e8be3
Mike <mike@sillyhouse.net>
2019-04-06 13:01:18 +0100

    makeHexString() incorrectly appends nul character to result (#1659)
    Fixes #1658

--- Sming/SmingCore/Data/HexString.cpp
+++ Sming/SmingCore/Data/HexString.cpp
@@ -12,20 +12,28 @@
 
 String makeHexString(const uint8_t* data, unsigned length, char separator)
 {
+	if(data == nullptr || length == 0) {
+		return nullptr;
+	}
+
 	String result;
 
-	unsigned charsPerByte = separator ? 3 : 2;
-	if(!result.setLength(charsPerByte * length + 1))
+	unsigned stringLength = length * 2; // 2 hex chars per byte
+	if(separator != '\0') {
+		stringLength += length - 1;
+	}
+	if(!result.setLength(stringLength)) {
 		return nullptr;
+	}
 
 	char* p = result.begin();
 	for(unsigned i = 0; i < length; ++i) {
-		if(i && separator)
+		if(i != 0 && separator != '\0') {
 			*p++ = separator;
+		}
 		*p++ = hexchar(data[i] >> 4);
 		*p++ = hexchar(data[i] & 0x0F);
 	}
-	*p = '\0';
 
 	return result;
 }
