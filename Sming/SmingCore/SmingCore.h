c929c93148ffd2ee136f9ea5ffe432ddba8168fd
mikee47 <mike@sillyhouse.net>
2019-04-30 15:49:53 +0100

    Merge SmingCore warning #pragmas

--- Sming/SmingCore/SmingCore.h
+++ Sming/SmingCore/SmingCore.h
@@ -1,3 +1,2 @@
 #include <Core/SmingCore.h>
-#pragma warning "Please update to #include <SmingCore.h>"
-#pragma warning "See https://github.com/SmingHub/Sming/wiki/migrate-v3.8-v4.0"
+#pragma warning "Please update to #include <SmingCore.h> - see https://github.com/SmingHub/Sming/wiki/migrate-v3.8-v4.0"
