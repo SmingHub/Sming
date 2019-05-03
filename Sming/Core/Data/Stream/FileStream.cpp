b0f607bb7238cf4dbd1c2803874d955270cc2953
mikee47 <mike@sillyhouse.net>
2019-05-03 14:21:39 +0100

    Remove un-necessary size check

--- Sming/Core/Data/Stream/FileStream.cpp
+++ Sming/Core/Data/Stream/FileStream.cpp
@@ -91,9 +91,7 @@ size_t FileStream::write(const uint8_t* buffer, size_t size)
 	int written = fileWrite(handle, buffer, size);
 	if(check(written)) {
 		pos += size_t(written);
-		if(pos > this->size) {
-			this->size = pos;
-		}
+		this->size = pos;
 	}
 
 	return written;
