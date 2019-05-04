962c15039f1d83f15234a4c8ff33f0cf5b613a48
Slavey Karadzhov <slav@attachix.com>
2019-05-04 15:25:12 +0200

    Fixed broken migration link.

--- Sming/SmingCore/SmingCore.h
+++ Sming/SmingCore/SmingCore.h
@@ -1,2 +1,2 @@
 #include <Core/SmingCore.h>
-#pragma warning "Please update to #include <SmingCore.h> - see https://github.com/SmingHub/Sming/wiki/migrate-v3.8-v4.0"
+#pragma warning "Please update to #include <SmingCore.h> - see https://github.com/SmingHub/Sming/wiki/Migrating-from-Sming-v3.8.x-to-v4"
