56001a2d6aab7e7dd86e6ff1eb95f2252d274354
mikee47 <mike@sillyhouse.net>
2019-06-24 21:30:50 +0100

    Revise esp8266 Component, includes SDK

--- Sming/Arch/Esp8266/Components/esp8266/component.mk
+++ Sming/Arch/Esp8266/Components/esp8266/component.mk
@@ -1,65 +1,38 @@
-
-# => SMING
-
-MODULES			+= $(ARCH_COMPONENTS)/esp8266
-EXTRA_INCDIR	+= $(ARCH_COMPONENTS)/esp8266/include $(SDK_INCDIR)
-
-
-## => Makefile-bsd.mk
 # base directory of the ESP8266 SDK package, absolute
-SDK_BASE	?= $(ESP_HOME)/sdk
-SDK_TOOLS	 ?= $(SDK_BASE)/tools
+COMPONENT_VARS			:= SDK_BASE
 
-## => Makefile-linux.mk
-# base directory of the ESP8266 SDK package, absolute
-SDK_BASE	?= $(ESP_HOME)/sdk
-SDK_TOOLS	 ?= $(SDK_BASE)/tools
-
-## => Makefile-macos.mk
-# base directory of the ESP8266 SDK package, absolute
-SDK_BASE	?= $(ESP_HOME)/sdk
-SDK_TOOLS	 ?= $(SDK_BASE)/tools
-
-## => Makefile-windows.mk
-# base directory of the ESP8266 SDK package, absolute
-SDK_BASE	?= $(ESP_HOME)/ESP8266_SDK
-SDK_TOOLS	 ?= $(ESP_HOME)/utils
-
-
-# => SMING
-
-ifeq ($(SDK_INTERNAL), 1)
-	SUBMODULES		+= $(SDK_COMPONENT)
-	CUSTOM_TARGETS	+= $(SDK_COMPONENT)/bin/esp_init_data_default.bin
-
-$(SDK_COMPONENT)/bin/esp_init_data_default.bin:
-	$(Q) cp -f $(dir $@)esp_init_data_default_v08.bin $@
+ifeq ($(UNAME),Windows)
+SDK_BASE				?= $(ESP_HOME)/ESP8266_SDK
+SDK_TOOLS				?= $(ESP_HOME)/utils
+else
+SDK_BASE				?= $(ESP_HOME)/sdk
+SDK_TOOLS				?= $(SDK_BASE)/tools
 endif
 
+SDK_BASE				:= $(call FixPath,$(SDK_BASE))
 
 # => 'Internal' SDK - for SDK Version 3+ as submodule in Sming repository
 # SDK_BASE just needs to point into our repo as it's overridden with the correct submodule path
 # This provides backward-compatiblity, so $(SMING)/third-party/ESP8266_NONOS_SDK) still works
-CONFIG_VARS += SDK_BASE SDK_INTERNAL
-SDK_BASE := $(call FixPath,$(abspath $(SDK_BASE)))
+DEBUG_VARS				+= SDK_INTERNAL
 ifneq (,$(findstring $(SMING_HOME),$(SDK_BASE)))
-	SDK_COMPONENT	:= $(ARCH_COMPONENTS)/Sdk/ESP8266_NONOS_SDK
-	SDK_BASE		:= $(SMING_HOME)/$(SDK_COMPONENT)
-	CFLAGS			+= -DSDK_INTERNAL
-	SDK_INTERNAL = 1
+GLOBAL_CFLAGS			+= -DSDK_INTERNAL=1
+SDK_INTERNAL			:= 1
+COMPONENT_SUBMODULES	:= ESP8266_NONOS_SDK
+SDK_BASE				:= $(COMPONENT_PATH)/ESP8266_NONOS_SDK
+COMPONENT_TARGETS		:= $(SDK_BASE)/bin/esp_init_data_default.bin
+
+$(COMPONENT_RULE)$(SDK_BASE)/bin/esp_init_data_default.bin:
+	$(Q) cp -f $(@D)/esp_init_data_default_v08.bin $@
 else
-	SDK_INTERNAL = 0
+SDK_INTERNAL			:= 0
 endif
 
-# various paths from the SDK used in this project
-SDK_LIBDIR	:= $(SDK_BASE)/lib
-SDK_INCDIR	:= $(SDK_BASE)/include
+DEBUG_VARS				+= SDK_LIBDIR SDK_INCDIR
+SDK_LIBDIR				:= $(SDK_BASE)/lib
+SDK_INCDIR				:= $(SDK_BASE)/include
+COMPONENT_INCDIRS		:= include $(SDK_INCDIR)
 
-
-
-# => APP
-
-EXTRA_INCDIR += $(ARCH_COMPONENTS)/esp8266/include $(SDK_INCDIR)
-
-
-
+export SDK_INTERNAL
+export SDK_LIBDIR
+export SDK_INCDIR
