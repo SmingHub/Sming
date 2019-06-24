2a9a17ca094f8e71fb4c42ac38399d677cf8fd3e
mikee47 <mike@sillyhouse.net>
2019-06-24 21:31:10 +0100

    Revise Esp8266 custom_heap Component

--- Sming/Arch/Esp8266/Components/custom_heap/component.mk
+++ Sming/Arch/Esp8266/Components/custom_heap/component.mk
@@ -1,39 +1,15 @@
 # umm_malloc (custom heap allocation)
 
-# => SMING
+COMPONENT_SUBMODULES	:= umm_malloc
+COMPONENT_SRCDIRS		:= . umm_malloc/src
+COMPONENT_INCDIRS		:= umm_malloc/src umm_malloc/includes/c-helper-macros
 
-CONFIG_VARS += ENABLE_CUSTOM_HEAP
-ENABLE_CUSTOM_HEAP		?= 0
-ifeq ($(ENABLE_CUSTOM_HEAP), 1)
-	CUSTOM_HEAP_BASE	:= $(ARCH_COMPONENTS)/custom_heap
-	UMM_MALLOC_BASE		:= $(CUSTOM_HEAP_BASE)/umm_malloc
-	SUBMODULES			+= $(UMM_MALLOC_BASE)
-	MODULES				+= $(CUSTOM_HEAP_BASE) $(UMM_MALLOC_BASE)/src
-	EXTRA_INCDIR		+= $(UMM_MALLOC_BASE)/src $(UMM_MALLOC_BASE)/includes/c-helper-macros
-	LIBMAINMM			:= mainmm
-	LIBS				+= $(LIBMAINMM)
+LIBMAIN					= mainmm
+LIBMAIN_SRC				= $(COMPONENT_LIBDIR)/libmainmm.a
+COMPONENT_TARGETS		+= $(LIBMAIN_SRC)
 
 # Make copy of libmain and remove mem_manager.o module
-$(call UserLibPath,$(LIBMAINMM)): $(SDK_LIBDIR)/libmain.a | $(UMM_MALLOC_BASE)/.submodule
-	$(vecho) "Enabling custom heap implementation"
+$(COMPONENT_RULE)$(LIBMAIN_SRC): $(SDK_LIBDIR)/libmain.a
+	$(info Enabling custom heap implementation)
 	$(Q) cp $^ $@
 	$(Q) $(AR) -d $@ mem_manager.o
-endif
-
-
-# => APP
-
-CONFIG_VARS += ENABLE_CUSTOM_HEAP
-ENABLE_CUSTOM_HEAP ?= 0
-ifeq ($(ENABLE_CUSTOM_HEAP),1)
-	LIBMAIN			:= mainmm
-	LIBMAIN_SRC		:= $(call UserLibPath,$(LIBMAIN))
-	CUSTOM_TARGETS	+= $(LIBMAIN_SRC)
-
-$(LIBMAIN_SRC):
-	$(call MakeLibrary,$@,ENABLE_CUSTOM_HEAP=1)
-else
-    LIBMAIN			:= main
-	LIBMAIN_SRC		:= $(SDK_LIBDIR)/lib$(LIBMAIN).a
-endif
-
