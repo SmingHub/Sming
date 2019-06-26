863a785edb056b5530f4aeb039b2e85eac0b6294
mikee47 <mike@sillyhouse.net>
2019-06-26 12:14:50 +0100

    Revise HostTests
    Remove use of `hostmsg`, emulator now defaults to console output if uarts not redirected
    Use component.mk file

--- tests/HostTests/Makefile-user.mk
+++
@@ -1,9 +0,0 @@
-## Local build configuration
-## Parameters configured here will override default and ENV values.
-
-## SPIFFS options
-DISABLE_SPIFFS = 1
-# SPIFF_FILES = files
-
-DEBUG_VERBOSE_LEVEL = 3
-SPI_SIZE = 4M
---
+++ tests/HostTests/component.mk
@@ -0,0 +1,6 @@
+DISABLE_SPIFFS = 1
+DEBUG_VERBOSE_LEVEL = 3
+SPI_SIZE = 4M
+
+.PHONY: execute
+execute: flash
