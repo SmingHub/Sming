2be464f9603588e907be10f919fc0da1b2f57233
mikee47 <mike@sillyhouse.net>
2019-07-06 16:28:19 +0100

    Add Basic_WebSkeletonApp from LTS branch

---
+++ samples/Basic_WebSkeletonApp_LTS/app/application.cpp
@@ -0,0 +1,60 @@
+#include <user_config.h>
+#include <tytherm.h>
+
+Timer counterTimer;
+void counter_loop();
+unsigned long counter = 0;
+
+void STADisconnect(String ssid, uint8_t ssid_len, uint8_t bssid[6], uint8_t reason);
+void STAGotIP(IPAddress ip, IPAddress mask, IPAddress gateway);
+
+void init()
+{
+	spiffs_mount();					// Mount file system, in order to work with files
+	Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
+	Serial.systemDebugOutput(false);
+	Serial.commandProcessing(false);
+
+	//SET higher CPU freq & disable wifi sleep
+	system_update_cpu_freq(SYS_CPU_160MHZ);
+	wifi_set_sleep_type(NONE_SLEEP_T);
+
+	ActiveConfig = loadConfig();
+
+	// Attach Wifi events handlers
+	WifiEvents.onStationDisconnect(STADisconnect);
+	WifiEvents.onStationGotIP(STAGotIP);
+
+	startWebServer();
+
+	counterTimer.initializeMs(1000, counter_loop).start();
+}
+
+void counter_loop()
+{
+	counter++;
+}
+
+void STADisconnect(String ssid, uint8_t ssid_len, uint8_t bssid[6], uint8_t reason)
+{
+	debugf("DISCONNECT - SSID: %s, REASON: %d\n", ssid.c_str(), reason);
+
+	if(!WifiAccessPoint.isEnabled()) {
+		debugf("Starting OWN AP");
+		WifiStation.disconnect();
+		WifiAccessPoint.enable(true);
+		WifiStation.connect();
+	}
+}
+
+void STAGotIP(IPAddress ip, IPAddress mask, IPAddress gateway)
+{
+	debugf("GOTIP - IP: %s, MASK: %s, GW: %s\n", ip.toString().c_str(), mask.toString().c_str(),
+		   gateway.toString().c_str());
+
+	if(WifiAccessPoint.isEnabled()) {
+		debugf("Shutdown OWN AP");
+		WifiAccessPoint.enable(false);
+	}
+	// Add commands to be executed after successfully connecting to AP and got IP from it
+}
---
+++ samples/Basic_WebSkeletonApp_LTS/include/configuration.h
@@ -0,0 +1,27 @@
+#ifndef INCLUDE_CONFIGURATION_H_
+#define INCLUDE_CONFIGURATION_H_
+
+#include <user_config.h>
+#include <SmingCore.h>
+
+const char THERM_CONFIG_FILE[] = ".therm.conf"; // leading point for security reasons :)
+
+struct ThermConfig {
+	ThermConfig()
+	{
+		StaEnable = 1; //Enable WIFI Client
+	}
+
+	String StaSSID;
+	String StaPassword;
+	uint8_t StaEnable;
+
+	// ThermControl settings
+};
+
+ThermConfig loadConfig();
+void saveConfig(ThermConfig& cfg);
+
+extern ThermConfig ActiveConfig;
+
+#endif /* INCLUDE_CONFIGURATION_H_ */
