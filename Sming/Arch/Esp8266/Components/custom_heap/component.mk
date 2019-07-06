e59da53c17509b19049df616d6448e30e51dba55
Mike <mike@sillyhouse.net>
2019-07-06 20:32:22 +0100

    Fix custom heap (#1745)
    * Fix custom heap
    
    * Update umm_malloc to latest version
    
    * Add support for poison checking

--- Sming/Arch/Esp8266/Components/custom_heap/component.mk
+++ Sming/Arch/Esp8266/Components/custom_heap/component.mk
@@ -1,12 +1,18 @@
 # umm_malloc (custom heap allocation)
 
 COMPONENT_SUBMODULES	:= umm_malloc
-COMPONENT_SRCDIRS		:= . umm_malloc/src
+COMPONENT_SRCDIRS		:=
+COMPONENT_SRCFILES		:= heap.c umm_malloc/src/umm_malloc.c
 COMPONENT_INCDIRS		:= umm_malloc/src umm_malloc/includes/c-helper-macros
 
 LIBMAIN					= mainmm
 LIBMAIN_SRC				= $(COMPONENT_LIBDIR)/libmainmm.a
 COMPONENT_TARGETS		+= $(LIBMAIN_SRC)
+
+COMPONENT_VARS			+= UMM_POISON_CHECK
+ifeq ($(UMM_POISON_CHECK),1)
+GLOBAL_CFLAGS			+= -DUMM_POISON_CHECK
+endif
 
 # Make copy of libmain and remove mem_manager.o module
 $(COMPONENT_RULE)$(LIBMAIN_SRC): $(SDK_LIBDIR)/libmain.a
