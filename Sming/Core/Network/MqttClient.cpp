3056d20e873e0029cd01be0d6f1a113303b6a57b
Slavey Karadzhov <slav@attachix.com>
2019-07-11 16:00:00 +0200

    Force push smaller MQTT publish messages.

--- Sming/Core/Network/MqttClient.cpp
+++ Sming/Core/Network/MqttClient.cpp
@@ -271,7 +271,12 @@ bool MqttClient::publish(const String& topic, const String& content, uint8_t fla
 		return false;
 	}
 
-	return requestQueue.enqueue(message);
+	bool success = requestQueue.enqueue(message);
+	if(success && message->publish.content.length < 300) {
+		onReadyToSendData(TcpConnectionEvent::eTCE_Poll);
+	}
+
+	return success;
 }
 
 bool MqttClient::publish(const String& topic, IDataSourceStream* stream, uint8_t flags)
@@ -300,7 +305,12 @@ bool MqttClient::publish(const String& topic, IDataSourceStream* stream, uint8_t
 	message->publish.content.length = MQTT_PUBLISH_STREAM;
 	message->publish.content.data = (uint8_t*)stream;
 
-	return requestQueue.enqueue(message);
+	bool success = requestQueue.enqueue(message);
+	if(success && stream->available() < 300) {
+		onReadyToSendData(TcpConnectionEvent::eTCE_Poll);
+	}
+
+	return success;
 }
 
 bool MqttClient::subscribe(const String& topic)
