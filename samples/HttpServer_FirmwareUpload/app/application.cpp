b6267359c9bd3be8a2ab02e169de9015cb618287
aemseemann <aemseemann@gmail.com>
2019-08-04 20:18:29 +0200

    Extend HttpServer_FirmwareUpload with signatures.

--- samples/HttpServer_FirmwareUpload/app/application.cpp
+++ samples/HttpServer_FirmwareUpload/app/application.cpp
@@ -3,9 +3,119 @@
 #include <Data/Stream/LimitedMemoryStream.h>
 #include <MultipartParser/HttpMultipartResource.h>
 #include <Data/Stream/RbootOutputStream.h>
+#include <sodium/crypto_sign.h>
+#include "FirmwareVerificationKey.h"
 
 HttpServer server;
 String lastModified;
+
+class SignedRbootOutputStream : public RbootOutputStream
+{
+	const uint8_t* const verificationKey_;
+
+	struct {
+		uint32_t magic;
+		uint32_t loadAddress;
+		uint8_t signature[crypto_sign_BYTES];
+	} header_;
+
+	uint8_t* headerPtr_;
+	size_t missingHeaderBytes_;
+	static const uint32_t HEADER_MAGIC_EXPECTED = 0xf01af02a;
+
+	bool ok_;
+	bool closeCalled_ = false;
+	uint32_t startAddress_;
+
+	crypto_sign_state verifierState_;
+	String errorMessage_;
+
+	void setError(const char* message)
+	{
+		ok_ = false;
+		errorMessage_ = message;
+		Serial.println(errorMessage_);
+	}
+
+public:
+	SignedRbootOutputStream(int32_t startAddress, size_t maxLength, const uint8_t* verificationKey)
+		: RbootOutputStream(startAddress, maxLength), verificationKey_(verificationKey),
+		  headerPtr_(reinterpret_cast<uint8_t*>(&header_)), missingHeaderBytes_(sizeof(header_)), ok_(true),
+		  startAddress_(startAddress)
+	{
+		crypto_sign_init(&verifierState_);
+	}
+
+	bool ok() const
+	{
+		return ok_;
+	}
+	const String& errorMessage() const
+	{
+		return errorMessage_;
+	}
+
+	size_t write(const uint8_t* data, size_t size)
+	{
+		size_t consumed = 0;
+		if(ok_) {
+			if(missingHeaderBytes_ > 0) {
+				const size_t chunkSize = std::min(size, missingHeaderBytes_);
+				memcpy(headerPtr_, data, chunkSize);
+				headerPtr_ += chunkSize;
+				missingHeaderBytes_ -= chunkSize;
+				data += chunkSize;
+				size -= chunkSize;
+				consumed += chunkSize;
+				if(missingHeaderBytes_ == 0) {
+					Serial.printf("Receive image for load address 0x%08X, slot starts at 0x%08X\n", header_.loadAddress,
+								  startAddress_);
+					const bool magicOk = (header_.magic == HEADER_MAGIC_EXPECTED);
+					const bool loadAddressOk = ((header_.loadAddress & 0x000FFFFF) == (startAddress_ & 0x000FFFFF));
+					ok_ = magicOk && loadAddressOk;
+					if(!ok_) {
+						setError(magicOk ? "Unexpected load address. Try image for other slot."
+										 : "Invalid image received.");
+						return 0;
+					}
+
+					init();
+				}
+			}
+
+			if(size > 0) {
+				crypto_sign_update(&verifierState_, static_cast<const unsigned char*>(data), size);
+				consumed += RbootOutputStream::write(data, size);
+			}
+		}
+		return consumed;
+	}
+
+	bool close()
+	{
+		if(closeCalled_) {
+			return true;
+		}
+		closeCalled_ = true;
+
+		Serial.printf("SignedRbootOutputStream::close()\n");
+		if(ok_) {
+			if(!RbootOutputStream::close()) {
+				return false;
+			}
+
+			Serial.printf("Verify signature\n");
+			const bool signatureMatch =
+				(crypto_sign_final_verify(&verifierState_, header_.signature, verificationKey_) == 0);
+			if(!signatureMatch) {
+				// destroy start sector of updated ROM to avoid accidental booting an unsanctioned firmware
+				spi_flash_erase_sector(startAddress / SECTOR_SIZE);
+				setError("Signature mismatch");
+			}
+		}
+		return ok_;
+	}
+};
 
 void onIndex(HttpRequest& request, HttpResponse& response)
 {
@@ -22,7 +132,7 @@ void onFile(HttpRequest& request, HttpResponse& response)
 	}
 
 	String file = request.uri.getRelativePath();
-
+	Serial.printf("Requested file: %s\n", file.c_str());
 	if(file[0] == '.')
 		response.code = HTTP_STATUS_FORBIDDEN;
 	else {
@@ -37,42 +147,33 @@ void onFile(HttpRequest& request, HttpResponse& response)
 
 int onUpload(HttpServerConnection& connection, HttpRequest& request, HttpResponse& response)
 {
+	Serial.printf("onUpload\n");
 	ReadWriteStream* file = request.files["firmware"];
 	if(file == nullptr) {
 		debug_e("Something went wrong with the file upload");
 		return 1;
 	}
 
-	debugf("Uploaded length: %d", file->available());
+	SignedRbootOutputStream* uploadStream = static_cast<SignedRbootOutputStream*>(file);
+	uploadStream->close();
 
-	rboot_config bootConfig = rboot_get_config();
-	uint8_t slot = bootConfig.current_rom;
-	slot = (slot == 0 ? 1 : 0);
-	Serial.printf("Firmware updated, rebooting to rom %d...\r\n", slot);
-	rboot_set_current_rom(slot);
-	System.restart(5); // defer the restart with 5 seconds to give time to the web server to return the response
+	if(uploadStream->ok()) {
+		rboot_config bootConfig = rboot_get_config();
+		uint8_t slot = bootConfig.current_rom;
+		slot = (slot == 0 ? 1 : 0);
+		Serial.printf("Firmware updated, rebooting to rom %d...\r\n", slot);
+		rboot_set_current_rom(slot);
+		System.restart(5); // defer the restart with 5 seconds to give time to the web server to return the response
 
-	response.sendFile("restart.html");
-
+		response.sendFile("restart.html");
+	} else {
+		response.code = HTTP_STATUS_BAD_REQUEST;
+		response.setContentType(MIME_HTML);
+		String html = "<H2 color='#444'>" + uploadStream->errorMessage() + "</H2>";
+		response.headers[HTTP_HEADER_CONTENT_LENGTH] = html.length();
+		response.sendString(html);
+	}
 	return 0;
-}
-
-void simpleUploadMapper(HttpFiles& files)
-{
-	/*
-	 * On a normal computer the file uploads are usually using
-	 * temporary space on the hard disk or in memory to store the incoming data.
-	 *
-	 * On an embedded device that is a luxury that we can hardly afford.
-	 * Therefore we should define a `map` that specifies explicitly
-	 * where a specific form field will be stored.
-	 *
-	 * If a field is not specified then its content will be discarded.
-	 */
-
-	// Below we instruct the server to store max 1024 bytes in memory
-	// from the incoming data for the "firmware" form fields.
-	files["firmware"] = new LimitedMemoryStream(1024);
 }
 
 void fileUploadMapper(HttpFiles& files)
@@ -97,7 +198,7 @@ void fileUploadMapper(HttpFiles& files)
 	size_t maxLength = 0; // 0  means that there is no max length.
 						  // Set this according to your flash memory layout
 
-	files["firmware"] = new RbootOutputStream(romStartAddress, maxLength);
+	files["firmware"] = new SignedRbootOutputStream(romStartAddress, maxLength, firmwareVerificationKey);
 }
 
 void startWebServer()
@@ -113,6 +214,9 @@ void startWebServer()
 
 void init()
 {
+	Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
+	Serial.systemDebugOutput(true); // Enable debug output to serial
+
 	spiffs_mount(); // Mount file system, in order to work with files
 
 	if(fileExist(".lastModified")) {
@@ -121,10 +225,9 @@ void init()
 		lastModified.trim();
 	}
 
-	Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
-	Serial.systemDebugOutput(true); // Enable debug output to serial
-
 	WifiStation.enable(true);
+	WifiStation.config(WIFI_SSID, WIFI_PWD);
+	WifiStation.enableDHCP(true);
 
 	// Run WEB server on system ready
 	System.onReady(startWebServer);
---
+++ samples/HttpServer_FirmwareUpload/signtool.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python2
+
+import argparse
+import sys, os
+import struct
+# image signing via libsodium
+try:
+    import nacl.bindings
+    import nacl.encoding
+    import nacl.signing
+except:
+    sys.stderr.write("""\
+It seems you have not installed libsodium bindings for python.
+To install them, use a command like 'python -m pip install pynacl' or
+search your distribution's package repository. The package name may be
+'python-nacl', 'python2-pynacl' or something similar.
+""")
+    sys.exit(2)
+    
+    
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Generate signed firmware update image")
+    parser.add_argument('--genkey', dest='genkey', action='store_true', default=False, help='Generate new signing key')
+    parser.add_argument('--keyfile', dest='keyfile', help='Path to signing key file (output if \'--genkey\' is given, otherwise input).')
+    parser.add_argument('--pubkey-header', dest='pubkey_header', default='', help='Generate header file with verification key (public key)')
+    parser.add_argument('--rom', dest='rom', default='', help='ROM image file with load address, e.g. address=path/to/image')
+    parser.add_argument('--out', dest='out', default='', help='Path to signed firmware update file.')
+    return parser.parse_args()
+
+def write_pubkey_header(pk, filepath):
+    with open(filepath, 'w') as header:
+        header.write('''\
+#ifndef FIRMWARE_VERIFICATION_KEY_H
+#define FIRMWARE_VERIFICATION_KEY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static const uint8_t firmwareVerificationKey[''' + str(len(pk)) + '''] = {
+    ''' + ', '.join('0x%02X' % ord(b) for b in pk) + '''
+};
+#ifdef __cplusplus
+}
+#endif
+#endif // FIRMWARE_VERIFICATION_KEY_H
+''')
+    print("Verification key written to C header file '%s'\n" % filepath)
+
+def parse_address(string):
+    try:
+        if string.startswith('0x') or string.startswith('0X'):
+            return int(string, 16)
+        else:
+            return int(string, 10)
+    except:
+        sys.stderr.write(string + ' is not a valid start address\n')
+        raise
+    
+def load_rom_image(address_file_pair):
+    [address, filepath] = address_file_pair.split('=', 1)
+    address = parse_address(address)
+    try:
+        print('Read image file "' + filepath + '"...')
+        with open(filepath, 'rb') as f:
+            image_content = f.read()
+    except:
+        sys.stderr.write('Failed to read "' + filepath + '"\n')
+        raise
+
+    return (address, image_content)
+    
+def make_signed_image(address, image, sk, pk = None):
+    # signed image format:
+    # - 4 byte magic
+    # - 4 byte load address of ROM image
+    # - 64 byte signature
+    # - ROM image content (variable length)
+    
+    magic = 0xf01af02a
+    
+    signed_image = struct.pack('II', magic, address)
+    
+    sig_state = nacl.bindings.crypto_sign_ed25519ph_state()
+    nacl.bindings.crypto_sign_ed25519ph_update(sig_state, image)
+    signature = nacl.bindings.crypto_sign_ed25519ph_final_create(sig_state, sk)
+    print("OTA signature (EdDSA25519ph): " + signature.encode('Hex'))
+    
+    if pk is not None:
+        # test signature verification
+        print("Verification key: " + pk.encode('Hex'))
+        verify_state = nacl.bindings.crypto_sign_ed25519ph_state()
+        nacl.bindings.crypto_sign_ed25519ph_update(verify_state, image)
+        nacl.bindings.crypto_sign_ed25519ph_final_verify(verify_state, signature, pk)
+    
+    signed_image += signature
+    signed_image += image
+    
+    return signed_image
+
+    
+def main():
+    args = parse_arguments()
+    
+    if args.genkey:
+        print('Generate signing key...')
+        sk_seed = nacl.bindings.randombytes(nacl.bindings.crypto_sign_SEEDBYTES)
+        
+        if args.keyfile:                
+            with open(args.keyfile, 'wb') as privkeyfile:
+                privkeyfile.write(sk_seed.encode('Hex'))
+            print("Private signing key written to '%s'." % args.keyfile)
+        else:
+            sys.stderr.write("Warning: Not output file for generated key is given ('--keyfile'). Generated key will be lost.\n")
+    elif args.keyfile:
+        print("Read signing key from '%s'" % args.keyfile)
+        with open(args.keyfile, 'r') as keyfile:
+            sk_seed = keyfile.read().decode('Hex')
+    
+    else:
+        sys.exit("Neither '--genkey' nor '--keyfile' is given. No signing key available.\n");
+    
+    (pk, sk) = nacl.bindings.crypto_sign_seed_keypair(sk_seed)
+        
+    if args.pubkey_header:
+        write_pubkey_header(pk, args.pubkey_header)
+    
+    if args.out:
+        if not args.rom:
+            sys.exit('No Rom image given. Cannot generate signed update file.')
+        
+        address, image = load_rom_image(args.rom)
+        signed_image = make_signed_image(address, image, sk, pk)
+
+        try:
+            print('Write signed update file to "' + args.out + '"')
+            with open(args.out, 'wb') as f:
+                f.write(signed_image)
+        except:
+            sys.stderr.write('Failed to write to "' + args.out + '"\n')
+    else:
+        if args.rom:
+            sys.exit('Output file (\'-o\') missing.\n')
+
+if __name__ == "__main__":
+    main()
+    
