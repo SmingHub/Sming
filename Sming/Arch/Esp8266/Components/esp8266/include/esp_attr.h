6ae1fbfd56a54006618f36c52dd4f8a435a04518
mikee47 <mike@sillyhouse.net>
2019-08-29 22:10:17 +0100

    Place IRAM and FLASH data in separate segments to resolve issues with section conflicts
    Thanks to https://github.com/esp8266/Arduino/pull/5116
    
    Commonly occurs when attempting to inline header methods, generally more of a problem with GCC 4.8.

--- Sming/Arch/Esp8266/Components/esp8266/include/esp_attr.h
+++ Sming/Arch/Esp8266/Components/esp8266/include/esp_attr.h
@@ -1,25 +1,29 @@
 // ESP8266 attribute definitions (previously in c_types.h)
 #pragma once
 
-#define IRAM_ATTR __attribute__((section(".iram.text")))
-#define STORE_TYPEDEF_ATTR __attribute__((aligned(4),packed))
+// http://stackoverflow.com/a/35441900
+#define MACROQUOT(x) #x
+#define MACROQUOTE(x) MACROQUOT(x)
+
+#define STORE_TYPEDEF_ATTR __attribute__((aligned(4), packed))
 #define STORE_ATTR __attribute__((aligned(4)))
 
 #define DMEM_ATTR __attribute__((section(".bss")))
 #define SHMEM_ATTR
 
-#ifdef ICACHE_FLASH
-#define ICACHE_FLASH_ATTR __attribute__((section(".irom0.text")))
-#define ICACHE_RODATA_ATTR __attribute__((section(".irom.text")))
-#else
-#define ICACHE_FLASH_ATTR
-#define ICACHE_RODATA_ATTR
-#endif
+#define ICACHE_FLASH_ATTR \
+	__attribute__((section(".irom0.text." __FILE__ MACROQUOTE(__LINE__) MACROQUOTE(__COUNTER__))))
+#define ICACHE_RAM_ATTR \
+	__attribute__((section(".iram.text." __FILE__ MACROQUOTE(__LINE__) MACROQUOTE(__COUNTER__))))
+#define ICACHE_RODATA_ATTR \
+	__attribute__((section(".irom0.text." __FILE__ MACROQUOTE(__LINE__) MACROQUOTE(__COUNTER__))))
+
+#define IRAM_ATTR ICACHE_RAM_ATTR
 
 #define STORE_ATTR __attribute__((aligned(4)))
 
 #ifdef ENABLE_GDB
-	#define GDB_IRAM_ATTR IRAM_ATTR
+#define GDB_IRAM_ATTR IRAM_ATTR
 #else
-	#define GDB_IRAM_ATTR
+#define GDB_IRAM_ATTR
 #endif
