3cd2379a2d741b3edc6f895a40af4c94c74c4a22
mikee47 <mike@sillyhouse.net>
2019-08-29 22:10:17 +0100

    Place IRAM and FLASH data in separate segments to resolve issues with section conflicts
    Thanks to https://github.com/esp8266/Arduino/pull/5116
    
    Commonly occurs when attempting to inline header methods, generally more of a problem with GCC 4.8.

--- Sming/Arch/Esp8266/Components/esp8266/include/esp_attr.h
+++ Sming/Arch/Esp8266/Components/esp8266/include/esp_attr.h
@@ -1,25 +1,32 @@
 // ESP8266 attribute definitions (previously in c_types.h)
 #pragma once
 
-#define IRAM_ATTR __attribute__((section(".iram.text")))
-#define STORE_TYPEDEF_ATTR __attribute__((aligned(4),packed))
+#define STORE_TYPEDEF_ATTR __attribute__((aligned(4), packed))
 #define STORE_ATTR __attribute__((aligned(4)))
 
 #define DMEM_ATTR __attribute__((section(".bss")))
 #define SHMEM_ATTR
 
-#ifdef ICACHE_FLASH
-#define ICACHE_FLASH_ATTR __attribute__((section(".irom0.text")))
+// The following two macros cause a parameter to be enclosed in quotes
+#define ICACHE_RAM_ATTR __attribute__((section(".iram.text")))
+// by the preopressor (i.e. for concatenating ints to strings)
 #define ICACHE_RODATA_ATTR __attribute__((section(".irom.text")))
-#else
-#define ICACHE_FLASH_ATTR
-#define ICACHE_RODATA_ATTR
-#endif
+#define __ICACHE_STRINGIZE_NX(A) #A
+#define ATTR_STRINGIZE__(A) __ICACHE_STRINGIZE_NX(A)
+#define ICACHE_FLASH_ATTR                                                                                              \
+	__attribute__((                                                                                                    \
+		section("\".irom0.text." __FILE__ "." ATTR_STRINGIZE__(__LINE__) "." ATTR_STRINGIZE__(__COUNTER__) "\"")))
+#define IRAM_ATTR                                                                                                      \
+	__attribute__(                                                                                                     \
+		(section("\".iram.text." __FILE__ "." ATTR_STRINGIZE__(__LINE__) "." ATTR_STRINGIZE__(__COUNTER__) "\"")))
+#define ICACHE_RODATA_ATTR                                                                                             \
+	__attribute__(                                                                                                     \
+		(section("\".irom.text." __FILE__ "." ATTR_STRINGIZE__(__LINE__) "." ATTR_STRINGIZE__(__COUNTER__) "\"")))
 
 #define STORE_ATTR __attribute__((aligned(4)))
 
 #ifdef ENABLE_GDB
-	#define GDB_IRAM_ATTR IRAM_ATTR
+#define GDB_IRAM_ATTR IRAM_ATTR
 #else
-	#define GDB_IRAM_ATTR
+#define GDB_IRAM_ATTR
 #endif
