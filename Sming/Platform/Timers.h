2dabd194ef80a153d6f43e3bf8c0905aec547ee8
mikee47 <mike@sillyhouse.net>
2019-09-11 22:40:55 +0100

    Define Clocks and Polled Timers
    `NanoTime` provides time calculation support and base template classes for Clocks and Time Sources.
    `Platform/Clocks.h` parameterises all clock sources
    
    `PolledTimer.h` provides template class for implementing polled (elapse and timeout) timers
    
    `Platform/Timers.h` defines available timer types
    
    Add implementation of `esp_get_count()' to `esp_clk.h`, together with `ets_get_cpu_frequency` declaration.

---
+++ Sming/Platform/Timers.h
@@ -0,0 +1,59 @@
+/****
+ * Sming Framework Project - Open Source framework for high efficiency native ESP8266 development.
+ * Created 2015 by Skurydin Alexey
+ * http://github.com/SmingHub/Sming
+ * All files of the Sming Core are provided under the LGPL v3 license.
+ *
+ * PolledTimer.h - template class to assist with measurement of elapsed time periods
+ *
+ * @author mikee47 <mike@sillyhouse.net>
+ *
+ * Developed from the excellent esp8266 Arduino project's PolledTimer.h
+ * 	Copyright (c) 2018 Daniel Salazar. All rights reserved.
+ * 	https://github.com/esp8266/Arduino/blob/master/cores/esp8266/PolledTimeout.h
+ *
+ * Generally Sming uses timer callbacks but for some applications a polled timer
+ * is more appropriate, especially if timer intervals are less than few hundred
+ * microseconds, or even in nanoseconds.
+ *
+ ****/
+
+#pragma once
+
+#include <PolledTimer.h>
+#include "Clocks.h"
+
+/** @ingroup polled_timer
+ *  @{
+*/
+
+/*
+ * Elapse timers
+ */
+
+template <NanoTime::Unit unit> using OneShotElapseTimer = PolledTimer::OneShot<Timer2Clock, unit>;
+template <NanoTime::Unit unit> using PeriodicElapseTimer = PolledTimer::Periodic<Timer2Clock, unit>;
+
+using OneShotFastMs = OneShotElapseTimer<NanoTime::Milliseconds>;
+using OneShotFastUs = OneShotElapseTimer<NanoTime::Microseconds>;
+
+using ElapseTimer = OneShotFastUs;
+
+/*
+ * CPU Cycle timers
+ */
+
+template <NanoTime::Unit units> using OneShotCpuCycleTimer = PolledTimer::OneShot<CpuCycleClockNormal, units>;
+template <NanoTime::Unit units> using PeriodicCpuCycleTimer = PolledTimer::Periodic<CpuCycleClockNormal, units>;
+template <NanoTime::Unit units> using OneShotCpuCycleTimerFast = PolledTimer::OneShot<CpuCycleClockFast, units>;
+template <NanoTime::Unit units> using PeriodicCpuCycleTimerFast = PolledTimer::Periodic<CpuCycleClockFast, units>;
+using CpuCycleTimer = OneShotCpuCycleTimer<NanoTime::Nanoseconds>;
+using CpuCycleTimerFast = OneShotCpuCycleTimerFast<NanoTime::Nanoseconds>;
+
+/*
+ * Arduino-compatible types
+ */
+
+typedef OneShotFastMs oneShotFastMs SMING_DEPRECATED;
+typedef OneShotFastUs oneShotFastUs SMING_DEPRECATED;
+/** @} */
