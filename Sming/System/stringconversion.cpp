SmingHub/Sming

commit 1e9e9b0f4c9c83d325c8fbcf593e9e0a650c1b3a

Add support for 64-bit number printing (#1822)
Adds support to `Print` and `String` classes for `long long` and `unsigned long long` numbers using revised `stringconversion` routines.
`Print` class revised to use `stringconversion` routines for consistency with `String`.
Note that Print used its own code for this which produced hex in uppercase, rather than lowercase.

`stringconversion` updates:

* Fix incorrect `width` parameter passing in `ultoa_wp()`
* Move alias functions into header as static inline (removes redundant function calls)
* Eliminate code duplication by calling `ultoa_wp` from `ltoa_wp`
* Avoid un-necessary use of strlen in conversions
* Check `base` parameter and default to base 10 if invalid - this was in Print.cpp but moving it here ensures it's applied consistently
* Add 64-bit string conversion functions for `long long` and `unsigned long long`.

Sming/System/stringconversion.cpp
@@ -14,86 +14,97 @@
 #include "stringconversion.h"
 #include "stringutil.h"
 
-//Since C does not support default func parameters, keep this function as used by framework
-//and create extended _w funct to handle width
-char* ltoa(long val, char* buffer, int base)
-{
-	return ltoa_w(val, buffer, base, 0);
-}
-
-char* itoa (int, char*, int) __attribute__((alias("ltoa")));
-
-char* ltoa_w(long val, char* buffer, int base, int width)
-{
-	return ltoa_wp(val, buffer, base, width, ' ');
-}
 
 char* ltoa_wp(long val, char* buffer, int base, int width, char pad)
 {
-	int i = 38, p = 0;
-	char buf[40] = {0};
-	bool ngt = val < 0;
-	if (ngt) val = -val;
-
-	for(; val && i ; --i, p++, val /= base)
-		buf[i] = hexchar(val % base);
-	if (p == 0) buf[i--] = '0'; // case for zero
-
-	if (ngt)
-		buf[i--] = '-';
-
-	if(width != 0)
-	{
-		width -= strlen(&buf[i+1]);
-		if(width > 0)
-		{
-			memset(buffer, pad, width);
-		}
-		else width = 0;
+	if(val < 0) {
+		*buffer++ = '-';
+		val = -val;
 	}
-
-	strcpy(buffer + width, &buf[i+1]);
-	return buffer;
+	return ultoa_wp((unsigned long)val, buffer, base, width, pad);
 }
 
-//Since C does not support default func parameters, keep this function as used by framework
-//and create extended _w funct to handle width
-char* ultoa(unsigned long val, char* buffer, unsigned int base)
-{
-	return ultoa_w(val, buffer, base, 0);
-}
-
-char* ultoa_w(unsigned long val, char* buffer, unsigned int base, int width)
-{
-	return ultoa_wp(val, buffer, base, 0, ' ');
-}
 char* ultoa_wp(unsigned long val, char* buffer, unsigned int base, int width, char pad)
 {
-	int i = 38, p = 0;
-	char buf[40] = {0};
+	char buf[40];
+	constexpr int nulpos = sizeof(buf) - 1;
+	buf[nulpos] = '\0';
 
-	for(; val && i ; --i, p++, val /= base)
+	// prevent crash if called with base == 1
+	if(base < 2 || base > 16) {
+		base = 10;
+	}
+
+	int i = nulpos - 1;
+	int p = 0;
+	for(; val != 0 && i != 0; --i, p++, val /= base) {
 		buf[i] = hexchar(val % base);
-	if (p == 0) buf[i--] = '0'; // case for zero
+	}
+	if (p == 0) {
+		buf[i--] = '0'; // case for zero
+	}
 
 	if(width != 0)
 	{
-		width -= strlen(&buf[i+1]);
+		width -= (nulpos - i - 1);
 		if(width > 0)
 		{
 			memset(buffer, pad, width);
 		}
-		else width = 0;
+		else {
+			width = 0;
+		}
 	}
-	strcpy(buffer + width, &buf[i+1]);
+	memcpy(buffer + width, &buf[i+1], nulpos - i);
 
 	return buffer;
 }
 
-char *dtostrf(double floatVar, int minStringWidthIncDecimalPoint, int numDigitsAfterDecimal, char *outputBuffer)
+char* lltoa_wp(long long val, char* buffer, int base, int width, char pad)
 {
-	return dtostrf_p(floatVar, minStringWidthIncDecimalPoint, numDigitsAfterDecimal, outputBuffer, ' ');
+	if(val < 0) {
+		*buffer++ = '-';
+		val = -val;
+	}
+	return ulltoa_wp((unsigned long long)val, buffer, base, width, pad);
 }
+
+char* ulltoa_wp(unsigned long long val, char* buffer, unsigned int base, int width, char pad)
+{
+	char buf[80];
+	constexpr int nulpos = sizeof(buf) - 1;
+	buf[nulpos] = '\0';
+
+	// prevent crash if called with base == 1
+	if(base < 2) {
+		base = 10;
+	}
+
+	int i = nulpos - 1;
+	int p = 0;
+	for(; val != 0 && i != 0; --i, p++, val /= base) {
+		buf[i] = hexchar(val % base);
+	}
+	if (p == 0) {
+		buf[i--] = '0'; // case for zero
+	}
+
+	if(width != 0)
+	{
+		width -= (nulpos - i - 1);
+		if(width > 0)
+		{
+			memset(buffer, pad, width);
+		}
+		else {
+			width = 0;
+		}
+	}
+	memcpy(buffer + width, &buf[i+1], nulpos - i);
+
+	return buffer;
+}
+
 // Author zitron: http://forum.arduino.cc/index.php?topic=37391#msg276209
 // modified by ADiea: remove dependencies strcat, floor, round; reorganize+speedup code
 char *dtostrf_p(double floatVar, int minStringWidthIncDecimalPoint, int numDigitsAfterDecimal, char *outputBuffer, char pad)

Sming/System/include/stringconversion.h
@@ -17,20 +17,75 @@
 extern "C" {
 #endif
 
-int atoi(const char *nptr); // Already implemented
+/*
+ * Since C does not support default func parameters, we define inline versions which
+ * translate to ltoa_wp, ultoa_wp, lltoa_wp and ulltoa_wp functions.
+ */
 
 extern char* ltoa_wp(long val, char* buffer, int base, int width, char pad);
-extern char* ltoa_w (long, char*, int, int width);
-extern char* ltoa (long, char*, int);
+
+static inline char* ltoa_w(long val, char* buffer, int base, int width)
+{
+	return ltoa_wp(val, buffer, base, width, ' ');
+}
+
+static inline char* ltoa (long val, char* buffer, int base)
+{
+	return ltoa_wp(val, buffer, base, 0, ' ');
+}
+
+static inline char* itoa (int val, char* buffer, int base)
+{
+	return ltoa_wp(val, buffer, base, 0, ' ');
+}
 
 extern char* ultoa_wp(unsigned long val, char* buffer, unsigned int base, int width, char pad);
-extern char* ultoa_w(unsigned long val, char* buffer, unsigned int base, int width);
-extern char* ultoa(unsigned long val, char* buffer, unsigned int base);
+
+static inline char* ultoa_w(unsigned long val, char* buffer, unsigned int base, int width)
+{
+	return ultoa_wp(val, buffer, base, width, ' ');
+}
+
+static inline char* ultoa(unsigned long val, char* buffer, unsigned int base)
+{
+	return ultoa_wp(val, buffer, base, 0, ' ');
+}
+
+extern char* lltoa_wp(long long val, char* buffer, int base, int width, char pad);
+
+static inline char* lltoa_w(long long val, char* buffer, int base, int width)
+{
+	return lltoa_wp(val, buffer, base, width, ' ');
+}
+
+static inline char* lltoa(long long val, char* buffer, int base)
+{
+	return lltoa_wp(val, buffer, base, 0, ' ');
+}
+
+
+extern char* ulltoa_wp(unsigned long long val, char* buffer, unsigned int base, int width, char pad);
+
+static inline char* ulltoa_w(unsigned long long val, char* buffer, unsigned int base, int width)
+{
+	return ulltoa_wp(val, buffer, base, width, ' ');
+}
+
+static inline char* ulltoa(unsigned long long val, char* buffer, unsigned int base)
+{
+	return ulltoa_wp(val, buffer, base, 0, ' ');
+}
+
 
 extern char* itoa(int, char*, int);
 
 extern char *dtostrf_p(double floatVar, int minStringWidthIncDecimalPoint, int numDigitsAfterDecimal, char *outputBuffer, char pad);
-extern char *dtostrf(double floatVar, int minStringWidthIncDecimalPoint, int numDigitsAfterDecimal, char *outputBuffer);
+
+static inline char *dtostrf(double floatVar, int minStringWidthIncDecimalPoint, int numDigitsAfterDecimal, char *outputBuffer)
+{
+	return dtostrf_p(floatVar, minStringWidthIncDecimalPoint, numDigitsAfterDecimal, outputBuffer, ' ');
+}
+
 long atol(const char *nptr);
 extern long os_strtol(const char* str, char** endptr, int base);
 extern double os_strtod(const char* str, char** endptr);

Sming/Wiring/Print.cpp
@@ -36,166 +36,31 @@ size_t Print::write(const uint8_t *buffer, size_t size)
 }
 
 
-// Base method (character)
-size_t Print::print(char c)
-{
-  return write(c);
-}
-
-// Base method (string)
-size_t Print::print(const char c[])
-{
-  return write(c);
-}
-
-
-// Base method (unsigned)
-size_t Print::print(unsigned long n, int base)
-{
-  if (base == 0) return write(n);
-  else return printNumber(n, base);
-}
-
 // Base method (signed)
-size_t Print::print(long n, int base)
+size_t Print::print(long num, int base)
 {
   if (base == 0)
   {
-    return write(n);
+    return write(num);
   }
-  else if (base == 10)
+
+  if (base == 10 && num < 0)
   {
-    // why must this only be in base 10?
-    if (n < 0)
-    {
-      int t = print('-');
-      n = -n;
-      return printNumber(n, 10) + t;
-    }
-    return printNumber(n, 10);
+    return print('-') + printNumber((unsigned long)-num, base);
   }
-  else
+
+  return printNumber((unsigned long)num, base);
+}
+
+// Overload (signed long long)
+size_t Print::print(const long long& num, int base)
+{
+  if (base == 10 && num < 0)
   {
-    return printNumber(n, base);
+    return print('-') + printNumber((unsigned long long)-num, base);
   }
-}
 
-
-// Overload (unsigned)
-size_t Print::print(unsigned int n, int base)
-{
-  return print((unsigned long)n, base);
-}
-
-// Overload (unsigned)
-size_t Print::print(unsigned char n, int base)
-{
-  return print((unsigned long) n, base);
-}
-
-// Overload (signed)
-size_t Print::print(int n, int base)
-{
-  return print((long)n, base);
-}
-
-
-size_t Print::print(double n, int digits)
-{
-  return printFloat(n, digits);
-}
-
-
-size_t Print::print(const Printable &p)
-{
-  return p.printTo(*this);
-}
-
-size_t Print::print(const String &s)
-{
-  return write(s.c_str(), s.length());
-}
-
-
-size_t Print::println()
-{
-  size_t n = print('\r');
-  n += print('\n');
-  return n;
-}
-
-
-size_t Print::println(const String &s)
-{
-  size_t n = print(s);
-  n += println();
-  return n;
-}
-
-
-size_t Print::println(char c)
-{
-  size_t n = print(c);
-  n += println();
-  return n;
-}
-
-size_t Print::println(const char c[])
-{
-  size_t n = print(c);
-  n += println();
-  return n;
-}
-
-
-size_t Print::println(unsigned long num, int base)
-{
-  size_t n = print(num, base);
-  n += println();
-  return n;
-}
-
-size_t Print::println(unsigned int num, int base)
-{
-  size_t n = print(num, base);
-  n += println();
-  return n;
-}
-
-size_t Print::println(unsigned char b, int base)
-{
-  size_t n = print(b, base);
-  n += println();
-  return n;
-}
-
-size_t Print::println(long num, int base)
-{
-  size_t n = print(num, base);
-  n += println();
-  return n;
-}
-
-size_t Print::println(int num, int base)
-{
-  size_t n = print(num, base);
-  n += println();
-  return n;
-}
-
-size_t Print::println(double num, int digits)
-{
-  size_t n = print(num, digits);
-  n += println();
-  return n;
-}
-
-
-size_t Print::println(const Printable &p)
-{
-  size_t n = print(p);
-  n += println();
-  return n;
+  return printNumber((unsigned long long)num, base);
 }
 
 size_t Print::printf(const char *fmt, ...)
@@ -228,46 +93,18 @@ size_t Print::printf(const char *fmt, ...)
 
 // private methods
 
-size_t Print::printNumber(unsigned long n, uint8_t base)
+size_t Print::printNumber(unsigned long num, uint8_t base)
 {
-  /* BH: new version to be implemented
-    uint8_t buf[sizeof(char) * sizeof(int32_t)];
-    uint32_t i = 0;
+  char buf[8 * sizeof(num) + 1]; // Assumes 8-bit chars plus zero byte.
+  ultoa(num, buf, base);
+  return write(buf);
+}
 
-    if (n == 0)
-    {
-      write('0');
-      return;
-    }
-
-    while (n > 0)
-    {
-      buf[i++] = n % base;
-      n /= base;
-    }
-
-    for (; i > 0; i--)
-      write((buf[i - 1] < 10 ?
-            '0' + buf[i - 1] :
-            'A' + buf[i - 1] - 10));
-  */
-
-  char buf[8 * sizeof(long) + 1]; // Assumes 8-bit chars plus zero byte.
-  char *str = &buf[sizeof(buf) - 1];
-  
-  *str = '\0';
-  
-  // prevent crash if called with base == 1
-  if (base < 2) base = 10;
-  
-  do {
-    unsigned long m = n;
-    n /= base;
-    char c = m - base * n;
-    *--str = c < 10 ? c + '0' : c + 'A' - 10;
-  } while(n);
-  
-  return write(str);
+size_t Print::printNumber(const unsigned long long& num, uint8_t base)
+{
+  char buf[8 * sizeof(num) + 1]; // Assumes 8-bit chars plus zero byte.
+  ulltoa(num, buf, base);
+  return write(buf);
 }
 
 size_t Print::printFloat(double number, uint8_t digits)
@@ -288,8 +125,9 @@ size_t Print::printFloat(double number, uint8_t digits)
   
   // Round correctly so that print(1.999, 2) prints as "2.00"
   double rounding = 0.5;
-  for (uint8_t i=0; i<digits; ++i)
-  rounding /= 10.0;
+  for (uint8_t i=0; i<digits; ++i) {
+	rounding /= 10.0;
+  }
   
   number += rounding;
   

Sming/Wiring/Print.h
@@ -22,10 +22,9 @@
 
 #include "WiringFrameworkDependencies.h"
 #include "Printable.h"
+#include "WString.h"
 
 #define INITIAL_PRINTF_BUFFSIZE 128
-
-class String;
 
 /** @defgroup print Print stream functions
     @brief  Provides functions that format output to streams, e.g. Serial
@@ -90,20 +89,44 @@ class Print
     /** @brief  Prints a single character to output stream
 	  * @retval size_t Quantity of characters written to stream
 	  */
-    size_t print(char);
+    size_t print(char c)
+    {
+      return write(c);
+    }
 
     /** @brief  Prints a c-string to output stream
 	  * @param  char[] c-string to print
 	  * @retval size_t Quantity of characters written to output stream
 	  */
-	size_t print(const char[]);
+	size_t print(const char c[])
+	{
+	  return write(c);
+	}
 
     /** @brief  Prints a number to output stream
 	  * @param  "unsigned long" Number to print
 	  * @param  base The base for output (Default: Decimal (base 10))
 	  * @retval size_t Quantity of characters written to stream
 	  */
-    size_t print(unsigned long, int base = DEC);
+    size_t print(unsigned long num, int base = DEC)
+    {
+      if (base == 0) {
+    	  return write(num);
+      } else {
+    	  return printNumber(num, base);
+      }
+    }
+
+
+    /** @brief  Prints a number to output stream
+	  * @param  "unsigned long long" Number to print
+	  * @param  base The base for output (Default: Decimal (base 10))
+	  * @retval size_t Quantity of characters written to stream
+	  */
+    size_t print(const unsigned long long& num, int base = DEC)
+    {
+    	return printNumber(num, base);
+    }
 
     /** @brief  Prints a number to output stream
 	  * @param  "long" Number to print
@@ -113,115 +136,193 @@ class Print
     size_t print(long, int base = DEC);
 
     /** @brief  Prints a number to output stream
+	  * @param  "long long" Number to print
+	  * @param  base The base for output (Default: Decimal (base 10))
+	  * @retval size_t Quantity of characters written to stream
+	  */
+    size_t print(const long long&, int base = DEC);
+
+    /** @brief  Prints a number to output stream
 	  * @param  "unsigned int" Number to print
 	  * @param  base The base for output (Default: Decimal (base 10))
 	  * @retval size_t Quantity of characters written to stream
 	  */
-    size_t print(unsigned int, int base = DEC);
+    size_t print(unsigned int num, int base = DEC)
+    {
+      return print((unsigned long)num, base);
+    }
 
     /** @brief  Prints a number to output stream
 	  * @param  "unsigned char" Number to print
 	  * @param  base The base for output (Default: Decimal (base 10))
 	  * @retval size_t Quantity of characters written to stream
 	  */
-    size_t print(unsigned char, int base = DEC);
+    size_t print(unsigned char num, int base = DEC)
+    {
+      return print((unsigned long)num, base);
+    }
 
     /** @brief  Prints a number to output stream
 	  * @param  "int" Number to print
 	  * @param  base The base for output (Default: Decimal (base 10))
 	  * @retval size_t Quantity of characters written to stream
 	  */
-    size_t print(int, int base = DEC);
+    size_t print(int num, int base = DEC)
+    {
+      return print((long)num, base);
+    }
 
     /** @brief  Prints a number to output stream
 	  * @param  "double" Number to print
 	  * @param  digits The decimal places to print (Default: 2, e.g. 21.35)
 	  * @retval size_t Quantity of characters written to stream
 	  */
-	size_t print(double, int digits = 2);
+	size_t print(double num, int digits = 2)
+	{
+	  return printFloat(num, digits);
+	}
 
     /** @brief  Prints a Printable object to output stream
 	  * @param  p Object to print
 	  * @retval size_t Quantity of characters written to stream
 	  */
-    size_t print(const Printable &p);
+    size_t print(const Printable &p)
+    {
+      return p.printTo(*this);
+    }
 
     /** @brief  Prints a String to output stream
 	  * @param  s String to print
 	  * @retval size_t Quantity of characters written to stream
 	  */
-    size_t print(const String &s);
+    size_t print(const String &s)
+    {
+      return write(s.c_str(), s.length());
+    }
   
     /** @brief  Prints a newline to output stream
 	  * @retval size_t Quantity of characters written to stream
 	  */
-	size_t println(void);
+	size_t println()
+	{
+	  return print("\r\n");
+	}
 
     /** @brief  Prints a c-string to output stream, appending newline
 	  * @param  "char[]" c-string to print
 	  * @retval size_t Quantity of characters written to stream
 	  */
-    size_t println(const char[]);
+    size_t println(const char c[])
+    {
+      return print(c) + println();
+    }
 
     /** @brief  Prints a single character to output stream, appending newline
 	  * @param  "char" Character to print
 	  * @retval size_t Quantity of characters written to stream
 	  */
-    size_t println(char);
+    size_t println(char c)
+    {
+      return print(c) + println();
+    }
 
     /** @brief  Prints a number to output stream, appending newline
 	  * @param  "unsigned char" Number to print
 	  * @param  base The base for output (Default: Decimal (base 10))
 	  * @retval size_t Quantity of characters written to stream
 	  */
-    size_t println(unsigned char, int base = DEC);
+    size_t println(unsigned char b, int base = DEC)
+    {
+      return print(b, base) + println();
+    }
 
     /** @brief  Prints a number to output stream, appending newline
 	  * @param  "unsigned int" Number to print
 	  * @param  base The base for output (Default: Decimal (base 10))
 	  * @retval size_t Quantity of characters written to stream
 	  */
-    size_t println(unsigned int, int base = DEC);
+    size_t println(unsigned int num, int base = DEC)
+    {
+      return print(num, base) + println();
+    }
 
     /** @brief  Prints a number to output stream, appending newline
 	  * @param  "unsigned long" Number to print
 	  * @param  base The base for output (Default: Decimal (base 10))
 	  * @retval size_t Quantity of characters written to stream
 	  */
-    size_t println(unsigned long, int base = DEC);
+    size_t println(unsigned long num, int base = DEC)
+    {
+      return print(num, base) + println();
+    }
+
+    /** @brief  Prints a number to output stream, appending newline
+	  * @param  "unsigned long long" Number to print
+	  * @param  base The base for output (Default: Decimal (base 10))
+	  * @retval size_t Quantity of characters written to stream
+	  */
+    size_t println(const unsigned long long& num, int base = DEC)
+    {
+      return print(num, base) + println();
+    }
 
     /** @brief  Prints a number to output stream, appending newline
 	  * @param  "int" Number to print
 	  * @param  base The base for output (Default: Decimal (base 10))
 	  * @retval size_t Quantity of characters written to stream
 	  */
-    size_t println(int, int base = DEC);
+    size_t println(int num, int base = DEC)
+    {
+      return print(num, base) + println();
+    }
 
     /** @brief  Prints a number to output stream, appending newline
 	  * @param  "long" Number to print
 	  * @param  base The base for output (Default: Decimal (base 10))
 	  * @retval size_t Quantity of characters written to stream
 	  */
-	size_t println(long, int base = DEC);
+	size_t println(long num, int base = DEC)
+	{
+	  return print(num, base) + println();
+	}
+
+    /** @brief  Prints a number to output stream, appending newline
+	  * @param  "long long" Number to print
+	  * @param  base The base for output (Default: Decimal (base 10))
+	  * @retval size_t Quantity of characters written to stream
+	  */
+	size_t println(const long long& num, int base = DEC)
+	{
+	  return print(num, base) + println();
+	}
 
     /** @brief  Prints a number to output stream, appending newline
 	  * @param  "double" Number to print
 	  * @param  digits The decimal places to print (Default: 2, e.g. 21.35)
 	  * @retval size_t Quantity of characters written to stream
 	  */
-    size_t println(double, int digits = 2);
+    size_t println(double num, int digits = 2)
+    {
+      return print(num, digits) + println();
+    }
 
     /** @brief  Prints a Printable object to output stream, appending newline
 	  * @param  p Object to print
 	  * @retval size_t Quantity of characters written to stream
 	  */
-    size_t println(const Printable &p);
+    size_t println(const Printable &p)
+    {
+      return print(p) + println();
+    }
 
     /** @brief  Prints a String to output stream, appending newline
 	  * @param  s String to print
 	  * @retval size_t Quantity of characters written to stream
 	  */
-    size_t println(const String &s);
+    size_t println(const String &s)
+    {
+      return print(s) + println();
+    }
 
     /** @brief  Prints a formatted c-string to output stream
 	  * @param  fmt Pointer to formated c-string to print
@@ -229,12 +330,13 @@ class Print
 	  * @retval size_t Quantity of characters written to stream
 	  * @note   Use standard printf placeholders, e.g. %d for integer, %s for c-string, etc.
 	  */
-    size_t printf(const char *fmt, ...);
+    size_t printf(const char *fmt, ...) __attribute__ ((format (printf, 2, 3)));
 
   private:
     int write_error = 0;
-    size_t printNumber(unsigned long, uint8_t);
-    size_t printFloat(double, uint8_t);
+    size_t printNumber(unsigned long num, uint8_t base);
+    size_t printNumber(const unsigned long long& num, uint8_t base);
+    size_t printFloat(double num, uint8_t digits);
   protected:
     void setWriteError(int err = 1) { write_error = err; }
 };

Sming/Wiring/WString.cpp
@@ -95,10 +95,24 @@ String::String(long value, unsigned char base)
   *this = buf;
 }
 
+String::String(long long value, unsigned char base)
+{
+  char buf[8 + 8 * sizeof(value)];
+  lltoa(value, buf, base);
+  *this = buf;
+}
+
 String::String(unsigned long value, unsigned char base)
 {
   char buf[8 + 8 * sizeof(value)];
   ultoa(value, buf, base);
+  *this = buf;
+}
+
+String::String(unsigned long long value, unsigned char base)
+{
+  char buf[8 + 8 * sizeof(value)];
+  ulltoa(value, buf, base);
   *this = buf;
 }
 
@@ -327,10 +341,24 @@ bool String::concat(long num)
   return concat(buf, strlen(buf));
 }
 
+bool String::concat(long long num)
+{
+  char buf[8 + 3 * sizeof(num)];
+  lltoa(num, buf, 10);
+  return concat(buf, strlen(buf));
+}
+
 bool String::concat(unsigned long num)
 {
   char buf[8 + 3 * sizeof(num)];
   ultoa(num, buf, 10);
+  return concat(buf, strlen(buf));
+}
+
+bool String::concat(unsigned long long num)
+{
+  char buf[8 + 3 * sizeof(num)];
+  ulltoa(num, buf, 10);
   return concat(buf, strlen(buf));
 }
 

Sming/Wiring/WString.h
@@ -134,7 +134,9 @@ class String
     explicit String(int, unsigned char base = 10);
     explicit String(unsigned int, unsigned char base = 10);
     explicit String(long, unsigned char base = 10);
+    explicit String(long long, unsigned char base = 10);
     explicit String(unsigned long, unsigned char base = 10);
+    explicit String(unsigned long long, unsigned char base = 10);
     explicit String(float, unsigned char decimalPlaces=2);
     explicit String(double, unsigned char decimalPlaces=2);
     ~String(void);
@@ -183,7 +185,9 @@ class String
     bool concat(int num);
     bool concat(unsigned int num);
     bool concat(long num);
+    bool concat(long long num);
     bool concat(unsigned long num);
+    bool concat(unsigned long long num);
     bool concat(float num);
     bool concat(double num);
   
@@ -224,7 +228,17 @@ class String
       concat(num);
       return (*this);
     }
+    String & operator += (long long num)
+    {
+      concat(num);
+      return (*this);
+    }
     String & operator += (unsigned long num)
+    {
+      concat(num);
+      return (*this);
+    }
+    String & operator += (unsigned long long num)
     {
       concat(num);
       return (*this);
@@ -248,6 +262,7 @@ class String
     friend StringSumHelper & operator + (const StringSumHelper &lhs, unsigned int num);
     friend StringSumHelper & operator + (const StringSumHelper &lhs, long num);
     friend StringSumHelper & operator + (const StringSumHelper &lhs, unsigned long num);
+    friend StringSumHelper & operator + (const StringSumHelper &lhs, unsigned long long num);
     friend StringSumHelper & operator + (const StringSumHelper &lhs, float num);
     friend StringSumHelper & operator + (const StringSumHelper &lhs, double num);
 
@@ -375,7 +390,9 @@ class StringSumHelper : public String
     StringSumHelper(int num) : String(num) {}
     StringSumHelper(unsigned int num) : String(num) {}
     StringSumHelper(long num) : String(num) {}
+    StringSumHelper(long long num) : String(num) {}
     StringSumHelper(unsigned long num) : String(num) {}
+    StringSumHelper(unsigned long long num) : String(num) {}
     StringSumHelper(float num) : String(num) {}
     StringSumHelper(double num) : String(num) {}
 };

commit fac183c620e2d3216df14ec5f64babc65954d360

bugfix: Return value from `ltoa_wp` and `lltoa_wp` incorrect for negative values
Introduced in #1822

Sming/System/stringconversion.cpp
@@ -17,11 +17,13 @@
 
 char* ltoa_wp(long val, char* buffer, int base, int width, char pad)
 {
+	char* buf_ptr = buffer;
 	if(val < 0) {
-		*buffer++ = '-';
+		*buf_ptr++ = '-';
 		val = -val;
 	}
-	return ultoa_wp((unsigned long)val, buffer, base, width, pad);
+	ultoa_wp((unsigned long)val, buf_ptr, base, width, pad);
+	return buffer;
 }
 
 char* ultoa_wp(unsigned long val, char* buffer, unsigned int base, int width, char pad)
@@ -62,11 +64,13 @@ char* ultoa_wp(unsigned long val, char* buffer, unsigned int base, int width, ch
 
 char* lltoa_wp(long long val, char* buffer, int base, int width, char pad)
 {
+	char* buf_ptr = buffer;
 	if(val < 0) {
-		*buffer++ = '-';
+		*buf_ptr++ = '-';
 		val = -val;
 	}
-	return ulltoa_wp((unsigned long long)val, buffer, base, width, pad);
+	ulltoa_wp((unsigned long long)val, buf_ptr, base, width, pad);
+	return buffer;
 }
 
 char* ulltoa_wp(unsigned long long val, char* buffer, unsigned int base, int width, char pad)