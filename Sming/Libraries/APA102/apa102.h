04b47beccbdd7afb2bf0afeed2377899ee1ab193
mikee47 <mike@sillyhouse.net>
2019-09-30 12:05:26 +0100

    Refactor and simplify APA102 library.

--- Sming/Libraries/APA102/apa102.h
+++ Sming/Libraries/APA102/apa102.h
@@ -6,67 +6,87 @@
  * APA102 library by HappyCodingRobot@github.com
  ****/
 
-#ifndef _APA102_H_
-#define _APA102_H_
+#pragma once
 
 #include <SPIBase.h>
 #include <SPISettings.h>
 
-typedef struct {
-    //uint8_t br;
+struct col_t {
+    uint8_t br;
     uint8_t r;
     uint8_t g;
     uint8_t b;
-} col_t;
+};
 
 
 class APA102 {
 public:
     APA102(uint16_t n);
-    APA102(uint16_t n, SPIBase & spiRef);
-    ~APA102(void);
+    APA102(uint16_t n, SPIBase& spiRef);
 
-    void begin(void);
-    void begin(SPISettings & mySettings);
-    void end(void);
-    
+    ~APA102()
+    {
+    	delete LEDbuffer;
+    }
+
+    void begin();
+    void begin(SPISettings& mySettings);
+    void end();
+
     /* send data buffer to LEDs, including start & stop sequences */
-    void show(void);
-    void show(int16_t SPos);
-    
+    void show();
+    void show(uint16_t startPos);
+
     /* clear data buffer */
-    void clear(void);
-    
+    void clear();
+
     /* set pixel color */
     void setPixel(uint16_t n, uint8_t r, uint8_t g, uint8_t b);
     void setPixel(uint16_t n, uint8_t r, uint8_t g, uint8_t b, uint8_t br);
-    void setPixel(uint16_t n, col_t* p);
+
+    void setPixel(uint16_t n, const col_t& col);
+
+    void setPixel(uint16_t n, const col_t* col)
+    {
+    	if(col != nullptr) {
+    		setPixel(n, *col);
+    	}
+    }
+
     void setAllPixel(uint8_t r, uint8_t g, uint8_t b);
-    //void LEDsetAllPixel(uint8_t r, uint8_t g, uint8_t b, uint8_t br);
-    void setAllPixel(col_t*);
-    
+
+    void setAllPixel(const col_t& col);
+
+    void setAllPixel(const col_t* col) {
+    	if(col != nullptr) {
+    		setAllPixel(*col);
+    	}
+    }
+
     /* set global LED brightness level */
-    void setBrightness(uint8_t);
+    void setBrightness(uint8_t br);
+
     /* get global LED brightness level */
-    uint8_t getBrightness(void);
-    
-    
+    uint8_t getBrightness() const
+    {
+    	return brightness;
+    }
+
+
     /* send start sequence */
-    void sendStart(void);
+    void sendStart();
     /* send stop sequence */
-    void sendStop(void);
+    void sendStop();
     /* direct write single LED data */
     void directWrite(uint8_t r, uint8_t g, uint8_t b, uint8_t br);
 
 protected:
-    uint16_t numLEDs;
-    uint8_t brightness;                 // global brightness 0..31 -> 0..100%
-    uint8_t *LEDbuffer;
-    
-    SPISettings SPI_APA_Settings = SPISettings(4000000, MSBFIRST, SPI_MODE3);
-    SPIBase & pSPI;
+    uint16_t numLEDs = 0;
+    uint8_t brightness = 0;                 // global brightness 0..31 -> 0..100%
+    col_t *LEDbuffer = nullptr;
+
+    SPISettings SPI_APA_Settings;
+    SPIBase& pSPI;
 
 private:
 };
-
-#endif /* _APA102_H_ */
