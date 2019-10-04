30c70b9f7833ea288c2247a7ec289eb8ef5ff085
mikee47 <mike@sillyhouse.net>
2019-10-04 22:11:16 +0100

    Expand sample to demonstrate four channels.

--- samples/Basic_Servo/app/application.cpp
+++ samples/Basic_Servo/app/application.cpp
@@ -5,55 +5,127 @@
 
 #include <SmingCore.h>
 #include <Libraries/Servo/ServoChannel.h>
+#include <Libraries/Servo/Servo.h>
 
-#define noPins 1
-#define SERVO_PIN 2 // GPIO2
+// Un-comment to use raw time values instead of degrees for servo positioning
+//#define UPDATE_RAW
 
-ServoChannel* channel;
+#define UPDATE_INTERVAL 500
 
-Timer procTimer;
-TimerDelegate procDelegate;
+// Enable to show current frame timing
+//#define SHOW_FRAME_TIMING
 
-uint16 centerdelay = 0;
-uint32 value = 0;
-int degree = 0;
-
-void calcValue()
+class MyServoChannel : public ServoChannel
 {
-	// wait some time at middle of the servo range
+public:
+	bool begin(uint8_t pin, unsigned intervalMs)
+	{
+		if(!attach(pin)) {
+			return false; // Out of channels
+		}
+		return timer.initializeMs(intervalMs, TimerDelegate(&MyServoChannel::calcValue, this)).start();
+	}
+
+protected:
+	void calcValue();
+
+private:
+	Timer timer;
+	uint16 centerdelay = 0;
+	uint32 value = 0;
+	int degree = 0;
+};
+
+static MyServoChannel channels[4];
+
+void MyServoChannel::calcValue()
+{
+	const char indent[] = "                    ";
+
+	auto pin = getPin();
+	Serial.write(indent, pin);
+	Serial.print("GPIO");
+	Serial.print(pin);
+
+#ifdef UPDATE_RAW
+
+	// Wait some time at middle of the servo range
 	if(value == 1000) {
 		centerdelay--;
-		if(centerdelay == 0)
-			value += 10;  // restart after waiting
+		if(centerdelay == 0) {
+			value += 10; // restart after waiting
+		}
 	} else {			  // linear ramp by increasing the value in steps
 		centerdelay = 50; // delay 50 times 200ms = 10s
 		value += 10;
 	}
 
-	if(value >= 2000)
+	if(value >= 2000) {
 		value = 0; // overflow and restart linear ramp
+	}
 
-	Serial.print(" Value: ");
+	Serial.print(" value = ");
 	Serial.print(value);
-	Serial.println();
 
-#ifdef raw
-	channel->setValue(value); // set all values
+	if(!setValue(value)) {
+		Serial.print(": setValue(");
+		Serial.print(value);
+		Serial.print(") failed!");
+	}
+
 #else
-	if(degree++ > 90)
+
+	if(degree == 90) {
 		degree = -90;
-	channel->setDegree(degree);
+	} else {
+		++degree;
+	}
+
+	Serial.print(" degree = ");
+	Serial.print(degree);
+
+	if(!setDegree(degree)) {
+		Serial.print(": setDegree(");
+		Serial.print(degree);
+		Serial.print(") failed!");
+	}
+
 #endif
+
+#ifdef SHOW_FRAME_TIMING
+	// List active frame details
+	auto& frame = servo.getActiveFrame();
+	String buf = ": ";
+	buf.reserve(256);
+	for(unsigned i = 0; i < frame.slotCount; ++i) {
+		if(i > 0) {
+			buf += ", ";
+		}
+		buf += frame.slots[i];
+	}
+	Serial.print(buf);
+#endif
+
+	Serial.println();
 }
 
 void init()
 {
+	Serial.begin(SERIAL_BAUD_RATE);
+	Serial.systemDebugOutput(true);
+	Serial.setTxWait(false);
+	Serial.setTxBufferSize(1024);
+
 	Serial.println("Init Basic Servo Sample");
 	System.setCpuFrequency(eCF_80MHz);
 
-	channel = new(ServoChannel);
-	channel->attach(SERVO_PIN);
+#ifdef ARCH_HOST
+	setDigitalHooks(nullptr);
+#endif
 
-	procDelegate = calcValue;
-	procTimer.initializeMs(2000, procDelegate).start();
+	// Set up two channels, one updating slowly the other more quickly
+	channels[0].begin(2, UPDATE_INTERVAL * 1.3);
+	channels[1].begin(4, UPDATE_INTERVAL);
+	channels[2].begin(5, UPDATE_INTERVAL * 0.5);
+	channels[3].begin(0, UPDATE_INTERVAL * 0.25);
 }
