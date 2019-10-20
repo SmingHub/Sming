SmingHub/Sming

commit 72632cf09b7b8f68ffcd0b9226170761d90f9fd7

Makefile updates (EXTRA_LDFLAGS, dos2unix) (#1744)
* Add `EXTRA_LDFLAGS` for Components to use

Move linker flags into their appropriate Components

* Suppress rboot build output

* Add `dos2unix` target

* SMING_TARGET_OPTIONS no longer used, and no longer required

* Build run script with separate target

Sming/Arch/Esp8266/Components/esp8266/component.mk
@@ -36,3 +36,9 @@ COMPONENT_INCDIRS		:= include $(SDK_INCDIR)
 export SDK_INTERNAL
 export SDK_LIBDIR
 export SDK_INCDIR
+
+# Required to ensure user-provided function gets linked instead of our 'weak' one
+COMPONENT_LDFLAGS += -u custom_crash_callback
+
+# Crash handler hooks this so debugger can be invoked
+COMPONENT_LDFLAGS += -Wl,-wrap,system_restart_local 

Sming/Arch/Esp8266/Components/esp_wifi/component.mk
@@ -6,3 +6,11 @@ ifeq ($(ENABLE_WPS), 1)
    GLOBAL_CFLAGS	+= -DENABLE_WPS=1
    EXTRA_LIBS		:= wps
 endif
+
+EXTRA_LIBS := \
+	phy \
+	pp \
+	net80211 \
+	wpa \
+	crypto \
+	smartconfig

Sming/Arch/Esp8266/Components/rboot/component.mk
@@ -96,7 +96,7 @@ ifeq ($(RBOOT_GPIO_SKIP_ENABLED),1)
 endif
 
 $(RBOOT_BIN):
-	$(MAKE) -C $(ARCH_COMPONENTS)/rboot/rboot
+	$(Q) $(MAKE) -C $(ARCH_COMPONENTS)/rboot/rboot
 
 
 # rBoot big flash support requires a slightly modified version of libmain (just one symbol gets weakened)
@@ -104,6 +104,7 @@ $(RBOOT_BIN):
 LIBMAIN_RBOOT			:= $(LIBMAIN)-rboot
 LIBMAIN_RBOOT_DST		:= $(APP_LIBDIR)/lib$(LIBMAIN_RBOOT).a
 CUSTOM_TARGETS			+= $(LIBMAIN_RBOOT_DST)
+COMPONENT_LDFLAGS		:= -u Cache_Read_Enable_New
 
 $(LIBMAIN_RBOOT_DST): $(LIBMAIN_SRC)
 	@echo "OC $@"

.appveyor/build.cmd
@@ -28,7 +28,6 @@ if "%SMING_ARCH%" == "Host" (
 	%MAKE_PARALLEL% Basic_Serial Basic_ProgMem STRICT=1 V=1 || goto :error
 
 	REM Run basic tests
-	set SMING_TARGET_OPTIONS="--flashfile=$(FLASH_BIN) --flashsize=$(SPI_SIZE)"
 	%MAKE_PARALLEL% tests || goto :error
 
 ) else (

Sming/Arch/Esp8266/app.mk
@@ -10,23 +10,13 @@ LIBS += \
 	microgcc \
 	stdc++ \
 	hal \
-	phy \
-	pp \
-	net80211 \
-	wpa \
-	crypto \
-	smartconfig \
 	$(LIBMAIN)
 
 # linker flags used to generate the main object file
-LDFLAGS		= -nostdlib \
+LDFLAGS	+= \
+	-nostdlib \
 	-u call_user_start \
-	-u Cache_Read_Enable_New \
-	-u custom_crash_callback \
 	-Wl,-static \
-	-Wl,--gc-sections \
-	-Wl,-Map=$(basename $@).map \
-	-Wl,-wrap,system_restart_local 
 
 
 .PHONY: application

Sming/Arch/Host/app.mk
@@ -5,10 +5,9 @@
 ###
 
 # linker flags used to generate the main object file
-LDFLAGS = \
-	-m32 \
-	-Wl,--gc-sections \
-	-Wl,-Map=$(basename $@).map
+LDFLAGS += \
+	-m32
+
 
 # Executable
 TARGET_OUT_0			:= $(FW_BASE)/$(APP_NAME)$(TOOL_EXT)
@@ -36,12 +35,15 @@ endif
 RUN_SCRIPT := $(FW_BASE)/run.sh
 
 .PHONY: run
-run: all ##Run the application image
-	$(Q) echo > $(RUN_SCRIPT); \
-	$(foreach id,$(ENABLE_HOST_UARTID),echo '$(call RunHostTerminal,$(id))' >> $(RUN_SCRIPT);) \
-	echo '$(TARGET_OUT_0) $(CLI_TARGET_OPTIONS)' >> $(RUN_SCRIPT); \
-	chmod +x $(RUN_SCRIPT); \
-	$(RUN_SCRIPT)
+run: all $(RUN_SCRIPT) ##Run the application image
+	$(Q) $(RUN_SCRIPT)
+
+$(RUN_SCRIPT)::
+	$(Q) echo '#!/bin/bash' > $@; \
+	$(foreach id,$(ENABLE_HOST_UARTID),echo '$(call RunHostTerminal,$(id))' >> $@;) \
+	echo '$(TARGET_OUT_0) $(CLI_TARGET_OPTIONS)' >> $@; \
+	chmod a+x $@
+
 
 .PHONY: flashfs
 flashfs: $(SPIFF_BIN_OUT) ##Write just the SPIFFS filesystem image

Sming/Makefile
@@ -140,6 +140,14 @@ cs: ##Apply coding style to all core files
 		clang-format -i -style=file $$FILE; \
 	done
 
+ALL_FILES = $(filter %.cpp %.h %.c %.mk %.sh %.cmd,$(shell $(GIT) ls-files))
+
+.PHONY: dos2unix
+dos2unix: ##Make sure all our files use Unix line endings
+	$(Q) dos2unix $(ALL_FILES)
+	@echo "$(words $(ALL_FILES)) files checked"
+
+
 ##@Help
 
 .PHONY: help

commit ce4ddeec53d74e462c9e06d27a4e920001099e0c

Fix bug where ROM2 fails to boot
Should be `EXTRA_LDFLAGS`, not `COMPONENT_LDFLAGS` (which doesn't exist).

The `Cache_Read_Enable_New()` library function is replaced to set the memory mapped address region,
so as this didn't happen it attempted to execute invalid code from the normal address space at 0x2000 (instead of0x202000).

Also corrected in the esp8266 component, which affects the crash handler.

Bug introduced in #1744.

Sming/Arch/Esp8266/Components/esp8266/component.mk
@@ -37,8 +37,5 @@ export SDK_INTERNAL
 export SDK_LIBDIR
 export SDK_INCDIR
 
-# Required to ensure user-provided function gets linked instead of our 'weak' one
-COMPONENT_LDFLAGS += -u custom_crash_callback
-
 # Crash handler hooks this so debugger can be invoked
-COMPONENT_LDFLAGS += -Wl,-wrap,system_restart_local 
+EXTRA_LDFLAGS += -Wl,-wrap,system_restart_local 

Sming/Components/rboot/component.mk
@@ -114,7 +114,7 @@ $(RBOOT_BIN):
 LIBMAIN_RBOOT			:= $(LIBMAIN)-rboot
 LIBMAIN_RBOOT_DST		:= $(APP_LIBDIR)/lib$(LIBMAIN_RBOOT).a
 CUSTOM_TARGETS			+= $(LIBMAIN_RBOOT_DST)
-COMPONENT_LDFLAGS		:= -u Cache_Read_Enable_New
+EXTRA_LDFLAGS			:= -u Cache_Read_Enable_New
 
 $(LIBMAIN_RBOOT_DST): $(LIBMAIN_SRC)
 	@echo "OC $@"