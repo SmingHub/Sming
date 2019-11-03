SmingHub/Sming

commit 40168cb62208efce017d0e56424a3d4b3f73ec3e

Fix some Coverity advisories (#1913)
[scan:coverity]

Sming/Arch/Host/Components/hostlib/hostlib.c
@@ -31,7 +31,7 @@ int msleep(unsigned ms)
 
 void getHostAppDir(char* path, size_t bufSize)
 {
-	size_t len __attribute__((unused));
+	size_t len;
 	char sep;
 #ifdef __WIN32
 	len = GetModuleFileName(NULL, path, bufSize);
@@ -40,6 +40,7 @@ void getHostAppDir(char* path, size_t bufSize)
 	len = readlink("/proc/self/exe", path, bufSize);
 	sep = '/';
 #endif
+	path[len] = '\0';
 	char* p = strrchr(path, sep);
 	if(p) {
 		p[1] = '\0';

Sming/Arch/Host/Components/spi_flash/flashmem.cpp
@@ -39,10 +39,10 @@ static const char* defaultFlashFileName = "flash.bin";
 bool host_flashmem_init(FlashmemConfig& config)
 {
 	if(config.filename != nullptr) {
-		strcpy(flashFileName, config.filename);
+		strncpy(flashFileName, config.filename, sizeof(flashFileName));
 	} else {
 		getHostAppDir(flashFileName, sizeof(flashFileName));
-		strcat(flashFileName, defaultFlashFileName);
+		strcpy(flashFileName, defaultFlashFileName);
 		config.filename = flashFileName;
 	}
 

Sming/Arch/Esp8266/Components/driver/SerialBuffer.h
@@ -23,7 +23,7 @@ struct SerialBuffer {
 public:
 	~SerialBuffer()
 	{
-		delete buffer;
+		delete[] buffer;
 	}
 
 	size_t getSize()

Sming/Arch/Host/Components/driver/uart.cpp
@@ -111,7 +111,7 @@ static bool realloc_buffer(SerialBuffer*& buffer, size_t new_size)
 {
 	if(buffer != nullptr) {
 		if(new_size == 0) {
-			uart_disable_interrupts();
+			(void)uart_disable_interrupts();
 			delete buffer;
 			buffer = nullptr;
 			uart_restore_interrupts();
@@ -207,7 +207,7 @@ size_t uart_rx_available(uart_t* uart)
 		return 0;
 	}
 
-	uart_disable_interrupts();
+	(void)uart_disable_interrupts();
 
 	size_t avail = 0;
 
@@ -260,7 +260,7 @@ size_t uart_tx_free(uart_t* uart)
 		return 0;
 	}
 
-	uart_disable_interrupts();
+	(void)uart_disable_interrupts();
 
 	size_t space = 0;
 	if(uart->tx_buffer != nullptr) {
@@ -324,7 +324,7 @@ void uart_flush(uart_t* uart, uart_mode_t mode)
 	bool flushRx = mode != UART_TX_ONLY && uart->mode != UART_TX_ONLY;
 	bool flushTx = mode != UART_RX_ONLY && uart->mode != UART_RX_ONLY;
 
-	uart_disable_interrupts();
+	(void)uart_disable_interrupts();
 	if(flushRx && uart->rx_buffer != nullptr) {
 		uart->rx_buffer->clear();
 	}

Sming/Arch/Host/Components/hostlib/keyb.cpp
@@ -233,7 +233,7 @@ void keyb_restore()
 		attr.c_lflag |= ICANON | ECHO;
 		tcsetattr(STDIN_FILENO, TCSANOW, &attr);
 
-		fcntl(0, F_SETFL, g_orig_flags);
+		(void)fcntl(0, F_SETFL, g_orig_flags);
 		g_values_changed = false;
 	}
 #endif
@@ -250,7 +250,7 @@ void keyb_raw()
 	}
 
 	// make stdin non-blocking
-	fcntl(0, F_SETFL, g_orig_flags | O_NONBLOCK);
+	(void)fcntl(0, F_SETFL, g_orig_flags | O_NONBLOCK);
 	//
 	struct termios attr = g_orig_attr;
 	attr.c_lflag &= ~(ICANON | ECHO);

Sming/Arch/Host/Components/hostlib/sockets.cpp
@@ -441,7 +441,7 @@ CSocket* CServerSocket::try_connect()
 	struct sockaddr sa;
 	socklen_t len = sizeof(sa);
 	int fd = ::accept(m_fd, &sa, &len);
-	if(fd <= 0) {
+	if(fd < 0) {
 		return nullptr;
 	}
 

Sming/Arch/Host/Components/hostlib/startup.cpp
@@ -94,7 +94,7 @@ static void pause(int secs)
 {
 	if(secs == 0) {
 		hostmsg("Hit ENTER to continue.");
-		getchar();
+		(void)getchar();
 	} else if(secs > 0) {
 		hostmsg("Waiting for %u seconds...", secs);
 		msleep(secs * 1000);

Sming/Arch/Host/Components/lwip/.patches/lwip.patch
@@ -1,8 +1,22 @@
 diff --git a/contrib/ports/unix/port/sys_arch.c b/contrib/ports/unix/port/sys_arch.c
-index a56d7de1..dd3f16e1 100644
+index a56d7de1..6fc9f434 100644
 --- a/contrib/ports/unix/port/sys_arch.c
 +++ b/contrib/ports/unix/port/sys_arch.c
-@@ -730,13 +730,3 @@ sys_arch_unprotect(sys_prot_t pval)
+@@ -68,10 +68,12 @@
+ #include "lwip/stats.h"
+ #include "lwip/tcpip.h"
+ 
++extern u32_t os_random(void);
++
+ u32_t
+ lwip_port_rand(void)
+ {
+-  return (u32_t)rand();
++  return os_random();
+ }
+ 
+ static void
+@@ -730,13 +732,3 @@ sys_arch_unprotect(sys_prot_t pval)
  }
  #endif /* SYS_LIGHTWEIGHT_PROT */
  

commit e7afb0f1d806d3d2fc3736571798c1a099add853

Fix strncpy + flash.bin bug (#1935)
* Fix strncpy

* Fix `getHostAppDir()`, broken in #1913.

Sming/Arch/Host/Components/hostlib/hostlib.c
@@ -29,16 +29,16 @@ int msleep(unsigned ms)
 	return nanosleep(&req, &rem);
 }
 
-void getHostAppDir(char* path, size_t bufSize)
+size_t getHostAppDir(char* path, size_t bufSize)
 {
 	if(path == NULL ||bufSize == 0) {
-		return;
+		return 0;
 	}
 
 	size_t len;
 	char sep;
 #ifdef __WIN32
-	len = GetModuleFileName(NULL, path, bufSize - 1);
+	len = GetModuleFileName(NULL, path, bufSize);
 	sep = '\\';
 #else
 	len = readlink("/proc/self/exe", path, bufSize - 1);
@@ -46,7 +46,9 @@ void getHostAppDir(char* path, size_t bufSize)
 #endif
 	path[len] = '\0';
 	char* p = strrchr(path, sep);
-	if(p) {
+	if(p != NULL) {
 		p[1] = '\0';
+		len = 1 + p - path;
 	}
+	return len;
 }

Sming/Arch/Host/Components/hostlib/hostlib.h
@@ -55,8 +55,13 @@ extern "C" {
 
 int msleep(unsigned ms);
 
-// Include trailing path separator
-void getHostAppDir(char* path, size_t bufSize);
+/**
+ * @brief Get directory where application is executing from
+ * @param path Receives directory path, including trailing path separator
+ * @param bufSize
+ * @retval size_t Number of characters written, excluding NUL
+ */
+size_t getHostAppDir(char* path, size_t bufSize);
 
 #ifdef __cplusplus
 }

Sming/Arch/Host/Components/spi_flash/flashmem.cpp
@@ -24,7 +24,7 @@
 static int flashFile = -1;
 static size_t flashFileSize = 0x400000U;
 static char flashFileName[256];
-static const char* defaultFlashFileName = "flash.bin";
+static const char defaultFlashFileName[] = "flash.bin";
 
 #define SPI_FLASH_SEC_SIZE 4096
 
@@ -42,8 +42,11 @@ bool host_flashmem_init(FlashmemConfig& config)
 		strncpy(flashFileName, config.filename, sizeof(flashFileName));
 		flashFileName[sizeof(flashFileName) - 1] = '\0';
 	} else {
-		getHostAppDir(flashFileName, sizeof(flashFileName));
-		strcpy(flashFileName, defaultFlashFileName);
+		size_t len = getHostAppDir(flashFileName, sizeof(flashFileName));
+		if(len + sizeof(defaultFlashFileName) > sizeof(flashFileName)) {
+			return false;
+		}
+		memcpy(&flashFileName[len], defaultFlashFileName, sizeof(defaultFlashFileName));
 		config.filename = flashFileName;
 	}
 

Sming/Components/spiffs/spiffs.patch
@@ -70,3 +70,23 @@ index 235aaaa..4df4b4e 100644
    (void)fh;
    spiffs_page_object_ix_header objix_hdr;
 
+diff --git a/src/spiffs_nucleus.c b/src/spiffs_nucleus.c
+index f811d93..ff9db29 100644
+--- a/src/spiffs_nucleus.c
++++ b/src/spiffs_nucleus.c
+@@ -945,6 +945,7 @@ s32_t spiffs_object_create(
+   oix_hdr.type = type;
+   oix_hdr.size = SPIFFS_UNDEFINED_LEN; // keep ones so we can update later without wasting this page
+   strncpy((char*)oix_hdr.name, (const char*)name, SPIFFS_OBJ_NAME_LEN);
++  oix_hdr.name[SPIFFS_OBJ_NAME_LEN - 1] = '\0';
+ #if SPIFFS_OBJ_META_LEN
+   if (meta) {
+     _SPIFFS_MEMCPY(oix_hdr.meta, meta, SPIFFS_OBJ_META_LEN);
+@@ -1008,6 +1009,7 @@ s32_t spiffs_object_update_index_hdr(
+   // change name
+   if (name) {
+     strncpy((char*)objix_hdr->name, (const char*)name, SPIFFS_OBJ_NAME_LEN);
++    objix_hdr->name[SPIFFS_OBJ_NAME_LEN - 1] = '\0';
+   }
+ #if SPIFFS_OBJ_META_LEN
+   if (meta) {