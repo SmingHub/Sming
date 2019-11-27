abbd56c2f8d8d183edc0571ec7a372cda7768ccc
mikee47 <mike@sillyhouse.net>
2019-11-27 08:23:59 +0000

    Change SPI flash defaults
    A recurring problem is mis-configured flash. The defaults aren't very helpful,
    and neither is the reported error message from SDK 3.
    
    SDK 3.0.1 produces partition error for SPI_SIZE < 1M so add note.
    Change defaults for SPI_SIZE=1M (from 512K) and SPI_MODE=dio (from qio)
    
    I've assumed that if flash chip supports QIO mode then it would also support DIO.

--- Sming/Arch/Esp8266/Components/esp8266/startup.cpp
+++ Sming/Arch/Esp8266/Components/esp8266/startup.cpp
@@ -133,6 +133,7 @@ extern "C" void ICACHE_FLASH_ATTR WEAK_ATTR user_pre_init(void)
 			auto& part = partitions[i];
 			os_printf("partition[%u]: %u, 0x%08x, 0x%08x\n", i, part.type, part.addr, part.size);
 		}
+		os_printf("** Note: SDK 3.0.1 requires SPI_SIZE >= 1M\n");
 		while(1) {
 			// Cannot proceed
 		};
