703f8ab070cda3bf189acbfda762c81eb90622dc
mikee47 <mike@sillyhouse.net>
2019-12-08 13:22:29 +0000

    Use `umoddi3` and `udivdi3` from ROM.
    These entries were copied from Arduino, but they're unsafe.
    
    Ostensibly, `modsi3` falls into the same category but it breaks the `RingTonePlayer` sample
    since we do some complex stuff inside an interrupt service routine. As these are replacing
    intrinsics or inline code they shouldn't be in flash.
    
    `umoddi3` and `udivdi3` are available in ROM so have stripped these two from libgcc.a
    and put it back into `newlib`.
    
    x

--- Sming/Arch/Esp8266/Components/libc/component.mk
+++ Sming/Arch/Esp8266/Components/libc/component.mk
@@ -4,6 +4,7 @@ COMPONENT_SRCFILES		:= libc.c
 
 ifeq ($(USE_NEWLIB),1)
 COMPONENT_SRCFILES		+= libc_replacements.c
+LIBDIRS					+= $(COMPONENT_PATH)/newlib
 EXTRA_LIBS				+= m c gcc
 else
 COMPONENT_SRCFILES		+= pgmspace.c
