ae6129c9b4efb59c4dfc95ae5f7144b4cfc7e35d
mikee47 <mike@sillyhouse.net>
2019-12-08 08:58:34 +0000

    Add tests to confirm ROM versions of standard functions are linked

--- tests/HostTests/app/test-libc.cpp
+++ tests/HostTests/app/test-libc.cpp
@@ -19,6 +19,9 @@ A a2 __attribute__((init_priority(1000)));
 A a1 __attribute__((init_priority(990)));
 } // namespace libc_initorder
 
+// Simple check to determine if a pointer refers to ROM
+#define isRomPtr(ptr) (uint32_t(ptr) >= 0x40000000 && uint32_t(ptr) < 0x40100000)
+
 /*
  * Anything to do with main C/C++ libraries, startup code, etc.
  */
@@ -40,6 +43,21 @@ public:
 			REQUIRE(a3.order == 3);
 			REQUIRE(a4.order == 4);
 		}
+
+#ifdef ARCH_ESP8266
+		TEST_CASE("functions in ROM")
+		{
+			REQUIRE(isRomPtr(memcpy));
+			REQUIRE(isRomPtr(memmove));
+			REQUIRE(isRomPtr(memset));
+			REQUIRE(isRomPtr(memcmp));
+			REQUIRE(isRomPtr(strlen));
+			REQUIRE(isRomPtr(strcmp));
+			REQUIRE(isRomPtr(strncmp));
+			REQUIRE(isRomPtr(strstr));
+			REQUIRE(isRomPtr(bzero));
+		}
+#endif
 	}
 };
 
