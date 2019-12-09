51614062031d85f9b9964ba499b582ced9d69662
Kaloyan Mihaylov <kaloyan.mihaylov@gmail.com>
2019-12-09 14:46:02 +0200

    Make mbMaster static

--- samples/ModbusMaster/app/application.cpp
+++ samples/ModbusMaster/app/application.cpp
@@ -1,7 +1,7 @@
 #include <SmingCore.h>
 #include <Debug.h>
 #include "application.h"
-#include "ModbusMaster.h"
+#include <ModbusMaster.h>
 
 #define RS485_RE_PIN 15
 #define MB_SLAVE_ADDR 1
@@ -9,7 +9,7 @@
 
 Timer mbLoopTimer;
 
-ModbusMaster *mbMaster;
+ModbusMaster mbMaster;
 HardwareSerial Serial1(UART1);
 
 uint16_t globalSeconds = 0;
@@ -19,28 +19,28 @@ void mbLoop() {
 
 	uint8_t numberOfRegistersToWrite = 1;
 	uint8_t bufferPosition = 0;
-	mbMaster->begin(MB_SLAVE_ADDR, Serial);
-	mbMaster->setTransmitBuffer(bufferPosition, globalSeconds);
-	mbMaster->writeMultipleRegisters(SLAVE_REG_ADDR, numberOfRegistersToWrite);
+	mbMaster.begin(MB_SLAVE_ADDR, Serial);
+	mbMaster.setTransmitBuffer(bufferPosition, globalSeconds);
+	mbMaster.writeMultipleRegisters(SLAVE_REG_ADDR, numberOfRegistersToWrite);
 
 
 	uint8_t nrOfRegistersToRead = 1;
-	uint8_t mbResult = mbMaster->readHoldingRegisters(SLAVE_REG_ADDR, nrOfRegistersToRead); //see also readInputRegisters
+	uint8_t mbResult = mbMaster.readHoldingRegisters(SLAVE_REG_ADDR, nrOfRegistersToRead); //see also readInputRegisters
 	uint16_t result = 0;
 
-	if(mbResult == mbMaster->ku8MBSuccess) {
+	if(mbResult == mbMaster.ku8MBSuccess) {
 		/*
 		for(i = 0; i < nrOfRegisters; i++) {
-			buffer[i] = mbMaster->getResponseBuffer(i);
+			buffer[i] = mbMaster.getResponseBuffer(i);
 			Serial1.printf("Reg %d: %d\r\n", i, buffer[i]);
 		}
 		*/
-		debugf("Data from slave: %d", mbMaster->getResponseBuffer(0));
+		debugf("Data from slave: %d", mbMaster.getResponseBuffer(0));
 	} else {
 		debugf("Res err: %d", result);
 	}
 
-	mbMaster->clearResponseBuffer();
+	mbMaster.clearResponseBuffer();
 }
 
 void init()
@@ -56,11 +56,10 @@ void init()
 	Debug.start();
 	Debug.printf("This is the debug output\r\n");
 
-	mbMaster = new ModbusMaster;
-	mbMaster->preTransmission(preTransmission);
-	mbMaster->postTransmission(postTransmission);
-	mbMaster->logReceive(mbLogReceive);
-	mbMaster->logTransmit(mbLogTransmit);
+	mbMaster.preTransmission(preTransmission);
+	mbMaster.postTransmission(postTransmission);
+	mbMaster.logReceive(mbLogReceive);
+	mbMaster.logTransmit(mbLogTransmit);
 
 	mbLoopTimer.initializeMs(1000, mbLoop).start();
 }
@@ -76,33 +75,33 @@ void postTransmission() {
 }
 
 void mbLogReceive(const uint8_t *adu, size_t aduSize, uint8_t status ){
-	if( status != mbMaster->ku8MBSuccess ) {
+	if( status != mbMaster.ku8MBSuccess ) {
 		switch( status ) {
-		case mbMaster->ku8MBIllegalFunction:
+		case mbMaster.ku8MBIllegalFunction:
 			debugf("MB Illegal Function");
 			break;
-		case mbMaster->ku8MBIllegalDataAddress:
+		case mbMaster.ku8MBIllegalDataAddress:
 			debugf("MB Illegal Address");
 			break;
-		case mbMaster->ku8MBIllegalDataValue:
+		case mbMaster.ku8MBIllegalDataValue:
 			debugf("MB Illegal Data Value");
 			break;
-		case mbMaster->ku8MBSlaveDeviceFailure:
+		case mbMaster.ku8MBSlaveDeviceFailure:
 			debugf("MB Slave Device Failure");
 			break;
-		case mbMaster->ku8MBInvalidSlaveID:
+		case mbMaster.ku8MBInvalidSlaveID:
 			debugf("MB Invalid Slave ID");
 			break;
-		case mbMaster->ku8MBInvalidFunction:
+		case mbMaster.ku8MBInvalidFunction:
 			debugf("MB Invalid function");
 			break;
-		case mbMaster->ku8MBResponseTimedOut:
+		case mbMaster.ku8MBResponseTimedOut:
 			debugf("MB Response Timeout");
 			break;
-		case mbMaster->ku8MBInvalidCRC:
+		case mbMaster.ku8MBInvalidCRC:
 			debugf("MB Invalid CRC");
 			break;
-		case mbMaster->ku8MBResponseTooLarge:
+		case mbMaster.ku8MBResponseTooLarge:
 			debugf("MB Response too large");
 			break;
 		}
