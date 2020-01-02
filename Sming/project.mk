637070d00c8ba9766727f5c90abd1cdd5e4fe000
Slavey Karadzhov <slav@attachix.com>
2020-01-02 16:16:47 +0100

    Consistency.

--- Sming/project.mk
+++ Sming/project.mk
@@ -404,14 +404,14 @@ clean: ##Remove all generated build files (but leave build config intact)
 	@echo Cleaning application...
 	-$(Q) rm -rf $(BUILD_BASE) $(FW_BASE) $(APP_LIBDIR)
 
+##@Tools
+
 .PHONY: cs
 cs: .clang-format ##Apply coding style to selected project directories
 	$(SMING_MAKE) cs CS_ROOT_DIRS=$(PROJECT_DIR)
 
 .clang-format:
 	$(Q) cp $(SMING_HOME)/../.clang-format $@
-
-##@Tools
 
 .PHONY: gdb
 gdb: kill_term ##Run the debugger console
