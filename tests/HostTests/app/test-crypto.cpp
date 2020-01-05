cadede4b2047646ad2c50353fb03d19070e7641f
mikee47 <mike@sillyhouse.net>
2020-01-05 17:26:53 +0000

    Revise crypto test module, confirm hashes as expected.

--- tests/HostTests/app/test-crypto.cpp
+++ tests/HostTests/app/test-crypto.cpp
@@ -1,6 +1,9 @@
 #include <SmingTest.h>
 #include <Crypto.h>
 
+IMPORT_FSTR_LOCAL(plainText, PROJECT_DIR "/files/abstract.txt");
+DEFINE_FSTR_LOCAL(hmacKey, "very small key");
+
 class CryptoTest : public TestGroup
 {
 public:
@@ -8,17 +11,39 @@ public:
 	{
 	}
 
+	template <class Hash> void check(const Hash& hash, const String& expected)
+	{
+		auto s = toString(hash);
+		Serial.print(Hash::Engine::name);
+		Serial.print(" = ");
+		Serial.println(s);
+		REQUIRE(s == expected);
+	}
+
+	/*
+	 * Verification values obtained via:
+	 *
+	 * 		https://www.fileformat.info/tool/hash.htm
+	 * 		https://hash.online-convert.com/md5-generator
+	 */
 	void execute() override
 	{
-		String s = F("haggis basher");
-		Serial.print("MD5 = ");
-		Serial.println(Crypto::Md5::calculate(s).toString());
-		Serial.print("Sha1 = ");
-		Serial.println(Crypto::Sha1::calculate(s).toString());
-		Serial.print("Sha256 = ");
-		Serial.println(Crypto::Sha256::calculate(s).toString());
-		Serial.print("HMAC.MD5 = ");
-		Serial.println(Crypto::Hmac<Crypto::Md5, 64>::calculate(s, F("very small key")).toString());
+		TEST_CASE("Hashes")
+		{
+#define CTX_CHECK(Context, expected) check(Context::calculate(plainText), F(expected))
+			CTX_CHECK(Crypto::Md5, "4c9f6bb2c9891159487912899f1f2db1");
+			CTX_CHECK(Crypto::Sha1, "e4ba625521d569372fc25ca89acd7e9e5322ef27");
+			CTX_CHECK(Crypto::Sha256, "b23a4fd1eccb0a4779665c85cb05802cac583e51b7be126cb61a73660dd1059a");
+		}
+
+		TEST_CASE("HMAC")
+		{
+#define HMAC_CHECK(Hmac, expected) check(Hmac::calculate(String(plainText), String(hmacKey)), expected)
+			HMAC_CHECK(DECL((Crypto::Hmac<Crypto::Md5, 64>)), F("da512014e8f20e3c139cce4d1770a36b"));
+			HMAC_CHECK(DECL((Crypto::Hmac<Crypto::Sha1, 64>)), F("d191a9454a2345c4c4d4a07ad84d16b52815ae42"));
+			HMAC_CHECK(DECL((Crypto::Hmac<Crypto::Sha256, 64>)),
+					   F("68ee9b090f274c4eabbe4f96721ca3bb5f054d9690129f83734af64bb33e36a3"));
+		}
 	}
 };
 
