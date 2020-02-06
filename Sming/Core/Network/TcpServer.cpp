SmingHub/Sming

commit 738a0990228c4d290cd70529e2d54fb4dc27fdd0

Enforce optional TCP connection limit (#1942)
As per https://github.com/SmingHub/Sming/pull/1942#issuecomment-553451304, this PR includes a connection count check before accepting a TCP connection. If a connection limit has been specified and accepting the new connection would exceed this value, then the connection is aborted.

Discussion #1940

Sming/Core/Network/TcpServer.cpp
@@ -10,8 +10,6 @@
 
 #include "TcpServer.h"
 
-uint16_t TcpServer::totalConnections = 0;
-
 TcpConnection* TcpServer::createClient(tcp_pcb* clientTcp)
 {
 	debug_d("TCP Server createClient %sNULL\r\n", clientTcp ? "not" : "");
@@ -65,6 +63,12 @@ err_t TcpServer::onAccept(tcp_pcb* clientTcp, err_t err)
 	if(system_get_free_heap_size() < minHeapSize) {
 		debug_w("\r\n\r\nCONNECTION DROPPED\r\n\t(free heap: %u)\r\n\r\n", system_get_free_heap_size());
 		return ERR_MEM;
+	}
+
+	// Obey any requested connection limit
+	if(maxConnections != 0 && connections.count() >= maxConnections) {
+		debug_w("\r\n\r\nCONNECTION DROPPED\r\n\t(Existing connections: %u)\r\n\r\n", connections.count());
+		return ERR_ABRT;
 	}
 
 #ifdef NETWORK_DEBUG

Sming/Core/Network/HttpServer.cpp
@@ -20,6 +20,7 @@ void HttpServer::configure(const HttpServerSettings& settings)
 	if(settings.minHeapSize > -1) {
 		minHeapSize = settings.minHeapSize;
 	}
+	maxConnections = settings.maxActiveConnections;
 
 	if(settings.useDefaultBodyParsers) {
 		setBodyParser(MIME_FORM_URL_ENCODED, formUrlParser);

Sming/Core/Network/HttpServer.h
@@ -25,9 +25,9 @@
 #include "Http/HttpBodyParser.h"
 
 typedef struct {
-	int maxActiveConnections = 10; ///< maximum number of concurrent requests..
-	int keepAliveSeconds = 0;	  ///< default seconds to keep the connection alive before closing it
-	int minHeapSize = -1;		   ///< min heap size that is required to accept connection, -1 means use server default
+	uint16_t maxActiveConnections = 10; ///< maximum number of concurrent requests..
+	uint16_t keepAliveSeconds = 0;		///< default seconds to keep the connection alive before closing it
+	int minHeapSize = -1; ///< min heap size that is required to accept connection, -1 means use server default
 	bool useDefaultBodyParsers = 1; ///< if the default body parsers,  as form-url-encoded, should be used
 	bool closeOnContentError =
 		true; ///< close the connection if a body parser or resource fails to parse the body content.

Sming/Core/Network/TcpServer.h
@@ -65,6 +65,11 @@ public:
 
 	void shutdown();
 
+	const Vector<TcpConnection*>& getConnections() const
+	{
+		return connections;
+	}
+
 protected:
 	// Overload this method in your derived class!
 	virtual TcpConnection* createClient(tcp_pcb* clientTcp);
@@ -79,11 +84,11 @@ private:
 	static err_t staticAccept(void* arg, tcp_pcb* new_tcp, err_t err);
 
 public:
-	static uint16_t totalConnections; ///< @deprecated not updated by framework
 	uint16_t activeClients = 0;
 
 protected:
 	size_t minHeapSize = 16384;
+	uint16_t maxConnections = 0; ///< By default, don't limit connection count
 
 	bool active = true;
 	Vector<TcpConnection*> connections;

commit ab890bfad589fc901fc0c2eca22d8a5277c8044c

Fixed issues introduced in #1942.

Sming/Core/Network/TcpServer.cpp
@@ -25,12 +25,6 @@ TcpConnection* TcpServer::createClient(tcp_pcb* clientTcp)
 	return con;
 }
 
-//Timer stateTimer;
-void list_mem()
-{
-	debug_d("Free heap size=%u, K=%u", system_get_free_heap_size(), TcpServer::totalConnections);
-}
-
 void TcpServer::setKeepAlive(uint16_t seconds)
 {
 	debug_d("Server keep-alive updating: %u -> %u", keepAlive, seconds);
@@ -53,7 +47,6 @@ bool TcpServer::listen(int port, bool useSsl)
 	tcp = tcp_listen(tcp);
 	tcp_accept(tcp, staticAccept);
 
-	//stateTimer.initializeMs(3500, list_mem).start();
 	return true;
 }
 
@@ -72,8 +65,8 @@ err_t TcpServer::onAccept(tcp_pcb* clientTcp, err_t err)
 	}
 
 #ifdef NETWORK_DEBUG
-	debug_d("onAccept, tcp: %p, state: %d K=%d", clientTcp, err, connections.count());
-	list_mem();
+	debug_d("onAccept, tcp: %p, state: %d K=%d, Free heap size=%u", clientTcp, err, connections.count(),
+			system_get_free_heap_size());
 #endif
 
 	if(err != ERR_OK) {