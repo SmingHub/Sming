4a47fdf023e01ca30941965192b35177296a71e6
Kaloyan Mihaylov <kaloyan.mihaylov@gmail.com>
2020-02-08 00:04:55 +0200

    Towards event driven modbusino

--- Sming/Libraries/modbusino/samples/generic/app/application.cpp
+++ Sming/Libraries/modbusino/samples/generic/app/application.cpp
@@ -2,32 +2,26 @@
 #include <Debug.h>
 #include <Modbusino.h>
 
-Timer mbSlaveLoopTimer;
 Timer mbPrintTimer;
-ModbusinoSlave MbSlave(MB_SLAVE_ADDR);
+
+#define ARRLEN 3
+uint16_t mbDataArray[ARRLEN] = {0,0,0};
+ModbusinoSlave MbSlave(MB_SLAVE_ADDR, mbDataArray, ARRLEN);
+
 HardwareSerial debugComPort(UART1);
 
-uint16_t tab_reg[3] = {0,0,0};
-
-void mbSlaveLoop() {
-	uint8_t res = MbSlave.loop(tab_reg,3);
-	if( res < 0 ) debugf("error: %d\r\n");
-}
-
 void mbPrint() {
-	debugf("Register values, #0: %d, #1: %d, #2: %d", tab_reg[0], tab_reg[1], tab_reg[2]);
+	debugf("Register values, #0: %d, #1: %d, #2: %d", mbDataArray[0], mbDataArray[1], mbDataArray[2]);
 }
 
 void init()
 {
-	pinMode(15, OUTPUT);
 	debugComPort.begin(SERIAL_BAUD_RATE, SERIAL_8N1,
 					  SERIAL_TX_ONLY);
 	debugComPort.systemDebugOutput(true);
 	Debug.setDebug(debugComPort);
 	Debug.start();
-	MbSlave.setup();
+	MbSlave.setup(SERIAL_BAUD_RATE);
 
-	mbSlaveLoopTimer.initializeMs(10,mbSlaveLoop).start();
-	mbPrintTimer.initializeMs(1000,mbPrint).start();
+	mbPrintTimer.initializeMs(2500,mbPrint).start();
 }
