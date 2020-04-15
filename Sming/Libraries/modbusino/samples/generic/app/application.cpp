d9f3e54433aef4b2a9e8536cf3e24f4692afd201
Kaloyan Mihaylov <kaloyan.mihaylov@gmail.com>
2020-04-15 13:46:16 +0300

    make cs

--- Sming/Libraries/modbusino/samples/generic/app/application.cpp
+++ Sming/Libraries/modbusino/samples/generic/app/application.cpp
@@ -3,22 +3,22 @@
 #include <Modbusino.h>
 
 #define ARRLEN 3
-uint16_t mbDataArray[ARRLEN] = {0,0,0};
-ModbusinoSlave MbSlave(MB_SLAVE_ADDR, mbDataArray, ARRLEN);
+uint16_t mbDataArray[ARRLEN] = {0, 0, 0};
+ModbusinoSlave mbSlave(MB_SLAVE_ADDR, mbDataArray, ARRLEN);
 
 HardwareSerial debugComPort(UART1);
 
-void mbPrint() {
+void mbPrint()
+{
 	debugf("Register values, #0: %d, #1: %d, #2: %d", mbDataArray[0], mbDataArray[1], mbDataArray[2]);
 }
 
 void init()
 {
-	debugComPort.begin(SERIAL_BAUD_RATE, SERIAL_8N1,
-					  SERIAL_TX_ONLY);
+	debugComPort.begin(SERIAL_BAUD_RATE, SERIAL_8N1, SERIAL_TX_ONLY);
 	debugComPort.systemDebugOutput(true);
 	Debug.setDebug(debugComPort);
 	Debug.start();
-	MbSlave.setup(SERIAL_BAUD_RATE);
-	MbSlave.setRxCallback(mbPrint);
+	mbSlave.setup(SERIAL_BAUD_RATE);
+	mbSlave.setRxCallback(mbPrint);
 }
