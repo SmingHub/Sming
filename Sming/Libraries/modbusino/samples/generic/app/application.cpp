6ad6d86d6f3b89b5bd1501c8efa5b3dca574b46d
kmihaylov <kaloyan.mihaylov@gmail.com>
2020-05-08 10:33:48 +0300

    [Modbusino] - simple modbus rtu library (#2043)
    * Add modbusino library + patch + sample + Readme.
    
    Co-authored-by: mikee47 <mike@sillyhouse.net>

---
+++ Sming/Libraries/modbusino/samples/generic/app/application.cpp
@@ -0,0 +1,24 @@
+#include <SmingCore.h>
+#include <Debug.h>
+#include <Modbusino.h>
+
+#define ARRLEN 3
+uint16_t mbDataArray[ARRLEN] = {0, 0, 0};
+ModbusinoSlave mbSlave(MB_SLAVE_ADDR, mbDataArray, ARRLEN);
+
+HardwareSerial debugComPort(UART1);
+
+void mbPrint()
+{
+	debugf("Register values, #0: %d, #1: %d, #2: %d", mbDataArray[0], mbDataArray[1], mbDataArray[2]);
+}
+
+void init()
+{
+	debugComPort.begin(SERIAL_BAUD_RATE, SERIAL_8N1, SERIAL_TX_ONLY);
+	debugComPort.systemDebugOutput(true);
+	Debug.setDebug(debugComPort);
+	Debug.start();
+	mbSlave.setup(SERIAL_BAUD_RATE);
+	mbSlave.setRxCallback(mbPrint);
+}
