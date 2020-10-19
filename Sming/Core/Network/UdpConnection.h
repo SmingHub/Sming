dd48b63f1b1f0338fdaf9813534ac3902d7fecbc
Slavey Karadzhov <slav@attachix.com>
2020-10-19 11:10:07 +0200

    Added UDP multicast methods (when the used LWIP version supports it).

--- Sming/Core/Network/UdpConnection.h
+++ Sming/Core/Network/UdpConnection.h
@@ -70,6 +70,27 @@ public:
 		return sendTo(remoteIP, remotePort, data.c_str(), data.length());
 	}
 
+	/**
+	 * @brief Sets the UDP multicast IP.
+	 * @param ip
+	 *
+	 * @retval true when LWIP supports this operation, false otherwise
+	 *
+	 * @note This method works only when LWIP is compiled with LWIP_MULTICAST_TX_OPTIONS
+	 */
+	bool setMulticast(IpAddress ip);
+
+	/**
+	 * @brief Sets the UDP multicast Time-To-Live(TTL).
+	 * @param ttl - time to live in hops.
+	 *              For example if a milticast UDP packet needs to pass through two routes to reach the receiver then the TTL should be set to 2
+	 *
+	 * @retval true when LWIP supports this operation, false otherwise
+	 *
+	 * @note This method works only when LWIP is compiled with LWIP_MULTICAST_TX_OPTIONS
+	 */
+	bool setMulticastTtl(size_t ttl);
+
 protected:
 	virtual void onReceive(pbuf* buf, IpAddress remoteIP, uint16_t remotePort);
 
