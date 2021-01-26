1ba9aceca1d78b718c4590de6b0aefa4a73d3fe4
mikee47 <mike@sillyhouse.net>
2021-01-26 14:27:07 +0000

    Add `tcp_serial_redirect.py` to support GDB in WSL2

---
+++ Tools/WSL/tcp_serial_redirect.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+#
+# Redirect data from a TCP/IP connection to a serial port and vice versa.
+#
+# (C) 2002-2020 Chris Liechti <cliechti@gmx.net>
+#
+# SPDX-License-Identifier:    BSD-3-Clause
+#
+
+#
+# Amended 2021 mikee47 <mike@sillyhouse.net>
+#
+# Original source: https://github.com/pyserial/pyserial/blob/master/examples/tcp_serial_redirect.py
+#
+# Amended as follows:
+#   - Client mode removed, server-only;
+#   - Serial port is not opened until client connected
+#   - Serial port closed on client disconnect
+#
+# This allows for simpler sharing of the serial port by other applications (e.g. esptool).
+#
+
+import sys
+import socket
+import serial
+import serial.threaded
+import time
+
+
+class SerialToNet(serial.threaded.Protocol):
+    """serial->socket"""
+
+    def __init__(self):
+        self.socket = None
+
+    def __call__(self):
+        return self
+
+    def data_received(self, data):
+        if self.socket is not None:
+            self.socket.sendall(data)
+
+
+def openSerial(ser):
+    try:
+        ser.open()
+    except serial.SerialException as e:
+        sys.stderr.write('Could not open serial port {}: {}\n'.format(ser.name, e))
+        sys.exit(1)
+
+
+
+if __name__ == '__main__':  # noqa
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description='Simple Serial to Network (TCP/IP) redirector.',
+        epilog="""\
+NOTE: no security measures are implemented. Anyone can remotely connect
+to this service over the network.
+
+Only one connection at once is supported. When the connection is terminated
+it waits for the next connect.
+""")
+
+    parser.add_argument(
+        'SERIALPORT',
+        help="serial port name")
+
+    parser.add_argument(
+        'BAUDRATE',
+        type=int,
+        nargs='?',
+        help='set baud rate, default: %(default)s',
+        default=115200)
+
+    parser.add_argument(
+        '-q', '--quiet',
+        action='store_true',
+        help='suppress non error messages',
+        default=False)
+
+    parser.add_argument(
+        '--develop',
+        action='store_true',
+        help='Development mode, prints Python internals on errors',
+        default=False)
+
+    group = parser.add_argument_group('serial port')
+
+    group.add_argument(
+        "--bytesize",
+        choices=[5, 6, 7, 8],
+        type=int,
+        help="set bytesize, one of {5 6 7 8}, default: 8",
+        default=8)
+
+    group.add_argument(
+        "--parity",
+        choices=['N', 'E', 'O', 'S', 'M'],
+        type=lambda c: c.upper(),
+        help="set parity, one of {N E O S M}, default: N",
+        default='N')
+
+    group.add_argument(
+        "--stopbits",
+        choices=[1, 1.5, 2],
+        type=float,
+        help="set stopbits, one of {1 1.5 2}, default: 1",
+        default=1)
+
+    group.add_argument(
+        '--rtscts',
+        action='store_true',
+        help='enable RTS/CTS flow control (default off)',
+        default=False)
+
+    group.add_argument(
+        '--xonxoff',
+        action='store_true',
+        help='enable software flow control (default off)',
+        default=False)
+
+    group.add_argument(
+        '--rts',
+        type=int,
+        help='set initial RTS line state (possible values: 0, 1)',
+        default=None)
+
+    group.add_argument(
+        '--dtr',
+        type=int,
+        help='set initial DTR line state (possible values: 0, 1)',
+        default=None)
+
+    group = parser.add_argument_group('network settings')
+
+    exclusive_group = group.add_mutually_exclusive_group()
+
+    exclusive_group.add_argument(
+        '-P', '--localport',
+        type=int,
+        help='local TCP port',
+        default=7777)
+
+    args = parser.parse_args()
+
+    # connect to serial port
+    ser = serial.serial_for_url(args.SERIALPORT, do_not_open=True)
+    ser.baudrate = args.BAUDRATE
+    ser.bytesize = args.bytesize
+    ser.parity = args.parity
+    ser.stopbits = args.stopbits
+    ser.rtscts = args.rtscts
+    ser.xonxoff = args.xonxoff
+
+    if args.rts is not None:
+        ser.rts = args.rts
+
+    if args.dtr is not None:
+        ser.dtr = args.dtr
+
+    if not args.quiet:
+        sys.stderr.write(
+            '--- TCP/IP to Serial redirect on {p.name}  {p.baudrate},{p.bytesize},{p.parity},{p.stopbits} ---\n'
+            '--- type Ctrl-C / BREAK to quit\n'.format(p=ser))
+
+
+    srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    srv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    srv.bind(('', args.localport))
+    srv.listen(1)
+    try:
+        # Determine primary IP address
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            # doesn't even have to be reachable
+            s.connect(('10.255.255.255', 1))
+            IP = s.getsockname()[0]
+        except Exception:
+            IP = '127.0.0.1'
+        finally:
+            s.close()
+        
+        while True:
+            sys.stderr.write('Waiting for connection on %s::%d...\n' % (str(IP), args.localport))
+            client_socket, addr = srv.accept()
+            sys.stderr.write('Connected by {}\n'.format(addr))
+            # More quickly detect bad clients who quit without closing the
+            # connection: After 1 second of idle, start sending TCP keep-alive
+            # packets every 1 second. If 3 consecutive keep-alive packets
+            # fail, assume the client is gone and close the connection.
+            try:
+                client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 1)
+                client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 1)
+                client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, 3)
+                client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+            except AttributeError:
+                pass # XXX not available on windows
+            client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+
+            # Open serial port and set up a worker thread to communicate with it
+            openSerial(ser)
+            ser_to_net = SerialToNet()
+            ser_to_net.socket = client_socket
+            serial_worker = serial.threaded.ReaderThread(ser, ser_to_net)
+            serial_worker.start()
+
+            try:
+                # enter network <-> serial loop
+                while True:
+                    try:
+                        data = client_socket.recv(1024)
+                        if not data:
+                            break
+                        ser.write(data)                 # get a bunch of bytes and send them
+                    except socket.error as msg:
+                        if args.develop:
+                            raise
+                        sys.stderr.write('ERROR: {}\n'.format(msg))
+                        # probably got disconnected
+                        break
+            except socket.error as msg:
+                if args.develop:
+                    raise
+                sys.stderr.write('ERROR: {}\n'.format(msg))
+            finally:
+                ser_to_net.socket = None
+                sys.stderr.write('Disconnected\n')
+                client_socket.close()
+                serial_worker.stop()
+                ser.close()
+    except KeyboardInterrupt:
+        pass
+
+    sys.stderr.write('\n--- exit ---\n')
