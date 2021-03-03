366b393a102a70c102ee39b5e2eec79aa1724513
mikee47 <mike@sillyhouse.net>
2021-03-03 21:13:16 +0000

    Install fixes

--- Sming/Arch/Esp8266/Tools/install.sh
+++ Sming/Arch/Esp8266/Tools/install.sh
@@ -2,14 +2,15 @@
 
 # Old toolchain
 if [ -n "$UDK_ROOT" ]; then
-    if [ -d "$EQT_ROOT" ]; then
-        printf "\n\n** Skipping Esp8266 tools installation: '$EQT_ROOT' exists\n\n"
+    if [ -d "$UDK_ROOT" ]; then
+        printf "\n\n** Skipping Esp8266 tools installation: '$UDK_ROOT' exists\n\n"
     else
         TOOLCHAIN=esp-open-sdk-linux-x86_64.tar.gz
         $WGET $SMINGTOOLS/$TOOLCHAIN -O $DOWNLOADS/$TOOLCHAIN
-        tar -zxf $DOWNLOADS/$TOOLCHAIN
         mkdir -p $UDK_ROOT
-        ln -s $(pwd)/esp-open-sdk/xtensa-lx106-elf $UDK_ROOT/.
+        tar -zxf $DOWNLOADS/$TOOLCHAIN -C $UDK_ROOT --totals
+        mv $UDK_ROOT/esp-open-sdk/* $UDK_ROOT
+        rmdir $UDK_ROOT/esp-open-sdk
     fi
 fi
 
