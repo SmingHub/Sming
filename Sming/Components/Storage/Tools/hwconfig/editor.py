e6a9fcc134ac31ce2bcc4cd8b2d756aacf4dddd6
mikee47 <mike@sillyhouse.net>
2021-03-18 07:46:31 +0000

    Processing item selection

--- Sming/Components/Storage/Tools/hwconfig/editor.py
+++ Sming/Components/Storage/Tools/hwconfig/editor.py
@@ -9,7 +9,7 @@ class Editor:
         root.title('Sming Hardware Profile Editor')
         self.main = ttk.Frame(root)
         self.main.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-        self.createWidgets()
+        # self.createWidgets()
 
     def createWidgets(self):
         quitButton = tk.Button(self.main, text='Quit', command=self.main.quit)
@@ -18,35 +18,65 @@ class Editor:
     def setConfig(self, config):
         self.config = config
 
+        # Frame to contain treeview plus vertical scrollbar
         f = ttk.Frame(self.main)
         f.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-        tree = ttk.Treeview(f, columns=('address', 'size', 'type', 'subtype', 'filename'))
+        tree = ttk.Treeview(f, columns=['address', 'size', 'type', 'subtype', 'filename'])
         tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
         s = ttk.Scrollbar(f, orient=tk.VERTICAL, command=tree.yview)
         s.pack(side=tk.RIGHT, fill=tk.Y, expand=True)
         tree['yscrollcommand'] = s.set
 
-        tree.heading('#0', text='Device / Partition Name')
+        # tree.heading('#0', text='Device / Partition Name')
         tree.heading('address', text='Address')
         tree.heading('size', text='Size')
         tree.heading('type', text='Type')
         tree.heading('subtype', text='Sub-Type')
         tree.heading('filename', text='Image filename')
 
-        # tree.insert('', 'end', 'widgets', text='Widget Tour')
-
+        # Devices are our root nodes
         for dev in config.devices:
-            tree.insert('', 'end', dev.name, text=dev.name, open=True)
+            tree.insert('', 'end', dev.name, text=dev.name, open=True,
+                tags = ['device'],
+                values=['', dev.size_str(), dev.type_str()])
 
+        # Partitions are children
         for p in config.map():
             # id = p.device.name + '/' + p.address_str()
-            tree.insert(p.device.name, 'end', text=p.name,
-                values=(p.address_str(), p.size_str(), p.type_str(), p.subtype_str(), p.filename))
+            tree.insert(p.device.name, 'end', text=p.name if p.name != '(unused)' else '',
+                tags = ['unused' if p.type == 0xff else 'normal'],
+                values=[p.address_str(), p.size_str(), p.type_str(), p.subtype_str(), p.filename])
+
+        tree.tag_configure('device', font='+1')
+        # tree.tag_configure('normal', font='+1')
+
+        def device_from_id(id):
+            item = tree.item(id)
+            if 'device' in item['tags']:
+                return self.config.devices.find_by_name(item['text'])
+            else:
+                return None
+
+        # Status label
+        status = ttk.Label(self.main, text='-')
+        status.pack(side=tk.BOTTOM, fill=tk.X, expand=True)
+        def select(*args):
+            id = tree.focus()
+            item = tree.item(id)
+            if 'device' in item['tags']:
+                dev = device_from_id(id)
+                s = 'Device: ' + dev.to_json()
+            else:
+                dev = device_from_id(tree.parent(id))
+                addr = item['values'][0]
+                part = self.config.map().find_by_address(dev, addr)
+                s = 'Partition: ' + part.to_json()
+            status.configure(text=s)
+        tree.bind('<<TreeviewSelect>>', select)
 
         # Option checkboxes
-
         for k, v in self.config.option_library.items():
             btn = tk.Checkbutton(self.main, text = k + ': ' + v['description'])
             btn.pack(side=tk.BOTTOM, fill=tk.X)
