5d878ac350fcf912c13e60977098c3b3c157ffc7
mikee47 <mike@sillyhouse.net>
2021-04-09 20:11:53 +0100

    Make filesystem helper functions available for general use

--- Sming/Core/FileSystem.cpp
+++ Sming/Core/FileSystem.cpp
@@ -25,20 +25,7 @@ void fileSetFileSystem(IFS::IFileSystem* fileSystem)
 	}
 }
 
-namespace
-{
-template <typename T> Storage::Partition findDefaultPartition(T subType)
-{
-	auto part = *Storage::findPartition(subType);
-	if(part) {
-		debug_i("[%s] Found '%s'", part.typeString().c_str(), part.name().c_str());
-	} else {
-		debug_e("[%s] No partition found", toString(subType).c_str());
-	}
-	return part;
-}
-
-bool mount(IFS::IFileSystem* fs)
+bool fileMountFileSystem(IFS::IFileSystem* fs)
 {
 	if(fs == nullptr) {
 		debug_e("Failed to created filesystem object");
@@ -58,43 +45,42 @@ bool mount(IFS::IFileSystem* fs)
 	debug_i("File system initialised");
 	return true;
 }
-} // namespace
 
 bool spiffs_mount()
 {
-	auto part = findDefaultPartition(Storage::Partition::SubType::Data::spiffs);
+	auto part = Storage::findDefaultPartition(Storage::Partition::SubType::Data::spiffs);
 	return part ? spiffs_mount(part) : false;
 }
 
 bool spiffs_mount(Storage::Partition partition)
 {
 	auto fs = IFS::createSpiffsFilesystem(partition);
-	return mount(fs);
+	return fileMountFileSystem(fs);
 }
 
 bool fwfs_mount()
 {
-	auto part = findDefaultPartition(Storage::Partition::SubType::Data::fwfs);
+	auto part = Storage::findDefaultPartition(Storage::Partition::SubType::Data::fwfs);
 	return part ? fwfs_mount(part) : false;
 }
 
 bool fwfs_mount(Storage::Partition partition)
 {
 	auto fs = IFS::createFirmwareFilesystem(partition);
-	return mount(fs);
+	return fileMountFileSystem(fs);
 }
 
 bool hyfs_mount()
 {
-	auto fwfsPart = findDefaultPartition(Storage::Partition::SubType::Data::fwfs);
-	auto spiffsPart = findDefaultPartition(Storage::Partition::SubType::Data::spiffs);
+	auto fwfsPart = Storage::findDefaultPartition(Storage::Partition::SubType::Data::fwfs);
+	auto spiffsPart = Storage::findDefaultPartition(Storage::Partition::SubType::Data::spiffs);
 	return (fwfsPart && spiffsPart) ? hyfs_mount(fwfsPart, spiffsPart) : false;
 }
 
 bool hyfs_mount(Storage::Partition fwfsPartition, Storage::Partition spiffsPartition)
 {
 	auto fs = IFS::createHybridFilesystem(fwfsPartition, spiffsPartition);
-	return mount(fs);
+	return fileMountFileSystem(fs);
 }
 
 Vector<String> fileList()
