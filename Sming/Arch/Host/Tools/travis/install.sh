e0a2128292f6665ced733e8bd2796250c3ce5e3f
Slavey Karadzhov <slav@attachix.com>
2021-04-16 08:49:28 +0200

    Travis is up and clang-format has to be updated.

--- Sming/Arch/Host/Tools/travis/install.sh
+++ Sming/Arch/Host/Tools/travis/install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 set -ex # exit with nonzero exit code if anything fails
 
-sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-6.0 100
+sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-8 200
+
+
+sudo update-alternatives --query clang-format
+
+sudo rm /usr/local/clang-7.0.0/bin/clang-format
+
+clang-format --version
+whereis clang-format
+export CLANG_FORMAT=/usr/bin/clang-format
+$CLANG_FORMAT --version
+
 python3 -m pip install -q --upgrade pip
 
 if [ "$BUILD_DOCS" == "1" ]; then
