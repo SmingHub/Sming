71a11b8c93a8ae2876123cd8e0b98d37545c566d
mikee47 <mike@sillyhouse.net>
2021-04-18 17:03:24 +0100

    Make `base64_min_encode_len` and `base64_min_decode_len` functions available publicly.

--- Sming/Core/Network/WebHelpers/base64.cpp
+++ Sming/Core/Network/WebHelpers/base64.cpp
@@ -14,32 +14,21 @@
 #include "libb64/cencode.h"
 #include "libb64/cdecode.h"
 
-namespace
-{
-/*
- * Base-64 encodes 6 bits into one character (4 output chars for every 3 input bytes).
- * However, it also requires padding such that the output size is a multiple of 4 characters.
- */
-size_t MIN_ENCODE_LEN(size_t in_len)
+size_t base64_min_encode_len(size_t in_len)
 {
 	int groups = 1 + ((int(in_len) - 1) / 3);
 	return groups * 4;
 }
 
-/*
- * For decoding, do not assume that input is padded.
- */
-size_t MIN_DECODE_LEN(size_t in_len)
+size_t base64_min_decode_len(size_t in_len)
 {
 	return ((in_len * 3) + 3) / 4;
 }
 
-} // namespace
-
 int base64_encode(size_t in_len, const unsigned char* in, size_t out_len, char* out)
 {
 	// Base-64 encoding produces 3 output bytes for every 2 input bytes
-	unsigned min_out_len = MIN_ENCODE_LEN(in_len);
+	unsigned min_out_len = base64_min_encode_len(in_len);
 	if(out_len < min_out_len) {
 		return -1;
 	}
@@ -54,7 +43,7 @@ int base64_encode(size_t in_len, const unsigned char* in, size_t out_len, char* 
 String base64_encode(const unsigned char* in, size_t in_len)
 {
 	String s;
-	if(!s.setLength(MIN_ENCODE_LEN(in_len))) {
+	if(!s.setLength(base64_min_encode_len(in_len))) {
 		return nullptr;
 	}
 
@@ -70,7 +59,7 @@ String base64_encode(const unsigned char* in, size_t in_len)
 /* decode a base64 string in one shot */
 int base64_decode(size_t in_len, const char* in, size_t out_len, unsigned char* out)
 {
-	if(out_len < MIN_DECODE_LEN(in_len)) {
+	if(out_len < base64_min_decode_len(in_len)) {
 		return -1;
 	}
 
@@ -82,7 +71,7 @@ int base64_decode(size_t in_len, const char* in, size_t out_len, unsigned char* 
 String base64_decode(const char* in, size_t in_len)
 {
 	String s;
-	if(!s.setLength(MIN_DECODE_LEN(in_len))) {
+	if(!s.setLength(base64_min_decode_len(in_len))) {
 		return nullptr;
 	}
 
