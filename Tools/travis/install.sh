e51a07cd64005bf697729067d5f789a67b940cad
mikee47 <mike@sillyhouse.net>
2021-04-29 16:51:18 +0100

    x

--- Tools/travis/install.sh
+++ Tools/travis/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex # exit with nonzero exit code if anything fails
 
-sudo apt-get install -y g++-9-multilib linux-libc-dev:i386
+sudo apt-get install -f -y g++-9-multilib linux-libc-dev:i386
 
 if [ -f "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh" ]; then
 	source "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh"
