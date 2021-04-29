c82db21981102dc2920eac1bf18aa5b9b8b74bbd
Slavey Karadzhov <slav@attachix.com>
2021-04-29 21:21:11 +0200

    Trying to fix package installation...

--- Tools/travis/install.sh
+++ Tools/travis/install.sh
@@ -2,8 +2,9 @@
 set -ex # exit with nonzero exit code if anything fails
 
 sudo apt-get update 
-sudo apt-get upgrade
-sudo apt-get install -y g++-9-multilib
+sudo apt-get -y upgrade
+sudo apt-mark showhold
+sudo apt-get install -y gcc-9-multilib g++-9-multilib
 
 if [ -f "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh" ]; then
 	source "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh"
