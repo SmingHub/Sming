87174a45bf2872a6cc421fcaa2d2fb05a8c19fcd
Slavey Karadzhov <slav@attachix.com>
2021-04-29 21:11:01 +0200

    Trying to fix package installation.

--- Tools/travis/install.sh
+++ Tools/travis/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex # exit with nonzero exit code if anything fails
 
-sudo dpkg --add-architecture i386
-sudo apt-get install -f -y g++-9-multilib linux-libc-dev:i386
+sudo apt-get update 
+sudo apt-get install -y g++-9-multilib
 
 if [ -f "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh" ]; then
 	source "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh"
