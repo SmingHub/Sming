dc20550e5d21e75054d2d35b64acbe0ca1e23651
mikee47 <mike@sillyhouse.net>
2021-04-29 17:34:07 +0100

    ffffffff

--- Tools/travis/install.sh
+++ Tools/travis/install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -ex # exit with nonzero exit code if anything fails
 
-sudo apt-get install -y g++-9-multilib
 sudo dpkg --add-architecture i386
-sudo apt-get install -y linux-libc-dev:i386
+sudo apt-get install -f -y g++-9-multilib linux-libc-dev:i386
 
 if [ -f "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh" ]; then
 	source "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh"
