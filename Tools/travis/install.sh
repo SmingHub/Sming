8ede06c74bd65c2c7d0a0e477077ce4d84db332a
mikee47 <mike@sillyhouse.net>
2021-04-30 10:10:35 +0100

    mmmmmmmmmmm

--- Tools/travis/install.sh
+++ Tools/travis/install.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 set -ex # exit with nonzero exit code if anything fails
 
-uname -a
-cat /etc/apt/sources.list | grep ^[^#]
-echo "======================================="
-# sudo apt-get update 
-# sudo apt-get -y upgrade
-sudo apt-mark showhold
-sudo apt-get remove --dry-run g++-9-multilib
-
-sudo apt-get remove -y gcc-multilib
-sudo apt-get autoremove
-
-# lib32gcc-9-dev
-# lib32stdc++6
-# libx32gcc-9-dev
-# libx32stdc++6
-sudo apt-get install -y lib32stdc++-9-dev libx32stdc++-9-dev
-sudo apt-get install -y gcc-9-multilib
-sudo apt-get install -y g++-9-multilib
-
 if [ -f "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh" ]; then
 	source "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh"
 fi
