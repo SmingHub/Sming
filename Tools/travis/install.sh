8bb96d666e9e0613e43d98ef95a7f35567dd29ec
mikee47 <mike@sillyhouse.net>
2021-04-30 09:52:58 +0100

    shhhhhhhhhh

--- Tools/travis/install.sh
+++ Tools/travis/install.sh
@@ -8,6 +8,14 @@ echo "======================================="
 # sudo apt-get -y upgrade
 sudo apt-mark showhold
 
+
+sudo apt-get remove -y gcc-multilib
+sudo apt-get autoremove
+
+# lib32gcc-9-dev
+# lib32stdc++6
+# libx32gcc-9-dev
+# libx32stdc++6
 sudo apt-get install -y lib32stdc++-9-dev libx32stdc++-9-dev
 sudo apt-get install -y gcc-9-multilib
 sudo apt-get install -y g++-9-multilib
