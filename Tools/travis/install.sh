80e7bbc79c7a85d5c9730d8ae440a5059cce1ab5
mikee47 <mike@sillyhouse.net>
2021-04-30 09:38:09 +0100

    gggggggg

--- Tools/travis/install.sh
+++ Tools/travis/install.sh
@@ -4,14 +4,17 @@ set -ex # exit with nonzero exit code if anything fails
 uname -a
 cat /etc/apt/sources.list | grep ^[^#]
 echo "======================================="
-sudo apt-get update 
-sudo apt-get -y upgrade
+# sudo apt-get update 
+# sudo apt-get -y upgrade
 sudo apt-mark showhold
 
-dpkg -l | grep ^iU | awk '{print $2}' | xargs sudo dpkg --purge
+sudo apt-get --fix-missing
+sudo apt-get install -f -y
 
 
-sudo apt-get install gcc --only-upgrade
+# dpkg -l | grep ^iU | awk '{print $2}' | xargs sudo dpkg --purge
+
+# sudo apt-get install gcc --only-upgrade
 sudo apt-get install -y g++-9-multilib
 
 if [ -f "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh" ]; then
