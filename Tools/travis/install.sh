a698fa558aca35b38587d5a6883ce1fcd0e19ec1
mikee47 <mike@sillyhouse.net>
2021-04-30 12:51:24 +0100

    specific multilib version?

--- Tools/travis/install.sh
+++ Tools/travis/install.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -ex # exit with nonzero exit code if anything fails
 
-sudo apt-get clean
-sudo apt-get update
-
 # sudo apt-get remove -y libc6 libc6-dev --allow-remove-essential
 # sudo apt-get autoremove
 
-sudo apt-get install -y g++-9-multilib
+sudo apt-get install -y g++-9-multilib=9.3.0-17ubuntu1~20.04
 
 
 if [ -f "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh" ]; then
