11bbe3152117739632293b41528b262436001616
mikee47 <mike@sillyhouse.net>
2021-05-01 16:32:59 +0100

    dsafasdfgasfd

--- Tools/travis/install.sh
+++ Tools/travis/install.sh
@@ -3,10 +3,14 @@ set -ex # exit with nonzero exit code if anything fails
 
 # apt-rdepends -r libc6 libc6-dev
 
-sudo apt-get remove libc6-prof
-sudo apt-get autoremove
-sudo apt-get clean
-sudo apt-get install -y g++-9-multilib
+apt-depends -r lib32stdc++-9-dev libx32stdc++-9-dev
+apt-depends libc6 libc6-dev
+
+
+# sudo apt-get remove libc6-prof
+# sudo apt-get autoremove
+# sudo apt-get clean
+# sudo apt-get install -y g++-9-multilib
 
 
 if [ -f "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh" ]; then
