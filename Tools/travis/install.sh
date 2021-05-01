dc6bce5b42dc80a6a0fcbbce0d76f27cb4adffba
mikee47 <mike@sillyhouse.net>
2021-05-01 16:27:25 +0100

    remove libc6-prof ?

--- Tools/travis/install.sh
+++ Tools/travis/install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -ex # exit with nonzero exit code if anything fails
 
-apt-rdepends -r libc6 libc6-dev
+# apt-rdepends -r libc6 libc6-dev
+
+sudo apt-get remove libc6-prof
+sudo apt-get autoremove
+sudo apt-get clean
+sudo apt-get install -y g++-9-multilib
+
 
 if [ -f "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh" ]; then
 source "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh"
