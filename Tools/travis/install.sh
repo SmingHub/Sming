cfcaf3d760a67df5d741febf2789d582eb484d5b
mikee47 <mike@sillyhouse.net>
2021-05-01 07:35:03 +0100

    x

--- Tools/travis/install.sh
+++ Tools/travis/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex # exit with nonzero exit code if anything fails
 
-apt-rdepends -r lib32stdc++-9-dev libx32stdc++-9-dev
+apt-rdepends -r libc6 libc6-dev
 
 if [ -f "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh" ]; then
 source "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh"
