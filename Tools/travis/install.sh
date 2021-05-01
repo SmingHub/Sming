c7e23af6bdd316e848da0f46e5a06778c5b89eac
mikee47 <mike@sillyhouse.net>
2021-05-01 07:30:48 +0100

    rdepends

--- Tools/travis/install.sh
+++ Tools/travis/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex # exit with nonzero exit code if anything fails
 
+apt-rdepends -r lib32stdc++-9-dev libx32stdc++-9-dev
+
 if [ -f "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh" ]; then
 source "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh"
 fi
