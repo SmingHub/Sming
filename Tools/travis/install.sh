27a2ea2f809beb55ff97cfadabdbcc635af8a1e5
Slavey Karadzhov <slav@attachix.com>
2021-05-03 10:57:10 +0200

    Downgrade libc until the other packages catch-up.

--- Tools/travis/install.sh
+++ Tools/travis/install.sh
@@ -4,7 +4,7 @@ set -ex # exit with nonzero exit code if anything fails
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install software-properties-common
-sudo aptitude install g++-9-multilib
+sudo aptitude install -y libc6-dev=2.31-0ubuntu9.2 libc6=2.31-0ubuntu9.2 g++-9-multilib
 
 if [ -f "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh" ]; then
 source "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh"
