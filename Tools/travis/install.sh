e05fa284d312e714a3bc87fbca478b38d2a6c705
Slavey Karadzhov <slav@attachix.com>
2021-05-03 11:20:42 +0200

    Travis should work as expected now.

--- Tools/travis/install.sh
+++ Tools/travis/install.sh
@@ -7,5 +7,5 @@ sudo apt install software-properties-common
 sudo aptitude install -y libc6-dev=2.31-0ubuntu9.2 libc6=2.31-0ubuntu9.2 g++-9-multilib linux-libc-dev:i386
 
 if [ -f "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh" ]; then
-source "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh"
+	source "$TRAVIS_BUILD_DIR/Sming/Arch/$SMING_ARCH/Tools/travis/install.sh"
 fi
