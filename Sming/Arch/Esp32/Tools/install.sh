2a0b1f16b3e5178f72c2d25dd449438b5b5d6da5
mikee47 <mike@sillyhouse.net>
2021-08-20 15:23:56 +0100

    Codacy advisories

--- Sming/Arch/Esp32/Tools/install.sh
+++ Sming/Arch/Esp32/Tools/install.sh
@@ -31,13 +31,13 @@ $PKG_INSTALL ${PACKAGES[*]}
 if [ -d "$IDF_PATH" ]; then
     printf "\n\n** Skipping ESP-IDF clone: '$IDF_PATH' exists\n\n"
 else
-    git clone -b release/v4.3 https://github.com/espressif/esp-idf.git $IDF_PATH
+    git clone -b release/v4.3 https://github.com/espressif/esp-idf.git "$IDF_PATH"
 fi
 
 # Apply IDF patches
-IDF_PATCH=$(dirname $BASH_SOURCE)/idf.patch
-pushd $IDF_PATH
-git apply -v --ignore-whitespace --whitespace=nowarn $IDF_PATCH
+IDF_PATCH="$(dirname $BASH_SOURCE)/idf.patch"
+pushd "$IDF_PATH"
+git apply -v --ignore-whitespace --whitespace=nowarn "$IDF_PATCH"
 
 # Install IDF tools and packages
 python3 tools/idf_tools.py install
