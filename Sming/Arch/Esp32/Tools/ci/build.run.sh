13a3e95726593e510aa0ea2b7d53417262e4010b
mikee47 <mike@sillyhouse.net>
2021-08-27 16:46:57 +0100

    Add basic CI build for esp32 variants

--- Sming/Arch/Esp32/Tools/ci/build.run.sh
+++ Sming/Arch/Esp32/Tools/ci/build.run.sh
@@ -2,3 +2,15 @@
 
 $MAKE_PARALLEL Basic_Blink Basic_WiFi HttpServer_ConfigNetwork DEBUG_VERBOSE_LEVEL=3 STRICT=1
 $MAKE_PARALLEL Basic_Ssl ENABLE_SSL=Bearssl DEBUG_VERBOSE_LEVEL=3 STRICT=1
+
+# esp32s2
+make clean components-clean
+$MAKE_PARALLEL Basic_Blink ESP_VARIANT=esp32s2
+
+# esp32c3
+make clean components-clean
+$MAKE_PARALLEL Basic_Blink ESP_VARIANT=esp32c3
+
+# esp32s3
+make clean components-clean
+$MAKE_PARALLEL Basic_Blink ESP_VARIANT=esp32s3
