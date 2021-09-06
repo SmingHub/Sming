6c5a3d9df96e1008e05ccb284e8cdb6a52bb7973
mikee47 <mike@sillyhouse.net>
2021-09-06 13:54:33 +0100

    Route `_write_r` to `m_nputs` and set IDF logging function to `m_vprintf`

--- Sming/Arch/Esp32/Components/libc/component.mk
+++ Sming/Arch/Esp32/Components/libc/component.mk
@@ -1,4 +1,8 @@
 COMPONENT_SRCDIRS		:= src
-COMPONENT_INCDIRS		:= include
+COMPONENT_INCDIRS		:= src/include
 
-COMPONENT_DOXYGEN_INPUT	:= include/sys
+COMPONENT_DOXYGEN_INPUT	:= src/include/sys
+
+EXTRA_LDFLAGS := \
+	-Wl,-wrap,_write_r \
+	-Wl,-wrap,_read_r \
