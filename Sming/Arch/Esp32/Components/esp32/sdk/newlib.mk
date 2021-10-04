bcb64260f7d0748f01b0c1213a3477e1d3212217
mikee47 <mike@sillyhouse.net>
2021-10-04 22:08:02 +0100

    Wrap esp32 printf functions so they don't get pulled in from C library
    Saves about 60K flash (and a little RAM)

--- Sming/Arch/Esp32/Components/esp32/sdk/newlib.mk
+++ Sming/Arch/Esp32/Components/esp32/sdk/newlib.mk
@@ -8,3 +8,6 @@ SDK_UNDEF_SYMBOLS += \
 	newlib_include_heap_impl \
 	newlib_include_syscalls_impl \
 	newlib_include_pthread_impl
+
+# Must link before standard C library
+LIBS := newlib $(LIBS)
--- Sming/Arch/Esp32/Components/libc/component.mk
+++ Sming/Arch/Esp32/Components/libc/component.mk
@@ -3,7 +3,18 @@ COMPONENT_INCDIRS		:= src/include
 
 COMPONENT_DOXYGEN_INPUT	:= src/include/sys
 
-EXTRA_LDFLAGS := $(call Wrap,_write_r _read_r)
+LIBC_WRAPSYMS := \
+	_write_r \
+	_read_r \
+	putchar \
+	puts \
+	vprintf \
+	printf \
+	vsnprintf \
+	vsprintf \
+	sprintf
+
+EXTRA_LDFLAGS := $(call Wrap,$(LIBC_WRAPSYMS))
 
 EXTRA_LIBS := \
 	c \
