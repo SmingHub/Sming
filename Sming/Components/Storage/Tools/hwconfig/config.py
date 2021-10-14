07244665d924af0de257db2c341a0d5f92267440
Slavey Karadzhov <slav@attachix.com>
2021-10-14 11:28:38 +0200

    Codacy has for python conflicting requirements. Should disable some of them.

--- Sming/Components/Storage/Tools/hwconfig/config.py
+++ Sming/Components/Storage/Tools/hwconfig/config.py
@@ -121,8 +121,7 @@ class Config(object):
         self.parse_dict(data)
 
     def parse_options(self, options):
-        """
-        Apply any specified options.
+        """Apply any specified options.
         
         Each option can be applied more than once to ensure overrides work as expected.
         """
