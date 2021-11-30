2b07de0838c8db4a64a8bcaf9612701c80e7043d
mikee47 <mike@sillyhouse.net>
2021-11-30 13:10:32 +0000

    Change CI tools directory

--- Tools/ci/setenv.ps1
+++ Tools/ci/setenv.ps1
@@ -1,14 +1,19 @@
+if($IsWindows) {
+    $TOOLS_DIR = "C:\tools"
+} else {
+    $TOOLS_DIR = "/opt"
+}
 
 # Esp8266 
-$env:ESP_HOME = Join-Path $env:CI_BUILD_DIR "opt/esp-quick-toolchain"
+$env:ESP_HOME = Join-Path $TOOLS_DIR "esp-quick-toolchain"
 
 # Esp32
-$env:IDF_PATH = Join-Path $env:CI_BUILD_DIR "opt/esp-idf"
-$env:IDF_TOOLS_PATH = Join-Path $env:CI_BUILD_DIR "opt/tools/esp32"
+$env:IDF_PATH = Join-Path $TOOLS_DIR "esp-idf"
+$env:IDF_TOOLS_PATH = Join-Path $TOOLS_DIR "esp32"
 $env:IDF_BRANCH = "sming/release/v4.3"
 
 # Rp2040
-$env:PICO_TOOLCHAIN_PATH = Join-Path $env:CI_BUILD_DIR "opt/rp2040"
+$env:PICO_TOOLCHAIN_PATH = Join-Path $TOOLS_DIR "rp2040"
 
 # General
 $env:SMINGTOOLS = "https://github.com/SmingHub/SmingTools/releases/download/1.0"
