0e48b08d489733027c0fd5c7d3338ee57ae8d576
mikee47 <mike@sillyhouse.net>
2021-12-01 12:24:34 +0000

    Windows install.cmd doesn't install docs as expected

--- Tools/ci/install.cmd
+++ Tools/ci/install.cmd
@@ -16,21 +16,20 @@ rmdir /s /q c:\MinGW
 curl -Lo MinGW.7z %SMINGTOOLS%/MinGW-2020-10-19.7z
 7z -oC:\ x MinGW.7z
 
-if "%1" == "all" (
-    call :install Host Esp8266 Esp32 Rp2040
-    goto :EOF
-)
-
 :install
 if "%1" == "" goto :EOF
-echo.
-echo.
-echo ** Installing %1 toolchain
-echo.
-if "%1" == "doc" (
-    call %SMING_HOME%\..\docs\Tools\install.cmd
+if "%1" == "all" (
+    call :install Host Esp8266 Esp32 Rp2040
 ) else (
-    call %SMING_HOME%\Arch\%1\Tools\ci\install.cmd
+    echo.
+    echo.
+    echo ** Installing %1 toolchain
+    echo.
+    if "%1" == "doc" (
+        call %SMING_HOME%\..\docs\Tools\install.cmd
+    ) else (
+        call %SMING_HOME%\Arch\%1\Tools\ci\install.cmd
+    )
 )
 shift
 goto :install
