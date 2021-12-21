79529b7c49976a565e6a3528de627b9e52dccfbf
mikee47 <mike@sillyhouse.net>
2021-12-21 14:57:15 +0000

    Revise `sample` link role to search intelligently for sample based on document location
    i.e. if used within a library then first look  in library for a sample with the given name.
    If not found, use default samples location.

--- docs/source/link-roles.py
+++ docs/source/link-roles.py
@@ -12,6 +12,9 @@ import sys
 from docutils import nodes, utils
 from sphinx import roles, addnodes
 from sphinx.util.nodes import set_role_source_info, split_explicit_title
+from sphinx.util import logging
+
+logger = logging.getLogger(__name__)
 
 github_url = 'https://github.com/SmingHub/Sming'
 
@@ -43,7 +46,7 @@ def setup(app):
     app.add_role('issue', autolink('Issue #{0} <' + github_url + '/issue/{0}>'))
     app.add_role('pull-request', autolink('Pull Request #{0} <' + github_url + '/pull/{0}>'))
 
-    app.add_role('sample', doclink('/_inc/samples/{}/index'))
+    app.add_role('sample', SampleRole)
     app.add_role('component', doclink('/_inc/Sming/Components/{}/index'))
     app.add_role('component-esp8266', doclink('/_inc/Sming/Arch/Esp8266/Components/{}/index'))
     app.add_role('component-esp32', doclink('/_inc/Sming/Arch/Esp32/Components/{}/index'))
@@ -51,9 +54,11 @@ def setup(app):
     app.add_role('component-rp2040', doclink('/_inc/Sming/Arch/Rp2040/Components/{}/index'))
     app.add_role('library', doclink('/_inc/Sming/Libraries/{}/index'))
 
-# Insert a link to a file or issue in the repo
-# Both pattern and user text may use optional format, e.g. `title <link {}>`
 def autolink(pattern):
+    """Insert a link to a file or issue in the repo.
+    Both pattern and user text may use optional format, e.g. `title <link {}>`
+    """
+
     def role(name, rawtext, text, lineno, inliner, options={}, content=[]):
         text_has_explicit_title, link_text, link = split_explicit_title(text)
         pattern_has_explicit_title, pattern_text, pattern_link = split_explicit_title(pattern)
@@ -65,8 +70,9 @@ def autolink(pattern):
     return role
 
 
-# Default title is extracted from target document
 def doclink(pattern):
+    """Default title is extracted from target document."""
+
     def DocumentRole(typ, rawtext, text, lineno, inliner, options={}, content=[]):
         # type: (unicode, unicode, unicode, int, Inliner, Dict, List[unicode]) -> Tuple[List[nodes.Node], List[nodes.Node]]  # NOQA
         env = inliner.document.settings.env
@@ -87,3 +93,55 @@ def doclink(pattern):
         return [refnode], []
 
     return DocumentRole
+
+
+def SampleRole(typ, rawtext, text, lineno, inliner, options={}, content=[]):
+    """Sample may be in current component/library or main sample repository."""
+
+    # split title and target in role content
+    has_explicit_title, title, target = split_explicit_title(text)
+    title = utils.unescape(title)
+    target = utils.unescape(target)
+
+    sampleTarget = None
+
+    env = inliner.document.settings.env
+    docname = env.docname
+
+    path = getComponentPath(docname)
+    if path is not None:
+        path += f"/samples/{target}/index"
+        docpath = env.doc2path(path)
+        if os.path.exists(docpath):
+            sampleTarget = f"/{path}"
+
+    if sampleTarget is None:
+        sampleTarget = f"/_inc/samples/{target}/index"
+
+    logger.verbose(f">> sample '{target}' -> '{sampleTarget}'")
+
+    # create the reference node
+    refnode = addnodes.pending_xref(rawtext, reftype='doc', refdomain='std', refexplicit=has_explicit_title)
+    # we may need the line number for warnings
+    set_role_source_info(inliner, lineno, refnode)  # type: ignore
+    # now that the target and title are finally determined, set them
+    refnode['reftarget'] = sampleTarget
+    refnode += nodes.inline(rawtext, title, classes=['xref', 'doc'])
+    # we also need the source document
+    refnode['refdoc'] = docname
+    refnode['refwarn'] = True
+    return [refnode], []
+
+
+def getComponentPath(path):
+    """Obtain name of Component or library from document path.
+    Return None if document is not in a Component or library.
+    """
+
+    a, b, c = path.partition("/Components/")
+    if b == "":
+        a, b, c = path.partition("/Libraries/")
+    if b == "":
+        return None
+
+    return f"{a}{b}{c.split('/')[0]}"
