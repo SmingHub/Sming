355ad96b0acfd36d8fe63e408ed6240a81c19804
mikee47 <mike@sillyhouse.net>
2021-12-30 21:01:14 +0000

    Add network sample to CI build

--- Sming/Arch/Rp2040/Tools/ci/build.run.cmd
+++ Sming/Arch/Rp2040/Tools/ci/build.run.cmd
@@ -1,6 +1,6 @@
 REM Rp2040 build.run.cmd
 
-set RP2040_PROJECTS=Basic_Blink Basic_Serial Basic_Storage
+set RP2040_PROJECTS=Basic_Blink Basic_Serial Basic_Storage HttpServer_ConfigNetwork
 
 %MAKE_PARALLEL% %RP2040_PROJECTS% DEBUG_VERBOSE_LEVEL=3 || goto :error
 
