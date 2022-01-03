7eb1e3530b6154b3ed4360256de2ba060a9f4f92
mikee47 <mike@sillyhouse.net>
2022-01-03 09:28:12 +0000

    Update tests to include mode checks.

--- tests/HostTests/modules/SPI.cpp
+++ tests/HostTests/modules/SPI.cpp
@@ -31,40 +31,97 @@ public:
 
 	void execute() override
 	{
+		// SPI.setup(SpiBus::SPI3);
 		SPI.begin();
 
-		TEST_CASE("Scope check")
+		for(uint8_t mode : {SPI_MODE1, SPI_MODE2, SPI_MODE3}) {
+			uint8_t mode_num = ((mode & 0x10) >> 3) | (mode & 0x01);
+			debug_i("MODE 0x%02x %u", mode, mode_num);
+		}
+
+		TEST_CASE("Bit Patterns")
 		{
 			debug_w("Connect scope and observe bit pattern");
 			constexpr unsigned duration{10};
 			constexpr unsigned loopInterval{250};
 			loopCount = duration * 1000 / loopInterval;
-			timer.initializeMs<loopInterval>([&]() {
-				for(uint8_t bitMode : {MSBFIRST, LSBFIRST}) {
-					settings.bitOrder = bitMode;
-					SPI.beginTransaction(settings);
-					SPI.transfer32(0x00AA00AA);
-					SPI.transfer32(0x12345678);
-					SPI.endTransaction();
-				}
-				m_putc('.');
-				if(loopCount-- == 0) {
-					this->loopbackTests();
-					this->complete();
-				}
-			});
+			timer.initializeMs<loopInterval>([this]() { bitPatterns(); });
 			timer.start();
 			return pending();
 		}
+	}
+
+	/*
+	 * 6 transactions:
+	 *
+	 * - MODE0 MSBFIRST       0xAA00AA00    0x12345678    0x12 0x34 0x56 0x78 0x9A
+	 * 	   Viewed MSB first:    00 AA 00 AA   12 34 56 78   12 34 56 78 9A
+	 *     Viewed LSB first:    00 55 00 55   48 2C 6A 1E   48 2C 6A 1E 59
+	 * - MODE0 LSBFIRST       0xAA00AA00    0x12345678    0x12 0x34 0x56 0x78 0x9A
+	 *     Viewed MSB first:    55 00 55 00   1E 6A 2C 48   48 2C 6A 1E 59
+	 * 	   Viewed LSB first:    AA 00 AA 00   78 56 34 12   12 34 56 78 9A
+	 * - MODE0 MSBFIRST 0xAA (13 bits)
+	 *     Clock idles LOW, latch on RISING edge
+	 * - MODE1 MSBFIRST 0xAA (13 bits)
+	 *     Clock idles LOW, latch on FALLING edge
+	 * - MODE2 MSBFIRST 0xAA (13 bits)
+	 *     Clock idles HIGH, latch on FALLINGg edge
+	 * - MODE3 MSBFIRST 0xAA (13 bits)
+	 *     Clock idles HIGH, latch on RISING edge
+	 */
+	void bitPatterns()
+	{
+		settings.speed = 100000;
+		settings.bitOrder = MSBFIRST;
+		settings.dataMode = SPI_MODE0;
+		for(uint8_t bitOrder : {MSBFIRST, LSBFIRST}) {
+			settings.bitOrder = bitOrder;
+			SPI.beginTransaction(settings);
+			SPI.transfer32(0x00AA00AA);
+			SPI.transfer32(0x12345678);
+			uint8_t data[]{0x12, 0x34, 0x56, 0x78, 0x9A};
+			SPI.transfer(data, ARRAY_SIZE(data));
+			SPI.endTransaction();
+			delayMicroseconds(100);
+		}
+
+		delayMicroseconds(200);
+		for(uint8_t dataMode : {SPI_MODE0, SPI_MODE1, SPI_MODE2, SPI_MODE3}) {
+			settings.bitOrder = MSBFIRST;
+			settings.dataMode = dataMode;
+			SPI.beginTransaction(settings);
+			SPI.transfer32(0xAA, 13);
+			SPI.endTransaction();
+			delayMicroseconds(100);
+		}
+
+		// Leave bus in mode 0
+		settings.dataMode = SPI_MODE0;
+		SPI.beginTransaction(settings);
+		SPI.endTransaction();
+
+		m_putc('.');
+
+		if(loopCount-- != 0) {
+			return;
+		}
+
+		timer.stop();
+		loopbackTests();
+		SPI.end();
+		complete();
 	}
 
 	void loopbackTests()
 	{
 		if(!SPI.loopback(true)) {
 			debug_w("WARNING: SPI loopback not supported. Manual connection required.");
-			debug_w("ESP8266: Connect MISO (GPIO12/D6) <-> MISO (GPIO13/D7)");
+			debug_w("ESP8266: Connect MISO (GPIO12/D6) <-> MOSI (GPIO13/D7)");
 			allowFailure = true;
 		}
+
+		settings.speed = 8000000;
+		settings.dataMode = SPI_MODE0;
 
 		TEST_CASE("32-bit values")
 		{
@@ -103,7 +160,6 @@ public:
 
 	void send(uint32_t outValue, uint8_t bits, uint8_t bitOrder)
 	{
-		SPISettings settings;
 		settings.bitOrder = bitOrder;
 		SPI.beginTransaction(settings);
 		outValue &= BIT(bits) - 1;
@@ -135,7 +191,6 @@ public:
 		auto bufptr = reinterpret_cast<uint8_t*>(inData.begin() + startOffset);
 		auto length = inData.length() - startOffset;
 
-		SPISettings settings;
 		settings.bitOrder = bitOrder;
 		SPI.beginTransaction(settings);
 		debug_i("TX %u bytes, startOffset %u, %cSB first", length, startOffset, bitOrder == MSBFIRST ? 'M' : 'L');
@@ -155,7 +210,7 @@ public:
 
 private:
 	Timer timer;
-	SPISettings settings{1000000, MSBFIRST, SPI_MODE0};
+	SPISettings settings;
 	unsigned loopCount{0};
 	bool allowFailure{false};
 };
