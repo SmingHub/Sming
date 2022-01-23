04c15e370d9648bb87346d422dfd7cd1f6254b85
mikee47 <mike@sillyhouse.net>
2022-01-23 13:51:00 +0000

    Fetch & patch npcap as pre-requisite
    Code required by both CMake and sming library builds

--- Sming/Components/lwip/src/Arch/Host/arch.mk
+++ Sming/Components/lwip/src/Arch/Host/arch.mk
@@ -3,13 +3,33 @@
 #
 
 ifeq ($(UNAME),Windows)
+
 	LWIP_ARCH_SRCDIR	:= $(LWIP_ARCH_SRCDIR)/Windows
+	NPCAP_SRCDIR		:= $(LWIP_ARCH_SRCDIR)/npcap
+	LWIP_CMAKE_OPTIONS	+= -DNPCAP_SRCDIR=$(NPCAP_SRCDIR)
 	COMPONENT_INCDIRS	+= \
 		lwip/contrib/ports/win32/include \
-		$(LWIP_ARCH_SRCDIR)/src/npcap/Include
+		$(NPCAP_SRCDIR)/Include
+	COMPONENT_PREREQUISITES += $(NPCAP_SRCDIR)/.ok
+	PCAP_SRC := npcap-sdk-1.05.zip
+
+$(NPCAP_SRCDIR)/.ok:
+	@echo Fetching npcap...
+	$(Q) \
+		rm -rf $(@D) && \
+		mkdir -p $(@D) && \
+		cd $(@D) && \
+		powershell -Command "Set-Variable ProgressPreference SilentlyContinue; \
+			Invoke-WebRequest https://nmap.org/npcap/dist/$(PCAP_SRC) -OutFile $(PCAP_SRC); \
+			Expand-Archive $(PCAP_SRC) ." && \
+		$(call ApplyPatch,$(LWIP_ARCH_SRCDIR)/npcap.patch) && \
+		touch $@
+
 else
+
 	COMPONENT_INCDIRS	+= lwip/contrib/ports/unix/port/include
 	LWIP_ARCH_SRCDIR	:= $(LWIP_ARCH_SRCDIR)/Linux
+
 endif
 
 COMPONENT_SRCDIRS		+= $(LWIP_ARCH_SRCDIR)
