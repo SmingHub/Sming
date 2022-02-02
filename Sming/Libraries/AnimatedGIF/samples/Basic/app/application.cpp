7a286e4323dffc2c48911695f1ec682a035167e9
mikee47 <mike@sillyhouse.net>
2022-02-02 17:08:30 +0000

    Basic implementation - should double-buffer

--- Sming/Libraries/AnimatedGIF/samples/Basic/app/application.cpp
+++ Sming/Libraries/AnimatedGIF/samples/Basic/app/application.cpp
@@ -1,142 +1,129 @@
 #include <SmingCore.h>
 #include <AnimatedGIF.h>
+#include <Graphics/SampleConfig.h>
 
+namespace
+{
 AnimatedGIF gif;
-
-#define DISPLAY_WIDTH 240
-#define DISPLAY_HEIGHT 320
-// #define USE_DMA 1
-#define BUFFER_SIZE 256            // Optimum is >= GIF width or integral division of width
-
 Timer playTimer;
+Graphics::Surface* surface;
 
 IMPORT_FSTR(gifData, PROJECT_DIR "/files/frog.gif")
 
-void draw(GIFDRAW *pDraw)
+void draw(GIFDRAW* pDraw)
 {
-//  uint8_t *s;
-//  uint16_t *d, *usPalette;
-//  int x, y, iWidth, iCount;
-//
-//  // Display bounds check and cropping
-//  iWidth = pDraw->iWidth;
-//  if (iWidth + pDraw->iX > DISPLAY_WIDTH)
-//    iWidth = DISPLAY_WIDTH - pDraw->iX;
-//  usPalette = pDraw->pPalette;
-//  y = pDraw->iY + pDraw->y; // current line
-//  if (y >= DISPLAY_HEIGHT || pDraw->iX >= DISPLAY_WIDTH || iWidth < 1 || (max_line > -1 && y > max_line))
-//    return;
-//
-//  // Old image disposal
-//  s = pDraw->pPixels;
-//  if (pDraw->ucDisposalMethod == 2) // restore to background color
-//  {
-//    for (x = 0; x < iWidth; x++)
-//    {
-//      if (s[x] == pDraw->ucTransparent)
-//        s[x] = pDraw->ucBackground;
-//    }
-//    pDraw->ucHasTransparency = 0;
-//  }
-//
-//  // Apply the new pixels to the main image
-//  if (pDraw->ucHasTransparency) // if transparency used
-//  {
-//    uint8_t *pEnd, c, ucTransparent = pDraw->ucTransparent;
-//    pEnd = s + iWidth;
-//    x = 0;
-//    iCount = 0; // count non-transparent pixels
-//    while (x < iWidth)
-//    {
-//      c = ucTransparent - 1;
-//      d = &usTemp[0][0];
-//      while (c != ucTransparent && s < pEnd && iCount < BUFFER_SIZE )
-//      {
-//        c = *s++;
-//        if (c == ucTransparent) // done, stop
-//        {
-//          s--; // back up to treat it like transparent
-//        }
-//        else // opaque
-//        {
-//          *d++ = usPalette[c];
-//          iCount++;
-//        }
-//      } // while looking for opaque pixels
-//      if (iCount) // any opaque pixels?
-//      {
-//        // DMA would degrade performance here due to short line segments
-//        tft->setAddrWindow(pDraw->iX + x, y, iCount, 1);
-//        tft->pushPixels(usTemp, iCount);
-//        x += iCount;
-//        iCount = 0;
-//      }
-//      // no, look for a run of transparent pixels
-//      c = ucTransparent;
-//      while (c == ucTransparent && s < pEnd)
-//      {
-//        c = *s++;
-//        if (c == ucTransparent)
-//          x++;
-//        else
-//          s--;
-//      }
-//    }
-//  }
-//  else
-//  {
-//    s = pDraw->pPixels;
-//
-//    // Unroll the first pass to boost DMA performance
-//    // Translate the 8-bit pixels through the RGB565 palette (already byte reversed)
-//    if (iWidth <= BUFFER_SIZE)
-//      for (iCount = 0; iCount < iWidth; iCount++) usTemp[dmaBuf][iCount] = usPalette[*s++];
-//    else
-//      for (iCount = 0; iCount < BUFFER_SIZE; iCount++) usTemp[dmaBuf][iCount] = usPalette[*s++];
-//
-//#ifdef USE_DMA // 71.6 fps (ST7796 84.5 fps)
-//    tft->dmaWait();
-//    tft->setAddrWindow(pDraw->iX, y, iWidth, 1);
-//    tft->pushPixelsDMA(&usTemp[dmaBuf][0], iCount);
-//    dmaBuf = !dmaBuf;
-//#else // 57.0 fps
-//    tft->setAddrWindow(pDraw->iX, y, iWidth, 1);
-//    tft->pushPixels(&usTemp[0][0], iCount);
-//#endif
-//
-//    iWidth -= iCount;
-//    // Loop if pixel buffer smaller than width
-//    while (iWidth > 0)
-//    {
-//      // Translate the 8-bit pixels through the RGB565 palette (already byte reversed)
-//      if (iWidth <= BUFFER_SIZE)
-//        for (iCount = 0; iCount < iWidth; iCount++) usTemp[dmaBuf][iCount] = usPalette[*s++];
-//      else
-//        for (iCount = 0; iCount < BUFFER_SIZE; iCount++) usTemp[dmaBuf][iCount] = usPalette[*s++];
-//
-//#ifdef USE_DMA
-//      tft->dmaWait();
-//      tft->pushPixelsDMA(&usTemp[dmaBuf][0], iCount);
-//      dmaBuf = !dmaBuf;
-//#else
-//      tft->pushPixels(&usTemp[0][0], iCount);
-//#endif
-//      iWidth -= iCount;
-//    }
-//  }
-} /* GIFDraw() */
+	const auto& tftSize = tft.getSize();
+	const int DISPLAY_WIDTH = tftSize.w;
+	const int DISPLAY_HEIGHT = tftSize.w;
+
+	auto pixelFormat = tft.getPixelFormat();
+	auto bytesPerPixel = Graphics::getBytesPerPixel(pixelFormat);
+
+	uint16_t usTemp[DISPLAY_WIDTH];
+	Graphics::SharedBuffer buffer(bytesPerPixel * DISPLAY_WIDTH);
+
+	int iWidth = pDraw->iWidth;
+	if(iWidth + pDraw->iX > DISPLAY_WIDTH) {
+		iWidth = DISPLAY_WIDTH - pDraw->iX;
+	}
+	const uint16_t* usPalette = pDraw->pPalette;
+	int y = pDraw->iY + pDraw->y; // current line
+	if(y >= DISPLAY_HEIGHT || pDraw->iX >= DISPLAY_WIDTH || iWidth < 1) {
+		return;
+	}
+
+	auto s = pDraw->pPixels;
+	if(pDraw->ucDisposalMethod == 2) // restore to background color
+	{
+		for(int x = 0; x < iWidth; x++) {
+			if(s[x] == pDraw->ucTransparent) {
+				s[x] = pDraw->ucBackground;
+			}
+		}
+		pDraw->ucHasTransparency = 0;
+	}
+
+	// Apply the new pixels to the main image
+	if(pDraw->ucHasTransparency) // if transparency used
+	{
+		uint8_t ucTransparent = pDraw->ucTransparent;
+		uint8_t* pEnd = s + iWidth;
+		int x = 0;
+		int iCount = 0; // count non-transparent pixels
+		while(x < iWidth) {
+			uint8_t c = ucTransparent - 1;
+			uint16_t* d = usTemp;
+			while(c != ucTransparent && s < pEnd) {
+				c = *s++;
+				if(c == ucTransparent) {
+					// done, stop: back up to treat it like transparent
+					s--;
+				} else {
+					// opaque
+					*d++ = __builtin_bswap16(usPalette[c]);
+					iCount++;
+				}
+			}				// while looking for opaque pixels
+			if(iCount != 0) // any opaque pixels?
+			{
+				Graphics::convert(usTemp, Graphics::PixelFormat::RGB565, buffer.get(), pixelFormat, iCount);
+				Graphics::Rect r(pDraw->iX + x, y, iCount, 1);
+				surface->reset();
+				surface->setAddrWindow(r);
+				surface->writeDataBuffer(buffer, 0, iCount * bytesPerPixel);
+				surface->present();
+				x += iCount;
+				iCount = 0;
+			}
+			// no, look for a run of transparent pixels
+			c = ucTransparent;
+			while(c == ucTransparent && s < pEnd) {
+				c = *s++;
+				if(c == ucTransparent) {
+					iCount++;
+				} else {
+					s--;
+				}
+			}
+			if(iCount != 0) {
+				// skip these
+				x += iCount;
+				iCount = 0;
+			}
+		}
+	} else {
+		// No transparency
+		s = pDraw->pPixels;
+		// Translate the 8-bit pixels through the RGB565 palette (already byte reversed)
+		for(int x = 0; x < iWidth; x++) {
+			usTemp[x] = __builtin_bswap16(usPalette[*s++]);
+		}
+		Graphics::convert(usTemp, Graphics::PixelFormat::RGB565, buffer.get(), tft.getPixelFormat(), iWidth);
+		Graphics::Rect r(pDraw->iX, y, iWidth, 1);
+		surface->reset();
+		surface->setAddrWindow(r);
+		surface->writeDataBuffer(buffer, 0, iWidth * bytesPerPixel);
+		surface->present();
+	}
+}
 
 void nextFrame()
 {
-    gif.playFrame(true, nullptr);
+	gif.playFrame(true, nullptr);
 }
+
+} // namespace
 
 void init()
 {
-    // TODO: setup display
+	Serial.begin(SERIAL_BAUD_RATE);
+	Serial.systemDebugOutput(true);
 
-    auto binaryData = String(gifData);
-    gif.open(reinterpret_cast<uint8_t*>(const_cast<char*>(binaryData.c_str())), gifData.length(), draw);
+	initDisplay();
+	tft.setOrientation(Graphics::Orientation::deg270);
+	surface = tft.createSurface();
 
-    playTimer.initializeMs(100, nextFrame).start();
+	gif.openFLASH((uint8_t*)gifData.data(), gifData.length(), draw);
+
+	playTimer.initializeMs(500, nextFrame).start();
 }
