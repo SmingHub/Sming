658cf597afc6494546ef50d2958f58509c8474bf
Slavey Karadzhov <slav@attachix.com>
2022-05-02 11:46:40 +0200

    Added Switch Joycon sample.

---
+++ Sming/Libraries/SwitchJoycon/src/SwitchJoyconConnection.h
@@ -0,0 +1,36 @@
+#ifndef ESP32_BLE_CONNECTION_STATUS_H
+#define ESP32_BLE_CONNECTION_STATUS_H
+#include "sdkconfig.h"
+#if defined(CONFIG_BT_ENABLED)
+
+#include "nimconfig.h"
+#if defined(CONFIG_BT_NIMBLE_ROLE_PERIPHERAL)
+
+#include <NimBLEServer.h>
+#include <NimBLECharacteristic.h>
+
+class SwitchJoyconConnection : public NimBLEServerCallbacks
+{
+public:
+	using Callback = Delegate<void(NimBLEServer& server)>;
+
+	bool connected{false};
+	NimBLECharacteristic* inputGamepad{nullptr};
+
+	SwitchJoyconConnection(Callback onConnected = nullptr, Callback onDisconnected = nullptr)
+	{
+		connectCallback = onConnected;
+		disconnectCallback = onDisconnected;
+	}
+
+	void onConnect(NimBLEServer* pServer);
+	void onDisconnect(NimBLEServer* pServer);
+
+private:
+	Callback connectCallback;
+	Callback disconnectCallback;
+};
+
+#endif // CONFIG_BT_NIMBLE_ROLE_PERIPHERAL
+#endif // CONFIG_BT_ENABLED
+#endif // ESP32_BLE_CONNECTION_STATUS_H
