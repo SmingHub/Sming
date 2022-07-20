c126bb72551ae4d68abbb910cb6d8d58fb917f88
Mike <mike@sillyhouse.net>
2022-07-20 08:15:34 +0100

    Implement basic ESP32 deep sleep (#2527)
    * Propagate return value from `system_deep_sleep`
    
    * Implement basic ESP32 deep sleep

--- Sming/Arch/Host/Components/esp_hal/sleep.c
+++ Sming/Arch/Host/Components/esp_hal/sleep.c
@@ -1,7 +1,8 @@
 #include "include/esp_sleep.h"
 
-void system_deep_sleep(uint32_t time_in_us)
+bool system_deep_sleep(uint32_t time_in_us)
 {
+	return false;
 }
 
 bool system_deep_sleep_set_option(uint8_t option)
