a9cb48a3e3a4801203d73b3a45fcbe06f2989fa6
mikee47 <mike@sillyhouse.net>
2022-09-20 16:26:33 +0100

    fix malloc-count

--- Sming/Components/malloc_count/component.mk
+++ Sming/Components/malloc_count/component.mk
@@ -8,21 +8,21 @@ ifeq ($(ENABLE_MALLOC_COUNT),1)
 COMPONENT_CXXFLAGS += -DENABLE_MALLOC_COUNT=1
 
 # Hook all the memory allocation functions we need to monitor heap activity
-ifeq ($(SMING_ARCH),Esp8266)
-MC_WRAP_FUNCS := \
-	pvPortMalloc \
-	pvPortCalloc \
-	pvPortRealloc \
-	pvPortZalloc \
-	pvPortZallocIram \
-	vPortFree
-else
 MC_WRAP_FUNCS := \
 	malloc \
 	calloc \
 	realloc \
 	free \
 	strdup
+ifeq ($(SMING_ARCH),Esp8266)
+MC_WRAP_FUNCS += \
+	realloc \
+	pvPortMalloc \
+	pvPortCalloc \
+	pvPortRealloc \
+	pvPortZalloc \
+	pvPortZallocIram \
+	vPortFree
 endif
 
 EXTRA_LDFLAGS := $(call UndefWrap,$(MC_WRAP_FUNCS))
