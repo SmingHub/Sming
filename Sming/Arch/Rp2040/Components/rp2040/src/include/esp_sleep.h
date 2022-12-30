c40d6097c86d7ad45cb60f7c7ea1e346cdf0e334
mikee47 <mike@silyhouse.net>
2022-12-30 11:11:54 +0000

    Add prototypes and stubs for wifi sleep functions

--- Sming/Arch/Rp2040/Components/rp2040/src/include/esp_sleep.h
+++ Sming/Arch/Rp2040/Components/rp2040/src/include/esp_sleep.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <c_types.h>
+#include <driver/gpio.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -14,6 +15,20 @@ enum sleep_type {
 
 bool system_deep_sleep(uint32_t time_in_us);
 bool system_deep_sleep_set_option(uint8_t option);
+
+enum sleep_level {
+	MIN_SLEEP_T,
+	MAX_SLEEP_T,
+};
+
+void wifi_enable_gpio_wakeup(uint32_t i, enum GPIO_INT_TYPE intr_status);
+void wifi_disable_gpio_wakeup(void);
+bool wifi_set_sleep_type(enum sleep_type type);
+enum sleep_type wifi_get_sleep_type(void);
+bool wifi_set_sleep_level(enum sleep_level level);
+enum sleep_level wifi_get_sleep_level(void);
+bool wifi_set_listen_interval(uint8_t interval);
+uint8_t wifi_get_listen_interval(void);
 
 #ifdef __cplusplus
 }
--- Sming/Arch/Rp2040/Components/rp2040/src/sleep.c
+++ Sming/Arch/Rp2040/Components/rp2040/src/sleep.c
@@ -9,3 +9,45 @@ bool system_deep_sleep_set_option(uint8_t option)
 {
 	return false;
 }
+
+/* GPIO */
+
+void wifi_enable_gpio_wakeup(uint32_t i, enum GPIO_INT_TYPE intr_status)
+{
+}
+
+void wifi_disable_gpio_wakeup(void)
+{
+}
+
+/* WiFi */
+
+bool wifi_set_sleep_type(enum sleep_type type)
+{
+	return type == NONE_SLEEP_T;
+}
+
+enum sleep_type wifi_get_sleep_type(void)
+{
+	return NONE_SLEEP_T;
+}
+
+bool wifi_set_sleep_level(enum sleep_level level)
+{
+	return false;
+}
+
+enum sleep_level wifi_get_sleep_level(void)
+{
+	return MIN_SLEEP_T;
+}
+
+bool wifi_set_listen_interval(uint8_t interval)
+{
+	return false;
+}
+
+uint8_t wifi_get_listen_interval(void)
+{
+	return 0;
+}
