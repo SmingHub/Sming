09cc1b72f11888527fcdacf5e6e3c11bb7c5bdcf
mikee47 <mike@silyhouse.net>
2023-02-06 13:32:20 +0000

    Fix bootloader flash speed settings

--- Sming/Arch/Esp32/Components/esp32/component.mk
+++ Sming/Arch/Esp32/Components/esp32/component.mk
@@ -365,6 +365,10 @@ $(SDK_CONFIG_DEFAULTS): $(SDK_CUSTOM_CONFIG_PATH)
 	$(Q) $(foreach f,$(SDK_CONFIG_FILES),\
 		$(if $(wildcard $f),cat $f >> $@;) \
 	)
+	$(Q) printf "\n# ESPTOOL (bootloader) settings\n" >> $@
+	$(Q) echo "CONFIG_ESPTOOLPY_FLASHMODE_$(SPI_MODE)=y" >> $@
+	$(Q) echo "CONFIG_ESPTOOLPY_FLASHFREQ_$(SPI_SPEED)M=y" >> $@
+	$(Q) echo "CONFIG_ESPTOOLPY_FLASHSIZE_$(SPI_SIZE)B=y" >> $@
 
 ##@Configuration
 
