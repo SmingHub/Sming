a2f962891e138a68003ea50f713cbf7a29fe2f91
Peter Jakobs <pjakobs@mobileme.de>
2023-02-12 11:32:40 +0100

    implemented requested changes from initial PR (now *with* changes. Sigh)

--- Sming/Arch/Esp32/Core/HardwarePWM.cpp
+++ Sming/Arch/Esp32/Core/HardwarePWM.cpp
@@ -7,126 +7,10 @@
  * HardwarePWM.cpp
  *
  * Original Author: https://github.com/hrsavla
- * Esp32 version:   https://github.com/pljakobs
  *
- * This HardwarePWM library enables Sming framework uses to use the ESP32 ledc PWM api
- * 
- * the ESP32 PWM Hardware is much more powerful than the ESP8266, allowing wider PWM timers (up to 20 bit)
- * as well as much higher PWM frequencies (up to 40MHz for a 1 Bit wide PWM)
- * 
- * Overview:
- * +------------------------------------------------------------------------------------------------+
- * | LED_PWM                                                                                        |
- * |  +-------------------------------------------+   +-------------------------------------------+ |
- * |  | High_Speed_Channels¹                      |   | Low_Speed_Channels                        | |
- * |  |                   +-----+     +--------+  |   |                   +-----+     +--------+  | |
- * |  |                   |     | --> | h_ch 0 |  |   |                   |     | --> | l_ch 0 |  | |
- * |  | +-----------+     |     |     +--------+  |   | +-----------+     |     |     +--------+  | |
- * |  | | h_timer 0 | --> |     |                 |   | | l_timer 0 | --> |     |                 | |
- * |  | +-----------+     |     |     +--------+  |   | +-----------+     |     |     +--------+  | |
- * |  |                   |     | --> | h_ch 1 |  |   |                   |     | --> | l_ch 1 |  | |
- * |  |                   |     |     +--------+  |   |                   |     |     +--------+  | |
- * |  |                   |     |                 |   |                   |     |                 | |
- * |  |                   |     |     +--------+  |   |                   |     |     +--------+  | |
- * |  |                   |     | --> | h_ch 2 |  |   |                   |     | --> | l_ch 2 |  | |
- * |  | +-----------+     |     |     +--------+  |   | +-----------+     |     |     +--------+  | |
- * |  | | h_timer 1 | --> |     |                 |   | | l_timer 1 | --> |     |                 | |
- * |  | +-----------+     |     |     +--------+  |   | +-----------+     |     |     +--------+  | |
- * |  |                   |     | --> | h_ch 3 |  |   |                   |     | --> | l_ch 3 |  | |
- * |  |                   |     |     +--------+  |   |                   |     |     +--------+  | |
- * |  |                   | MUX |                 |   |                   | MUX |                 | |
- * |  |                   |     |     +--------+  |   |                   |     |     +--------+  | |
- * |  |                   |     | --> | h_ch 4 |  |   |                   |     | --> | l_ch 4 |  | |
- * |  | +-----------+     |     |     +--------+  |   | +-----------+     |     |     +--------+  | |
- * |  | | h_timer 2 | --> |     |                 |   | | l_timer 2 | --> |     |                 | |
- * |  | +-----------+     |     |     +--------+  |   | +-----------+     |     |     +--------+  | |
- * |  |                   |     | --> | h_ch 5 |  |   |                   |     | --> | l_ch 5 |  | |
- * |  |                   |     |     +--------+  |   |                   |     |     +--------+  | |
- * |  |                   |     |                 |   |                   |     |                 | |
- * |  |                   |     |     +--------+  |   |                   |     |     +--------+  | |
- * |  |                   |     | --> | h_ch 6 |  |   |                   |     | --> | l_ch 6²|  | |
- * |  | +-----------+     |     |     +--------+  |   | +-----------+     |     |     +--------+  | |
- * |  | | h_timer 3 | --> |     |                 |   | | l_timer 3 | --> |     |                 | |
- * |  | +-----------+     |     |     +--------+  |   | +-----------+     |     |     +--------+  | |
- * |  |                   |     | --> | h_ch 7 |  |   |                   |     | --> | l_ch 7²|  | |
- * |  |                   |     |     +--------+  |   |                   |     |     +--------+  | |
- * |  |                   +-----+                 |   |                   +-----+                 | |
- * |  +-------------------------------------------+   +-------------------------------------------+ |
- * +------------------------------------------------------------------------------------------------+
- * ¹ High speed channels are only available when SOC_LEDC_SUPPORT_HS_MODE is defined as 1
- * ² The ESP32C3 does only support six channels, so 6 and 7 are not available on that SoC
- * 
- * The nomenclature of timers in the high speed / low speed blocks is a bit misleading as the idf api 
- * speaks of "speed mode", which, to me, implies that this would be a mode configurable in a specific timer
- * while in reality, it does select a block of timers. I am considering re-naming that to "speed mode block"
- * in my interface impmenentation.
- * 
- * As an example, I would use
- * setTimerFrequency(speedModeBlock, timer, frequency);
- * 
- * ToDo: see, how this can be implemented to provide maximum overlap with the RP2040 pwm hardware so code does 
- * not become overly SoC specific.
- * 
- * Maximum Timer width for PWM:
- * ============================
- * esp32   SOC_LEDC_TIMER_BIT_WIDE_NUM  (20)
- * esp32c3 SOC_LEDC_TIMER_BIT_WIDE_NUM  (14)
- * esp32s2 SOC_LEDC_TIMER_BIT_WIDE_NUM  (14)
- * esp32s3 SOC_LEDC_TIMER_BIT_WIDE_NUM  (14)
- * 
- * Number of Channels:
- * ===================
- * esp32   SOC_LEDC_CHANNEL_NUM         (8)
- * esp32c3 SOC_LEDC_CHANNEL_NUM         (6)
- * esp32s2 SOC_LEDC_CHANNEL_NUM         (8)
- * esp32s3 SOC_LEDC_CHANNEL_NUM 		(8)
- * 
- * Some SoSs support a mode called HIGHSPEED_MODE which is essentially another full block of PWM hardware 
- * that adds SOC_LEDC_CHANNEL_NUM channels. 
- * Those Architectures have SOC_LEDC_SUPPORT_HS_MODE defined as 1.
- * In esp-idf-4.3 that's currently only the esp32 SOC 
- * 
- * Supports highspeed mode:
- * ========================
- * esp32 SOC_LEDC_SUPPORT_HS_MODE	(1)
- *
- * ToDo: implement awareness of hs mode availablility
- * ==================================================
- * currently, the code just uses a % 8 operation on the pin index to calculate whether to assign a pin to either
- * high speed or low speed pwm blocks. This doesn't make a whole lot of sense since it makes it impossible
- * for Sming devs to actually use the functionality behind it. 
- * Also, it currently does not reflect the fact that different SOCs have a different number of channels per block
- * (specifically, the esp32c3 only has six channels and no highspeed mode). 
- * I will continue in two ways: 
- * - implement the "vanilla" Sming HardwarePWM i
- nterface that will hide the underlying architecture but allow up to 16 
- *   channels on an ESP32 
- * - implement overloads for the relevant functions that allow selecting hs mode where applicable. 
- * 
- * ToDo: implement PWM bit width control
- * =====================================
- * the current HardwarePWM implementation does not care about the PWM timer bit width. To leverage the functionality 
- * of the ESP32 hardware, it's necessary to make this configurable. As the width is per timer and all the Sming defined
- * functions are basically per pin, this needs to be an extension of the overal model, exposing at least timers. 
- * This, too, will require a compatible "basic" interface and an advanced interface that allows assiging pins (channels) 
- * to timers and the configuration of the timers themselves. 
- * The esp_idf does not provide a way to read the bit width configured for a channel, but I think it'll be useful to be able
- * to read back this value, not least to find the value for getMaxDuty() for a channel. It will either have to be stored in the
- * module or maybe read from the hardware directly (LEDC_[HL]STIMERx_CONF_REG & 0x1f)
- * 
- * hardware technical reference: 
- * =============================
- * https://www.espressif.com/sites/default/files/documentation/esp32_technical_reference_manual_en.pdf#ledpwm
- * 
- * Overview of the whole ledc-system here: 
- * https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/ledc.html
- * 
- * the ledc interface also exposes some advanced functions such as fades that are then done in hardware. 
- * ToDo: implement a Sming interface for fades
- * 
-
-
-
+ * This HardwarePWM library enables Sming framework user to use ESP SDK PWM API
+ * Period of PWM is fixed to 1000us / Frequency = 1khz
+ * Duty at 100% = 22222. Duty at 0% = 0
  * You can use function setPeriod() to change frequency/period.
  * Calculate the max duty as per the formulae give in ESP8266 SDK
  * Max Duty = (Period * 1000) / 45
@@ -145,8 +29,17 @@
 //#include <soc/soc_caps.h>
 #include "driver/ledc.h"
 #include "esp_err.h"
+#include "hal/ledc_types.h"
 #include <HardwarePWM.h>
 
+namespace{
+	ledc_mode_t pinToGroup(uint8_t pin);
+	ledc_channel_t pinToChannel(uint8_t pin);
+	ledc_timer_t pinToTimer(uint8_t pin);
+	uint32_t periodToFrequency(uint32_t period);
+	uint32_t frequencyToPeriod(uint32_t freq);
+	uint32_t maxDuty(ledc_timer_bit_t bits);
+}
 
 // orig #include <HardwarePWM.h>
 
@@ -172,53 +65,52 @@ HardwarePWM::HardwarePWM(uint8_t* pins, uint8_t no_of_pins) : channel_count(no_o
 {
 	ledc_timer_config_t ledc_timer;
 	ledc_channel_config_t ledc_channel;
-	debugf("starting HardwarePWM init");
+	debug_d("starting HardwarePWM init");
 	periph_module_enable(PERIPH_LEDC_MODULE);
-	if((no_of_pins > 0) && (no_of_pins < SOC_LEDC_CHANNEL_NUM))
+	if((no_of_pins == 0) || (no_of_pins > SOC_LEDC_CHANNEL_NUM))
 	{
-		uint32_t io_info[SOC_LEDC_CHANNEL_NUM][3];	// pin information
-		uint32_t pwm_duty_init[SOC_LEDC_CHANNEL_NUM]; // pwm duty
-		for(uint8_t i = 0; i < no_of_pins; i++) {
-			/* todo: update the digitalPins map
-			io_info[i][0] = EspDigitalPins[pins[i]].mux;
-			io_info[i][1] = EspDigitalPins[pins[i]].gpioFunc;
-			io_info[i][2] = EspDigitalPins[pins[i]].id;
-			*/
-			pwm_duty_init[i] = 0; // Start with zero output
-			channels[i] = pins[i];
+		// is there a return code for this?
+		return ;
+	}
+	uint32_t io_info[SOC_LEDC_CHANNEL_NUM][3];	// pin information
+	uint32_t pwm_duty_init[SOC_LEDC_CHANNEL_NUM]; // pwm duty
+	for(uint8_t i = 0; i < no_of_pins; i++) {
+		
+		pwm_duty_init[i] = 0; // Start with zero output
+		channels[i] = pins[i];
 
-			/* 
-			/  Prepare and then apply the LEDC PWM timer configuration
-			/  this may cofigure the same timer more than once (in fact up to 8 times)
-			/  which should not be an issue, though, since the values should be the same for all timers
-			*/
-			ledc_timer.speed_mode      = pinToGroup(i);  // the two groups (if available) are operating in different speed modes, hence speed mode is an alias for group or vice versa
-			ledc_timer.timer_num       = pinToTimer(i);
-			ledc_timer.duty_resolution = LEDC_TIMER_10_BIT; // todo: make configurable later
-			ledc_timer.freq_hz         = periodToFrequency(DEFAULT_PERIOD); // todo: make configurable later
-			ledc_timer.clk_cfg         = LEDC_AUTO_CLK;
+		/* 
+		/  Prepare and then apply the LEDC PWM timer configuration
+		/  this may cofigure the same timer more than once (in fact up to 8 times)
+		/  which should not be an issue, though, since the values should be the same for all timers
+		*/
+		ledc_timer.speed_mode      = pinToGroup(i);  // the two groups (if available) are operating in different speed modes, hence speed mode is an alias for group or vice versa
+		ledc_timer.timer_num       = pinToTimer(i);
+		ledc_timer.duty_resolution = LEDC_TIMER_10_BIT; // todo: make configurable later
+		ledc_timer.freq_hz         = periodToFrequency(DEFAULT_PERIOD); // todo: make configurable later
+		ledc_timer.clk_cfg         = LEDC_AUTO_CLK;
 
-			debugf("ledc_timer.\n\tspeed_mode: %i\n\ttimer_num: %i\n\tduty_resolution: %i\n\tfreq: %i\n\tclk_cfg: %i\n\n",
-				   (uint32_t)ledc_timer.speed_mode, (uint32_t)ledc_timer.timer_num,
-				   (uint32_t)ledc_timer.duty_resolution, (uint32_t)ledc_timer.freq_hz, (uint32_t)ledc_timer.clk_cfg);
-			ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+		debug_d("ledc_timer.\n\tspeed_mode: %i\n\ttimer_num: %i\n\tduty_resolution: %i\n\tfreq: %i\n\tclk_cfg: %i\n\n",
+				(uint32_t)ledc_timer.speed_mode, (uint32_t)ledc_timer.timer_num,
+				(uint32_t)ledc_timer.duty_resolution, (uint32_t)ledc_timer.freq_hz, (uint32_t)ledc_timer.clk_cfg);
+		ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
 
-			/*
-			/   Prepare and then apply the LEDC PWM channel configuration
-			*/
-			ledc_channel.speed_mode     = pinToGroup(i);
-			ledc_channel.channel        = pinToChannel(i);
-			ledc_channel.timer_sel      = pinToTimer(i);
-			ledc_channel.intr_type      = LEDC_INTR_DISABLE;
-			ledc_channel.gpio_num       = pins[i];
-			ledc_channel.duty           = 0; // Set duty to 0%
-			ledc_channel.hpoint         = 0;
-			debugf("ledc_channel\n\tspeed_mode: %i\n\tchannel: %i\n\ttimer_sel %i\n\tinr_type: %i\n\tgpio_num: "
-				   "%i\n\tduty: %i\n\thpoint: %i\n\n",
-				   pinToGroup(i) , pinToChannel(i), pinToTimer(i), LEDC_INTR_DISABLE, pins[i], 0, 0);
-			ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
-			ledc_bind_channel_timer(pinToGroup(i), pinToChannel(i), pinToTimer(i));
-		}
+		/*
+		/   Prepare and then apply the LEDC PWM channel configuration
+		*/
+		ledc_channel.speed_mode     = pinToGroup(i);
+		ledc_channel.channel        = pinToChannel(i);
+		ledc_channel.timer_sel      = pinToTimer(i);
+		ledc_channel.intr_type      = LEDC_INTR_DISABLE;
+		ledc_channel.gpio_num       = pins[i];
+		ledc_channel.duty           = 0; // Set duty to 0%
+		ledc_channel.hpoint         = 0;
+		debug_d("ledc_channel\n\tspeed_mode: %i\n\tchannel: %i\n\ttimer_sel %i\n\tinr_type: %i\n\tgpio_num: "
+				"%i\n\tduty: %i\n\thpoint: %i\n\n",
+				pinToGroup(i) , pinToChannel(i), pinToTimer(i), LEDC_INTR_DISABLE, pins[i], 0, 0);
+		ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+		ledc_bind_channel_timer(pinToGroup(i), pinToChannel(i), pinToTimer(i));
+
 		maxduty=maxDuty(DEFAULT_RESOLUTION);
 		const int initial_period=DEFAULT_PERIOD;
 	}
@@ -226,7 +118,11 @@ HardwarePWM::HardwarePWM(uint8_t* pins, uint8_t no_of_pins) : channel_count(no_o
 
 HardwarePWM::~HardwarePWM()
 {
-	// There is no function in the SDK to stop PWM output, yet.
+	for(uint8_t i = 0; i < no_of_pins; i++) {
+		//stop pwm for all pins and set idle level to 0.
+		
+	ledc_stop(pinToGroup(pins[i]), pinToChannel(pins[i]), uint32_t 0); 
+	}
 }
 
 /* Function Name: getChannel
@@ -237,7 +133,7 @@ uint8_t HardwarePWM::getChannel(uint8_t pin)
 {
 	for(uint8_t i = 0; i < channel_count; i++) {
 		if(channels[i] == pin) {
-			//debugf("getChannel %d is %d", pin, i);
+			//debug_d("getChannel %d is %d", pin, i);
 			return i;
 		}
 	}
@@ -277,7 +173,7 @@ bool HardwarePWM::setDutyChan(uint8_t chan, uint32_t duty, bool update)
 		}
 		return true;
 	} else {
-		debugf("Duty cycle value too high for current period.");
+		debug_d("Duty cycle value too high for current period.");
 		return false;
 	}
 }
@@ -330,11 +226,15 @@ ledc_timer_t HardwarePWM::pinToTimer(uint8_t pin){
 }
 
 uint32_t HardwarePWM::periodToFrequency(uint32_t period){
-	return (1000000 / period);
+	if(period!=0){
+		return (1000000 / period);
+	}
 }
 
 uint32_t HardwarePWM::frequencyToPeriod(uint32_t freq){
-	return (1000000 / freq);
+	if(freq!=0){
+		return (1000000 / freq);
+	}
 }
 
 uint32_t HardwarePWM::maxDuty(ledc_timer_bit_t bits){
