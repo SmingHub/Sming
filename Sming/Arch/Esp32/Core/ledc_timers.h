6eceda35892fa649e93f8092df5514311e32ab3e
Peter Jakobs <pjakobs@mobileme.de>
2023-04-03 19:51:38 +0200

    added class to abstractly manage timers

---
+++ Sming/Arch/Esp32/Core/ledc_timers.h
@@ -0,0 +1,32 @@
+#ifndef LEDC_TIMERS_H
+#define LEDC_TIMERS_H
+#include <debug_progmem.h>
+#include "driver/ledc.h"
+#include "esp_err.h"
+#include "singleton.h"
+#include <hal/ledc_types.h>
+
+class ledc_timer {
+    public:
+        esp_err_t ledc_timer(ledc_mode_t mode, ledc_timer_bit_t duty_resolution, ledc_timer_bit_t bit_num, uint32_t freq, ledc_clk_cfg_t clk_cfg);
+        esp_err_t ~ledc_timer(void);
+        esp_err_t setTimer(uint32_t clock_divider, uint32_t duty_resolution, ledc_clk_src_t clk_src);
+        esp_err_t setTimerFrequency(uint32_t freq);
+        esp_err_t timerReset(void);
+        esp_err_t timerPause(void);
+        esp_err_t timerResume(void);
+        esp_err_t timerBindChannel(ledc_channel_t channel);
+
+        ledc_timer_t getTimerNumber(void);
+        ledc_mode_t getTimerMode(void);
+
+        uint32_t getDutyResolution(void);        
+        uint32_t getTimerFrequency(void);
+        uint32_t getClockDivider(void);
+    private:
+    ledc_timer_config_t timer_conf;
+    ledc_timer_t timer;
+    uint32_t clock_divider;
+}
+
+#endif
