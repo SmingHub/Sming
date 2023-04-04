1025cd83d2f4aed1d996850ebbc08068af2ff4a5
Peter Jakobs <pjakobs@mobileme.de>
2023-04-04 07:44:07 +0200

    more channel work

--- Sming/Arch/Esp32/Core/ledc_channel.cpp
+++ Sming/Arch/Esp32/Core/ledc_channel.cpp
@@ -32,34 +32,47 @@ ledc_channel::ledc_channel(ledc_speedmode_t mode){
 ledc_channel::~ledc_channel(){
     stop((uint32_t)0);
 }
-
+/*
 ledc_channel::channelConfig(const ledc_channel_config_t *ledc_conf){
 
 }
-
+*/
 ledc_channel::updateDuty(){
-
+    return ledc_update_duty();
 }
-ledc_channel::setPin(int gpio_num){
 
+ledc_channel::setPin(int gpio_num){
+    esp_err_t err = ledc_set_pin(gpio_num, channel_config.mode, channel_config.channel);
+    if(esp_err==ESP_OK){
+        channel_config.gpio_num=gpio_num;
+    }
+    return err;
 }
 
 ledc_channel::stop(uint32_t idle_level){
-
-}
-
-ledc_channel::setFreq(uint32_t freq){
-
+    return ledc_stop(channel_config.mode, channel_config.channel, idle_level);
 }
 
 ledc_channel::setDutyWithHpoint(uint32_t duty, uint32_t hpoint){
-
+    esp_err_t err = ledc_set_duty_with_hpoint(channel_config.mode, chanel_config.channel, duty, hpoint);
+    if(err==ESP_OK){
+        channel_config.duty=duty;
+        channel_config.hpoint=hpoint;
+    }
+    return err;
 }
 
 ledc_channel::setDuty(uint32_t duty){
-
+    esp_err_t err=ledc_set_duty(channel_config.mode, channel_config.channel, duty);
+    if(err==ESP_OK){
+        channel_config.duty=duty;
+    }
+    return err;
 }
 
 ledc_channel::bindChannelTimer(ledc_timer_t timer){
-
+    esp_err_t err = ledc_bind_channel_timer(channel_config.mode, channel_config.channel, timer);
+    if(err==ESP_OK){
+        channel_config.timer=timer;
+    }
 }
