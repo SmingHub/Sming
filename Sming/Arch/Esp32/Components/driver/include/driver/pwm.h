e1413ff0f780d77983e26688cf698bb8bd144d50
Peter Jakobs <pjakobs@mobileme.de>
2023-04-23 14:18:22 +0200

    corrected the PWM_CHANNEL_NUM_MAX define in pwm.h to reflect the correct SoC config

--- Sming/Arch/Esp32/Components/driver/include/driver/pwm.h
+++ Sming/Arch/Esp32/Components/driver/include/driver/pwm.h
@@ -1,3 +1,18 @@
+/****
+ * Sming Framework Project - Open Source framework for high efficiency native ESP8266 development.
+ * Created 2015 by Skurydin Alexey
+ * http://github.com/SmingHub/Sming
+ * All files of the Sming Core are provided under the LGPL v3 license.
+ *
+ * pwm.h
+ *
+ ****/
+
 #pragma once
 
-#define PWM_CHANNEL_NUM_MAX 8
+#ifdef SOC_LEDC_CHANNEL_NUM
+    #define PWM_CHANNEL_NUM_MAX SOC_LEDC_CHANNEL_NUM
+#else
+    // this should not happen if the correct esp32 includes are used, just to be absolutely sure
+    #define PWM_CHANNEL_NUM_MAX 8
+#endif
