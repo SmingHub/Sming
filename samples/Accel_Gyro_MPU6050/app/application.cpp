1a1c3ced0880ab346272e06f417146c428b7432f
xeonqq <xeonqq@gmail.com>
2023-07-28 15:01:19 +0200

    Refactor application.cpp for MPU6050 sample

--- samples/Accel_Gyro_MPU6050/app/application.cpp
+++ samples/Accel_Gyro_MPU6050/app/application.cpp
@@ -1,24 +1,13 @@
 #include <Libraries/MPU6050/MPU6050.h>
 #include <SmingCore.h>
 
-constexpr float mainLoopInterval = 0.02; // sec
+constexpr uint16_t mainLoopInterval = 20; // ms
 SimpleTimer mainLoopTimer;
 MPU6050 mpu;
 
 void printAccelGyro(int16_t ax, int16_t ay, int16_t az, int16_t gx, int16_t gy, int16_t gz)
 {
-	Serial.print("a/g:\t");
-	Serial.print(ax);
-	Serial.print("\t");
-	Serial.print(ay);
-	Serial.print("\t");
-	Serial.print(az);
-	Serial.print("\t");
-	Serial.print(gx);
-	Serial.print("\t");
-	Serial.print(gy);
-	Serial.print("\t");
-	Serial.println(gz);
+	Serial << "a/g:\t" << ax << "\t" << ay << "\t" << az << "\t" << gx << "\t" << gy << "\t" << gz << endl;
 }
 
 void mainLoop()
@@ -38,5 +27,5 @@ void init()
 	mpu.initialize();
 	Serial.println(mpu.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 
-	mainLoopTimer.initializeMs(static_cast<int>(mainLoopInterval * 1000), mainLoop).start();
+	mainLoopTimer.initializeMs<mainLoopInterval>(mainLoop).start();
 }
