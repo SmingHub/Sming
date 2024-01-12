517312948d633e1a36e075b9d0908ba829391224
Mike <mike@sillyhouse.net>
2024-01-12 10:25:45 +0000

    Fix bluetooth build with esp32s3 for latest IDF 4.4 (#2708)
    It appears esp32s3 just uses esp32c3 for all IDF versions now.

--- Sming/Arch/Esp32/Components/esp32/component.mk
+++ Sming/Arch/Esp32/Components/esp32/component.mk
@@ -169,7 +169,7 @@ endif
 endif
 
 ifeq ($(ENABLE_BLUETOOTH),1)
-ifeq (esp32s3-1,$(ESP_VARIANT)-$(IDF_VERSION_5x))
+ifeq (esp32s3,$(ESP_VARIANT))
 ESP_BT_VARIANT := esp32c3
 else
 ESP_BT_VARIANT := $(ESP_VARIANT)
