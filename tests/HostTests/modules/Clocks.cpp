SmingHub/Sming

commit 4d56649f28be1fbcd5ac1350768c62599618ca8a

Fix esp32 clock definitions (#2456)
#2455 changed the timer2 timebase so it no longer corresponds with the microsecond system clock used for OsTimer.

Added check in HostTests to catch this.
Also improve Host CPU clock emulation to accurately reflect selected frequency.

Also fix rp2040 esp_get_ccount return value, now passes tests.

tests/HostTests/modules/Clocks.cpp
@@ -6,6 +6,7 @@
 
 #include <HostTests.h>
 #include <Platform/Timers.h>
+#include <HardwareTimer.h>
 
 template <class Clock, typename TimeType> class ClockTestTemplate : public TestGroup
 {
@@ -37,8 +38,8 @@ public:
 		TEST_CASE("vs. system time")
 		{
 			constexpr uint32_t duration{2000000};
-			auto startTicks = Clock::ticks();
 			auto startTime = system_get_time();
+			auto startTicks = Clock::ticks();
 			uint32_t time;
 			while((time = system_get_time()) < startTime + duration) {
 				//
@@ -48,7 +49,12 @@ public:
 			debug_w("System time elapsed: %u", time - startTime);
 			debug_w("%s ticks: %u", Clock::typeName(), endTicks - startTicks);
 			debug_w("Ratio: x %f", float(endTicks - startTicks) / (time - startTime));
-			debug_w("Apparent time: %u", uint32_t(Micros::ticksToTime(endTicks - startTicks)));
+			uint32_t us = Micros::ticksToTime(endTicks - startTicks);
+			debug_w("Apparent time: %u", us);
+			// Up-timers may report 0 if inactive
+			if(endTicks != 0 || startTicks != 0) {
+				REQUIRE(abs(int(us - duration)) < 500); // Allow some latitude
+			}
 		}
 	}
 
@@ -238,6 +244,20 @@ private:
 	CpuCycleTimes calcCycles;
 };
 
+template <hw_timer_clkdiv_t clkdiv>
+class Timer1ClockTestTemplate : public ClockTestTemplate<Timer1Clock<clkdiv>, uint32_t>
+{
+public:
+	void execute() override
+	{
+		// Configure the hardware to match selected clock divider
+		Timer1Api<clkdiv, eHWT_Maskable> timer;
+		timer.arm(false);
+
+		ClockTestTemplate<Timer1Clock<clkdiv>, uint32_t>::execute();
+	}
+};
+
 template <class Clock, typename TimeType> class CpuClockTestTemplate : public ClockTestTemplate<Clock, TimeType>
 {
 public:
@@ -246,9 +266,11 @@ public:
 	void execute() override
 	{
 		uint32_t curFreq = system_get_cpu_freq();
-		System.setCpuFrequency(Clock::cpuFrequency());
+		if(!System.setCpuFrequency(Clock::cpuFrequency())) {
+			debug_e("Failed to set CPU frequency, skipping test");
+			return;
+		}
 
-		// delay(100);
 		debug_i("CPU freq: %u -> %u MHz", curFreq, system_get_cpu_freq());
 		ClockTestTemplate<Clock, TimeType>::execute();
 		System.setCpuFrequency(CpuCycleClockNormal::cpuFrequency());
@@ -472,8 +494,8 @@ void REGISTER_TEST(Clocks)
 
 	registerGroup<TimerCalcTest>();
 
-	registerGroup<ClockTestTemplate<Timer1Clock<TIMER_CLKDIV_16>, uint32_t>>();
-	registerGroup<ClockTestTemplate<Timer1Clock<TIMER_CLKDIV_256>, uint32_t>>();
+	registerGroup<Timer1ClockTestTemplate<TIMER_CLKDIV_16>>();
+	registerGroup<Timer1ClockTestTemplate<TIMER_CLKDIV_256>>();
 
 	registerGroup<ClockTestTemplate<Timer2Clock, uint32_t>>();
 

Sming/Arch/Esp32/Platform/Clocks.h
@@ -16,9 +16,8 @@
 
 /**
  * @brief Clock implementation for os_timer API
- * @see See IDF components/esp_common/src/ets_timer_legacy.c
  */
-struct OsTimerClock : public NanoTime::Clock<OsTimerClock, HW_TIMER2_CLK, uint32_t, 0xFFFFFFFFU> {
+struct OsTimerClock : public NanoTime::Clock<OsTimerClock, 1000000U, uint32_t, 0xFFFFFFFFU> {
 	static constexpr const char* typeName()
 	{
 		return "OsTimerClock";
@@ -26,7 +25,7 @@ struct OsTimerClock : public NanoTime::Clock<OsTimerClock, HW_TIMER2_CLK, uint32
 
 	static uint32_t __forceinline ticks()
 	{
-		return hw_timer2_read();
+		return system_get_time();
 	}
 };
 
@@ -34,7 +33,7 @@ struct OsTimerClock : public NanoTime::Clock<OsTimerClock, HW_TIMER2_CLK, uint32
  * @brief Clock implementation for polled timers
  * @note The Esp32 timer is actually 64-bit but for now stick with 32-bits for more efficient code.
  */
-using PolledTimerClock = OsTimerClock;
+using PolledTimerClock = Timer2Clock;
 
 using CpuCycleClockSlow = CpuCycleClock<eCF_80MHz>;
 using CpuCycleClockNormal = CpuCycleClock<eCF_160MHz>;

Sming/Arch/Host/Components/esp_hal/clk.c
@@ -3,15 +3,42 @@
 
 // The current CPU frequency in MHz (ticks per us)
 static uint8_t cpu_frequency = SYS_CPU_80MHZ;
+static uint64_t base_nanos;
+static uint32_t base_ccount;
+
+/*
+ * Ensure values returned increase monotonically.
+ */
+static uint32_t get_ccount(uint64_t nanos)
+{
+	uint32_t ccount;
+	if(base_nanos == 0) {
+		base_nanos = nanos;
+		base_ccount = nanos / cpu_frequency;
+		ccount = base_ccount;
+	} else {
+		ccount = base_ccount + cpu_frequency * ((nanos - base_nanos) / 1000);
+	}
+
+	return ccount;
+}
 
 bool system_update_cpu_freq(uint8 freq)
 {
-	if(freq == SYS_CPU_80MHZ || freq == SYS_CPU_160MHZ) {
-		cpu_frequency = freq;
+	if(freq == cpu_frequency) {
 		return true;
-	} else {
+	}
+
+	if(freq != SYS_CPU_80MHZ && freq != SYS_CPU_160MHZ) {
 		return false;
 	}
+
+	uint64_t nanos = os_get_nanoseconds();
+	base_ccount = get_ccount(nanos);
+	base_nanos = nanos;
+	cpu_frequency = freq;
+
+	return true;
 }
 
 uint8_t ets_get_cpu_frequency(void)
@@ -24,19 +51,7 @@ uint8 system_get_cpu_freq(void)
 	return ets_get_cpu_frequency();
 }
 
-/*
- * The 'correct' conversion is actually:
- *
- * 		`os_get_nanoseconds() / (1000UL * cpu_frequency)`
- *
- * However, in use this just ends up returning 0 all the time which is
- * not particularly useful.
- *
- * On my dev. system a straight nanosecond count gives quite useful
- * values when evaluating code paths. Try :sample:`Basic_Delegates`.
- *
- */
 uint32_t esp_get_ccount()
 {
-	return os_get_nanoseconds();
+	return get_ccount(os_get_nanoseconds());
 }

Sming/Arch/Rp2040/Components/rp2040/src/clk.c
@@ -17,12 +17,12 @@ static void IRAM_ATTR systick_overflow_isr()
 
 uint32_t IRAM_ATTR esp_get_ccount()
 {
-	extern volatile uint32_t systick_overflow;
 	uint32_t ovf = systick_overflow;
 	if(ovf != systick_overflow) {
 		ovf = systick_overflow;
 	}
-	return systick_hw->cvr | (ovf << 24);
+	// CVR is a down-counter
+	return ((1 + ovf) << 24) - systick_hw->cvr;
 }
 
 /*! \brief Check if a given system clock frequency is valid/attainable

tests/HostTests/app/application.cpp
@@ -54,6 +54,7 @@ void init()
 	debug_e("WELCOME to SMING! Host Tests application running.");
 
 	spiffs_mount();
+	fileSystemFormat();
 
 	registerTests();
 

tests/HostTests/modules/Timers.cpp
@@ -82,7 +82,10 @@ public:
 			}
 
 			auto mem = MallocCount::getCurrent();
-			String s = statusTimer;
+			String s;
+			s += system_get_time();
+			s += " ";
+			s += statusTimer;
 			s += " fired, timercount = ";
 			s += activeTimerCount;
 			s += ", mem = ";

commit 3d86fb369f6bdb7b7c0fd6ed41f8a47e4335332b

Fix esp8266 timer1 testing (#2709)
This PR fixes an issue with `HostTests` which hangs consistently when testing Timer1 (used by HardwareTimer) on the Esp8266.

The problem occurs whilst checking each timer for consistency against system time.
This happens because the test code doesn't set a callback interrupt routine.
It is only a problem on the Esp8266 because the callback routine is set directly on the interrupt vector.
Other architectures use indirection and so do nothing if the callback isn't set.
NB. Doing this with the Esp8266 would increase interrupt latency which would affect PWM timing, etc.

Normal code uses the `HardwareTimer` class which contains logic to ensure this doesn't happen during setup.

Timer1 has only a 23-bit counter so fails test with /16 divisor as it wraps at around 1.7s (test duration is 2 seconds).
Fixed by restricting duration to timer maximum (less 1ms).

Elapsed tick calculation check also improved by first checking whether timer is an UP or DOWN counter,
and using timer maximum tick value to handle wraps correctly.

This PR also reduces the number of test loops on the Host (from 2000 to 50).

Note: Bug was introduced in #2456. Timer1 needed to be correctly configured (but inactive) to produce correct result for Esp32.

Sming/Core/HardwareTimer.h
@@ -141,11 +141,10 @@ private:
 template <hw_timer_clkdiv_t clkdiv, HardwareTimerMode mode> uint8_t Timer1Api<clkdiv, mode>::state;
 template <hw_timer_clkdiv_t clkdiv, HardwareTimerMode mode> uint32_t Timer1Api<clkdiv, mode>::interval;
 
-template <hw_timer_clkdiv_t clkdiv = TIMER_CLKDIV_16, HardwareTimerMode mode = eHWT_NonMaskable>
-
 /**
  * @brief Hardware Timer class template with selectable divider and interrupt mode
  */
+template <hw_timer_clkdiv_t clkdiv = TIMER_CLKDIV_16, HardwareTimerMode mode = eHWT_NonMaskable>
 using HardwareTimer1 = CallbackTimer<Timer1Api<clkdiv, mode>>;
 
 /**

tests/HostTests/modules/Clocks.cpp
@@ -26,7 +26,11 @@ public:
 	{
 		printLimits();
 
-		for(unsigned i = 0; i < 2000; ++i) {
+		unsigned loopCount{2000};
+#ifdef ARCH_HOST
+		loopCount = 50;
+#endif
+		while(loopCount--) {
 			auto value = os_random();
 			check<NanoTime::Milliseconds>(value);
 			check<NanoTime::Microseconds>(value);
@@ -37,19 +41,30 @@ public:
 
 		TEST_CASE("vs. system time")
 		{
-			constexpr uint32_t duration{2000000};
-			auto startTime = system_get_time();
+			// Determine whether this is an up or down-counter
 			auto startTicks = Clock::ticks();
+			os_delay_us(100);
+			auto endTicks = Clock::ticks();
+			bool isDownCounter = (endTicks < startTicks);
+			debug_w("%s is %s counter", Clock::typeName(), isDownCounter ? "DOWN" : "UP");
+
+			// Run for a second or two and check timer ticks correspond approximately with system clock
+			constexpr uint64_t maxDuration = Clock::maxTicks().template as<NanoTime::Microseconds>() - 5000ULL;
+			constexpr uint32_t duration = std::min(2000000ULL, maxDuration);
+			auto startTime = system_get_time();
+			startTicks = Clock::ticks();
 			uint32_t time;
-			while((time = system_get_time()) < startTime + duration) {
+			while((time = system_get_time()) - startTime < duration) {
 				//
 			}
-			auto endTicks = Clock::ticks();
-			// Handle both up and down counters
-			auto elapsedTicks = (endTicks >= startTicks) ? endTicks - startTicks : startTicks - endTicks;
+			endTicks = Clock::ticks();
+			if(isDownCounter) {
+				std::swap(startTicks, endTicks);
+			}
+			uint32_t elapsedTicks = (endTicks - startTicks) % (Clock::maxTicks() + 1);
 
 			debug_w("System time elapsed: %u", time - startTime);
-			debug_w("%s ticks: %u", Clock::typeName(), elapsedTicks);
+			debug_w("Ticks: %u (%u - %u)", elapsedTicks, startTicks, endTicks);
 			debug_w("Ratio: x %f", float(elapsedTicks) / (time - startTime));
 			uint32_t us = Micros::ticksToTime(elapsedTicks);
 			debug_w("Apparent time: %u", us);
@@ -252,10 +267,15 @@ template <hw_timer_clkdiv_t clkdiv>
 class Timer1ClockTestTemplate : public ClockTestTemplate<Timer1Clock<clkdiv>, uint32_t>
 {
 public:
+	static void IRAM_ATTR callback(void*)
+	{
+	}
+
 	void execute() override
 	{
 		// Configure the hardware to match selected clock divider
 		Timer1Api<clkdiv, eHWT_Maskable> timer;
+		timer.setCallback(callback, nullptr);
 		timer.arm(false);
 
 		ClockTestTemplate<Timer1Clock<clkdiv>, uint32_t>::execute();