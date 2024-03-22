fdbc1f05c4c3c785c62280140add6a4ae1faf00e
Mike <mike@sillyhouse.net>
2024-03-22 07:48:48 +0000

    Provide consistent serial console interface for samples (#2739)
    This PR provides a consistent mechanism for a basic serial console interface using the `LineBuffer` class.
    
    **Refactor LineBuffer**
    
    Implement LineBufferBase so we take non-trivial methods out of header.
    
    **Add `bool`, `String` operators, `printTo` method&**
    
    Simplify usage
    
    **Add `process`, `processKey` methods and update samples**
    
    Use LineBuffer for LiveDebug, Basic_Ota and Basic_FlashIP samples.
    These new methods are used to simplify application code.
    
    Note that the `CommandProcessing::Handler` class hasn't been updated here,
    mainly because it requires customisation of EOL handling (setCommandEOL, getCommandEOL).
    Presumably this is because of the variation in EOL sequences (CR, LF, CRLF or LFCR combinations).
    The LineBuffer `processKey` method doesn't care, it handles all of these.

--- samples/Basic_Ota/app/application.cpp
+++ samples/Basic_Ota/app/application.cpp
@@ -3,6 +3,7 @@
 #include <Storage/PartitionStream.h>
 #include <Storage/SpiFlash.h>
 #include <Ota/Upgrader.h>
+#include <Data/Buffer/LineBuffer.h>
 
 // If you want, you can define WiFi settings globally in Eclipse Environment Variables
 #ifndef WIFI_SSID
@@ -10,9 +11,12 @@
 #define WIFI_PWD "PleaseEnterPass"
 #endif
 
+namespace
+{
 std::unique_ptr<Ota::Network::HttpUpgrader> otaUpdater;
 Storage::Partition spiffsPartition;
 OtaUpgrader ota;
+LineBuffer<16> commandBuffer;
 
 Storage::Partition findSpiffsPartition(Storage::Partition appPart)
 {
@@ -122,75 +126,113 @@ void showInfo()
 		   << " @ 0x" << String(after.address(), HEX) << endl;
 }
 
-void serialCallBack(Stream& stream, char arrivedChar, unsigned short availableCharsCount)
+void showPrompt()
 {
-	int pos = stream.indexOf('\n');
-	if(pos > -1) {
-		char str[pos + 1];
-		for(int i = 0; i < pos + 1; i++) {
-			str[i] = stream.read();
-			if(str[i] == '\r' || str[i] == '\n') {
-				str[i] = '\0';
+	Serial << _F("OTA> ") << commandBuffer;
+}
+
+void handleCommand(const String& str)
+{
+	if(F("connect") == str) {
+		Serial << _F("Connecting to '") << WIFI_SSID << "'..." << endl;
+		WifiStation.config(WIFI_SSID, WIFI_PWD);
+		WifiStation.enable(true);
+		WifiStation.connect();
+		return;
+	}
+
+	if(F("ip") == str) {
+		Serial << "ip: " << WifiStation.getIP() << ", mac: " << WifiStation.getMacAddress() << endl;
+		return;
+	}
+
+	if(F("ota") == str) {
+		doUpgrade();
+		return;
+	}
+
+	if(F("switch") == str) {
+		doSwitch();
+		return;
+	}
+
+	if(F("restart") == str) {
+		System.restart();
+		return;
+	}
+
+	if(F("ls") == str) {
+		Directory dir;
+		if(dir.open()) {
+			while(dir.next()) {
+				Serial << "  " << dir.stat().name << endl;
 			}
 		}
+		Serial << _F("filecount ") << dir.count() << endl;
+		return;
+	}
 
-		if(F("connect") == str) {
-			// connect to wifi
-			WifiStation.config(WIFI_SSID, WIFI_PWD);
-			WifiStation.enable(true);
-			WifiStation.connect();
-		} else if(F("ip") == str) {
-			Serial << "ip: " << WifiStation.getIP() << ", mac: " << WifiStation.getMacAddress() << endl;
-		} else if(F("ota") == str) {
-			doUpgrade();
-		} else if(F("switch") == str) {
-			doSwitch();
-		} else if(F("restart") == str) {
-			System.restart();
-		} else if(F("ls") == str) {
-			Directory dir;
-			if(dir.open()) {
-				while(dir.next()) {
-					Serial << "  " << dir.stat().name << endl;
-				}
-			}
-			Serial << _F("filecount ") << dir.count() << endl;
-		} else if(F("cat") == str) {
-			Directory dir;
-			if(dir.open() && dir.next()) {
-				auto filename = dir.stat().name.c_str();
-				Serial << "dumping file " << filename << ": " << endl;
-				// We don't know how big the is, so streaming it is safest
-				FileStream fs;
-				fs.open(filename);
-				Serial.copyFrom(&fs);
-				Serial.println();
-			} else {
-				Serial.println(F("Empty spiffs!"));
-			}
-		} else if(F("info") == str) {
-			showInfo();
-		} else if(F("help") == str) {
-			Serial.print(_F("\r\n"
-							"available commands:\r\n"
-							"  help - display this message\r\n"
-							"  ip - show current ip address\r\n"
-							"  connect - connect to wifi\r\n"
-							"  restart - restart the device\r\n"
-							"  switch - switch to the other rom and reboot\r\n"
-							"  ota - perform ota update, switch rom and reboot\r\n"
-							"  info - show device info\r\n"));
-
-			if(spiffsPartition) {
-				Serial.print(_F("  ls - list files in spiffs\r\n"
-								"  cat - show first file in spiffs\r\n"));
-			}
+	if(F("cat") == str) {
+		Directory dir;
+		if(dir.open() && dir.next()) {
+			auto filename = dir.stat().name.c_str();
+			Serial << "dumping file " << filename << ": " << endl;
+			// We don't know how big the is, so streaming it is safest
+			FileStream fs;
+			fs.open(filename);
+			Serial.copyFrom(&fs);
 			Serial.println();
 		} else {
-			Serial.println("unknown command");
+			Serial.println(F("Empty spiffs!"));
 		}
+		return;
+	}
+
+	if(F("info") == str) {
+		showInfo();
+		return;
+	}
+
+	if(F("help") == str) {
+		Serial.print(_F("\r\n"
+						"available commands:\r\n"
+						"  help - display this message\r\n"
+						"  ip - show current ip address\r\n"
+						"  connect - connect to wifi\r\n"
+						"  restart - restart the device\r\n"
+						"  switch - switch to the other rom and reboot\r\n"
+						"  ota - perform ota update, switch rom and reboot\r\n"
+						"  info - show device info\r\n"));
+
+		if(spiffsPartition) {
+			Serial.print(_F("  ls - list files in spiffs\r\n"
+							"  cat - show first file in spiffs\r\n"));
+		}
+		Serial.println();
+		return;
+	}
+
+	Serial << _F("unknown command: ") << str << endl;
+}
+
+void serialCallBack(Stream& stream, char arrivedChar, unsigned short availableCharsCount)
+{
+	switch(commandBuffer.process(stream, Serial)) {
+	case commandBuffer.Action::submit:
+		if(commandBuffer) {
+			handleCommand(String(commandBuffer));
+			commandBuffer.clear();
+		}
+		showPrompt();
+		break;
+	case commandBuffer.Action::clear:
+		showPrompt();
+		break;
+	default:;
 	}
 }
+
+} // namespace
 
 void init()
 {
@@ -207,10 +249,14 @@ void init()
 	}
 
 	WifiAccessPoint.enable(false);
+	WifiEvents.onStationGotIP([](IpAddress ip, IpAddress netmask, IpAddress gateway) { showPrompt(); });
 
-	Serial << _F("\r\nCurrently running ") << partition.name() << " @ 0x" << String(partition.address(), HEX) << '.'
+	Serial << endl
+		   << _F("Currently running ") << partition.name() << " @ 0x" << String(partition.address(), HEX) << '.' << endl
+		   << _F("Type 'help' and press enter for instructions.") << endl
 		   << endl;
-	Serial << _F("Type 'help' and press enter for instructions.") << endl << endl;
+
+	showPrompt();
 
 	Serial.onDataReceived(serialCallBack);
 }
