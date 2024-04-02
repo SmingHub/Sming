68cabd1afb17a6cc979029e7c177af32c626db2d
mikee47 <mike@silyhouse.net>
2024-04-02 16:17:22 +0100

    Basic_WebSkeletonApp

--- samples/Basic_WebSkeletonApp/app/configuration.cpp
+++ samples/Basic_WebSkeletonApp/app/configuration.cpp
@@ -7,6 +7,14 @@
 #define WIFI_PWD "PleaseEnterPass"
 #endif
 
+namespace
+{
+DEFINE_FSTR(KEY_NETWORK, "network")
+DEFINE_FSTR(KEY_SSID, "StaSSID")
+DEFINE_FSTR(KEY_PASS, "StaPassword")
+DEFINE_FSTR(KEY_ENABLE, "StaEnable")
+} // namespace
+
 ThermConfig activeConfig;
 
 ThermConfig loadConfig()
@@ -14,14 +22,14 @@ ThermConfig loadConfig()
 	StaticJsonDocument<ConfigJsonBufferSize> doc;
 	ThermConfig cfg;
 	if(Json::loadFromFile(doc, THERM_CONFIG_FILE)) {
-		JsonObject network = doc["network"];
-		cfg.StaSSID = network["StaSSID"].as<const char*>();
-		cfg.StaPassword = network["StaPassword"].as<const char*>();
-		cfg.StaEnable = network["StaEnable"];
+		JsonObject network = doc[KEY_NETWORK];
+		cfg.StaSSID = network[KEY_SSID].as<const char*>();
+		cfg.StaPassword = network[KEY_PASS].as<const char*>();
+		cfg.StaEnable = network[KEY_ENABLE];
 	} else {
 		// Factory defaults if no config file present, or could not access it
-		cfg.StaSSID = WIFI_SSID;
-		cfg.StaPassword = WIFI_PWD;
+		cfg.StaSSID = F(WIFI_SSID);
+		cfg.StaPassword = F(WIFI_PWD);
 	}
 	return cfg;
 }
@@ -30,10 +38,10 @@ void saveConfig(ThermConfig& cfg)
 {
 	StaticJsonDocument<ConfigJsonBufferSize> doc;
 
-	JsonObject network = doc.createNestedObject("network");
-	network["StaSSID"] = cfg.StaSSID;
-	network["StaPassword"] = cfg.StaPassword;
-	network["StaEnable"] = cfg.StaEnable;
+	JsonObject network = doc.createNestedObject(KEY_NETWORK);
+	network[KEY_SSID] = cfg.StaSSID;
+	network[KEY_PASS] = cfg.StaPassword;
+	network[KEY_ENABLE] = cfg.StaEnable;
 
 	Json::saveToFile(doc, THERM_CONFIG_FILE, Json::Pretty);
 }
