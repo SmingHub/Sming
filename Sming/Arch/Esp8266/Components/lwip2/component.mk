52ee9d0129a1bb434bde7b2df0cc2865c0005163
Mike <mike@sillyhouse.net>
2024-06-08 13:48:29 +0100

    Update lwip2, use upstream repo. (#2793)
    This PR at long last brings lwip2 out of the dark ages. Seems to work reliably, here it is for testing anyway.
    
    Instead of using a fork this uses the lwip2 repo used by esp8266 arduino, with some minimal patching.
    
    lwip2 now updated from 2.0.2 to 2.1.3.

--- Sming/Arch/Esp8266/Components/lwip2/component.mk
+++ Sming/Arch/Esp8266/Components/lwip2/component.mk
@@ -10,33 +10,69 @@ ifeq ($(SMING_RELEASE),1)
 endif
 
 COMPONENT_VARS		:= ENABLE_LWIPDEBUG ENABLE_ESPCONN
-ENABLE_LWIPDEBUG	?= 0
+ifneq ($(ENABLE_LWIPDEBUG),1)
+override ENABLE_LWIPDEBUG := 0
+endif
 ENABLE_ESPCONN		?= 0
 
 EXTRA_CFLAGS_LWIP  := \
 	-I$(SMING_HOME)/System/include \
 	-I$(ARCH_COMPONENTS)/esp8266/include \
-	-I$(ARCH_COMPONENTS)/libc/include
+	-I$(ARCH_COMPONENTS)/libc/include \
+	-DULWIPDEBUG=$(ENABLE_LWIPDEBUG)
 
-ifeq ($(ENABLE_LWIPDEBUG), 1)
-	EXTRA_CFLAGS_LWIP += -DLWIP_DEBUG
-endif
 
-ifeq ($(ENABLE_ESPCONN), 1)
-$(error LWIP2 does not support espconn_* functions. Make sure to set ENABLE_CUSTOM_LWIP to 0 or 1.)
-endif
-
-COMPONENT_SUBMODULES	:= lwip2
-COMPONENT_INCDIRS		:= lwip2/glue-esp/include-esp lwip2/include
+COMPONENT_SUBMODULES := lwip2
+COMPONENT_INCDIRS := \
+	lwip2/glue-lwip/arduino \
+	lwip2/glue-lwip \
+	lwip2/glue \
+	lwip2/lwip2-src/src/include
 
 LWIP2_PATH := $(COMPONENT_PATH)/lwip2
+LWIP2_LIBPATH := $(COMPONENT_LIBDIR)
+
+#
+COMPONENT_VARS += TCP_MSS LWIP_IPV6 LWIP_FEATURES
+TCP_MSS ?= 1460
+ifneq ($(LWIP_IPV6),1)
+override LWIP_IPV6 := 0
+endif
+ifneq ($(LWIP_FEATURES),1)
+LWIP_FEATURES := 0
+endif
+
+LWIP2_HASHVAL   := $(foreach v,$(COMPONENT_VARS),$v=$($v))
+LWIP2_LIBHASH   := $(call CalculateVariantHash,LWIP2_HASHVAL)
+LWIP2_BUILD_DIR := $(COMPONENT_BUILD_BASE)/$(LWIP2_LIBHASH)
+
+GLOBAL_CFLAGS += \
+	-DTCP_MSS=$(TCP_MSS) \
+	-DLWIP_IPV6=$(LWIP_IPV6) \
+	-DLWIP_FEATURES=$(LWIP_FEATURES) \
+	-DULWIPDEBUG=$(ENABLE_LWIPDEBUG)
 
 # Make is pretty complex for LWIP2, and mucks about with output sections so build as a regular library
 LWIP2_LIB			:= $(COMPONENT_NAME)
-LWIP2_TARGET		:= $(COMPONENT_LIBDIR)/lib$(LWIP2_LIB).a
+LWIP2_TARGET		:= $(LWIP2_BUILD_DIR)/lib$(LWIP2_LIB).a
 COMPONENT_TARGETS	:= $(LWIP2_TARGET)
 EXTRA_LIBS			:= $(LWIP2_LIB)
 
+LIBDIRS += $(LWIP2_BUILD_DIR)
+
+COMPONENT_PREREQUISITES := $(LWIP2_PATH)/glue-lwip/lwip-err-t.h
+
+$(LWIP2_PATH)/glue-lwip/lwip-err-t.h:
+	$(Q) $(MAKE) -C $(LWIP2_PATH) -f ../Makefile.sming patch
+
 $(COMPONENT_RULE)$(LWIP2_TARGET):
-	$(Q) $(MAKE) -C $(LWIP2_PATH) -f Makefile.sming BUILD=$(COMPONENT_BUILD_DIR) \
-				USER_LIBDIR=$(COMPONENT_LIBDIR)/ CFLAGS_EXTRA="$(EXTRA_CFLAGS_LWIP)" CC=$(CC) AR=$(AR) all
+	$(Q) $(MAKE) --no-print-directory -C $(LWIP2_PATH) -f ../Makefile.sming \
+		all \
+		CFLAGS_EXTRA="$(EXTRA_CFLAGS_LWIP)" \
+		LWIP_LIB_RELEASE=$(LWIP2_TARGET) \
+		TOOLS=$(TOOLSPEC) \
+		TCP_MSS=$(TCP_MSS) \
+		LWIP_FEATURES=$(LWIP_FEATURES) \
+		LWIP_IPV6=$(LWIP_IPV6) \
+		BUILD=$(LWIP2_BUILD_DIR)/build \
+		Q=$(Q) V=$(if $V,1,0)
