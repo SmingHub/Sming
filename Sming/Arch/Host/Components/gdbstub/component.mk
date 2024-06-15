89177496df5e7077e839852b339fcdc4fcbbbb81
mikee47 <mike@sillyhouse.net>
2024-06-15 12:08:09 +0100

    Add MacOS debugger support
    Implement gdb_do_break for arm64
    Add lldb target
    Update documentation

--- Sming/Arch/Host/Components/gdbstub/component.mk
+++ Sming/Arch/Host/Components/gdbstub/component.mk
@@ -4,3 +4,13 @@ GDBSTUB_DIR := $(COMPONENT_PATH)
 
 CACHE_VARS	+= GDB_CMDLINE
 GDB_CMDLINE = trap '' INT; $(GDB) -x $(GDBSTUB_DIR)/gdbcmds --args $(TARGET_OUT_0) $(CLI_TARGET_OPTIONS) --pause -- $(HOST_PARAMETERS)
+
+# LLVM debugger
+CACHE_VARS	+= LLDB_CMDLINE
+LLDB_CMDLINE = lldb --source $(GDBSTUB_DIR)/lldbcmds $(TARGET_OUT_0) -- $(CLI_TARGET_OPTIONS) --pause $(HOST_PARAMETERS)
+
+##@Tools
+
+.PHONY: lldb
+lldb: ##Run the LLVM debugger console
+	$(LLDB_CMDLINE)
