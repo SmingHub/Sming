05d3aadda6c78790a7e31f0c71a72a051c6fd5b3
mikee47 <mike@sillyhouse.net>
2024-06-27 16:12:07 +0100

    Fix `pgm_read_dword`, etc. for host on 64-bit builds
    Santizer caught invalid memory read because `unsigned long` is 64 bits.

--- Sming/Arch/Host/Components/libc/include/sys/pgmspace.h
+++ Sming/Arch/Host/Components/libc/include/sys/pgmspace.h
@@ -25,9 +25,9 @@ bool isFlashPtr(const void* ptr);
 #define PGM_P const char*
 #define PGM_VOID_P const void*
 
-#define pgm_read_byte(addr) (*(const unsigned char*)(addr))
-#define pgm_read_word(addr) (*(const unsigned short*)(addr))
-#define pgm_read_dword(addr) (*(const unsigned long*)(addr))
+#define pgm_read_byte(addr) (*(const uint8_t*)(addr))
+#define pgm_read_word(addr) (*(const uint16_t*)(addr))
+#define pgm_read_dword(addr) (*(const uint32_t*)(addr))
 #define pgm_read_float(addr) (*(const float*)(addr))
 
 #define pgm_read_byte_near(addr) pgm_read_byte(addr)
