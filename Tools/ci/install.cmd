SmingHub/Sming

commit 0706c25e9fc7b42939cd420a0dea23df4c1e94c5

CI ccache rework (#2857)
This PR makes a few changes to the way ccache is used for CI.

I did consider adding ccache detection to `export.sh` so it gets enabled by default.
Whilst ccache is great and seems quite robust, if things do go wrong it just adds more complication for users to sort out so requiring manual activation seems best I think.


**Simplify CI ccache use**

Hendrik Muhs `ccache-action` got things working easily, but it does installation and some other stuff which we don't need and creating versioned caches on every run is un-necessary.

Instead, just use the standard `cache` action as used for IDF tools, so pull requests use the `develop` branch cache if available or creates one. This keeps things clean and simple.

However, once created caches aren't update automatically, so I've added a workflows to rebuilding the tools which is required if the IDF toolchains get updated.

I've also added a second 'cache clean` workflow which removes any cache entries for pull requests (specifically, not develop).


**Tidy action scripts**

- Actions `if` clause doesn't require `${{ }}`
- Add workflow_dispatch for both CI builds so we can trigger rebuilds manually if necessary.


**Esp32 tools installation**

Installer is still re-installing the tools (gdb, etc.) we've previously pruned out.
So just skip esp32 tools installation for CI if directory already present.

IDF also insists on pulling unwanted submodules back in again, even though they're not used.
Fortunately there is `IDF_SKIP_CHECK_SUBMODULES=1` to disable this behaviour.

Also some improvements to the `clean-tools.py` script. Works much better. Have added some notes to the `Tools/ci/README.rst`.

This is also a useful step in identifying and eliminating stuff we don't need for Sming.
One thing I have in mind is getting rid of the IDF build step completely and just building the required code separately.

.github/workflows/library.yml
@@ -62,7 +62,7 @@ jobs:
           - os: macos-latest
             idf_version: "4.4"
 
-    concurrency: 
+    concurrency:
       group: ${{ github.workflow }}-${{ github.ref }}-${{ toJson(matrix) }}
       cancel-in-progress: true
 
@@ -89,7 +89,7 @@ jobs:
         python-version: "3.12"
 
     - name: Create library alias
-      if: ${{ inputs.alias }}
+      if: inputs.alias
       shell: pwsh
       run: |
         New-Item -ItemType SymbolicLink -Path "../${{ inputs.alias }}" -Target (Resolve-Path ".").path
@@ -107,12 +107,12 @@ jobs:
         "CI_MAKEFILE=" + (Resolve-Path "../../sming/Tools/ci/library/Makefile") >> $env:GITHUB_ENV
 
     - name: Fix permissions
-      if: ${{ matrix.os != 'windows-latest' }}
+      if: matrix.os != 'windows-latest'
       run: |
         sudo chown $USER /opt
 
     - name: Cache ESP-IDF and build tools
-      if: ${{ matrix.arch == 'Esp32' }}
+      if: matrix.arch == 'Esp32'
       uses: actions/cache@v4
       with:
         path: |
@@ -121,12 +121,12 @@ jobs:
         key: ${{ matrix.os }}-idf-${{ env.INSTALL_IDF_VER }}
 
     - name: Install build tools for Ubuntu / MacOS
-      if: ${{ matrix.os != 'windows-latest' }}
+      if: matrix.os != 'windows-latest'
       run: |
         $SMING_HOME/../Tools/ci/install.sh
 
-    - name: Install build tools for Windows   
-      if: ${{ matrix.os == 'windows-latest' }}
+    - name: Install build tools for Windows
+      if: matrix.os == 'windows-latest'
       run: |
         cd $env:SMING_HOME/..
         . Tools/ci/setenv.ps1
@@ -135,13 +135,13 @@ jobs:
     - name: Build and Test for ${{matrix.arch}} on Ubuntu / MacOS
       env:
         CLANG_FORMAT: clang-format-8
-      if: ${{ matrix.os != 'windows-latest' }}
+      if: matrix.os != 'windows-latest'
       run: |
         source $SMING_HOME/../Tools/export.sh
         make -j$(nproc) -f $CI_MAKEFILE
 
     - name: Build and Test for ${{matrix.arch}} on Windows
-      if: ${{ matrix.os == 'windows-latest' }}
+      if: matrix.os == 'windows-latest'
       run: |
         . "$env:SMING_HOME/../Tools/ci/setenv.ps1"
         make -j $env:NUMBER_OF_PROCESSORS -f $env:CI_MAKEFILE

Tools/ci/install.cmd
@@ -10,4 +10,10 @@ if "%BUILD_DOCS%" == "true" (
 
 call %CI_BUILD_DIR%\Tools\install.cmd %SMING_ARCH% %INSTALL_OPTS%
 
+REM Configure ccache
+ccache --set-config cache_dir="%CI_BUILD_DIR%\.ccache"
+ccache --set-config max_size=500M
+ccache -z
+
+REM Clean up tools installation
 python "%CI_BUILD_DIR%\Tools\ci\clean-tools.py" clean --delete

Tools/ci/install.sh
@@ -23,5 +23,11 @@ sudo chown "$USER" /opt
 
 fi
 
+# Configure ccache
+ccache --set-config cache_dir="$CI_BUILD_DIR/.ccache"
+ccache --set-config max_size=500M
+ccache -z
+
+# Clean up tools installation
 source "$CI_BUILD_DIR/Tools/export.sh"
 python "$CI_BUILD_DIR/Tools/ci/clean-tools.py" clean --delete

Sming/Arch/Esp32/Tools/install.cmd
@@ -33,6 +33,13 @@ goto :setup
 
 :setup
 
+REM Skip installation for CI if already present
+if "%CI_BUILD_DIR%" NEQ "" (
+    if exist "%IDF_TOOLS_PATH%\tools" (
+        goto :EOF
+    )
+)
+
 REM Install IDF tools and packages
 python "%IDF_PATH%\tools\idf_tools.py" --non-interactive install
 python -m pip install %SMINGTOOLS%/gevent-1.5.0-cp39-cp39-win_amd64.whl

Sming/Arch/Esp32/Tools/install.sh
@@ -60,6 +60,10 @@ fi
 rm -f "$IDF_PATH"
 ln -s "$IDF_CLONE_PATH" "$IDF_PATH"
 
+
+# Skip installation for CI if already present
+if [ -z "$CI_BUILD_DIR" ] || [ ! -d "$IDF_TOOLS_PATH/tools" ]; then
+
 # Install IDF tools and packages
 python3 "$IDF_PATH/tools/idf_tools.py" --non-interactive install
 if [ -n "$VIRTUAL_ENV" ]; then
@@ -72,5 +76,7 @@ python3 "$IDF_PATH/tools/idf_tools.py" --non-interactive install-python-env
 if [ -z "$KEEP_DOWNLOADS" ]; then
     rm -rf "$IDF_TOOLS_PATH/dist"
 fi
+
+fi # CI install
 
 fi

.github/workflows/cache-clean.yml
@@ -1,0 +1,25 @@
+name: Cache clean
+
+on:
+  workflow_dispatch:
+
+jobs:
+  cleanup:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Cleanup
+        run: |
+          gh extension install actions/gh-actions-cache
+
+          echo "Fetching list of cache keys"
+          cacheKeys=$(gh actions-cache list -R $REPO -L 100 | grep -v develop | cut -f 1 )
+
+          echo "Deleting caches..."
+          set +e
+          for cacheKey in $cacheKeys; do
+            gh actions-cache delete "$cacheKey" -R "$REPO" --confirm
+          done
+          echo "Done"
+        env:
+          GH_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+          REPO: ${{ github.repository }}

.github/workflows/cache-rebuild.yml
@@ -1,0 +1,97 @@
+name: Cache rebuild
+
+on:
+  workflow_dispatch:
+    inputs:
+      clean_all:
+        description: 'Clean all caches, not just esp32 ones'
+        default: false
+
+jobs:
+  cleanup:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Cleanup
+        id: cleanup
+        run: |
+          gh extension install actions/gh-actions-cache
+
+          echo "Fetching list of cache keys"
+          if [ -z "$CLEAN_ALL" ]; then
+            cacheKeys=$(gh actions-cache list -R "$REPO" -L 100 | cut -f 1 )
+          else
+            cacheKeys=$(gh actions-cache list -R "$REPO" -L 100 | grep -w "idf\|esp32" | cut -f 1 )
+          fi
+
+          echo "Deleting caches..."
+          set +e
+          for cacheKey in $cacheKeys; do
+            gh actions-cache delete "$cacheKey" -R "$REPO" --confirm
+          done
+          echo "Done"
+        env:
+          GH_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+          REPO: ${{ github.repository }}
+          CLEAN_ALL: ${{ inputs.clean_all }}
+
+  build:
+    needs: cleanup
+    strategy:
+      fail-fast: false
+      matrix:
+        os: [ubuntu-latest, macos-latest, windows-latest]
+        idf_version: ["4.4", "5.2"]
+        include:
+          - os: ubuntu-latest
+            idf_version: "4.3"
+          - os: ubuntu-latest
+            idf_version: "5.0"
+        exclude:
+          - os: macos-latest
+            idf_version: "4.4"
+
+    runs-on: ${{ matrix.os }}
+
+    env:
+      SMING_ARCH: Esp32
+      SMING_SOC: esp32
+      INSTALL_IDF_VER: ${{ matrix.idf_version }}
+
+    steps:
+    - name: Checkout code
+      uses: actions/checkout@v4
+
+    - name: Setup python
+      uses: actions/setup-python@v5
+      with:
+        python-version: ${{ matrix.idf_version == '4.3' && '3.8' || '3.12' }}
+
+    - name: Configure environment
+      shell: pwsh
+      run: |
+        "CI_BUILD_DIR=" + (Resolve-Path ".").path >> $env:GITHUB_ENV
+        "SMING_HOME=" + (Resolve-Path "Sming").path >> $env:GITHUB_ENV
+
+    - name: Fix permissions
+      if: matrix.os != 'windows-latest'
+      run: |
+        sudo chown $USER /opt
+
+    - name: Install build tools for Ubuntu / MacOS
+      if: matrix.os != 'windows-latest'
+      run: |
+        Tools/ci/install.sh
+
+    - name: Install build tools for Windows
+      if: matrix.os == 'windows-latest'
+      run: |
+         . Tools/ci/setenv.ps1
+         Tools/ci/install.cmd
+
+    - name: Cache ESP-IDF and build tools
+      uses: actions/cache/save@v4
+      with:
+        path: |
+          /opt/esp-idf-${{ matrix.idf_version }}
+          /opt/esp32
+        key: ${{ matrix.os }}-idf-${{ matrix.idf_version }}

.github/workflows/ci-esp32.yml
@@ -2,7 +2,9 @@ name: Continuous Integration (CI) for Esp32
 
 on:
   push:
-  
+
+  workflow_dispatch:
+
   pull_request:
     branches: [ develop ]
 
@@ -38,6 +40,7 @@ jobs:
       SMING_ARCH: Esp32
       SMING_SOC: ${{ matrix.variant }}
       INSTALL_IDF_VER: ${{ matrix.idf_version }}
+      IDF_SKIP_CHECK_SUBMODULES: 1
       ENABLE_CCACHE: 1
 
     steps:
@@ -60,7 +63,7 @@ jobs:
         "SMING_HOME=" + (Resolve-Path "Sming").path >> $env:GITHUB_ENV
 
     - name: Fix permissions
-      if: ${{ matrix.os != 'windows-latest' }}
+      if: matrix.os != 'windows-latest'
       run: |
         sudo chown $USER /opt
 
@@ -72,30 +75,34 @@ jobs:
           /opt/esp32
         key: ${{ matrix.os }}-idf-${{ matrix.idf_version }}
 
+    - name: Compiler Cache
+      uses: actions/cache@v4
+      with:
+        path: .ccache
+        key: ${{ matrix.os }}-ccache-${{ matrix.variant }}-${{ matrix.idf_version }}
+
     - name: Install build tools for Ubuntu / MacOS
-      if: ${{ matrix.os != 'windows-latest' }}
+      if: matrix.os != 'windows-latest'
       run: |
         Tools/ci/install.sh
 
-    - name: Install build tools for Windows   
-      if: ${{ matrix.os == 'windows-latest' }}
+    - name: Install build tools for Windows
+      if: matrix.os == 'windows-latest'
       run: |
          . Tools/ci/setenv.ps1
          Tools/ci/install.cmd
 
-    - name: ccache
-      uses: hendrikmuhs/ccache-action@v1.2
-      with:
-        key: ${{ matrix.os }}-${{ matrix.variant }}-${{ matrix.idf_version }}
-
     - name: Build and test for ${{matrix.variant}} with IDF v${{matrix.idf_version}} on Ubuntu / MacOS
-      if: ${{ matrix.os != 'windows-latest' }}
+      if: matrix.os != 'windows-latest'
       run: |
         source $SMING_HOME/../Tools/export.sh
         Tools/ci/build.sh
 
     - name: Build and test for ${{matrix.variant}} with IDF v${{matrix.idf_version}} on Windows
-      if: ${{ matrix.os == 'windows-latest' }}
+      if: matrix.os == 'windows-latest'
       run: |
         . Tools/ci/setenv.ps1
         Tools/ci/build.cmd
+
+    - name: Compiler Cache stats
+      run: ccache -sv

commit a9864daed4d372fecb1add373def863ddb4fc3e2

Fix CI library builds (#2858)
Broken by #2857. Also enable ccache.

- `CI_BUILD_DIR` does not point to Sming build: must use SMING_HOME.
- Don't set CI_BUILD_DIR or SMING_HOME in `setenv.ps1` - it's wrong and already set in `.yml`

.github/workflows/library.yml
@@ -74,6 +74,7 @@ jobs:
       INSTALL_IDF_VER: ${{ matrix.idf_version || '5.2' }}
       CLANG_BUILD: ${{ matrix.toolchain == 'clang' && '15' || '0' }}
       BUILD64: ${{ matrix.toolchain == 'gcc64' && 1 || 0 }}
+      ENABLE_CCACHE: 1
 
     steps:
     - name: Fix autocrlf setting
@@ -120,6 +121,12 @@ jobs:
           /opt/esp32
         key: ${{ matrix.os }}-idf-${{ env.INSTALL_IDF_VER }}
 
+    - name: Compiler Cache
+      uses: actions/cache@v4
+      with:
+        path: .ccache
+        key: ${{ matrix.os }}-ccache-${{ matrix.variant }}-${{ matrix.arch == 'Esp32' && env.INSTALL_IDF_VER || '' }}
+
     - name: Install build tools for Ubuntu / MacOS
       if: matrix.os != 'windows-latest'
       run: |
@@ -145,3 +152,6 @@ jobs:
       run: |
         . "$env:SMING_HOME/../Tools/ci/setenv.ps1"
         make -j $env:NUMBER_OF_PROCESSORS -f $env:CI_MAKEFILE
+
+    - name: Compiler Cache stats
+      run: ccache -sv

Tools/ci/install.cmd
@@ -1,6 +1,11 @@
 REM
 REM Windows CI install script
 REM
+REM May also be used for library CI builds. The following variables must be set:
+REM
+REM   CI_BUILD_DIR
+REM   SMING_HOME
+REM
 
 if "%SMING_TOOLS_PREINSTALLED%" NEQ "" goto :EOF
 
@@ -8,12 +13,14 @@ if "%BUILD_DOCS%" == "true" (
     set INSTALL_OPTS=doc
 )
 
-call %CI_BUILD_DIR%\Tools\install.cmd %SMING_ARCH% %INSTALL_OPTS%
+call %SMING_HOME%\..\Tools\install.cmd %SMING_ARCH% %INSTALL_OPTS%
 
 REM Configure ccache
-ccache --set-config cache_dir="%CI_BUILD_DIR%\.ccache"
-ccache --set-config max_size=500M
-ccache -z
+if "%ENABLE_CCACHE%"=="1" (
+    ccache --set-config cache_dir="%CI_BUILD_DIR%\.ccache"
+    ccache --set-config max_size=500M
+    ccache -z
+)
 
 REM Clean up tools installation
-python "%CI_BUILD_DIR%\Tools\ci\clean-tools.py" clean --delete
+python "%SMING_HOME%\..\Tools\ci\clean-tools.py" clean --delete

Tools/ci/install.sh
@@ -19,15 +19,17 @@ fi
 # Ensure default path is writeable
 sudo chown "$USER" /opt
 
-"$CI_BUILD_DIR/Tools/install.sh" "$SMING_ARCH" "${INSTALL_OPTS[@]}"
+"$SMING_HOME/../Tools/install.sh" "$SMING_ARCH" "${INSTALL_OPTS[@]}"
 
 fi
 
 # Configure ccache
-ccache --set-config cache_dir="$CI_BUILD_DIR/.ccache"
-ccache --set-config max_size=500M
-ccache -z
+if [ "$ENABLE_CCACHE" == "1" ]; then
+    ccache --set-config cache_dir="$CI_BUILD_DIR/.ccache"
+    ccache --set-config max_size=500M
+    ccache -z
+fi
 
 # Clean up tools installation
-source "$CI_BUILD_DIR/Tools/export.sh"
-python "$CI_BUILD_DIR/Tools/ci/clean-tools.py" clean --delete
+source "$SMING_HOME/../Tools/export.sh"
+python "$SMING_HOME/../Tools/ci/clean-tools.py" clean --delete

Tools/ci/setenv.ps1
@@ -1,8 +1,5 @@
 $TOOLS_DIR = "D:/opt"
 
-$env:CI_BUILD_DIR = (Resolve-Path "$PSScriptRoot/../..").Path
-$env:SMING_HOME = Join-Path $env:CI_BUILD_DIR "Sming"
-
 # Esp8266 
 $env:ESP_HOME = Join-Path $TOOLS_DIR "esp-quick-toolchain"
 