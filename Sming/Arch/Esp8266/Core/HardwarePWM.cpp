SmingHub/Sming

commit 7f3eba5899f7cb43f356155f16aa7fbf2370d142

Free some esp8266 RAM (#2752)
This PR frees some esp8266 RAM consumed by the `.rodata` segment. This can be examined with `objdump -s -j .rodata out/Esp8266/debug/build/app_0.out`

**Store `http-parser` method strings in flash**

Also remove unused `http_status_str()` function, consumes RAM even though it's never called.
Saves 1100 bytes RAM.

**More http-parser strings into flash**

No need for these to be in RAM.

**Update `http-parser` lib to final version**

**Put WS parser error strings in flash**

**Remove `EspDigitalPin`**

Only MUX addresses are required, and that only needs 1 byte.
Saves up to 196 byte RAM.

**Optimise rapidxml lookup tables**

Saves 2020 bytes RAM

**Add python script to scan/compare CI build logs**

Savings:

```
samples/Basic_Blink
  log                            data  rodata  bss   text  irom0_text  Total Used RAM  Free RAM  Free IRam
  -----------------------------  ----  ------  ----  ----  ----------  --------------  --------  ---------
  log-0e46891-develop.txt        58    712     1336  7265  55932       2106            79814     25521    
  log-9c72d313-flashstrings.txt  58    536     1336  7269  55932       1930            79990     25517    
  Difference                           -176          +4                -176            +176      -4       

samples/Basic_Capsense
  log                            data  rodata  bss    text   irom0_text  Total Used RAM  Free RAM  Free IRam
  -----------------------------  ----  ------  -----  -----  ----------  --------------  --------  ---------
  log-0e46891-develop.txt        1316  916     25416  25570  209856      27648           54272     7216     
  log-9c72d313-flashstrings.txt  1316  740     25416  25574  209856      27472           54448     7212     
  Difference                           -176           +4                 -176            +176      -4       

samples/Basic_HwPWM

Sming/Arch/Esp8266/Core/HardwarePWM.cpp
@@ -25,10 +25,13 @@
 
 #include <HardwarePWM.h>
 #include <Clock.h>
-#include "ESP8266EX.h"
 #include <debug_progmem.h>
+#include "pins_arduino.h"
+#include <espinc/pin_mux_register.h>
 
 #define PERIOD_TO_MAX_DUTY(x) (x * 25)
+
+extern const uint8_t esp8266_pinmuxOffset[];
 
 HardwarePWM::HardwarePWM(uint8_t* pins, uint8_t noOfPins) : channel_count(noOfPins)
 {
@@ -38,15 +41,22 @@ HardwarePWM::HardwarePWM(uint8_t* pins, uint8_t noOfPins) : channel_count(noOfPi
 
 	uint32_t ioInfo[PWM_CHANNEL_NUM_MAX][3];   // pin information
 	uint32_t pwmDutyInit[PWM_CHANNEL_NUM_MAX]; // pwm duty
+	unsigned pinCount = 0;
 	for(uint8_t i = 0; i < noOfPins; i++) {
-		ioInfo[i][0] = EspDigitalPins[pins[i]].mux;
-		ioInfo[i][1] = EspDigitalPins[pins[i]].gpioFunc;
-		ioInfo[i][2] = EspDigitalPins[pins[i]].id;
-		pwmDutyInit[i] = 0; // Start with zero output
-		channels[i] = pins[i];
+		auto pin = pins[i];
+		assert(pin < 16);
+		if(pin >= 16) {
+			continue;
+		}
+		ioInfo[pinCount][0] = PERIPHS_IO_MUX + esp8266_pinmuxOffset[pin];
+		ioInfo[pinCount][1] = esp8266_gpioToFn[pin];
+		ioInfo[pinCount][2] = pin;
+		pwmDutyInit[pinCount] = 0; // Start with zero output
+		channels[pinCount] = pin;
+		++pinCount;
 	}
 	const int initialPeriod = 1000;
-	pwm_init(initialPeriod, pwmDutyInit, noOfPins, ioInfo);
+	pwm_init(initialPeriod, pwmDutyInit, pinCount, ioInfo);
 	update();
 	maxduty = PERIOD_TO_MAX_DUTY(initialPeriod); // for period of 1000
 }

Sming/Arch/Esp8266/Core/Digital.cpp
@@ -9,19 +9,42 @@
  ****/
 
 #include <Digital.h>
-#include "ESP8266EX.h"
 #include <espinc/eagle_soc.h>
 #include <espinc/gpio_register.h>
 #include <espinc/pin_mux_register.h>
 #include <Platform/Timers.h>
 
+#define TOTAL_PINS 16
+
+#define PINMUX_OFFSET(addr) uint8_t((addr)-PERIPHS_IO_MUX)
+
+// Used for pullup/noPullup
+extern const uint8_t esp8266_pinmuxOffset[] = {
+	PINMUX_OFFSET(PERIPHS_IO_MUX_GPIO0_U),	// 0 FLASH
+	PINMUX_OFFSET(PERIPHS_IO_MUX_U0TXD_U),	// 1 TXD0
+	PINMUX_OFFSET(PERIPHS_IO_MUX_GPIO2_U),	// 2 TXD1
+	PINMUX_OFFSET(PERIPHS_IO_MUX_U0RXD_U),	// 3 RXD0
+	PINMUX_OFFSET(PERIPHS_IO_MUX_GPIO4_U),	// 4
+	PINMUX_OFFSET(PERIPHS_IO_MUX_GPIO5_U),	// 5
+	PINMUX_OFFSET(PERIPHS_IO_MUX_SD_CLK_U),   // 6 SD_CLK_U
+	PINMUX_OFFSET(PERIPHS_IO_MUX_SD_DATA0_U), // 7 SD_DATA0_U
+	PINMUX_OFFSET(PERIPHS_IO_MUX_SD_DATA1_U), // 8 SD_DATA1_U
+	PINMUX_OFFSET(PERIPHS_IO_MUX_SD_DATA2_U), // 9
+	PINMUX_OFFSET(PERIPHS_IO_MUX_SD_DATA3_U), // 10
+	PINMUX_OFFSET(PERIPHS_IO_MUX_SD_CMD_U),   // 11 SD_CMD_U
+	PINMUX_OFFSET(PERIPHS_IO_MUX_MTDI_U),	 // 12 HSPIQ
+	PINMUX_OFFSET(PERIPHS_IO_MUX_MTCK_U),	 // 13 HSPID
+	PINMUX_OFFSET(PERIPHS_IO_MUX_MTMS_U),	 // 14 HSPICLK
+	PINMUX_OFFSET(PERIPHS_IO_MUX_MTDO_U),	 // 15 HSPICS
+};
+
 // Prototype declared in esp8266-peri.h
-const uint8_t esp8266_gpioToFn[16] = {0x34, 0x18, 0x38, 0x14, 0x3C, 0x40, 0x1C, 0x20,
-									  0x24, 0x28, 0x2C, 0x30, 0x04, 0x08, 0x0C, 0x10};
+const uint8_t esp8266_gpioToFn[TOTAL_PINS] = {0x34, 0x18, 0x38, 0x14, 0x3C, 0x40, 0x1C, 0x20,
+											  0x24, 0x28, 0x2C, 0x30, 0x04, 0x08, 0x0C, 0x10};
 
 void pinMode(uint16_t pin, uint8_t mode)
 {
-	if(pin < 16) {
+	if(pin < TOTAL_PINS) {
 		if(mode == SPECIAL) {
 			GPC(pin) = (GPC(pin) &
 						(0xF << GPCI)); //SOURCE(GPIO) | DRIVER(NORMAL) | INT_TYPE(UNCHANGED) | WAKEUP_ENABLE(DISABLED)
@@ -100,38 +123,32 @@ void digitalWrite(uint16_t pin, uint8_t val)
 			pullup(pin);
 		else
 			noPullup(pin);
-	} else {
-		if(pin != 16)
-			GPIO_REG_WRITE((((val != LOW) ? GPIO_OUT_W1TS_ADDRESS : GPIO_OUT_W1TC_ADDRESS)), (1 << pin));
-		else
-			WRITE_PERI_REG(RTC_GPIO_OUT, (READ_PERI_REG(RTC_GPIO_OUT) & (uint32_t)0xfffffffe) | (uint32_t)(val & 1));
-
-		//GPIO_OUTPUT_SET(pin, (val ? 0xFF : 00));
-	}
+	} else if(pin != 16)
+		GPIO_REG_WRITE((((val != LOW) ? GPIO_OUT_W1TS_ADDRESS : GPIO_OUT_W1TC_ADDRESS)), (1 << pin));
+	else
+		WRITE_PERI_REG(RTC_GPIO_OUT, (READ_PERI_REG(RTC_GPIO_OUT) & (uint32_t)0xfffffffe) | (uint32_t)(val & 1));
 }
 
 uint8_t digitalRead(uint16_t pin)
 {
-	if(pin != 16)
+	if(pin != 16) {
 		return ((GPIO_REG_READ(GPIO_IN_ADDRESS) >> pin) & 1);
-	else
-		return (uint8_t)(READ_PERI_REG(RTC_GPIO_IN_DATA) & 1);
-
-	//return  GPIO_INPUT_GET(pin);
+	}
+	return (uint8_t)(READ_PERI_REG(RTC_GPIO_IN_DATA) & 1);
 }
 
 void pullup(uint16_t pin)
 {
-	if(pin >= 16)
-		return;
-	PIN_PULLUP_EN((EspDigitalPins[pin].mux));
+	if(pin < TOTAL_PINS) {
+		PIN_PULLUP_EN(PERIPHS_IO_MUX + esp8266_pinmuxOffset[pin]);
+	}
 }
 
 void noPullup(uint16_t pin)
 {
-	if(pin >= 16)
-		return;
-	PIN_PULLUP_DIS((EspDigitalPins[pin].mux));
+	if(pin < TOTAL_PINS) {
+		PIN_PULLUP_DIS(PERIPHS_IO_MUX + esp8266_pinmuxOffset[pin]);
+	}
 }
 
 /* Measures the length (in microseconds) of a pulse on the pin; state is HIGH

Sming/Arch/Esp8266/Core/ESP8266EX.cpp
@@ -1,48 +1,0 @@
-/****
- * Sming Framework Project - Open Source framework for high efficiency native ESP8266 development.
- * Created 2015 by Skurydin Alexey
- * http://github.com/SmingHub/Sming
- * All files of the Sming Core are provided under the LGPL v3 license.
- *
- * ESP8266EX.cpp
- *
- ****/
-
-#include "Digital.h"
-#include "ESP8266EX.h"
-#include "Digital.h"
-#include <espinc/pin_mux_register.h>
-
-const EspDigitalPin EspDigitalPins[] = {
-	{0, PERIPHS_IO_MUX_GPIO0_U, FUNC_GPIO0}, // FLASH
-	{1, PERIPHS_IO_MUX_U0TXD_U, FUNC_GPIO1}, // TXD0
-	{2, PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2}, // TXD1
-	{3, PERIPHS_IO_MUX_U0RXD_U, FUNC_GPIO3}, // RXD0
-	{4, PERIPHS_IO_MUX_GPIO4_U, FUNC_GPIO4},
-	{5, PERIPHS_IO_MUX_GPIO5_U, FUNC_GPIO5},
-	{6, 0, 0}, // SD_CLK_U
-	{7, 0, 0}, // SD_DATA0_U
-	{8, 0, 0}, // SD_DATA1_U
-	{9, PERIPHS_IO_MUX_SD_DATA2_U, FUNC_GPIO9},
-	{10, PERIPHS_IO_MUX_SD_DATA3_U, FUNC_GPIO10},
-	{11, 0, 0},								  // SD_CMD_U
-	{12, PERIPHS_IO_MUX_MTDI_U, FUNC_GPIO12}, // HSPIQ
-	{13, PERIPHS_IO_MUX_MTCK_U, FUNC_GPIO13}, // HSPID
-	{14, PERIPHS_IO_MUX_MTMS_U, FUNC_GPIO14}, // HSPICLK
-	{15, PERIPHS_IO_MUX_MTDO_U, FUNC_GPIO15}, // HSPICS
-};
-
-void EspDigitalPin::mode(uint8_t mode) const
-{
-	pinMode(id, mode);
-}
-
-void EspDigitalPin::write(uint8_t val) const
-{
-	digitalWrite(id, val);
-}
-
-uint8_t EspDigitalPin::read() const
-{
-	return digitalRead(id);
-}

Sming/Arch/Esp8266/Core/ESP8266EX.h
@@ -1,38 +1,0 @@
-/****
- * Sming Framework Project - Open Source framework for high efficiency native ESP8266 development.
- * Created 2015 by Skurydin Alexey
- * http://github.com/SmingHub/Sming
- * All files of the Sming Core are provided under the LGPL v3 license.
- *
- * ESP8266EX.h
- *
- ****/
-
-#pragma once
-
-#include <c_types.h>
-
-#define TOTAL_PINS 16
-#define NUM_DIGITAL_PINS TOTAL_PINS
-
-/** @brief  ESP GPIO pin configuration
- *  @ingroup constants
- */
-struct EspDigitalPin {
-	uint8_t id;
-	uint32_t mux;
-	uint8_t gpioFunc;
-
-	operator const int() const
-	{
-		return id;
-	}
-	void mode(uint8_t mode) const;
-	void write(uint8_t val) const;
-	uint8_t read() const;
-};
-
-/** @brief  ESP GPIO pin configuration
- *  @ingroup gpio
- */
-extern const EspDigitalPin EspDigitalPins[];

Sming/Components/.patches/http-parser.patch
@@ -1,8 +1,8 @@
 diff --git a/http_parser.h b/http_parser.h
-index df88252..5935bcf 100644
+index 3772b39..a1f4d38 100644
 --- a/http_parser.h
 +++ b/http_parser.h
-@@ -153,7 +153,7 @@ typedef int (*http_cb) (http_parser*);
+@@ -155,7 +155,7 @@ typedef int (*http_cb) (http_parser*);
  
  enum http_status
    {
@@ -11,7 +11,7 @@ index df88252..5935bcf 100644
    HTTP_STATUS_MAP(XX)
  #undef XX
    };
-@@ -206,7 +206,7 @@ enum http_status
+@@ -208,7 +208,7 @@ enum http_status
  
  enum http_method
    {
@@ -20,7 +20,7 @@ index df88252..5935bcf 100644
    HTTP_METHOD_MAP(XX)
  #undef XX
    };
-@@ -282,17 +282,13 @@ enum flags
+@@ -283,17 +283,13 @@ enum flags
  
  
  /* Define HPE_* values for each errno value above */
@@ -39,8 +39,24 @@ index df88252..5935bcf 100644
  struct http_parser {
    /** PRIVATE **/
    unsigned int type : 2;         /* enum http_parser_type */
+@@ -417,15 +413,6 @@ int http_should_keep_alive(const http_parser *parser);
+ /* Returns a string version of the HTTP method. */
+ const char *http_method_str(enum http_method m);
+ 
+-/* Returns a string version of the HTTP status code. */
+-const char *http_status_str(enum http_status s);
+-
+-/* Return a string name of the given error */
+-const char *http_errno_name(enum http_errno err);
+-
+-/* Return a string description of the given error */
+-const char *http_errno_description(enum http_errno err);
+-
+ /* Initialize all http_parser_url members to 0 */
+ void http_parser_url_init(struct http_parser_url *u);
+ 
 diff --git a/http_parser.c b/http_parser.c
-index 95ff42f..beaf1f7 100644
+index 9be003e..e6b0051 100644
 --- a/http_parser.c
 +++ b/http_parser.c
 @@ -20,11 +20,19 @@
@@ -65,7 +81,43 @@ index 95ff42f..beaf1f7 100644
  static uint32_t max_header_size = HTTP_MAX_HEADER_SIZE;
  
  #ifndef ULLONG_MAX
-@@ -186,7 +194,7 @@ static const char *method_strings[] =
+@@ -160,7 +168,6 @@ do {                                                                 \
+   }                                                                  \
+ } while (0)
+ 
+-
+ #define PROXY_CONNECTION "proxy-connection"
+ #define CONNECTION "connection"
+ #define CONTENT_LENGTH "content-length"
+@@ -170,10 +177,25 @@ do {                                                                 \
+ #define KEEP_ALIVE "keep-alive"
+ #define CLOSE "close"
+ 
++static const char STR_PROXY_CONNECTION[] PROGMEM_PSTR = "proxy-connection";
++static const char STR_CONNECTION[] PROGMEM_PSTR = "connection";
++static const char STR_CONTENT_LENGTH[] PROGMEM_PSTR = "content-length";
++static const char STR_TRANSFER_ENCODING[] PROGMEM_PSTR = "transfer-encoding";
++static const char STR_UPGRADE[] PROGMEM_PSTR = "upgrade";
++static const char STR_CHUNKED[] PROGMEM_PSTR = "chunked";
++static const char STR_KEEP_ALIVE[] PROGMEM_PSTR = "keep-alive";
++static const char STR_CLOSE[] PROGMEM_PSTR = "close";
+ 
+-static const char *method_strings[] =
++#define get_pstr_char(name, index) (char)pgm_read_byte(&STR_##name[index])
++
++
++#define XX(num, name, string) static const char method_string_##name[] PROGMEM_PSTR = #string;
++  HTTP_METHOD_MAP(XX)
++#undef XX
++
++static const char *method_strings[] PROGMEM_PSTR =
+   {
+-#define XX(num, name, string) #string,
++#define XX(num, name, string) method_string_##name,
+   HTTP_METHOD_MAP(XX)
+ #undef XX
+   };
+@@ -186,7 +208,7 @@ static const char *method_strings[] =
   *                    | "/" | "[" | "]" | "?" | "="
   *                    | "{" | "}" | SP | HT
   */
@@ -74,7 +126,7 @@ index 95ff42f..beaf1f7 100644
  /*   0 nul    1 soh    2 stx    3 etx    4 eot    5 enq    6 ack    7 bel  */
          0,       0,       0,       0,       0,       0,       0,       0,
  /*   8 bs     9 ht    10 nl    11 vt    12 np    13 cr    14 so    15 si   */
-@@ -220,19 +228,10 @@ static const char tokens[256] = {
+@@ -220,19 +242,10 @@ static const char tokens[256] = {
  /* 120  x   121  y   122  z   123  {   124  |   125  }   126  ~   127 del */
         'x',     'y',     'z',      0,      '|',      0,      '~',       0 };
  
@@ -98,7 +150,7 @@ index 95ff42f..beaf1f7 100644
  #if HTTP_PARSER_STRICT
  # define T(v) 0
  #else
-@@ -428,14 +427,14 @@ enum http_host_state
+@@ -428,14 +441,14 @@ enum http_host_state
    (c) == ';' || (c) == ':' || (c) == '&' || (c) == '=' || (c) == '+' || \
    (c) == '$' || (c) == ',')
  
@@ -115,7 +167,7 @@ index 95ff42f..beaf1f7 100644
  #define IS_URL_CHAR(c)                                                         \
    (BIT_AT(normal_url_char, (unsigned char)c) || ((c) & 0x80))
  #define IS_HOST_CHAR(c)                                                        \
-@@ -467,16 +466,6 @@ do {                                                                 \
+@@ -467,16 +480,6 @@ do {                                                                 \
  #endif
  
  
@@ -132,7 +184,7 @@ index 95ff42f..beaf1f7 100644
  int http_message_needs_eof(const http_parser *parser);
  
  /* Our URL parser.
-@@ -758,7 +747,7 @@ reexecute:
+@@ -760,7 +763,7 @@ reexecute:
            }
  
            parser->type = HTTP_REQUEST;
@@ -141,7 +193,7 @@ index 95ff42f..beaf1f7 100644
            parser->index = 2;
            UPDATE_STATE(s_req_method);
          }
-@@ -938,23 +927,23 @@ reexecute:
+@@ -940,23 +943,23 @@ reexecute:
          parser->method = (enum http_method) 0;
          parser->index = 1;
          switch (ch) {
@@ -180,7 +232,18 @@ index 95ff42f..beaf1f7 100644
            default:
              SET_ERRNO(HPE_INVALID_METHOD);
              goto error;
-@@ -983,8 +972,8 @@ reexecute:
+@@ -977,16 +980,17 @@ reexecute:
+         }
+ 
+         matcher = method_strings[parser->method];
+-        if (ch == ' ' && matcher[parser->index] == '\0') {
++        char matcher_char = pgm_read_byte(&matcher[parser->index]);
++        if (ch == ' ' && matcher_char == '\0') {
+           UPDATE_STATE(s_req_spaces_before_url);
+-        } else if (ch == matcher[parser->index]) {
++        } else if (ch == matcher_char) {
+           ; /* nada */
+         } else if ((ch >= 'A' && ch <= 'Z') || ch == '-') {
  
            switch (parser->method << 16 | parser->index << 8 | ch) {
  #define XX(meth, pos, ch, new_meth) \
@@ -191,7 +254,7 @@ index 95ff42f..beaf1f7 100644
  
              XX(POST,      1, 'U', PUT)
              XX(POST,      1, 'A', PATCH)
-@@ -1024,7 +1013,7 @@ reexecute:
+@@ -1026,7 +1030,7 @@ reexecute:
          if (ch == ' ') break;
  
          MARK(url);
@@ -200,7 +263,7 @@ index 95ff42f..beaf1f7 100644
            UPDATE_STATE(s_req_server_start);
          }
  
-@@ -1100,7 +1089,7 @@ reexecute:
+@@ -1102,7 +1106,7 @@ reexecute:
              UPDATE_STATE(s_req_http_H);
              break;
            case 'I':
@@ -209,7 +272,88 @@ index 95ff42f..beaf1f7 100644
                UPDATE_STATE(s_req_http_I);
                break;
              }
-@@ -1826,7 +1815,7 @@ reexecute:
+@@ -1303,7 +1307,7 @@ reexecute:
+             case h_matching_connection:
+               parser->index++;
+               if (parser->index > sizeof(CONNECTION)-1
+-                  || c != CONNECTION[parser->index]) {
++                  || c != get_pstr_char(CONNECTION, parser->index)) {
+                 parser->header_state = h_general;
+               } else if (parser->index == sizeof(CONNECTION)-2) {
+                 parser->header_state = h_connection;
+@@ -1315,7 +1319,7 @@ reexecute:
+             case h_matching_proxy_connection:
+               parser->index++;
+               if (parser->index > sizeof(PROXY_CONNECTION)-1
+-                  || c != PROXY_CONNECTION[parser->index]) {
++                  || c != get_pstr_char(PROXY_CONNECTION, parser->index)) {
+                 parser->header_state = h_general;
+               } else if (parser->index == sizeof(PROXY_CONNECTION)-2) {
+                 parser->header_state = h_connection;
+@@ -1327,7 +1331,7 @@ reexecute:
+             case h_matching_content_length:
+               parser->index++;
+               if (parser->index > sizeof(CONTENT_LENGTH)-1
+-                  || c != CONTENT_LENGTH[parser->index]) {
++                  || c != get_pstr_char(CONTENT_LENGTH, parser->index)) {
+                 parser->header_state = h_general;
+               } else if (parser->index == sizeof(CONTENT_LENGTH)-2) {
+                 parser->header_state = h_content_length;
+@@ -1339,7 +1343,7 @@ reexecute:
+             case h_matching_transfer_encoding:
+               parser->index++;
+               if (parser->index > sizeof(TRANSFER_ENCODING)-1
+-                  || c != TRANSFER_ENCODING[parser->index]) {
++                  || c != get_pstr_char(TRANSFER_ENCODING, parser->index)) {
+                 parser->header_state = h_general;
+               } else if (parser->index == sizeof(TRANSFER_ENCODING)-2) {
+                 parser->header_state = h_transfer_encoding;
+@@ -1352,7 +1356,7 @@ reexecute:
+             case h_matching_upgrade:
+               parser->index++;
+               if (parser->index > sizeof(UPGRADE)-1
+-                  || c != UPGRADE[parser->index]) {
++                  || c != get_pstr_char(UPGRADE, parser->index)) {
+                 parser->header_state = h_general;
+               } else if (parser->index == sizeof(UPGRADE)-2) {
+                 parser->header_state = h_upgrade;
+@@ -1597,7 +1601,7 @@ reexecute:
+             case h_matching_transfer_encoding_chunked:
+               parser->index++;
+               if (parser->index > sizeof(CHUNKED)-1
+-                  || c != CHUNKED[parser->index]) {
++                  || c != get_pstr_char(CHUNKED, parser->index)) {
+                 h_state = h_matching_transfer_encoding_token;
+               } else if (parser->index == sizeof(CHUNKED)-2) {
+                 h_state = h_transfer_encoding_chunked;
+@@ -1633,7 +1637,7 @@ reexecute:
+             case h_matching_connection_keep_alive:
+               parser->index++;
+               if (parser->index > sizeof(KEEP_ALIVE)-1
+-                  || c != KEEP_ALIVE[parser->index]) {
++                  || c != get_pstr_char(KEEP_ALIVE, parser->index)) {
+                 h_state = h_matching_connection_token;
+               } else if (parser->index == sizeof(KEEP_ALIVE)-2) {
+                 h_state = h_connection_keep_alive;
+@@ -1643,7 +1647,7 @@ reexecute:
+             /* looking for 'Connection: close' */
+             case h_matching_connection_close:
+               parser->index++;
+-              if (parser->index > sizeof(CLOSE)-1 || c != CLOSE[parser->index]) {
++              if (parser->index > sizeof(CLOSE)-1 || c != get_pstr_char(CLOSE, parser->index)) {
+                 h_state = h_matching_connection_token;
+               } else if (parser->index == sizeof(CLOSE)-2) {
+                 h_state = h_connection_close;
+@@ -1654,7 +1658,7 @@ reexecute:
+             case h_matching_connection_upgrade:
+               parser->index++;
+               if (parser->index > sizeof(UPGRADE) - 1 ||
+-                  c != UPGRADE[parser->index]) {
++                  c != get_pstr_char(UPGRADE, parser->index)) {
+                 h_state = h_matching_connection_token;
+               } else if (parser->index == sizeof(UPGRADE)-2) {
+                 h_state = h_connection_upgrade;
+@@ -1833,7 +1837,7 @@ reexecute:
            parser->upgrade =
                (parser->type == HTTP_REQUEST || parser->status_code == 101);
          } else {
@@ -218,7 +362,7 @@ index 95ff42f..beaf1f7 100644
          }
  
          /* Here we call the headers_complete callback. This is somewhat
-@@ -1874,7 +1863,7 @@ reexecute:
+@@ -1881,7 +1885,7 @@ reexecute:
  
          hasBody = parser->flags & F_CHUNKED ||
            (parser->content_length > 0 && parser->content_length != ULLONG_MAX);
@@ -227,7 +371,7 @@ index 95ff42f..beaf1f7 100644
                                  (parser->flags & F_SKIPBODY) || !hasBody)) {
            /* Exit, the rest of the message is in a different protocol. */
            UPDATE_STATE(NEW_MESSAGE());
-@@ -1991,7 +1980,7 @@ reexecute:
+@@ -1998,7 +2002,7 @@ reexecute:
          assert(nread == 1);
          assert(parser->flags & F_CHUNKED);
  
@@ -236,7 +380,7 @@ index 95ff42f..beaf1f7 100644
          if (UNLIKELY(unhex_val == -1)) {
            SET_ERRNO(HPE_INVALID_CHUNK_SIZE);
            goto error;
-@@ -2013,7 +2002,7 @@ reexecute:
+@@ -2020,7 +2024,7 @@ reexecute:
            break;
          }
  
@@ -245,16 +389,27 @@ index 95ff42f..beaf1f7 100644
  
          if (unhex_val == -1) {
            if (ch == ';' || ch == ' ') {
-@@ -2207,7 +2196,7 @@ const char *
- http_status_str (enum http_status s)
+@@ -2207,18 +2211,7 @@ http_should_keep_alive (const http_parser *parser)
+ const char *
+ http_method_str (enum http_method m)
  {
-   switch (s) {
+-  return ELEM_AT(method_strings, m, "<unknown>");
+-}
+-
+-const char *
+-http_status_str (enum http_status s)
+-{
+-  switch (s) {
 -#define XX(num, name, string) case HTTP_STATUS_##name: return #string;
-+#define XX(num, name, string) case HTTP_STATUS_CODE_##name: return #string;
-     HTTP_STATUS_MAP(XX)
- #undef XX
-     default: return "<unknown>";
-@@ -2231,18 +2220,6 @@ http_parser_settings_init(http_parser_settings *settings)
+-    HTTP_STATUS_MAP(XX)
+-#undef XX
+-    default: return "<unknown>";
+-  }
++  return ELEM_AT(method_strings, m, PSTR("<unknown>"));
+ }
+ 
+ void
+@@ -2238,18 +2231,6 @@ http_parser_settings_init(http_parser_settings *settings)
    memset(settings, 0, sizeof(*settings));
  }
  

Sming/Components/.patches/ws_parser.patch
@@ -1,16 +1,17 @@
 diff --git a/ws_parser.c b/ws_parser.c
-index d43adbe..beeb611 100644
+index d43adbe..17040e3 100644
 --- a/ws_parser.c
 +++ b/ws_parser.c
-@@ -3,6 +3,7 @@
+@@ -3,6 +3,8 @@
  #endif
  
  #include "ws_parser.h"
 +#include <string.h>
++#include <sys/pgmspace.h>
  
  enum {
      S_OPCODE = 0,
-@@ -27,6 +28,7 @@ enum {
+@@ -27,6 +29,7 @@ enum {
  void
  ws_parser_init(ws_parser_t* parser)
  {
@@ -18,7 +19,7 @@ index d43adbe..beeb611 100644
      parser->state = S_OPCODE;
      parser->fragment = 0;
  }
-@@ -247,7 +249,8 @@ ws_parser_execute(
+@@ -247,7 +250,8 @@ ws_parser_execute(
                  }
  
                  if(parser->mask_flag) {
@@ -28,3 +29,12 @@ index d43adbe..beeb611 100644
                          buff[i] ^= parser->mask[parser->mask_pos++];
                      }
                  }
+@@ -302,7 +306,7 @@ ws_parser_execute(
+ const char*
+ ws_parser_error(int rc)
+ {
+-    #define XX(name, code) if(rc == code) return #name;
++    #define XX(name, code) if(rc == code) return PSTR(#name);
+     WS_PARSER_ERROR_CODES(XX)
+     #undef XX
+ 

Sming/Components/Network/src/Network/Http/HttpCommon.h
@@ -121,7 +121,8 @@ inline String httpGetStatusText(unsigned code)
  */
 inline String toString(HttpMethod method)
 {
-	return http_method_str(http_method(method));
+	auto fstr = reinterpret_cast<flash_string_t>(http_method_str(http_method(method)));
+	return String(fstr);
 }
 
 /** @} */

commit dfcda27ef36c9109d2aa3914b5b635433b7bb38c

PWM doesn't initialise GPIO correctly
Bug introduced in #2752

Sming/Arch/Esp8266/Core/HardwarePWM.cpp
@@ -33,6 +33,25 @@
 
 extern const uint8_t esp8266_pinmuxOffset[];
 
+static const uint8_t gpioPinFunc[]{
+	FUNC_GPIO0,  //
+	FUNC_GPIO1,  //
+	FUNC_GPIO2,  //
+	FUNC_GPIO3,  //
+	FUNC_GPIO4,  //
+	FUNC_GPIO5,  //
+	FUNC_GPIO6,  //
+	FUNC_GPIO7,  //
+	FUNC_GPIO8,  //
+	FUNC_GPIO9,  //
+	FUNC_GPIO10, //
+	FUNC_GPIO11, //
+	FUNC_GPIO12, //
+	FUNC_GPIO13, //
+	FUNC_GPIO14, //
+	FUNC_GPIO15, //
+};
+
 HardwarePWM::HardwarePWM(uint8_t* pins, uint8_t noOfPins) : channel_count(noOfPins)
 {
 	if(noOfPins == 0) {
@@ -49,7 +68,7 @@ HardwarePWM::HardwarePWM(uint8_t* pins, uint8_t noOfPins) : channel_count(noOfPi
 			continue;
 		}
 		ioInfo[pinCount][0] = PERIPHS_IO_MUX + esp8266_pinmuxOffset[pin];
-		ioInfo[pinCount][1] = esp8266_gpioToFn[pin];
+		ioInfo[pinCount][1] = gpioPinFunc[pin];
 		ioInfo[pinCount][2] = pin;
 		pwmDutyInit[pinCount] = 0; // Start with zero output
 		channels[pinCount] = pin;