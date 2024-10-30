8b02f58abedc7fed12862b9d2acacb3a2dab6c6f
Peter Jakobs <pjakobs@mobileme.de>
2024-10-30 18:03:41 +0100

    minor edit

--- Sming/Arch/Esp32/Core/HardwarePWM.cpp
+++ Sming/Arch/Esp32/Core/HardwarePWM.cpp
@@ -140,12 +140,16 @@ uint32_t maxDuty(ledc_timer_bit_t bits)
 
 HardwarePWM::HardwarePWM(uint8_t* pins, uint8_t no_of_pins) : channel_count(no_of_pins)
 {
-	debug_d("starting HardwarePWM init");
-	periph_module_enable(PERIPH_LEDC_MODULE);
-	if((no_of_pins == 0) || (no_of_pins > SOC_LEDC_CHANNEL_NUM)) {
+	
+	assert(no_of_pins <= SOC_LEDC_CHANNEL_NUM);
+	
+	if(no_of_pins == 0)
+	{
 		return;
 	}
 
+	periph_module_enable(PERIPH_LEDC_MODULE);
+	
 	for(uint8_t i = 0; i < no_of_pins; i++) {
 		channels[i] = pins[i];
 
