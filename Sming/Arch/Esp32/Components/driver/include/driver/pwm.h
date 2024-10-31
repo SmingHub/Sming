f49281f1249fb09a8ea9d95b0b4d895f7977143e
Peter Jakobs <pjakobs@mobileme.de>
2024-10-31 10:54:17 +0100

    re-orderd things in pwm.h

--- Sming/Arch/Esp32/Components/driver/include/driver/pwm.h
+++ Sming/Arch/Esp32/Components/driver/include/driver/pwm.h
@@ -1,16 +1,15 @@
-/****
- * Sming Framework Project - Open Source framework for high efficiency native ESP8266 development.
- * Created 2015 by Skurydin Alexey
- * http://github.com/SmingHub/Sming
- * All files of the Sming Core are provided under the LGPL v3 license.
- *
- * pwm.h
- *
- ****/
+/****  
+ * Sming Framework Project - Open Source framework for high efficiency native ESP8266 development.  
+ * Created 2015 by Skurydin Alexey  
+ * http://github.com/SmingHub/Sming  
+ * All files of the Sming Core are provided under the LGPL v3 license.  
+ *  
+ * pwm.h  
+ *  
+ ****/  
 
-#include <soc/soc_caps.h>
+#pragma once  
 
+#include <soc/soc_caps.h>  
 
-#pragma once
-
-#define PWM_CHANNEL_NUM_MAX SOC_LEDC_CHANNEL_NUM
+#define PWM_CHANNEL_NUM_MAX SOC_LEDC_CHANNEL_NUM  
