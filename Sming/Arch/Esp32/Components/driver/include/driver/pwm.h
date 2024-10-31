c012764186d38c1ece86ea0375c96c399aab1a65
mikee47 <mike@sillyhouse.net>
2024-10-31 19:56:46 +0000

    Fix pwm.h, minimal patch

--- Sming/Arch/Esp32/Components/driver/include/driver/pwm.h
+++ Sming/Arch/Esp32/Components/driver/include/driver/pwm.h
@@ -1,11 +1,11 @@
-/****  
- * Sming Framework Project - Open Source framework for high efficiency native ESP8266 development.  
- * Created 2015 by Skurydin Alexey  
- * http://github.com/SmingHub/Sming  
- * All files of the Sming Core are provided under the LGPL v3 license.  
- *  
- * pwm.h  
- *  
+/****
+ * Sming Framework Project - Open Source framework for high efficiency native ESP8266 development.
+ * Created 2015 by Skurydin Alexey
+ * http://github.com/SmingHub/Sming
+ * All files of the Sming Core are provided under the LGPL v3 license.
+ *
+ * pwm.h
+ *
  ****/
 
 #pragma once
