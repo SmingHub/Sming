7a55f010077d215581472b6ea079852be014ffe6
mikee47 <mike@sillyhouse.net>
2024-11-02 19:51:35 +0000

    Looking at Rp2040 implementation

--- Sming/Arch/Rp2040/Core/HardwarePWM.cpp
+++ Sming/Arch/Rp2040/Core/HardwarePWM.cpp
@@ -9,9 +9,36 @@
  */
 
 #include <HardwarePWM.h>
+#include <hardware/pwm.h>
+#include <hardware/gpio.h>
+#include <algorithm>
+
+#define PWM_REF_CLOCK 125e6
+#define PWM_CLOCKDIV 100
+#define PWM_FREQ 1000
+#define PWM_CLOCK (PWM_REF_CLOCK / PWM_CLOCKDIV)
+#define PWM_COUNT_TOP (uint32_t((PWM_CLOCK / PWM_FREQ) + 0.5) + 1)
 
 HardwarePWM::HardwarePWM(uint8_t* pins, uint8_t noOfPins) : channel_count(noOfPins)
 {
+	assert(noOfPins > 0 && noOfPins <= PWM_CHANNEL_NUM_MAX);
+	noOfPins = std::min(uint8_t(PWM_CHANNEL_NUM_MAX), noOfPins);
+
+	for(uint8_t i = 0; i < noOfPins; i++) {
+		auto pwmPin = pins[noOfPins];
+		channels[i] = pwmPin;
+
+		auto slice_num = pwm_gpio_to_slice_num(pwmPin);
+		pwm_config cfg = pwm_get_default_config();
+		pwm_config_set_clkdiv_mode(&cfg, PWM_DIV_FREE_RUNNING);
+		pwm_config_set_clkdiv(&cfg, PWM_CLOCKDIV);
+		pwm_config_set_wrap(&cfg, PWM_COUNT_TOP);
+		pwm_init(slice_num, &cfg, true);
+		gpio_set_function(pwmPin, GPIO_FUNC_PWM);
+		gpio_set_dir(pwmPin, GPIO_OUT);
+		pwm_set_gpio_level(pwmPin, 0);
+	}
+	maxduty = maxDuty(DEFAULT_RESOLUTION);
 }
 
 HardwarePWM::~HardwarePWM()
