86ef7605bcb915116becadba6267a4fee9de6c9c
mikee47 <mike@sillyhouse.net>
2024-11-03 15:39:54 +0000

    Implement rest of Rp2040 HardwarePWM class

--- Sming/Arch/Rp2040/Core/HardwarePWM.cpp
+++ Sming/Arch/Rp2040/Core/HardwarePWM.cpp
@@ -11,36 +11,46 @@
 #include <HardwarePWM.h>
 #include <hardware/pwm.h>
 #include <hardware/gpio.h>
+#include <hardware/clocks.h>
 #include <algorithm>
+#include <debug_progmem.h>
 
-#define PWM_REF_CLOCK 125e6
-#define PWM_CLOCKDIV 100
-#define PWM_FREQ 1000
-#define PWM_CLOCK (PWM_REF_CLOCK / PWM_CLOCKDIV)
-#define PWM_COUNT_TOP (uint32_t((PWM_CLOCK / PWM_FREQ) + 0.5) + 1)
+/*
+Divisor is 8:4 fractional value, so 1 <= int <= 255, 0 <= frac <= 15
+Thus (2, 1) represents 2 1/16, 2.5 is (1, 8), etc.
 
-static_assert(PWM_CHANNEL_NUM_MAX == 16);
+We can also say (CLK_SYS * 16 / (CLKDIV * 16))
+DIV = (DIV_INT + DIV_FRAC / 16)
+
+Setting CSR_PH_CORRECT=0:
+
+F_PWM = F_SYS / ((TOP + 1) * DIV)
+
+TOP  = (F_SYS / F_PWM / DIV) - 1
+
+period (us) = 1e6 / F_PWM
+period = 1e6 / (F_SYS / ((TOP + 1) * DIV))
+period = 1e6 * ((TOP + 1) * DIV) / F_SYS
+
+
+1e6 * ((TOP + 1) * DIV) = 1e6 / F_SYS
+
+
+*/
+#define PWM_FREQ_DEFAULT 1000
 
 HardwarePWM::HardwarePWM(uint8_t* pins, uint8_t noOfPins) : channel_count(noOfPins)
 {
 	assert(noOfPins > 0 && noOfPins <= PWM_CHANNEL_NUM_MAX);
 	noOfPins = std::min(uint8_t(PWM_CHANNEL_NUM_MAX), noOfPins);
+	std::copy_n(pins, noOfPins, channels);
+	setPeriod(1e6 / PWM_FREQ_DEFAULT);
 
-	for(uint8_t i = 0; i < noOfPins; i++) {
-		auto pin = pins[i];
-		channels[i] = pin;
-
-		auto slice_num = pwm_gpio_to_slice_num(pin);
-		pwm_config cfg = pwm_get_default_config();
-		pwm_config_set_clkdiv_mode(&cfg, PWM_DIV_FREE_RUNNING);
-		pwm_config_set_clkdiv(&cfg, PWM_CLOCKDIV);
-		pwm_config_set_wrap(&cfg, PWM_COUNT_TOP);
-		pwm_init(slice_num, &cfg, true);
+	for(unsigned i = 0; i < noOfPins; ++i) {
+		auto pin = channels[i];
 		gpio_set_function(pin, GPIO_FUNC_PWM);
 		gpio_set_dir(pin, GPIO_OUT);
-		pwm_set_gpio_level(pin, 0);
 	}
-	maxduty = PWM_COUNT_TOP;
 }
 
 HardwarePWM::~HardwarePWM()
@@ -58,10 +68,12 @@ uint32_t HardwarePWM::getDutyChan(uint8_t chan)
 	}
 	auto pin = channels[chan];
 	auto slice_num = pwm_gpio_to_slice_num(pin);
-	return pwm_hw->slice[slice_num].top;
+	auto value = pwm_hw->slice[slice_num].cc;
+	value >>= pwm_gpio_to_channel(pin) ? PWM_CH0_CC_B_LSB : PWM_CH0_CC_A_LSB;
+	return value & 0xffff;
 }
 
-bool HardwarePWM::setDutyChan(uint8_t chan, uint32_t duty, bool update)
+bool HardwarePWM::setDutyChan(uint8_t chan, uint32_t duty, bool)
 {
 	if(chan >= channel_count) {
 		return false;
@@ -74,22 +86,86 @@ bool HardwarePWM::setDutyChan(uint8_t chan, uint32_t duty, bool update)
 
 uint32_t HardwarePWM::getPeriod()
 {
-	// TODO
-	return 0;
+	// All channels configured with same clock
+	auto slice_num = pwm_gpio_to_slice_num(channels[0]);
+	uint32_t top = pwm_hw->slice[slice_num].top;
+	uint32_t div = pwm_hw->slice[slice_num].div >> PWM_CH0_DIV_INT_LSB;
+	uint32_t t = (top + 1) * div;
+	uint32_t clk_sys_khz = clock_get_hz(clk_sys);
+	return 1'000'000ULL * t / clk_sys_khz;
 }
 
 void HardwarePWM::setPeriod(uint32_t period)
 {
-	// TODO
+	/*
+		TOP = (period * F_SYS / DIV / 1e6) - 1
+		DIV = period * F_SYS / 1e6 / (TOP + 1)
+
+		Range of DIV is 1 - 255
+		Range of TOP is 0 - 65535
+
+	Calculate value for DIV to maximise value TOP.
+
+	DIV = period * F_SYS / 1e6 / (65535 + 1)
+
+	DIV must be > 0
+		=> period * F_SYS / 1e6 / (65535 + 1) > 0
+		=> period * F_SYS / 1e6 >= (65535 + 1)
+		=> period * F_SYS >= 1e6 * (65535 + 1)
+		=> period >= 1e6 * (65535 + 1) / F_SYS
+
+	At 125MHz period <= 524.288us
+	*/
+
+	// TODO: Use fractional part of divider
+
+	const uint32_t topMax{0xffff};
+	const uint32_t divMax{0xff};
+	auto sysFreq = clock_get_hz(clk_sys);
+	uint32_t div = uint64_t(period) * sysFreq / 1e6 / (topMax + 1);
+	uint32_t top;
+	if(div > divMax) {
+		// Period too big, set to maximum
+		top = topMax;
+		div = divMax;
+	} else {
+		if(div == 0) {
+			// Period is very small, set div to minimum
+			div = 1;
+		}
+		top = (uint64_t(period) * sysFreq / div / 1e6) - 1;
+		if(top > topMax) {
+			debug_d("[PWM] TOP too big %u, div %u", __FUNCTION__, top, div);
+			++div;
+			top = (uint64_t(period) * sysFreq / div / 1e6) - 1;
+		}
+	}
+
+	debug_d("[PWM] %s(%u): div %u, top %u", __FUNCTION__, period, div, top);
+
+	pwm_config cfg = pwm_get_default_config();
+	pwm_config_set_clkdiv_mode(&cfg, PWM_DIV_FREE_RUNNING);
+	pwm_config_set_clkdiv_int_frac(&cfg, div, 0);
+	pwm_config_set_wrap(&cfg, top);
+
+	for(unsigned i = 0; i < channel_count; ++i) {
+		auto pin = channels[i];
+		auto slice_num = pwm_gpio_to_slice_num(pin);
+		pwm_init(slice_num, &cfg, true);
+	}
+
+	maxduty = top;
 }
 
 void HardwarePWM::update()
 {
-	// TODO
+	// Not implemented
 }
 
 uint32_t HardwarePWM::getFrequency(uint8_t pin)
 {
-	// TODO
-	return 0;
+	auto slice_num = pwm_gpio_to_slice_num(pin);
+	auto top = pwm_hw->slice[slice_num].top;
+	auto div = pwm_hw->slice[slice_num].div >> PWM_CH0_DIV_INT_LSB;
+	return clock_get_hz(clk_sys) / (div * (top + 1));
 }
