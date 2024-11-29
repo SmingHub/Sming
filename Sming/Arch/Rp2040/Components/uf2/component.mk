fb125357cc58aa2acd18947e507b5210b48b7715
mikee47 <mike@sillyhouse.net>
2024-11-29 13:07:51 +0000

    Update picotool and implement missing make targets

--- Sming/Arch/Rp2040/Components/uf2/component.mk
+++ Sming/Arch/Rp2040/Components/uf2/component.mk
@@ -22,13 +22,6 @@ else
 Uf2Conv = $(PYTHON) $(UF2CONV_PY) $(if $V,--verbose) $1
 endif
 
-
-# Read flash manufacturer ID and determine actual size
-define ReadFlashID
-	$(info Reading Flash ID)
-	$(call Uf2Conv,--list --verbose)
-endef
-
 # Write file contents to Flash
 # $1 -> List of `Offset=File` chunks
 define WriteFlash
@@ -45,24 +38,4 @@ define VerifyFlash
 		$(info VerifyFlash $1)
 		$(info ** NOT IMPLEMENTED **)
 	)
-endef
-
-# Read flash memory into file
-# $1 -> `Offset,Size` chunk
-# $2 -> Output filename
-define ReadFlash
-	$(info ReadFlash $1,$2)
-	$(info ** NOT IMPLEMENTED **)
-endef
-
-# Erase a region of Flash
-# $1 -> Offset,Size
-define EraseFlashRegion
-	$(info EraseFlashRegion $1)
-	$(info ** NOT IMPLEMENTED **)
-endef
-
-# Erase flash memory contents
-define EraseFlash
-	$(info ** NOT IMPLEMENTED **)
 endef
