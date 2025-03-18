45a181c9be4b908871d45604695d4d9e39628c1d
mikee47 <mike@sillyhouse.net>
2025-03-18 12:07:48 +0000

    Add task stats

--- samples/Esp32_Dual_Core/app/application.cpp
+++ samples/Esp32_Dual_Core/app/application.cpp
@@ -1,8 +1,14 @@
 #include <SmingCore.h>
+#include <Services/Profiling/TaskStat.h>
+
+namespace
+{
+Profiling::TaskStat taskStat(Serial);
+SimpleTimer statTimer;
 
 void handleNotification(uint32_t param)
 {
-	Serial << "NOTIFY " << param << endl;
+	debug_i("NOTIFY %u", param);
 }
 
 // Code to run on second CPU
@@ -10,12 +16,27 @@ void IRAM_ATTR app2_main(void*)
 {
 	static unsigned count;
 
+	// Using a polled timer has virtually zero overhead as it accesses timing hardware directly
+	PeriodicFastUs timer(100);
+
 	for(;;) {
-		System.queueCallback(handleNotification, count);
+		if(!timer.expired()) {
+			continue;
+		}
+
+		if(count % 10000 == 0) {
+			System.queueCallback(handleNotification, count);
+
+			debug_i("TICK");
+
+			// This is required if CONFIG_ESP_TASK_WDT_CHECK_IDLE_TASK_CPU1=y
+			// WDT.alive();
+		}
 		++count;
-		delay(500);
 	}
 }
+
+} // namespace
 
 void init()
 {
@@ -24,6 +45,11 @@ void init()
 
 	Serial.println(_F("ESP32 dual-core demo."));
 
+	// Provide some periodic status so we can see what tasks are active
+	statTimer.initializeMs<2000>([]() { taskStat.update(); });
+	statTimer.start();
+
+	// Create task running on second core
 	const unsigned stackSize = 2048;
 	const unsigned priority = 5;
 	xTaskCreatePinnedToCore(app2_main, "Sming2", stackSize, nullptr, priority, nullptr, 1);
