23ad6070b6f1643a785a0f8d1cc9183544f1f72c
mikee47 <mike@sillyhouse.net>
2025-09-16 09:45:51 +0100

    Use flags to avoid repeated setup/teardown in `analogRead`

--- Sming/Arch/Esp32/Core/Digital.cpp
+++ Sming/Arch/Esp32/Core/Digital.cpp
@@ -14,6 +14,8 @@
 #include <hal/gpio_ll.h>
 #include <driver/rtc_io.h>
 #include <driver/adc.h>
+#include <bitset>
+#include <debug_progmem.h>
 #if SOC_RTCIO_INPUT_OUTPUT_SUPPORTED
 #include <hal/rtc_io_ll.h>
 #if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(5, 2, 0)
@@ -21,6 +23,12 @@
 #define RTCIO_LL_FUNC_DIGITAL RTCIO_FUNC_DIGITAL
 #endif
 #endif
+
+namespace
+{
+adc_oneshot_unit_handle_t adcUnitHandles[SOC_ADC_PERIPH_NUM];
+std::bitset<GPIO_PIN_COUNT> adcInitFlags;
+} // namespace
 
 void pinMode(uint16_t pin, uint8_t mode)
 {
@@ -150,29 +158,35 @@ unsigned long pulseIn(uint16_t pin, uint8_t state, unsigned long timeout)
 
 uint16_t analogRead(uint16_t pin)
 {
-	adc_oneshot_unit_init_cfg_t init_config{};
+	adc_unit_t unit_id;
 	adc_channel_t channel;
-	esp_err_t err = adc_oneshot_io_to_channel(pin, &init_config.unit_id, &channel);
+	esp_err_t err = adc_oneshot_io_to_channel(pin, &unit_id, &channel);
 	if(err != ESP_OK) {
 		debug_e("Pin %u is not ADC pin!", pin);
 		return 0;
 	}
 
 	// Initialise unit
-	adc_oneshot_unit_handle_t adc_handle;
-	ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config, &adc_handle));
+	auto& adc_handle = adcUnitHandles[unit_id];
+	if(!adc_handle) {
+		adc_oneshot_unit_init_cfg_t init_config{
+			.unit_id = unit_id,
+		};
+		ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config, &adc_handle));
+	}
 
 	// Initialise channel
-	adc_oneshot_chan_cfg_t channel_config{
-		.atten = ADC_ATTEN_DB_0,
-		.bitwidth = ADC_BITWIDTH_DEFAULT,
-	};
-	ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle, channel, &channel_config));
+	if(!adcInitFlags[pin]) {
+		adc_oneshot_chan_cfg_t channel_config{
+			.atten = ADC_ATTEN_DB_0,
+			.bitwidth = ADC_BITWIDTH_DEFAULT,
+		};
+		ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle, channel, &channel_config));
+		adcInitFlags[pin] = 1;
+	}
 
 	int rawSampleValue{0};
 	ESP_ERROR_CHECK(adc_oneshot_read(adc_handle, channel, &rawSampleValue));
-
-	ESP_ERROR_CHECK(adc_oneshot_del_unit(adc_handle));
 
 	return rawSampleValue;
 }
