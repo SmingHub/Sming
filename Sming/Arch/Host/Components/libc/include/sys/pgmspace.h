8a02cd33c9079725df3b3299253167b146d15888
mikee47 <mike@sillyhouse.net>
2026-01-24 11:34:15 +0000

    Fix misaligned PSTR values for Host

--- Sming/Arch/Host/Components/libc/include/sys/pgmspace.h
+++ Sming/Arch/Host/Components/libc/include/sys/pgmspace.h
@@ -20,7 +20,12 @@ bool isFlashPtr(const void* ptr);
 
 #define PROGMEM STORE_ATTR
 #define PROGMEM_PSTR PROGMEM
-#define PSTR(str) (str)
+
+#define PSTR(str)                                                                                                      \
+	(__extension__({                                                                                                   \
+		static const char __pstr__[] PROGMEM_PSTR = str;                                                               \
+		&__pstr__[0];                                                                                                  \
+	}))
 
 #define PGM_P const char*
 #define PGM_VOID_P const void*
