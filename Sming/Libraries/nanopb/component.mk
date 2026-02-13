610cbedcdfe5a3564b0ca6d466f2c56923f4e88d
Mike <mike@sillyhouse.net>
2026-02-13 21:21:46 +0000

    Fix nanopb build issues (#3009)
    * Export `NANOPB_GENERATE`
    
    * Fix protopb CI build errors
    
    * Update GoogleCast

--- Sming/Libraries/nanopb/component.mk
+++ Sming/Libraries/nanopb/component.mk
@@ -2,3 +2,5 @@ COMPONENT_SRCDIRS := nanopb src
 COMPONENT_INCDIRS := nanopb src/include
 
 COMPONENT_SUBMODULES += nanopb
+
+export NANOPB_GENERATE := $(PYTHON) $(COMPONENT_PATH)/nanopb/generator/nanopb_generator.py
